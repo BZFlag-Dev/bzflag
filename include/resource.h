88a3bde8cb0c8cd3fb5690731fafd1c4f40b4225
cvs2svn <davidtrowbridge@users.sourceforge.net>
2000-03-05 06:46:22 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_7d_5'.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/tags/v1_7d_5/bzflag@8 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/resource.h
+++
@@ -1,6 +0,0 @@
-/* NOTICE */
-
-#define IDI_BZICON                      101
-#define IDB_BZ_LOGO                     102
-#define IDD_VIDEO_FORMAT                101
-#define IDC_VIDEO_FORMAT                101
