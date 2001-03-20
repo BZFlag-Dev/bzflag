c6dea87acf15eea7c8addbfea543ff3f0663f757
Chris Schoeneman <crs23@users.sourceforge.net>
2001-03-20 05:01:26 +0000

    fixed code to match comment
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@151 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/Protocol.h
+++ include/Protocol.h
@@ -123,7 +123,7 @@ const uint16_t		RejectServerFull = 0x0005;
 const uint16_t		MsgUDPLinkRequest = 0x6f66;		// 'of'
 const uint16_t		MsgUDPLinkEstablished = 0x6f67;		// 'og'
 const uint16_t		MsgUDPLinkUpdate = 0x6f68;		// 'oh'
-const uint16_t		MsgClientVersion = 0x6f69;		// 'oj'
+const uint16_t		MsgClientVersion = 0x6f6a;		// 'oj'
 
 // server control message
 
