c3639e0d53a42ce784b5d6e58ebfa3fbb7b5ee7b
Tim Riker <timriker@users.sourceforge.net>
2001-12-01 08:59:32 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@296 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bzflag/main.cpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+                          main.cpp  -  description
+                             -------------------
+    begin                : Sat Dec  1 01:46:23 MST 2001
+    copyright            : (C) 2001 by Tim Riker
+    email                : Tim@Rikers.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, World!" << endl;
+
+  return EXIT_SUCCESS;
+}
