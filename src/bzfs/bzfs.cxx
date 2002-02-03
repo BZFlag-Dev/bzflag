7eb0606eaa106c13855cd22410e51c3abe563219
Tim Riker <timriker@users.sourceforge.net>
2002-02-03 06:45:02 +0000

    revert as MsgEnter _does_ need to be mangled before getting to other players
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@327 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -1321,6 +1321,7 @@ static void patchMessage(PlayerId fromId, PlayerId toId, const void *msg)
   switch (code) {
     case MsgAddPlayer:
     case MsgCaptureFlag:
+    case MsgEnter:
     case MsgPlayerUpdate:
     case MsgRemovePlayer:
     case MsgScoreOver:
@@ -1365,7 +1366,6 @@ static void patchMessage(PlayerId fromId, PlayerId toId, const void *msg)
 	// server version
 	patchPlayerId(fromId, toId, msg, 4);
       ;;
-    case MsgEnter:
     case MsgAccept:
     case MsgClientVersion:
     case MsgExit:
