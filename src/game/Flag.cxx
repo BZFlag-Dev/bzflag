ff29450cee53a772c5066d3c4c381e90d775eed3
Nafees Bin Zafar <nafees@users.sourceforge.net>
2003-01-02 06:08:34 +0000

    Collated the flag name, abbreviation, and help arrays into one array of FlagTable classes.  Defined FlagTable class within Flag class.
    FlagTable class contains the name, abbreviation, type, and help.  I should also remove the dependency on ordering this list the same as the Flag enumeration order in global.h.  Use a std::map perhaps.
    
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@1030 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/game/Flag.cxx
+++ src/game/Flag.cxx
@@ -14,157 +14,115 @@
 #include "Pack.h"
 #include "Team.h"
 
-const char*				Flag::flagName[] = {
-								"Rogue",				// should never be used
-								"Red Team",
-								"Green Team",
-								"Blue Team",
-								"Purple Team",
-								"High Speed",
-								"Quick Turn",
-								"Oscillation Overthruster",
-								"Rapid Fire",
-								"Machine Gun",
-								"Guided Missile",
-								"Laser",
-								"Ricochet",
-								"Super Bullet",
-								"Invisible Bullet",
-								"Stealth",
-								"Tiny",
-								"Narrow",
-								"Shield",
-								"Steamroller",
-								"Shock Wave",
-								"Phantom Zone",
-								"Genocide",
-								"Jumping",
-								"Identify",
-								"Cloaking",
-								"Masquerade",
-								"Thief",
-								"Burrow",
-								"Seer",
-								"Colorblindness",
-								"Obesity",
-								"Left Turn Only",
-								"Right Turn Only",
-								"Momentum",
-								"Blindness",
-								"Jamming",
-								"Wide Angle",
-								"No Jumping",
-								"Long Reload",
-								"Antagonize",
-								"Trigger Happy",
-								"USeless flag",
-						};
 
-const char*				Flag::flagAbbv[] = {
-								"X*",						// rogue team
-								"R*",						// red team
-								"G*",						// green team
-								"B*",						// blue team
-								"P*",						// purple team
-								"V",						// high speed
-								"A",						// quick turn
-								"OO",						// oscillation over...
-								"F",						// rapid fire
-								"MG",						// machine gun
-								"GM",						// guided missile
-								"L",						// laser
-								"R",						// ricochet
-								"SB",						// super bullet
-								"IB",						// invisible bullet
-								"ST",						// stealth
-								"T",						// tiny
-								"N",						// narrow
-								"SH",						// shield
-								"SR",						// steamroller
-								"SW",						// shock wave
-								"PZ",						// phantom zone
-								"G",						// genocide
-								"JP",						// jumping
-								"ID",						// identify
-								"CL",						// cloaking
-								"MQ",						// masquerade
-								"TH",						// thief
-								"BU",						// burrow
-								"SE",						// seer
-								"CB",						// colorblindness
-								"O",						// obesity
-								"<-",						// left turn only
-								"->",						// right turn only
-								"M",						// momentum
-								"B",						// blindness
-								"JM",						// jamming
-								"WA",						// wide angle
-								"NJ",						// no jumping
-								"LR",						// long reload
-								"AN",						// antagonize
-								"TR",						// trigger happy
-								"US",						// useless flag
-						};
+/*! \internal The format of this flag table is:
+    flag name, flag name abbreviation, flag type, flag help
 
-const char*				Flag::flagHelp[] = {
-"You have no flag.",
-"",
-"",
-"",
-"",
-"Velocity (+V):  Tank moves faster.  Outrun bad guys.",
-"Angular velocity (+A):  Tank turns faster.  Dodge quicker.",
-"Oscillation Overthruster (+OO):  Can drive through buildings.  "
-				"Can't backup or shoot while inside.",
-"rapid Fire (+F):  Shoots more often.  Shells go faster but not as far.",
-"Machine Gun (+MG):  Very fast reload and very short range.",
-"Guided Missile (+GM):  Shots track a target.  "
-				"Lock on with right button.  "
-				"Can lock on or retarget after firing.",
-"Laser (+L):  Shoots a laser.  Infinite speed and range but long reload time.",
-"Ricochet (+R):  Shots bounce off walls.  Don't shoot yourself!",
-"SuperBullet (+SB):  Shoots through buildings.  Can kill Phantom Zone.",
-"Invisible Bullet (+IB):  Your shots don't appear on other radars.  "
-				"Can still see them out window.",
-"STealth (+ST):  Tank is invisible on radar.  "
-				"Shots are still visible.  Sneak up behind enemies!",
-"Tiny (+T):  Tank is small and can get through small openings.  "
-				"Very hard to hit.",
-"Narrow (+N):  Tank is super thin.  Very hard to hit from front but is "
-				"normal size from side.  Can get through small openings.",
-"SHield (+SH):  Getting hit only drops flag.  Flag flys an extra-long time.",
-"SteamRoller (+SR):  Destroys tanks you touch but you have to get really close.",
-"Shock Wave (+SW):  Firing destroys all tanks nearby.  "
-				"Don't kill teammates!  Can kill tanks on/in buildings.",
-"Phantom Zone (+PZ):  Teleporting toggles Zoned effect.  "
-				"Zoned tank can drive through buildings.  "
-				"Zoned tank can't shoot or be shot (except by "
-				"superbullet and shock wave).",
-"Genocide (+G):  Killing one tank kills that tank's whole team.",
-"JumPing (+JP):  Tank can jump.  Use Tab key.  Can't steer in the air.",
-"IDentify (+ID):  Identifies type of nearest flag.",
-"CLoaking (+CL):  Makes your tank invisible out-the-window.  "
-				"Still visible on radar.",
-"MasQuerade (+MQ):  Makes your tank look like a teammate out-the-window.  "
-				"Normal team colors everywhere else.",
-"THief (+TH):  Steal flags.  Small and fast but can't kill.",
-"BUrrow (+BU):  Drive partly underground. Can't be hit by regular shot."
-				"Don't get run over!",
-"SEer (+SE):  See Cloaked, Steathed and Masqueraded tanks as normal", 
-"ColorBlindness (-CB):  Can't tell team colors.  Don't shoot teammates!",
-"Obesity (-O):  Tank becomes very large.  Can't fit through teleporters.",
-"left turn only (- <-):  Can't turn right.",
-"right turn only (- ->):  Can't turn left.",
-"Momentum (-M):  Tank has inertia.  Acceleration is limited.",
-"Blindness (-B):  Can't see out window.  Radar still works.",
-"JaMming (-JM):  Radar doesn't work.  Can still see.",
-"Wide Angle (-WA):  Fish-eye lens distorts view.",
-"No Jumping (-NJ):  Tank cannot jump.",
-"Long Reload (-LR):  Tank takes twice as long to reload.",
-"Antagonize (-AN): Tank sends taunts to other players involuntarily",
-"TRigger happy (-TR): Tank fires shots as soon as they are available",
-"You have found the useless flag. Use it wisely",
-						};
+    Since the FlagTable class is defined within the Flag class, 
+	any declaration requires the Flag:: qualifier
+  */
+const Flag::FlagTable Flag::flagTable[] = {
+  Flag::FlagTable("Rogue", "X*", FlagNormal, "No flag for you!"),
+  Flag::FlagTable("Red Team", "R*", FlagNormal, ""),
+  Flag::FlagTable("Green Team", "G*", FlagNormal, ""),
+  Flag::FlagTable("Blue Team", "B*", FlagNormal, ""),
+  Flag::FlagTable("Purple Team", "P*", FlagNormal, ""),
+
+  Flag::FlagTable("High Speed", "V", FlagUnstable, 
+		  "Velocity (+V):  Tank moves faster.  Outrun bad guys."),
+  Flag::FlagTable("Quick Turn", "A", FlagUnstable, 
+		  "Angular velocity (+A):  Tank turns faster.  Dodge quicker."),
+  Flag::FlagTable("Oscillation Overthruster", "OO", FlagUnstable, 
+		  "Oscillation Overthruster (+OO):  Can drive through buildings.  "
+		  "Can't backup or shoot while inside."),
+  Flag::FlagTable("Rapid Fire", "F", FlagUnstable, 
+		  "rapid Fire (+F):  Shoots more often.  Shells go faster but not as far."),
+  Flag::FlagTable("Machine Gun", "MG", FlagUnstable, 
+		  "Machine Gun (+MG):  Very fast reload and very short range."),
+  Flag::FlagTable("Guided Missile", "GM", FlagUnstable, 
+		  "Guided Missile (+GM):  Shots track a target.  "
+		  "Lock on with right button.  "
+		  "Can lock on or retarget after firing."),
+  Flag::FlagTable("Laser", "L", FlagUnstable, 
+		  "Laser (+L):  Shoots a laser.  Infinite speed and range but long reload time."),
+  Flag::FlagTable("Ricochet", "R", FlagUnstable, 
+		  "Ricochet (+R):  Shots bounce off walls.  Don't shoot yourself!"),
+  Flag::FlagTable("Super Bullet", "SB", FlagUnstable, 
+		  "SuperBullet (+SB):  Shoots through buildings.  Can kill Phantom Zone."),
+  Flag::FlagTable("Invisible Bullet", "IB", FlagUnstable, 
+		  "Invisible Bullet (+IB):  Your shots don't appear on other radars.  "
+		  "Can still see them out window."),
+  Flag::FlagTable("Stealth", "ST", FlagUnstable, 
+		  "STealth (+ST):  Tank is invisible on radar.  "
+		  "Shots are still visible.  Sneak up behind enemies!"),
+  Flag::FlagTable("Tiny", "T", FlagUnstable, 
+		  "Tiny (+T):  Tank is small and can get through small openings.  "
+		  "Very hard to hit."),
+  Flag::FlagTable("Narrow", "N", FlagUnstable, 
+		  "Narrow (+N):  Tank is super thin.  Very hard to hit from front but is "
+		  "normal size from side.  Can get through small openings."),
+  Flag::FlagTable("Shield", "SH", FlagUnstable, 
+		  "SHield (+SH):  Getting hit only drops flag.  Flag flys an extra-long time."),
+  Flag::FlagTable("Steamroller", "SR", FlagUnstable, 
+		  "SteamRoller (+SR):  Destroys tanks you touch but you have to get really close."),
+  Flag::FlagTable("Shock Wave", "SW", FlagUnstable, 
+		  "Shock Wave (+SW):  Firing destroys all tanks nearby.  "
+		  "Don't kill teammates!  Can kill tanks on/in buildings."),
+  Flag::FlagTable("Phantom Zone", "PZ", FlagUnstable, 
+		  "Phantom Zone (+PZ):  Teleporting toggles Zoned effect.  "
+		  "Zoned tank can drive through buildings.  "
+		  "Zoned tank can't shoot or be shot (except by "
+		  "superbullet and shock wave)."),
+  Flag::FlagTable("Genocide", "G", FlagUnstable, 
+		  "Genocide (+G):  Killing one tank kills that tank's whole team."),
+  Flag::FlagTable("Jumping", "JP", FlagUnstable, 
+		  "JumPing (+JP):  Tank can jump.  Use Tab key.  Can't steer in the air."),
+  Flag::FlagTable("Identify", "ID", FlagUnstable, 
+		  "IDentify (+ID):  Identifies type of nearest flag."),
+  Flag::FlagTable("Cloaking", "CL", FlagUnstable, 
+		  "CLoaking (+CL):  Makes your tank invisible out-the-window.  "
+		  "Still visible on radar."),
+  Flag::FlagTable("Masquerade", "MQ", FlagUnstable, 
+		  "MasQuerade (+MQ):  Makes your tank look like a teammate out-the-window.  "
+		  "Normal team colors everywhere else."),
+  Flag::FlagTable("Thief", "TH", FlagUnstable, 
+		  "THief (+TH):  Steal flags.  Small and fast but can't kill."),
+  Flag::FlagTable("Burrow", "BU", FlagUnstable, 
+		  "BUrrow (+BU):  Drive partly underground. Can't be hit by regular shot."
+		  "Don't get run over!"),
+  Flag::FlagTable("Seer", "SE", FlagUnstable, 
+		  "SEer (+SE):  See Cloaked, Stealthed and Masqueraded tanks as normal"),
+
+  Flag::FlagTable("Colorblindness", "CB", FlagSticky, 
+		  "ColorBlindness (-CB):  Can't tell team colors.  Don't shoot teammates!"),
+  Flag::FlagTable("Obesity", "O", FlagSticky, 
+		  "Obesity (-O):  Tank becomes very large.  Can't fit through teleporters."),
+  Flag::FlagTable("Left Turn Only", "<-", FlagSticky, 
+		  "left turn only (- <-):  Can't turn right."),
+  Flag::FlagTable("Right Turn Only", "->", FlagSticky, 
+		  "right turn only (- ->):  Can't turn left."),
+  Flag::FlagTable("Momentum", "M", FlagSticky, 
+		  "Momentum (-M):  Tank has inertia.  Acceleration is limited."),
+  Flag::FlagTable("Blindness", "B", FlagSticky, 
+		  "Blindness (-B):  Can't see out window.  Radar still works."),
+  Flag::FlagTable("Jamming", "JM", FlagSticky, 
+		  "JaMming (-JM):  Radar doesn't work.  Can still see."),
+  Flag::FlagTable("Wide Angle", "WA", FlagSticky, 
+		  "Wide Angle (-WA):  Fish-eye lens distorts view."),
+  Flag::FlagTable("No Jumping", "NJ", FlagSticky, 
+		  "No Jumping (-NJ):  Tank cannot jump."),
+  Flag::FlagTable("Long Reload", "LR", FlagSticky, 
+		  "Long Reload (-LR):  Tank takes twice as long to reload."),
+  Flag::FlagTable("Antagonize", "AN", FlagSticky, 
+		  "Antagonize (-AN): Tank sends taunts to other players involuntarily"),
+  Flag::FlagTable("Trigger Happy", "TR", FlagSticky, 
+		  "TRigger happy (-TR): Tank fires shots as soon as they are available"),
+  Flag::FlagTable("USeless flag", "US", FlagSticky, 
+		  "You have found the useless flag. Use it wisely"),
+  Flag::FlagTable()
+};
+
+
 
 void*					Flag::pack(void* buf) const
 {
@@ -199,44 +157,22 @@ void*					Flag::unpack(void* buf)
 
 const char*				Flag::getName(FlagId id)
 {
-	return flagName[int(id)];
+	return flagTable[int(id)].getname();
 }
 
 const char*				Flag::getAbbreviation(FlagId id)
 {
-	return flagAbbv[int(id)];
+	return flagTable[int(id)].getabbv();
 }
 
 const char*				Flag::getHelp(FlagId id)
 {
-	return flagHelp[int(id)];
+	return flagTable[int(id)].gethelp();
 }
 
 FlagType				Flag::getType(FlagId id)
 {
-	switch (id) {
-		case NoFlag:
-		case RedFlag:
-		case GreenFlag:
-		case BlueFlag:
-		case PurpleFlag:
-			return FlagNormal;
-		case ColorblindnessFlag:
-		case ObesityFlag:
-		case LeftTurnOnlyFlag:
-		case RightTurnOnlyFlag:
-		case MomentumFlag:
-		case BlindnessFlag:
-		case JammingFlag:
-		case WideAngleFlag:
-		case NoJumpingFlag:
-		case LongReloadFlag:
-		case AntagonizeFlag:
-		case TriggerHappyFlag:
-			return FlagSticky;
-		default:
-			return FlagUnstable;
-	}
+	return flagTable[int(id)].gettype();
 }
 
 const float*			Flag::getColor(FlagId id)
@@ -257,18 +193,18 @@ const float*			Flag::getColor(FlagId id)
 FlagId					Flag::getIDFromName(const char* name)
 {
 	// start at 1 because "Rogue" isn't a valid flag name
-	for (unsigned int i = FirstFlag; i < countof(flagName); ++i)
-		if (strnocasecmp(flagName[i], name) == 0)
-			return static_cast<FlagId>(i);
+	for (unsigned int i = FirstFlag; i < countof(flagTable); ++i)
+		if (strnocasecmp(flagTable[i].getname(), name) == 0)
+			return FlagId(i);
 	return NullFlag;
 }
 
 FlagId					Flag::getIDFromAbbreviation(const char* abbv)
 {
 	// start after teams because team flags don't have abbreviations
-	for (unsigned int i = FirstFlag; i < countof(flagAbbv); ++i)
-		if (strnocasecmp(flagAbbv[i], abbv) == 0)
-			return static_cast<FlagId>(i);
+	for (unsigned int i = FirstFlag; i < countof(flagTable); ++i)
+		if (strnocasecmp(flagTable[i].getabbv(), abbv) == 0)
+			return FlagId(i);
 	return NullFlag;
 }
 // ex: shiftwidth=4 tabstop=4
