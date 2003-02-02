a57522d0f903344c7926a9857ac4452e4788b1dc
Frank Thilo <chestal@users.sourceforge.net>
2003-02-02 23:20:27 +0000

    no // in .c files
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1212 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/common/bzsignal.c
+++ src/common/bzsignal.c
@@ -18,7 +18,7 @@ SIG_PF bzSignal(int signo, SIG_PF func)
 {
 #ifdef _WIN32
   return signal(signo, func);
-#else // _WIN32
+#else /* _WIN32 */
   struct sigaction act, oact;
 
   act.sa_handler = func;
@@ -42,7 +42,7 @@ SIG_PF bzSignal(int signo, SIG_PF func)
   if (sigaction(signo, &act, &oact) < 0)
     return SIG_ERR;
   return oact.sa_handler;
-#endif // _WIN32
+#endif /* _WIN32 */
 }
 /*
  * ex: shiftwidth=2 tabstop=8
