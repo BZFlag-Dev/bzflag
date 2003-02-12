aa51698ce91d4d13871958db0c13766a29cb7b5a
Jeff Myers <jeffm2501@users.sourceforge.net>
2003-02-12 19:45:57 +0000

    Undid pyramid colision fix, so that all clients with theis protocoll verison have the same behavor.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1241 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/obstacle/PyramidBuilding.cxx
+++ src/obstacle/PyramidBuilding.cxx
@@ -177,6 +177,14 @@ void			PyramidBuilding::getCorner(int index,
 
 float			PyramidBuilding::shrinkFactor(float z) const
 {
+/* old way of doing it, but needed for legacy support */
+ if (z >= getHeight())
+	 return 0.0;
+
+ return (getHeight() - z) / getHeight();
+
+/* todo: put this bug fix back in after we break protocol so all clients use the same logic
+* Jeff Myers (2/12/03)
   const float *pos = getPosition();
   z -= pos[2];
 
@@ -185,7 +193,7 @@ float			PyramidBuilding::shrinkFactor(float z) const
   if (z > getHeight())
     return 0.0f;
 
-  return (getHeight() - z) / getHeight();
+  return (getHeight() - z) / getHeight(); */
 }
 
 //
