edffb61ff341083a2441227d50fae05e2162702a
Tim Riker <timriker@users.sourceforge.net>
2003-02-18 19:45:12 +0000

    add cache file
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1270 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/mrtg-bzflag.pl
+++ misc/mrtg-bzflag.pl
@@ -17,16 +17,29 @@ my $no_BZFlag;
 
 use Socket;
 use LWP::UserAgent;
+use File::stat;
 
-my $ua = new LWP::UserAgent;
+my $cacheFile = '/tmp/mrtg-bzflag.tmp';
+my @lines;
 
-$ua->timeout(5);
+if (time() - stat($cacheFile)->mtime < 10) {
+  open(CACHEFILE, "<$cacheFile");
+  chomp(@lines = <CACHEFILE>);
+  close(CACHEFILE);
+} else {
+  my $ua = new LWP::UserAgent;
+  $ua->timeout(5);
+  my $req = HTTP::Request->new('GET', 'http://list.bzflag.org:5156/');
+  my $res = $ua->request($req);
+  @lines = split("\n",$res->content);
+  open(CACHEFILE, ">$cacheFile") or die;
+  print(CACHEFILE join("\n", @lines));
+  close(CACHEFILE);
+}
 
-my $req = HTTP::Request->new('GET', 'http://list.bzflag.org:5156/');
-my $res = $ua->request($req);
 my $totalServers = 0;
 my $totalPlayers = 0;
-for my $line (split("\n",$res->content)) {
+for my $line (@lines) {
   my ($serverport, $version, $flags, $ip, $comments) = split(" ",$line,5);
   # not "(A4)18" to handle old dumb perl
   my ($style,$maxPlayers,$maxShots,
