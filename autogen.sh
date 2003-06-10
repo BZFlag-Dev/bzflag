0a623b3dccc58731de4683c7b6541dc3670618b1
Tim Riker <timriker@users.sourceforge.net>
2003-06-10 02:36:19 +0000

    don't build, just prep
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1464 08b3d480-bf2c-0410-a26f-811ee3361c24

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,11 @@
-aclocal
+#!/bin/sh
+# script to prepare bzflag sources
 touch NEWS
+aclocal
 autoheader
 automake --add-missing
 autoconf
-./configure --enable-robots
-make
+
+echo BZFlag sources are now prepared. To build here, run:
+echo " ./configure --enable-robots"
+echo " make"
