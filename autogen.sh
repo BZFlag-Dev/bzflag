f18ea456bf77fa3a3a31e59836892dc1733f1ea3
Tim Riker <timriker@users.sourceforge.net>
2003-06-10 05:55:03 +0000

    abort if dead
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1480 08b3d480-bf2c-0410-a26f-811ee3361c24

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # script to prepare bzflag sources
-aclocal
-autoheader
-automake --add-missing
-autoconf
+aclocal \
+&& autoheader \
+&& automake --add-missing \
+&& autoconf \
+|| exit 1
 
 if [ -z "$1" ] ; then
  echo BZFlag sources are now prepared. To build here, run:
