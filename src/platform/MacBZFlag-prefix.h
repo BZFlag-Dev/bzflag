c1702fbe3c69a094b3aa5aceb0c1637bf6759cb6
Alfredo Tupone <atupone@users.sourceforge.net>
2003-06-16 22:13:18 +0000

    Inserting Mac file again in src/platform
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1546 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/platform/MacBZFlag-prefix.h
@@ -0,0 +1,26 @@
+/* bzflag
+ * Copyright 1993-2003, Chris Schoeneman, Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include <Carbon/Carbon.h>
+#include <CoreServices/CoreServices.h>
+
+/* so that socklen_t is defined in bzfrelay.c */
+#define _BSD_SOCKLEN_T_
+/* BZFlag version */
+#define VERSION	10707001
+/* enable robots */
+#define ROBOT
+
+#undef TCP_NODELAY
+#undef TCP_MAXSEG
+/* temp compatibility define */
+#define __unix__
