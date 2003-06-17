23aa72b4e09feae45bc5d90eda70b0495a5c2f3c
Sean Morrison <brlcad@users.sourceforge.net>
2003-06-17 05:53:11 +0000

    Moved os x specific source up from src/platform and updated project builder file for optimized builds
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_7/bzflag@1548 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/platform/MacOSX/MacWindow.h
@@ -0,0 +1,55 @@
+#ifndef BZF_WINWINDOW_H
+#define  BZF_WINWINDOW_H
+
+#include <Carbon/Carbon.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+
+#include "BzfWindow.h"
+#include "MacDisplay.h"
+#include "MacVisual.h"
+
+class MacWindow : public BzfWindow {
+  public:
+    MacWindow(const MacDisplay*, MacVisual*);
+    ~MacWindow();
+
+    bool isValid() const;
+
+    void showWindow(bool);
+
+    void getPosition(int& x, int& y);
+    void getSize(int& width, int& height) const;
+
+    void setTitle(const char*);
+    void setPosition(int x, int y);
+    void setSize(int width, int height);
+    void setMinSize(int width, int height);
+    void setFullscreen();
+
+    void warpMouse(int x, int y);
+    void getMouse(int& x, int& y) const;
+    void grabMouse();
+    void ungrabMouse();
+    void showMouse();
+    void hideMouse();
+
+    void setGamma(float);
+    float getGamma() const;
+    bool hasGammaControl() const;
+
+    void makeCurrent();
+    void swapBuffers();
+    void makeContext();
+    void freeContext();
+
+  private:
+    Rect rect;
+
+    WindowRef window;
+    CGLContextObj context;
+};
+
+#endif // BZF_WINWINDOW_H
+// ex: shiftwidth=2 tabstop=8
