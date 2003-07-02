31a945ddf2682648a3e734e85e174f2159ca18f8
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2003-07-02 17:40:46 +0000

    add FileManager to build
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@1720 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/FileManager.h
@@ -0,0 +1,63 @@
+/* bzflag
+ * Copyright (c) 1993 - 2003 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef BZF_FILE_MANAGER_H
+#define BZF_FILE_MANAGER_H
+
+#ifdef _WIN32
+#pragma warning(4:4786)
+#endif
+
+#include <string>
+#include "common.h"
+#include "bzfio.h"
+
+#define FILEMGR (FileManager::getInstance())
+
+class FileManager {
+public:
+  ~FileManager();
+
+  // create an input stream for a file in the data directory.
+  // this will look in several places until it finds the file.
+  // if filename is an absolute path then only that place is
+  // checked.  if the file cannot be found or isn't readable
+  // then NULL is returned.
+  std::istream*		createDataInStream(const std::string& filename,
+					   bool binary = false) const;
+
+  // create an output stream in the data directory (or wherever
+  // indicated if filename is an absolute path).  if the file
+  // can be opened for writing then NULL is returned.
+  std::ostream*		createDataOutStream(const std::string& filename,
+					    bool binary = false,
+					    bool truncate = true) const;
+
+  // returns true if the path is absolute, false if relative
+  bool			isAbsolute(const std::string& path) const;
+
+  // concatenate two pathname components with a directory separator
+  // between them.
+  std::string		catPath(const std::string& a, const std::string& b) const;
+
+  // get the singleton instance
+  static FileManager*	getInstance();
+
+private:
+  FileManager();
+
+private:
+  static FileManager*	mgr;
+};
+
+#endif
+// ex: shiftwidth=2 tabstop=8
---
+++ src/common/FileManager.cxx
@@ -0,0 +1,184 @@
+/* bzflag
+ * Copyright (c) 1993 - 2003 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "FileManager.h"
+#include "string"
+#include "StateDatabase.h"
+#include <ctype.h>
+#include <fstream>
+
+
+//
+// FileManager
+//
+
+FileManager*			FileManager::mgr = NULL;
+
+FileManager::FileManager()
+{
+  // do nothing
+}
+
+FileManager::~FileManager()
+{
+  mgr = NULL;
+}
+
+FileManager*			FileManager::getInstance()
+{
+  if (mgr == NULL)
+    mgr = new FileManager;
+  return mgr;
+}
+
+std::istream*			FileManager::createDataInStream(
+						const std::string& filename,
+						bool binary) const
+{
+  // choose open mode
+  std::ios::openmode mode = std::ios::in;
+  if (binary)
+    mode |= std::ios::binary;
+
+  const bool relative = !isAbsolute(filename);
+  if (relative) {
+    // try directory stored in DB
+    if (BZDB->isSet("directory")) {
+      std::ifstream* stream = new std::ifstream(catPath(BZDB->get("directory"),
+						filename).c_str(), mode);
+      if (stream && *stream)
+        return stream;
+      delete stream;
+    }
+
+    // try data directory
+    {
+      std::ifstream* stream = new std::ifstream(catPath("data", filename).c_str(), mode);
+      if (stream && *stream)
+        return stream;
+      delete stream;
+    }
+  }
+
+  // try current directory (or absolute path)
+  {
+    std::ifstream* stream = new std::ifstream(filename.c_str(), mode);
+    if (stream && *stream)
+      return stream;
+    delete stream;
+  }
+
+  // try install directory
+#if defined(INSTALL_DATA_DIR)
+  if (relative) {
+    std::ifstream* stream = new std::ifstream(catPath(INSTALL_DATA_DIR,
+					      filename).c_str(), mode);
+    if (stream && *stream)
+      return stream;
+    delete stream;
+  }
+#endif
+
+  return NULL;
+}
+
+std::ostream*			FileManager::createDataOutStream(
+						const std::string& filename,
+						bool binary,
+						bool truncate) const
+{
+  // choose open mode
+  std::ios::openmode mode = std::ios::out;
+  if (binary)
+    mode |= std::ios::binary;
+  if (truncate)
+    mode |= std::ios::trunc;
+
+  const bool relative = !isAbsolute(filename);
+  if (relative) {
+    // try directory stored in DB
+    if (BZDB->isSet("directory")) {
+      std::ofstream* stream = new std::ofstream(catPath(BZDB->get("directory"),
+						filename).c_str(), mode);
+      if (stream && *stream)
+        return stream;
+      delete stream;
+      return NULL;
+    }
+
+    // try data directory
+    {
+      std::ofstream* stream = new std::ofstream(catPath("data", filename).c_str(), mode);
+      if (stream && *stream)
+        return stream;
+      delete stream;
+      return NULL;
+    }
+  } else {
+    // try absolute path
+    std::ofstream* stream = new std::ofstream(filename.c_str(), mode);
+    if (stream && *stream)
+      return stream;
+    delete stream;
+    return NULL;
+  }
+}
+
+bool				FileManager::isAbsolute(const std::string& path) const
+{
+  if (path.empty())
+    return false;
+
+#if defined(_WIN32)
+  const char* cpath = path.c_str();
+  if (cpath[0] == '\\' || cpath[0] == '/')
+    return true;
+  if (path.size() >= 3 && isalpha(cpath[0]) && cpath[1] == ':' &&
+      (cpath[2] == '\\' || cpath[2] == '/'))
+    return true;
+#elif defined(macintosh)
+#error FIXME -- what indicates an absolute path on mac?
+#else
+  if (path.c_str()[0] == '/')
+    return true;
+#endif
+
+  return false;
+}
+
+std::string			FileManager::catPath(
+						const std::string& a,
+						const std::string& b) const
+{
+  // handle trivial cases
+  if (a.empty())
+    return b;
+  if (b.empty())
+    return a;
+
+#if defined(_WIN32)
+  std::string c = a;
+  c += "\\";
+  c += b;
+  return c;
+#elif defined(macintosh)
+  std::string c = a;
+  c += ':';
+  c += b;
+#else
+  std::string c = a;
+  c += "/";
+  c += b;
+  return c;
+#endif
+}
+// ex: shiftwidth=4 tabstop=4
