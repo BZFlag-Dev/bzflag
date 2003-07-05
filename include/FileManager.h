11e43fd6befc8dfd5e5e5a0626bb7a71f9bdd427
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2003-07-05 05:45:34 +0000

    document FileManager for example
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@1767 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/FileManager.h
+++ include/FileManager.h
@@ -23,33 +23,51 @@
 
 #define FILEMGR (FileManager::getInstance())
 
+/**
+ Simple file management.
+ This class provides functions to create stream objects, and provides
+ some platform independence for files requiring directory names.
+*/
+
 class FileManager {
 public:
+  /** Destroy the FileManager */
   ~FileManager();
 
-  // create an input stream for a file in the data directory.
-  // this will look in several places until it finds the file.
-  // if filename is an absolute path then only that place is
-  // checked.  if the file cannot be found or isn't readable
-  // then NULL is returned.
+  /** Open an input stream.
+   create an input stream for a file in the data directory.
+   this will look in several places until it finds the file.
+   if filename is an absolute path then only that place is
+   checked.  if the file cannot be found or isn't readable
+   then NULL is returned.
+  */
   std::istream*		createDataInStream(const std::string& filename,
 					   bool binary = false) const;
 
-  // create an output stream in the data directory (or wherever
-  // indicated if filename is an absolute path).  if the file
-  // can be opened for writing then NULL is returned.
+  /** Open an output stream.
+   create an output stream in the data directory (or wherever
+   indicated if filename is an absolute path).  if the file
+   can be opened for writing then NULL is returned.
+  */
   std::ostream*		createDataOutStream(const std::string& filename,
 					    bool binary = false,
 					    bool truncate = true) const;
 
-  // returns true if the path is absolute, false if relative
+  /** Check for absolute path.
+   returns true if the path is absolute, false if relative
+  */
   bool			isAbsolute(const std::string& path) const;
 
-  // concatenate two pathname components with a directory separator
-  // between them.
+  /** Concatenate directory names.
+   concatenate two pathname components with a directory separator
+   between them.
+  */
   std::string		catPath(const std::string& a, const std::string& b) const;
 
-  // get the singleton instance
+  /** Get the singleton instance.
+   returns the existing singleton if available, if not, creates
+   an instance and returns it.
+  */
   static FileManager*	getInstance();
 
 private:
