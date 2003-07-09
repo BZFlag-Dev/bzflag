c5f10dea9a0024b8fce485c720e3d99c1bd86354
Sean Morrison <brlcad@users.sourceforge.net>
2003-07-09 02:00:16 +0000

    separated definition from implementation, added header, fixed whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@2002 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/WordFilter.h
+++ include/WordFilter.h
@@ -1,3 +1,15 @@
+/* bzflag
+ * Copyright (c) 1993 - 2003 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #ifndef __WORDFILTER_H__
 #define __WORDFILTER_H__
 
@@ -10,10 +22,10 @@
 #include <sys/types.h>
 #include <regex.h>
 
-/* !!! temporary */
-#define bzfrand()	((double)rand() / ((double)RAND_MAX + 1.0))
+#include "common.h"
 
-/* WordFilter will load a list of words and phrases from a file or one at
+
+/** WordFilter will load a list of words and phrases from a file or one at
  * a time or manually.
  *
  * Loading from file assumes that words/phrases are listed one per line;
@@ -61,6 +73,90 @@
  * absolutely everything you want to filter and all variations).	It is still
  * case-insensitive and ignores punctuation.
  */
+class WordFilter
+{
 
-#endif
+ private:
+  
+  /** used by the simple filter */
+  std::string alphabet;
+  
+  /** used by the agressive filter */
+  std::set<std::string> suffixes;
+  std::set<std::string> prefixes;
+  
+  std::string filterChars;
+  
+  /** structure containing the filter words and optional compiled version */
+  typedef struct badWord {
+    std::string word;
+    bool compiled;
+    std::string expression;
+    regex_t *compiledWord;
+  } badWord_t;
+  
+  /** word expressions to be filtered including compiled regexp versions */
+  struct expressionCompare {
+    bool operator () (const badWord_t& word1, const badWord_t& word2) const {
+      
+      
+      return strncasecmp(word1.word.c_str(), word2.word.c_str(), 1024) < 0;
+    }
+  };
+  std::set<badWord_t, expressionCompare> badWords;
+  
+ protected:
+
+  /** This filter does a simple case-insensitive
+   * word comparison that compares all filter 
+   * words with all alphabetic string sets in the
+   * input string. If test is a filter word, then
+   * input strings "test", "testy", and "test;"
+   * will get filtered to "****", "testy", and 
+   * "****;" respectively.
+   */
+  bool simpleFilter(char *input);
+  
+  
+  /** This filter will take a filter word and
+   * create a rather complex regular expression
+   * that catches a variety of variations on a 
+   * word.  Variations include automatic internal
+   * expansion, optional punctuation and
+   * whitespace, suffix matching (including
+   * plurality), leet-speak conversion and more.
+   * See the header above for more details.  This
+   * filter should be the default.
+   */
+  bool aggressiveFilter(char *input);
+  
+  /** provides a pointer to a fresh compiled
+   * expression for some given expression
+   */
+  regex_t *getCompiledExpression(const std::string &word);
+  
+ public:
+  
+  WordFilter(void);
+  ~WordFilter(void);
+  
+  /** loads a set of bad words from a specified file */
+  void loadFromFile(const std::string &fileName);
+
+  /** adds an individual word to the filter list */
+  bool addWord(const std::string &word);
+  
+  /** given an input string, filter the input
+   * using either the simple or agressive filter
+   */
+  bool filter(char *input, bool simple=false);
+
+  /** dump a list of words in the filter to stdout */
+  void outputWords(void);
+};
+
+
+#else
 class WordFilter;
+#endif
+// ex: shiftwidth=2 tabstop=8
