5671fa5ca2eff224488afa686986c02cdc0aae9f
Micah Elizabeth Scott <micahjd@users.sourceforge.net>
2003-07-09 06:25:34 +0000

    Remove FlagId code in radar flag rendering
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/remove_flag_id/bzflag@2043 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/RadarRenderer.cxx
+++ src/bzflag/RadarRenderer.cxx
@@ -424,10 +424,10 @@ void			RadarRenderer::render(SceneRenderer& renderer,
       const Flag& flag = world.getFlag(i);
       if (flag.status == FlagNoExist || flag.status == FlagOnTank)
 	continue;
-      if (flag.id >= FirstTeamFlag && flag.id <= LastTeamFlag)
+      if (flag.desc->flagTeam != NoTeam)
 	continue;
       const float cs = colorScale(flag.position[2], MuzzleHeight, BZDB->isTrue("enhancedradar"));
-      const float *flagcolor = Flag::getColor(flag.id);
+      const float *flagcolor = flag.desc->getColor();
       glColor3f(flagcolor[0] * cs, flagcolor[1] * cs, flagcolor[2] * cs);
       drawFlag(flag.position[0], flag.position[1], flag.position[2]);
     }
@@ -436,11 +436,11 @@ void			RadarRenderer::render(SceneRenderer& renderer,
       const Flag& flag = world.getFlag(i);
       if (flag.status == FlagNoExist || flag.status == FlagOnTank)
 	continue;
-      if (flag.id < FirstTeamFlag || flag.id > LastTeamFlag)
+      if (flag.desc->flagTeam == NoTeam)
 	continue;
       // Flags change color by height
       const float cs = colorScale(flag.position[2], MuzzleHeight, BZDB->isTrue("enhancedradar"));
-      const float *flagcolor = Flag::getColor(flag.id);
+      const float *flagcolor = flag.desc->getColor();
       glColor3f(flagcolor[0] * cs, flagcolor[1] * cs, flagcolor[2] * cs);
       // always draw team flags
       drawFlag(flag.position[0], flag.position[1], flag.position[2], true);
@@ -499,8 +499,8 @@ void			RadarRenderer::render(SceneRenderer& renderer,
     drawTank(0.0f, 0.0f, myTank->getPosition()[2]);
 
     // my flag
-    if (myTank->getFlag() != NoFlag) {
-      glColor3fv(Flag::getColor(myTank->getFlag()));
+    if (myTank->getFlag() != Flags::Null) {
+      glColor3fv(myTank->getFlag()->getColor());
       drawFlagOnTank(0.0f, 0.0f, myTank->getPosition()[2]);
     }
   }
