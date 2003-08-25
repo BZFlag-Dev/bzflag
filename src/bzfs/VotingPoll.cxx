06167122a7f0352528a9fcfb0ff64ee8035c27af
Sean Morrison <brlcad@users.sourceforge.net>
2003-08-25 00:54:56 +0000

    Initial VotingPoll class stubbed out
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3252 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzfs/VotingPoll.cxx
@@ -0,0 +1,39 @@
+/* bzflag
+ * Copyright (c) 1993 - 2003 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* no header other than VotingPoll.h should be included here */
+
+#ifdef _WIN32
+#pragma warning( 4:4786)
+#endif
+
+#include "VotingPoll.h"
+
+
+/* private */
+
+/* protected */
+
+/* public: */
+
+VotingPoll::VotingPoll() 
+{
+  return;
+}
+
+VotingPoll::~VotingPoll(void) 
+{
+  return;
+}
+
+
+// ex: shiftwidth=2 tabstop=8
---
+++ src/bzfs/VotingPoll.h
@@ -0,0 +1,58 @@
+/* bzflag
+ * Copyright (c) 1993 - 2003 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __VOTINGPOLL_H__
+#define __VOTINGPOLL_H__
+
+#include <string>
+#include <iostream>
+
+#include "VotingBooth.h"
+#include "TimeKeeper.h"
+
+
+/** VotingPoll is a means to manage and enforce a poll.  The poll will
+ * maintain a voting booth when a poll is started.  As the poll is updated,
+ * it verifies if a vote was succesfully passed.  If it is, the action
+ * specified (kick/ban/etc) is enforced.
+ */
+class VotingPoll
+{
+ private:
+  VotingBooth *_votingBooth;
+
+  unsigned short int _voteTime;
+  unsigned short int _vetoTime;
+  unsigned short int _votesRequired;
+  float _votePercentage;
+  unsigned short int _voteRepeatTime;
+   
+
+ protected:
+
+  
+ public:
+  
+  VotingPoll();
+  ~VotingPoll(void);
+
+  inline VotingBooth *getBooth(void) {
+    return _votingBooth;
+  }
+
+};
+
+
+#else
+class VotingPoll;
+#endif
+// ex: shiftwidth=2 tabstop=8
