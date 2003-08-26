d46f9deefd471f14266aac09eebcd2c40c22b647
Tim Riker <timriker@users.sourceforge.net>
2003-08-26 23:51:03 +0000

    no limits here
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3280 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzadmin/AutoCompleter.cxx
+++ src/bzadmin/AutoCompleter.cxx
@@ -11,7 +11,7 @@
  */
 
 #include <algorithm>
-#include <limits>
+//#include <limits>
 
 #include "AutoCompleter.h"
 
@@ -29,10 +29,10 @@ void AutoCompleter::unregisterWord(const string& str) {
 
 
 string AutoCompleter::complete(const string& str) {
-  
+
   if (str.size() == 0)
     return str;
-  
+
   // find the first and last word with the prefix str
   vector<string>::iterator first, last;
   first = lower_bound(words.begin(), words.end(), str);
@@ -41,7 +41,7 @@ string AutoCompleter::complete(const string& str) {
   string tmp = str;
   tmp[tmp.size() - 1]++;
   last = lower_bound(first, words.end(), tmp) - 1;
-  
+
   // return the largest common prefix
   unsigned int i;
   for (i = 0; i < first->size() && i < last->size(); ++i)
