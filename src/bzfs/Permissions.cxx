6fd528689bccc951f952f33b31d5c133ca1d0a03
Jeff Myers <jeffm2501@users.sourceforge.net>
2003-09-08 16:18:09 +0000

    Check to see if users have verified before you see if they have perms.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3421 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/Permissions.cxx
+++ src/bzfs/Permissions.cxx
@@ -71,6 +71,8 @@ bool removeGroup(PlayerAccessInfo& info, const std::string &group)
 
 bool hasPerm(PlayerAccessInfo& info, AccessPerm right)
 {
+  if (!info.verified)
+    return false
   if (info.explicitDenys.test(right))
     return false;
   if (info.explicitAllows.test(right))
