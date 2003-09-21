da5f1bce1aaafdf60c1f85c4a2855238693d8362
Dave Brosius <dbrosius@users.sourceforge.net>
2003-09-21 07:07:19 +0000

    less confusing tokenize
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3698 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/TextUtils.h
+++ include/TextUtils.h
@@ -54,28 +54,21 @@ class string_util {
     }
     // get a vector of strings from a string, using all of chars of the delims
     // string as separators
-    static std::vector<std::string> string_util::tokenize(std::string in, std::string delims){
+    static std::vector<std::string> string_util::tokenize(std::string& in, std::string delims){
       std::vector<std::string> out;
-      std::ostringstream outputString;
-      std::istringstream inStream (in);
 
-      std::string candidate;
-      std::string testme;
-
-      int character;
-      while ( (character = inStream.get()) != EOF){
-	testme = (char) character;
-	if (testme.find_first_of(delims) != std::string::npos){
-	  candidate = outputString.str();
-	  outputString.str(""); // clear it
-	  if (candidate != "")  out.push_back(candidate);
-	} else {
-	  outputString << (char)character;
+      int startPos = in.find_first_not_of(delims);
+      while (startPos != std::string::npos){
+	int endPos = in.find_first_of(delims,startPos);
+	if (endPos == std::string::npos) {
+	  out.push_back(in.substr(startPos));
+	  break;
 	}
-      }
+	else
+	  out.push_back(in.substr(startPos,endPos-startPos));
 
-      candidate = outputString.str();
-      if (candidate != "")  out.push_back(candidate);
+	startPos = in.find_first_not_of(delims,endPos);
+      }
       return out;
     }
 };
