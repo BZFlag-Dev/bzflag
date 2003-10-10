a9d930ef341c79cf54419ee3798b7863e2c12622
Tim Riker <timriker@users.sourceforge.net>
2003-10-10 17:42:31 +0000

    first hack at bzfls.pl
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3968 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ misc/bzfls.pl
@@ -0,0 +1,160 @@
+#!/usr/bin/perl
+# CGI bzfls
+# Copyright (c) 1993 - 2003 Tim Riker
+#
+# This package is free software;  you can redistribute it and/or
+# modify it under the terms of the license found in the file
+# named COPYING that should have accompanied this file.
+#
+# THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+use strict;
+use CGI qw(:standard);
+use DBI;
+use Socket;
+
+### CGI parameters ###
+
+# Common to all
+my $action   = param("action");
+my $nameport = param("nameport");
+
+# For ADD
+my $build    = param("build");
+my $version  = param("version");
+my $gameinfo = param("gameinfo");
+my $title    = param("title");
+
+# For SETNUM
+my $players  = param("players");
+
+###############################################################################
+
+# Connect to the server database.
+my $dbpath = "servers.dat";
+my $dbexist = stat $dbpath;
+my $servdb = DBI->connect ("dbi:SQLite:$dbpath", "", "",
+                {RaiseError => 1, AutoCommit => 1});
+
+# If the servers table doesn't exist, create it.
+if ( not $dbexist ) {
+  $servdb->do (
+    "CREATE TABLE servers (
+      id INTEGER PRIMARY KEY,
+      nameport,
+      build,
+      version,
+      gameinfo,
+      ipaddr,
+      title,
+      rogues,
+      reds,
+      greens,
+      blues,
+      purples,
+      lastmod
+    )"
+  );
+}
+# If the table already exists, then remove all inactive servers from the table
+else {
+  my $timeout = 30;    # timeout in seconds
+  my $staletime = time - $timeout;
+  $servdb->do("DELETE FROM servers WHERE lastmod < $staletime");
+}
+
+print header;
+
+###############################################################################
+# Do stuff based on what the 'action' is...
+#
+#  -- LIST --
+# Same as LIST in the old bzfls
+if ( not defined $action or $action eq "LIST" ) {
+  my $all = $servdb->selectall_arrayref
+    ("SELECT nameport,version,gameinfo,ipaddr,title FROM servers");
+  foreach my $row (@$all) {
+    my $line = join ' ', @$row;
+    print "$line\n";
+  }
+}
+#  -- ADD --
+# Server either requests to be added to DB, or to issue a keep-alive so that it
+# does not get dropped due to a timeout...
+elsif ( $action eq "ADD" ) {
+  # Filter out badly formatted or buggy versions
+  exit unless (
+    $version =~ /^BZFS/         and
+    $version ne "BZFS107x"
+  );
+  
+  # Test to see whether nameport is valid by attempting to establish a
+  # connection to it
+  my ($servname, $servport) = split /:/, $nameport;
+  $servport = 5155 unless defined $servport;
+  my $servip32 = inet_aton ($servname);
+  exit unless defined $servip32;
+  my $servip = inet_ntoa ($servip32);
+  my $sin = sockaddr_in ($servport, inet_aton ($servname));
+  socket (SH, AF_INET, SOCK_STREAM, getprotobyname('tcp')) or die $!;
+  connect (SH, $sin) or die $!;
+  close SH;
+
+  my $curtime = time;
+
+  # Server does not already exist in DB so insert into DB
+  if ( not defined $servdb->selectrow_array
+          ("SELECT id FROM servers WHERE nameport = '$nameport'") ) {
+    $servdb->do(
+      "INSERT INTO servers VALUES (
+          NULL,
+          '$nameport',
+          '$build',
+          '$version',
+          '$gameinfo',
+          '$servip',
+          '$title',
+          0, 0, 0, 0, 0,
+          $curtime
+       )"
+    );
+  }
+  # Server exists already, so update the table entry
+  # ASSUMPTION: only the 'lastmod' column of table needs updating since all
+  # else should remain the same as before
+  else {
+    $servdb->do(
+      "UPDATE servers SET
+          lastmod = $curtime 
+          WHERE nameport = '$nameport'"
+    );
+  }
+}
+#  -- REMOVE --
+# Server requests to be removed from the DB.
+elsif ( $action eq "REMOVE" ) {
+  $servdb->do("DELETE FROM servers WHERE nameport = '$nameport'");
+}
+#  -- SETNUM --
+# Server is updating its player count.
+# $players should look like <rogue count>:<red count>:<greens>:<blues>:<purples>
+elsif ( $action eq "SETNUM" ) {
+  my ($rogues, $reds, $greens, $blues, $purples) = split /:/, $players;
+  my $curtime = time;
+  $servdb->do(
+    "UPDATE servers SET
+      rogues  = $rogues,
+      reds    = $reds,
+      greens  = $greens,
+      blues   = $blues,
+      purples = $purples,
+      lastmod = $curtime
+      WHERE nameport = '$nameport'"
+  );
+}
+# Unknown command ....
+else { print "Unknown command: '$action'\n"; }
+
+# ex: shiftwidth=2 tabstop=8
