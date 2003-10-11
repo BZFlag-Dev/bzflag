1bd31ceedf38f1a8be13a164ee33cc22ddff1796
Sean Morrison <brlcad@users.sourceforge.net>
2003-10-11 17:48:10 +0000

    C-style comments for C headers
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@3993 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/bzfio.h
+++ include/bzfio.h
@@ -32,11 +32,13 @@ using namespace std;
 #endif
 
 
-// Local variables: ***
-// mode:C++ ***
-// tab-width: 8 ***
-// c-basic-offset: 2 ***
-// indent-tabs-mode: t ***
-// End: ***
-// ex: shiftwidth=2 tabstop=8
+/*
+ * Local variables: ***
+ * mode:C++ ***
+ * tab-width: 8 ***
+ * c-basic-offset: 2 ***
+ * indent-tabs-mode: t ***
+ * End: ***
+ * ex: shiftwidth=2 tabstop=8
+ */
 
--- include/bzsignal.h
+++ include/bzsignal.h
@@ -29,10 +29,12 @@ SIG_PF bzSignal(int signo, SIG_PF func);
 }
 #endif
 
-// Local variables: ***
-// mode:C++ ***
-// tab-width: 8 ***
-// c-basic-offset: 2 ***
-// indent-tabs-mode: t ***
-// End: ***
-// ex: shiftwidth=2 tabstop=8
+/*
+ * Local variables: ***
+ * mode:C++ ***
+ * tab-width: 8 ***
+ * c-basic-offset: 2 ***
+ * indent-tabs-mode: t ***
+ * End: ***
+ * ex: shiftwidth=2 tabstop=8
+ */
