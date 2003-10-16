3f1afe27ebadd8ea086629cc2fb5a62002edd9dd
Sean Morrison <brlcad@users.sourceforge.net>
2003-10-16 03:35:56 +0000

    print out how many votes are needed to pass the poll when the poll is started
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4069 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/commands.cxx
+++ src/bzfs/commands.cxx
@@ -1073,6 +1073,10 @@ void handlePollCmd(int t, const char *message)
 	sendMessage(ServerPlayer, AllPlayers, reply, true);
       }
     }
+    
+    unsigned int necessaryToSucceed = (unsigned int)((clOptions->votePercentage / 100.0) * (double)available);
+    sprintf(reply, "%d player%s available, %d affirming vote%s required to pass the poll (%f %%)", available, available==1?"":"s", necessaryToSucceed, necessaryToSucceed==1?"":"s", clOptions->votePercentage);
+    sendMessage(ServerPlayer, AllPlayers, reply, true);
 
     // set the number of available voters
     arbiter->setAvailableVoters(available);
