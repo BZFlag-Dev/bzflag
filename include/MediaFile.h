87ed1e0d9bf5cfcdb7cab27a1e398f526c4bb0cb
Sean Morrison <brlcad@users.sourceforge.net>
2003-10-20 03:09:38 +0000

    ws
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4122 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/MediaFile.h
+++ include/MediaFile.h
@@ -25,51 +25,51 @@
 
 class MediaFile {
 public:
-	// close the media file.  this does *not* destroy the stream.
-	virtual ~MediaFile();
+  // close the media file.  this does *not* destroy the stream.
+  virtual ~MediaFile();
 
-	// read an image file.  use delete[] to release the returned
-	// image.  returns NULL on failure.  images are stored RGBA,
-	// left to right, bottom to top.
-	static unsigned char* readImage(const std::string& filename,
-							int* width, int* height);
+  // read an image file.  use delete[] to release the returned
+  // image.  returns NULL on failure.  images are stored RGBA,
+  // left to right, bottom to top.
+  static unsigned char* readImage(const std::string& filename,
+				  int* width, int* height);
 
-	// read a sound file.  use delete[] to release the returned
-	// audio.  returns NULL on failure.  sounds are stored
-	// left/right.
-//	static float*		readSound(const std::string& filename,
-//							int* numFrames, int* rate);
+  // read a sound file.  use delete[] to release the returned
+  // audio.  returns NULL on failure.  sounds are stored
+  // left/right.
+  //	static float*		readSound(const std::string& filename,
+  //							int* numFrames, int* rate);
 
 protected:
-	MediaFile(std::istream*);
+  MediaFile(std::istream*);
 
-	// get the stream
-	std::istream*			getStream() const { return stream; }
+  // get the stream
+  std::istream*			getStream() const { return stream; }
 
-	// return true if the stream is in a readable state
-	bool				isOkay() const;
+  // return true if the stream is in a readable state
+  bool				isOkay() const;
 
-	// utility method to read raw data
-	void				readRaw(void* buffer, uint32_t bytes);
+  // utility method to read raw data
+  void				readRaw(void* buffer, uint32_t bytes);
 
-	// utility method to skip data
-	void				skip(uint32_t bytes);
+  // utility method to skip data
+  void				skip(uint32_t bytes);
 
-	// utility methods to read 2 and 4 byte number into host byte order
-	uint16_t			read16LE();
-	uint16_t			read16BE();
-	uint32_t			read32LE();
-	uint32_t			read32BE();
+  // utility methods to read 2 and 4 byte number into host byte order
+  uint16_t			read16LE();
+  uint16_t			read16BE();
+  uint32_t			read32LE();
+  uint32_t			read32BE();
 
-	// utility methods to byte swap in place into host byte order.  all
-	// return the byte swapped data.
-	static uint16_t		swap16LE(uint16_t*);
-	static uint16_t		swap16BE(uint16_t*);
-	static uint32_t		swap32LE(uint32_t*);
-	static uint32_t		swap32BE(uint32_t*);
+  // utility methods to byte swap in place into host byte order.  all
+  // return the byte swapped data.
+  static uint16_t		swap16LE(uint16_t*);
+  static uint16_t		swap16BE(uint16_t*);
+  static uint32_t		swap32LE(uint32_t*);
+  static uint32_t		swap32BE(uint32_t*);
 
 private:
-	std::istream*			stream;
+  std::istream*			stream;
 };
 
 #endif
