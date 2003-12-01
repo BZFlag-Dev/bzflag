f39f0491092fb4e19eec577fda641d36f5650bd3
Sean Morrison <brlcad@users.sourceforge.net>
2003-12-01 03:11:40 +0000

    simplified interface and removed the creation and threading policies (so just new/delete and not thread safe for now).  Documented usage as well.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4527 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/Singleton.h
+++ include/Singleton.h
@@ -15,175 +15,85 @@
 
 #include <stdlib.h>
 
-
 /* Singleton template class
  *
- * This template class pattern provides the Singleton pattern, allowing control
- * over how an object is created/destroyed and what kind of threading model
- * to use.
- */
-
-
-/** CreationPolicy using new and delete
- */
-template < typename T >
-class CreateUsingNew {
-
- private:
-
-  CreateUsingNew(CreateUsingNew const&) { } // unused
-  CreateUsingNew& operator=(CreateUsingNew const&) { return *this; } // usused
-
- protected:
-
-  CreateUsingNew() { } // unused
-  ~CreateUsingNew() { } // unused
-
-  static T* CreateInstance() {
-    return new T();
-  }
-
-  static void DestroyInstance(T* instance) {
-    delete instance;
-    instance = NULL;
-  }
-};
-
-
-/** CreationPolicy using malloc and free
+ * This template class pattern provides a traditional Singleton pattern.
+ * Allows you to designate a single-instance global class by inheriting
+ * off of the template class.
+ *
+ * Example:
+ *
+ *   class Whatever : public Singleton<Whatever> ...
+ *
+ * The class will need to provide either a public or a protected friend 
+ * constructor:
+ *
+ *   friend class Singleton<Whatever>;
+ *
+ * The class can easily be extended to support different allocation
+ * mechanisms or multithreading access.  This implementation, however, 
+ * only uses new/delete and is not thread safe.
  */
 template < typename T >
-class CreateUsingMalloc {
+class Singleton {
 
- private:
-
-  CreateUsingMalloc(CreateUsingMalloc const&) { } // unused
-  //  CreatUsingMalloc& operator=(CreateUsingMalloc const&) { return *this; } //usused
-
- protected:
-
-  CreateUsingMalloc() { } // unused
-  ~CreateUsingMalloc() { } // unused
-
-  static T* CreateInstance() {
-    return malloc(sizeof(T));
-  }
-
-  static void DestroyInstance(T* instance) {
-    free(instance);
-    instance = NULL;
-  }
-};
-
-
-/* CreationPolicy using a pure static on the stack is left as an exercise
- * to the needy.  :P
- */
-
-
-/** ThreadingModel that assumes single threaded access.
- * basically does nothing, but it exposes the interface that needs to
- * be implmented for a multi-threading model.
- */
-class SingleThreaded {
-
- private:
-
-  friend class LockThread;
-  SingleThreaded(SingleThreaded const&) { }
-  SingleThreaded& operator=(SingleThreaded const&) { return *this; }
-
- protected:
-
-  SingleThreaded() { }
-  ~SingleThreaded() { }
-
- public:
-
-  class LockThread {
-
-   private:
-
-    static void Lock() { }
-    static void Unlock() { }
-
-   public:
-
-    LockThread() {
-      Lock();
-    }
-    ~LockThread() {
-      Unlock();
-    }
-  };
-};
-
-
-/* ThreadingModel for multithreaded access is left as an exercise to the
- * the needy. :P
- */
-
-
-/** This template class implements a traditional singleton interface.
- * You provide a class, a creation policy, and a threading model.  Creation
- * policies need to define CreateInstance() and DestroyInstance().  Threading
- * model needs to define a default LockThread class.
- */
-template < typename T, typename CreationPolicy = CreateUsingNew<T>, typename ThreadingModel = SingleThreaded >
-class Singleton : public CreationPolicy, public ThreadingModel {
-
- private:
+private:
 
   static T* _instance;
 
+protected:
+  
+  // protection from instantiating a non-singleton Singleton
+  Singleton() { }
+  Singleton(T* pInstance) { _instance = pInstance; } 
   Singleton(const Singleton &) { } // do not use
   Singleton& operator=(const Singleton&) { return *this; } // do not use
+  ~Singleton() { _instance = 0; } // do not delete
 
   static void destroy() {
     if ( _instance != 0 ) {
-      LockThread lock;
-      if ( _instance != 0 ) {
-	CreationPolicy::DestroyInstance(_instance);
-	_instance = 0;
-      }
+      delete(_instance);
+      _instance = 0;
     }
   }
 
- protected:
-
-  // protection from instantiating a non-singleton Singleton
-  Singleton() { }
-  ~Singleton() { _instance = 0; } // do not delete
-
- public:
+public:
 
   /** returns a singleton
    */
-  static T& instance() {
+  inline static T& instance() {
     if ( _instance == 0 ) {
-      LockThread lock;
-      if ( _instance == 0 ) {
-	_instance = CreationPolicy::CreateInstance();
-	// destroy the singleton when the application terminates
-	std::atexit(Singleton::destroy);
-      }
+      _instance = new T;
+      // destroy the singleton when the application terminates
+      std::atexit(Singleton::destroy);
     }
-    return *(Singleton::_instance);
+    return *Singleton::_instance;
+  }
+
+  /** returns a singleton pointer
+   */
+  inline static T* pInstance() {
+    if (_instance == 0) {
+      _instance = new T;
+      std::atexit(Singleton::destroy);
+    }
+    return Singleton::_instance;
   }
 
   /** returns a const singleton reference
    */
-  inline static const T& constInstance() { return instance(); }
+  inline static const T& constInstance() { return *instance(); }
 };
 
 // statically initialize the instance to nothing
-template < typename T, typename CreationPolicy, typename ThreadingModel >
-T* Singleton<T, CreationPolicy, ThreadingModel>::_instance = 0;
+template < typename T >
+T* Singleton<T>::_instance = 0;
 
 #endif /* __SINGLETON_H__ */
 
-// Local variables: ***
-// mode:C++ ***
+
+// Local Variables: ***
+// mode: C++ ***
 // tab-width: 8 ***
 // c-basic-offset: 2 ***
 // indent-tabs-mode: t ***
