8a863eef817e432f7effacece45e41f60dea91f9
Tim Riker <timriker@users.sourceforge.net>
2003-12-09 06:24:27 +0000

    be normal
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4658 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/KeyManager.h
+++ include/KeyManager.h
@@ -95,7 +95,7 @@ private:
 
 // this is to be implemented within the requisite source file for the application using it.
 // in BZFlag's case, it happens to be in bzflag.cxx
-extern unsigned const int	numDefaultBindings;
+extern const unsigned int	numDefaultBindings;
 extern const char*		defaultBindings[];
 
 #endif // BZF_KEYMANAGER_H
