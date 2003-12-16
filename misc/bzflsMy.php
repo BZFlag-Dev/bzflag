97ab5f29807a5040a0cb20ba05606ae7f97f3dee
Sean Morrison <brlcad@users.sourceforge.net>
2003-12-16 20:32:06 +0000

    added a debugging/logging facility, made use of mysql_pconnect for persistent connections \(encourage reuse of the db connection\), made  the queries use the connected link instead of implicit, and force a transaction commit before ending in case server is configured non-default.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4799 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/bzflsMy.php
+++ misc/bzflsMy.php
@@ -27,32 +27,63 @@ $version  = $_GET['version'];
 $gameinfo = $_GET['gameinfo'];
 $title    = $_GET['title'];
 
-# Connect to the server database.
-$link = mysql_connect($dbhost, $dbuname, $dbpass)
-     or die("Could not connect: " . mysql_error());
-if (!mysql_select_db($dbname)) {
-  mysql_create_db($dbname) or die("Could not create db: " . mysql_error());
+# where to send debug printing
+$enableDebug	= 0;
+$debugFile 		= "/tmp/bzflsMy.php.txt";
+
+# log function
+if ($enableDebug) {
+	function debug ($filename, $message) {
+		$fp = fopen($filename, "a");
+		if ($fp) {
+			# output the message with a BSD-style timestamp
+			fwrite($fp, date("D M j G:i:s T Y") . " " . $_SERVER['REMOTE_ADDR'] . " " . $message . "\n");
+			fclose($fp);
+		} else {
+			print("Unable to write to to log file [$filename]");
+		}
+	}
+} else {
+	function debug ($filename, $message) {
+	}
 }
 
+debug($debugFile, "Connecting to the database");
+
+# Connect to the server database persistently.
+$link = mysql_pconnect($dbhost, $dbuname, $dbpass)
+     or die("Could not connect: " . mysql_error());
+if (!mysql_select_db($dbname)) {
+	
+	debug($debugFile, "Database did not exist, creating a new one");
+
+  mysql_create_db($dbname) or die("Could not create db: " . mysql_error());
+}
+$result = mysql_query("SELECT * FROM servers", $link);
+
 # If the servers table does not exist, create it.
-$result = mysql_query("SELECT 1 FROM servers");
 if (!$result) {
+
+	debug($debugFile, "Database table did not exist, creating a new one");
+
   mysql_query("CREATE TABLE servers " .
-	      "(nameport varchar(60) NOT NULL, " .
-	      " build varchar(20), " .
-	      " version varchar(9) NOT NULL, " .
-	      " gameinfo varchar(73) NOT NULL, " .
-	      " ipaddr varchar(17) NOT NULL, " .
-	      " title varchar(80), " .
-	      " lastmod INT NOT NULL DEFAULT '0', ".
-	      " PRIMARY KEY (nameport))")
+              "(nameport varchar(60) NOT NULL, " .
+              " build varchar(20), " .
+              " version varchar(9) NOT NULL, " .
+              " gameinfo varchar(73) NOT NULL, " .
+              " ipaddr varchar(17) NOT NULL, " .
+              " title varchar(80), " .
+              " lastmod INT NOT NULL DEFAULT '0', ".
+	      " PRIMARY KEY (nameport))", $link)
     or die ("Could not create table: " . mysql_error());
 }
+
+debug($debugFile, "Deleteing inactive servers from list");
 
 # remove all inactive servers from the table
 $timeout = 1800;    # timeout in seconds
 $staletime = time() - $timeout;
-mysql_query("DELETE FROM servers WHERE lastmod < $staletime")
+mysql_query("DELETE FROM servers WHERE lastmod < $staletime", $link)
      or die("Could not drop old items" . mysql_error());
 
 header("Content-type: text/plain");
@@ -61,13 +92,16 @@ header("Content-type: text/plain");
 #  -- LIST --
 # Same as LIST in the old bzfls
 if (!array_key_exists("action", $_GET) || $action == "LIST" ) {
+
+	debug($debugFile, "Fetching LIST");
+
   if ($version)
     $result = mysql_query("SELECT nameport,version,gameinfo,ipaddr,title "
-			  . " FROM servers WHERE version = '$version'")
+			  . " FROM servers WHERE version = '$version'", $link)
       or die ("Invalid query: ". mysql_error());
   else
     $result = mysql_query("SELECT nameport,version,gameinfo,ipaddr,title "
-			  . " FROM servers")
+			  . " FROM servers", $link)
       or die ("Invalid query: ". mysql_error());
   while (TRUE) {
     $row = mysql_fetch_row($result);
@@ -77,6 +111,9 @@ if (!array_key_exists("action", $_GET) || $action == "LIST" ) {
     print "$line\n";
   }
 } elseif ($action == "ADD") {
+
+	debug($debugFile, "Attempting to ADD $nameport $version $gameinfo $title");
+
 #  -- ADD --
 # Server either requests to be added to DB, or to issue a keep-alive so that it
 # does not get dropped due to a timeout...
@@ -94,6 +131,9 @@ if (!array_key_exists("action", $_GET) || $action == "LIST" ) {
   else
     $servport = 5154;
   $servip = gethostbyname($servname);
+
+	debug($debugFile, "Attempting to establish a connection to $servname $servport");
+
   $fp = fsockopen ($servname, $servport, $errno, $errstring, 30);
   if (!$fp) {
     print "failed to connect\n";
@@ -104,18 +144,24 @@ if (!array_key_exists("action", $_GET) || $action == "LIST" ) {
   $curtime = time();
 
   $result = mysql_query("SELECT * FROM servers " .
-			"WHERE nameport = '$nameport'")
+			"WHERE nameport = '$nameport'", $link)
     or die ("Invalid query: ". mysql_error());
   $count = mysql_num_rows($result);
   if (!$count) {
+
+		debug($debugFile, "Server does not already exist in database -- adding");
+
 # Server does not already exist in DB so insert into DB
     $result = mysql_query("INSERT INTO servers "
 			  . "(nameport, build, version, gameinfo, ipaddr,"
                           . " title, lastmod) VALUES "
 			  . "('$nameport', '$build', '$version',"
-			  . " '$gameinfo', '$servip', '$title', $curtime)")
+			  . " '$gameinfo', '$servip', '$title', $curtime)", $link)
       or die ("Invalid query: ". mysql_error());
   } else {
+
+		debug($debugFile, "Server already exists in database -- updating");
+
 # Server exists already, so update the table entry
 # ASSUMPTION: only the 'lastmod' column of table needs updating since all
 # else should remain the same as before
@@ -125,20 +171,42 @@ if (!array_key_exists("action", $_GET) || $action == "LIST" ) {
 			  "gameinfo = '$gameinfo', " .
 			  "title = '$title', " .
 			  "lastmod = $curtime " .
-			  "WHERE nameport = '$nameport'")
+			  "WHERE nameport = '$nameport'", $link)
       or die ("Invalid query: ". mysql_error());
   }
+
+	debug($debugFile, "ADD complete");
+
   print "ADD complete\n";
 } elseif ($action == "REMOVE") {
+
+	debug($debugFile, "REMOVE request from $nameport");
+
 #  -- REMOVE --
 # Server requests to be removed from the DB.
-  $result = mysql_query("DELETE FROM servers WHERE nameport = '$nameport'")
+  $result = mysql_query("DELETE FROM servers WHERE nameport = '$nameport'", $link)
     or die ("Invalid query: ". mysql_error());
+
 } else {
   print "Unknown command: '$action'\n";
 }
+
+
 # make sure the connection to mysql is severed
-mysql_close($link);
+if ($link) {
+
+	# for a transaction commit just in case
+	debug($debugFile, "Commiting any pending transactions");
+	mysql_query("COMMIT", $link);
+
+	# debug($debugFile, "Closing link to database");
+
+	# say bye bye (shouldn't need to ever really, especially for persistent..)
+	#	mysql_close($link);
+}
+
+debug($debugFile, "End session");
+
 
 # Local Variables: ***
 # mode:php ***
