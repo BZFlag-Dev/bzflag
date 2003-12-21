3a833f604daa7fb54af62659b840772bf9620d89
Sean Morrison <brlcad@users.sourceforge.net>
2003-12-21 18:11:30 +0000

    bump up the current cvs version post-release to keep the numbers straight
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@4867 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/version.h
+++ include/version.h
@@ -40,11 +40,11 @@
 #endif
 
 #ifndef BZ_REV
-#define BZ_REV                  2
+#define BZ_REV                  3
 #endif
 
 #ifndef BZ_BUILD_TYPE
-#define BZ_BUILD_TYPE		"RELEASE"
+#define BZ_BUILD_TYPE		"MAINTENANCE"
 #endif
 
 // Build Date will be defined at link time, can be different for each exe
