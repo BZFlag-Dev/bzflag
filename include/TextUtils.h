27dab1231ad7a74cf7049a827c996cf9a5cf38d3
Dave Brosius <dbrosius@users.sourceforge.net>
2004-01-20 02:37:25 +0000

    ifdef for win
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@5566 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/TextUtils.h
+++ include/TextUtils.h
@@ -42,7 +42,11 @@ class string_util {
       // not all platforms support vsnprintf so we'll use vsprintf and a
       // big temporary buffer and hope for the best.
       char buffer[8192];
+#ifdef _WIN32
+      _vsnprintf(buffer,8192, fmt, args);
+#else
       vsnprintf(buffer, 8192, fmt, args);
+#endif
       return std::string(buffer);
     }
     static std::string format(const char* fmt, ...) {
