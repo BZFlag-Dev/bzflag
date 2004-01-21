28d57b4bb4c6955c757fd1830762ce2bf9c88bf8
Dave Brosius <dbrosius@users.sourceforge.net>
2004-01-21 05:00:13 +0000

    fix ATLTRACE2 in release mode
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@5621 08b3d480-bf2c-0410-a26f-811ee3361c24

--- win32/VC6/config.h
+++ win32/VC6/config.h
@@ -58,7 +58,7 @@
 #include "ATLbase.h"
 
 #if defined(_MSC_VER) && (_MSC_VER == 1200)
-  inline void W32_DEBUG_TRACE (const char* buffer ) {ATLTRACE2(atlTraceUser, ATL_TRACE_LEVEL, buffer);}
+  inline void W32_DEBUG_TRACE (const char* buffer ) {ATLTRACE2(atlTraceUser, 0, buffer);}
 #elif defined(_MSC_VER) && (_MSC_VER == 1100)
   inline void W32_DEBUG_TRACE (const char* buffer ) {ATLTRACE(buffer);}
 #endif
