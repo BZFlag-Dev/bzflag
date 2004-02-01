2a737c6c5cb96d15bb746c2df116304684a752d5
Jeff Myers <jeffm2501@users.sourceforge.net>
2004-02-01 05:23:37 +0000

    put all SDL headers and tests in one area
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@5810 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/bzfSDL.h
@@ -0,0 +1,37 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __BZFSDL_H__
+#define __BZFSDL_H__
+
+/** this file contains headers necessary for SDL */
+
+#ifdef HAVE_SDL
+
+#ifdef WIN32
+#include <SDL.h>
+#include <SDL_thread.h>
+#else // cus linux is wierd
+#include "SDL.h"
+#include "SDL_thread.h"
+#endif //_WIN32
+#endif //HAVE_SDL
+
+#endif /* __BZFSDL_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
