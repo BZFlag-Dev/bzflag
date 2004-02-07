0aa563ea535a8b7275266b4b4e25a4de6d3bef2e
Sean Morrison <brlcad@users.sourceforge.net>
2004-02-07 20:20:36 +0000

    separate out region priority queue class into their own files
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@5922 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/RegionPriorityQueue.cxx
@@ -0,0 +1,79 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* interface header */
+#include "RegionPriorityQueue.h"
+
+
+// FIXME -- should use a heap
+
+RegionPriorityQueue::Node::Node(BzfRegion* _region, float _priority) :
+  next(0), region(_region), priority(_priority)
+{
+}
+
+RegionPriorityQueue::RegionPriorityQueue() : head(0)
+{
+}
+
+RegionPriorityQueue::~RegionPriorityQueue()
+{
+  removeAll();
+}
+
+void RegionPriorityQueue::insert(BzfRegion* region, float priority)
+{
+  Node* node = new Node(region, priority);
+  if (!head || priority < head->priority) {
+    node->next = head;
+    head = node;
+  }
+  else {
+    Node* scan = head;
+    while (scan->next && priority >= scan->next->priority)
+      scan = scan->next;
+    node->next = scan->next;
+    scan->next = node;
+  }
+}
+
+BzfRegion* RegionPriorityQueue::remove()
+{
+  Node* tmp = head;
+  head = head->next;
+  BzfRegion* region = tmp->region;
+  delete tmp;
+  return region;
+}
+
+void RegionPriorityQueue::removeAll()
+{
+  while (head) {
+    Node* next = head->next;
+    delete head;
+    head = next;
+  }
+  head = 0;
+}
+
+bool RegionPriorityQueue::isEmpty() const
+{
+  return (head == 0);
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/bzflag/RegionPriorityQueue.h
@@ -0,0 +1,53 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef	__REGIONPRIORITYQUEUE_H__
+#define	__REGIONPRIORITYQUEUE_H__
+
+/* local interface headers */
+#include "Region.h"
+
+
+class RegionPriorityQueue {
+ public:
+  RegionPriorityQueue();
+  ~RegionPriorityQueue();
+
+  void insert(BzfRegion* region, float priority);
+  BzfRegion* remove();
+  void removeAll();
+  bool isEmpty() const;
+
+ private:
+  struct Node {
+    public:
+     Node(BzfRegion* region, float priority);
+    public:
+     Node* next;
+     BzfRegion*	region;
+     float priority;
+  };
+
+ private:
+  Node* head;
+};
+
+
+#endif /* __REGIONPRIORITYQUEUE_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
