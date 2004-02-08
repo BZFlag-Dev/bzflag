c71b85c8605d31b059209dee328d727558483e09
Sean Morrison <brlcad@users.sourceforge.net>
2004-02-08 08:54:59 +0000

    separate out BaseLocalPlayer class into its own files
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@5938 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/BaseLocalPlayer.h
@@ -0,0 +1,63 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __BASELOCALPLAYER_H__
+#define __BASELOCALPLAYER_H__
+
+/* interface header */
+#include "Player.h"
+
+/* common interface headers */
+#include "Ray.h"
+#include "TimeKeeper.h"
+
+/* local interface headers */
+#include "ShotPath.h"
+
+
+class BaseLocalPlayer : public Player {
+ public:
+  BaseLocalPlayer(const PlayerId&, const char* name, const char* email);
+  ~BaseLocalPlayer();
+
+  void update();
+  Ray getLastMotion() const;
+  const float (*getLastMotionBBox() const)[3];
+
+  virtual void explodeTank() = 0;
+  virtual bool checkHit(const Player* source,
+			const ShotPath*& hit, float& minTime) const = 0;
+ protected:
+  int getSalt();
+  virtual void doUpdate(float dt) = 0;
+  virtual void doUpdateMotion(float dt) = 0;
+
+ protected:
+  TimeKeeper lastTime;
+  float lastPosition[3];
+  // bbox of last motion
+  float bbox[2][3];
+
+ private:
+  int salt;
+};
+
+
+#endif /* __BASELOCALPLAYER_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
