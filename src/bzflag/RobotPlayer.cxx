f7cec239af850242cf2ef1180f44705523c0bda7
Frank Thilo <chestal@users.sourceforge.net>
2004-02-12 16:14:03 +0000

    delete misleading comment
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_10branch/bzflag@5998 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/RobotPlayer.cxx
+++ src/bzflag/RobotPlayer.cxx
@@ -20,7 +20,7 @@
 #include <math.h>
 #include "common.h"
 #include "RobotPlayer.h"
-#include "World.h"
+//#include "World.h"
 #include "ServerLink.h"
 #include "BZDBCache.h"
 
@@ -46,7 +46,7 @@ void			RobotPlayer::doUpdate(float dt)
 
   float tankRadius = BZDBCache::tankRadius;
   float shotRange  = BZDB.eval(StateDatabase::BZDB_SHOTRANGE);
-  // separate shot by at least 0.4 sec
+
   timerForShot  -= dt;
   if (timerForShot < 0.0f)
     timerForShot = 0.0f;
