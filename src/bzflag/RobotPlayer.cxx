b79120ea6732e2c879ad596a668e79b251dfb45a
Frank Thilo <chestal@users.sourceforge.net>
2004-02-13 10:24:13 +0000

    ws
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6007 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/RobotPlayer.cxx
+++ src/bzflag/RobotPlayer.cxx
@@ -162,8 +162,8 @@ void			RobotPlayer::doUpdate(float dt)
     const float missby = fabs(azimuthDiff) *
       (targetdistance - BZDB.eval(StateDatabase::BZDB_TANKLENGTH));
     // only shoot if we miss by less than half a tanklength and no building inbetween
-    if (missby < 0.5*BZDB.eval(StateDatabase::BZDB_TANKLENGTH) &&
-	p1[2] < shotRadius) {
+    if (missby < 0.5f * BZDB.eval(StateDatabase::BZDB_TANKLENGTH) &&
+        p1[2] < shotRadius) {
       float pos[3] = {getPosition()[0], getPosition()[1],
 		      getPosition()[2] +  BZDB.eval(StateDatabase::BZDB_MUZZLEHEIGHT)};
       float dir[3] = {cosf(azimuth), sinf(azimuth), 0.0f};
