1ca034f550677a90ea243f0191ad3566d37ababe
Sean Morrison <brlcad@users.sourceforge.net>
2004-02-15 09:23:56 +0000

    add new FlashClock files pulled from HUDRenderer
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6021 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/FlashClock.cxx
@@ -0,0 +1,68 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* interface header */
+#include "FlashClock.h"
+
+/* system implementation headers */
+#include <math.h>
+
+
+FlashClock::FlashClock() : duration(0.0f), onDuration(0.0f), flashDuration(0.0f)
+{
+  // do nothing
+}
+
+FlashClock::~FlashClock()
+{
+  // do nothing
+}
+
+void FlashClock::setClock(float _duration)
+{
+  setClock(_duration, 0.0f, 0.0f);
+}
+
+void FlashClock::setClock(float _duration, float onTime, float offTime)
+{
+  startTime = TimeKeeper::getTick();
+  duration = _duration;
+  if (onTime <= 0.0f || offTime <= 0.0f) {
+    onDuration = 0.0f;
+    flashDuration = 0.0f;
+  }
+  else {
+    onDuration = onTime;
+    flashDuration = onTime + offTime;
+  }
+}
+
+bool FlashClock::isOn()
+{
+  if (duration == 0.0f) return false;
+  const float dt = TimeKeeper::getTick() - startTime;
+  if (duration > 0.0f && dt >= duration) {
+    duration = 0.0f;
+    return false;
+  }
+  if (flashDuration == 0.0f) return true;
+  return (fmodf(dt, flashDuration) < onDuration);
+}
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/bzflag/FlashClock.h
@@ -0,0 +1,50 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef	__FLASHCLOCK_H__
+#define	__FLASHCLOCK_H__
+
+/* common interface headers */
+#include "TimeKeeper.h"
+
+
+/**
+ * FlashClock
+ *	keeps track of time for something that flashes
+ */
+class FlashClock {
+  public:
+			FlashClock();
+			~FlashClock();
+
+    void		setClock(float time);
+    void		setClock(float time, float onTime, float offTime);
+
+    bool		isOn();
+
+  private:
+    TimeKeeper		startTime;
+    float		duration;
+    float		onDuration;
+    float		flashDuration;
+};
+
+
+#endif /* _FLASHCLOCK_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
