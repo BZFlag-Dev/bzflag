e427a16f1533c0caa42ded238b8f4f9b9c1fd8db
Daniel Remenak <dtremenak@users.sourceforge.net>
2004-02-21 22:41:01 +0000

    add some debugage
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6107 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/3D/TextureManager.cxx
+++ src/3D/TextureManager.cxx
@@ -35,7 +35,7 @@ ProcTextureInit procLoader[1];
 
 TextureManager::TextureManager()
 {
-	// fill out the standard proc textures
+  // fill out the standard proc textures
   procLoader[0].name = "noise";
   procLoader[0].filter = OpenGLTexture::Nearest;
   procLoader[0].proc = noiseProc;
@@ -81,8 +81,10 @@ int TextureManager::getTextureID( const char* name, bool reportFail )
     file.name = texName;
     ImageInfo info;
     OpenGLTexture *image = loadTexture(file,reportFail);
-    if (!image)
+    if (!image) {
+      DEBUG1("Image not found or unloadable: %s\n", name);
       return -1;
+    }
     return addTexture(name,image);
   }  
   return -1;
@@ -91,14 +93,15 @@ int TextureManager::getTextureID( const char* name, bool reportFail )
 bool TextureManager::bind ( int id )
 {
   TextureIDMap::iterator it = textureIDs.find(id);
-  if (it == textureIDs.end())
+  if (it == textureIDs.end()) {
+    DEBUG1("Unable to bind texture (by id): %d\n", id);
     return false;
+  }
 
- if (id != lastBoundID)
- {
+  if (id != lastBoundID) {
     it->second->texture->execute();
     lastBoundID = id;
- }
+  }
   return true;
 }
 
@@ -107,12 +110,13 @@ bool TextureManager::bind ( const char* name )
   std::string nameStr = name;
 
   TextureNameMap::iterator it = textureNames.find(nameStr);
-  if (it == textureNames.end())
+  if (it == textureNames.end()) {
+    DEBUG1("Unable to bind texture (by name): %s\n", name);
     return false;
+  }
 
   int id = it->second.id;
-  if (id != lastBoundID)
-  {
+  if (id != lastBoundID) {
     it->second.texture->execute();
     lastBoundID = id;
   }
@@ -158,11 +162,11 @@ int TextureManager::addTexture( const char* name, OpenGLTexture *texture )
   if (!name || !texture)
     return -1;
 
-   // if the texture allready exists kill it
-   // this is why IDs are way better then objects for this stuff
+   // if the texture already exists kill it
+   // this is why IDs are way better than objects for this stuff
    TextureNameMap::iterator it = textureNames.find(name);
-   if (it != textureNames.end())
-   {
+   if (it != textureNames.end()) {
+     DEBUG4("Texture %s already exists, overwriting\n", name);
      textureIDs.erase(textureIDs.find(it->second.id));
      delete it->second.texture;
    }
@@ -196,7 +200,7 @@ OpenGLTexture* TextureManager::loadTexture(FileTextureInit &init, bool reportFai
   }
   else
     nameToTry = init.name;
-  unsigned char* image =NULL;
+  unsigned char* image = NULL;
   if (nameToTry.size() && nameToTry.c_str())
     image = MediaFile::readImage( nameToTry.c_str(), &width, &height);
   if (!image)
@@ -237,7 +241,7 @@ int noiseProc(ProcTextureInit &init)
     noise[i+3] = n;
   }
   int texture = init.manager->newTexture(init.name.c_str(),noizeSize,noizeSize,noise,init.filter);
-  delete noise;
+  delete[] noise;
   return texture;
 }
 
