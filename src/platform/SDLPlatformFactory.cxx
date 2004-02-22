a1d589b19cd2a136e89d246f5cd0f28f97ca4309
Alfredo Tupone <atupone@users.sourceforge.net>
2004-02-22 00:05:50 +0000

    Creating an ad-hoc SDLPlatformFactory for all the fully SDL compliant plat
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6115 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/platform/SDLPlatformFactory.cxx
@@ -0,0 +1,67 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "SDLPlatformFactory.h"
+#include "SDLMedia.h"
+#include "SDLDisplay.h"
+
+PlatformFactory* PlatformFactory::getInstance()
+{
+  if (!instance)
+    instance = new SdlPlatformFactory;
+  return instance;
+}
+
+SdlPlatformFactory::SdlPlatformFactory()
+{
+  // do nothing
+}
+
+SdlPlatformFactory::~SdlPlatformFactory()
+{
+  // do nothing
+}
+
+BzfDisplay* SdlPlatformFactory::createDisplay(const char*, const char*)
+{
+  SDLDisplay* display = new SDLDisplay();
+  if (!display || !display->isValid()) {
+    delete display;
+    return NULL;
+  }
+  return display;
+}
+
+BzfVisual* SdlPlatformFactory::createVisual(const BzfDisplay* display)
+{
+  return new SDLVisual((const SDLDisplay*)display);
+}
+
+BzfWindow* SdlPlatformFactory::createWindow(const BzfDisplay* display,
+					    BzfVisual* visual)
+{
+  return new SDLWindow((const SDLDisplay*)display, (SDLVisual*)visual);
+}
+
+BzfMedia* SdlPlatformFactory::createMedia()
+{
+  return new SDLMedia;
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
---
+++ src/platform/SDLPlatformFactory.h
@@ -0,0 +1,45 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* SDLPlatformFactory:
+ *	Factory for SDL platform stuff.
+ */
+
+#ifndef BZF_SDL_PLATFORM_FACTORY_H
+#define	BZF_SDL_PLATFORM_FACTORY_H
+
+#include "PlatformFactory.h"
+
+class SdlPlatformFactory : public PlatformFactory {
+ public:
+  SdlPlatformFactory();
+  ~SdlPlatformFactory();
+  
+  BzfDisplay*		createDisplay(const char* name, const char*);
+  BzfVisual*		createVisual(const BzfDisplay*);
+  BzfWindow*		createWindow(const BzfDisplay*, BzfVisual*);
+  BzfMedia*	        createMedia();
+ private:
+  SdlPlatformFactory(const SdlPlatformFactory&);
+  SdlPlatformFactory& operator=(const SdlPlatformFactory&);
+};
+
+#endif // BZF_SDL_PLATFORM_FACTORY_H
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
