5ec745c49adc65583be691e10d10a8be75025ea1
Daniel Remenak <dtremenak@users.sourceforge.net>
2004-03-07 21:45:59 +0000

    Add the .cxx to CVS, otherwise having it in the makefile doesn't do a whole lot of good
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6264 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/platform/BzfJoystick.cxx
@@ -0,0 +1,61 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "ErrorHandler.h"
+#include "BzfJoystick.h"
+
+BzfJoystick::BzfJoystick()
+{
+}
+
+BzfJoystick::~BzfJoystick()
+{
+}
+
+void			BzfJoystick::initJoystick(const char* joystickName)
+{
+  if (strcmp(joystickName, "off") && strcmp(joystickName, "")) {
+    std::vector<std::string> args;
+    args.push_back(joystickName);
+    printError("Joystick '{1}' not supported...", &args);
+  }
+}
+
+unsigned long		BzfJoystick::getJoyButtons() const
+{
+  return 0;
+}
+
+bool			BzfJoystick::joystick() const
+{
+  return false;
+}
+
+void			BzfJoystick::getJoy(int& x, int& y) const
+{
+  x = y = 0;
+}
+
+void                    BzfJoystick::getJoyDevices(std::vector<std::string>
+						 &list) const 
+{
+  list.clear();
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
