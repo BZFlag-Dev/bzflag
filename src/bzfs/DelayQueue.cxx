9d7a4aedd1459810072d96d1d4c40c91b1145753
trepan <trepan@users.sourceforge.net>
2004-03-22 05:40:02 +0000

    adding DelayQueue files for "Lag Flag"
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6475 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzfs/DelayQueue.cxx
@@ -0,0 +1,110 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED `AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "common.h"
+#include "global.h"
+#include "DelayQueue.h"
+
+
+DelayQueue::DelayQueue ()
+{
+  init();
+}
+
+
+DelayQueue::~DelayQueue ()
+{
+  dequeuePackets();
+}
+
+
+void DelayQueue::init ()
+{
+  input = NULL;
+  output = NULL;
+}
+
+
+void DelayQueue::dequeuePackets ()
+{
+  DelayNode *dn = output;
+  DelayNode *tmp;
+  
+  while (dn != NULL) {
+    free (dn->data);
+    tmp = dn;
+    dn = dn->next;
+    delete tmp;
+  }
+}
+
+
+bool DelayQueue::addPacket (int length, const void *data, float delay)
+{
+  DelayNode *dn = new DelayNode;
+
+  dn->length = length;
+  dn->data = malloc (length);
+  memcpy (dn->data, data, length);
+  
+  dn->sendtime = TimeKeeper::getCurrent();
+  dn->sendtime += delay;
+  
+  dn->next = NULL;
+  
+  if (output == NULL) {
+    output = dn;
+  }
+  
+  if (input != NULL) {
+    input->next = dn;
+  }
+
+  input = dn;
+  
+  return true;
+};
+
+
+bool DelayQueue::getPacket (int *length, void **data)
+{
+  if (nextPacketTime() > 0.0f) {
+    return false;
+  }
+
+  // output is not NULL (nextPacketTime() checks this)
+  
+  DelayNode *dn = output;
+
+  *length = dn->length;
+  *data = dn->data;
+
+  if (dn->next == NULL) {
+    input = NULL;
+  }
+  output = dn->next;
+
+  delete dn;
+    
+  return true;
+};
+
+
+float DelayQueue::nextPacketTime ()
+{
+  if (output == NULL) {
+    return +Infinity;
+  }
+  else {
+    return TimeKeeper::getCurrent() - output->sendtime;
+  }
+}
