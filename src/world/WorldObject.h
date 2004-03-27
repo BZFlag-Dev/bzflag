d5fbecd2aed67de26d7cf9b5fee5a01db3d89087
Daniel Remenak <dtremenak@users.sourceforge.net>
2004-03-27 04:59:33 +0000

    let's see how I did - begin the libworld
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@6530 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/world/WorldObject.h
@@ -0,0 +1,27 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#ifndef __WORLDOBJECT_H__
+#define __WORLDOBJECT_H__
+
+class WorldObject {
+public:
+  WorldObject() { };
+  virtual ~WorldObject() { };
+  
+  virtual std::string myToken() const;
+  virtual bool deserialize(std::istream&);
+  virtual bool serialize() const;
+}
+
+#endif //__WORLDOBJECT_H__
+
+
