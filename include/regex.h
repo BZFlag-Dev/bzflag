b59cc6802c63388621aeb33eb1df8b1687fde116
Sean Morrison <brlcad@users.sourceforge.net>
2004-05-30 06:46:27 +0000

    just forget about non-prototypes -- we don't have a non-prototyping compiler to even support.  otherwise irix needs to get added to the list as well also or configure.ac fixed to profide USE_PROTOTYPES.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v1_10branch/bzflag@6962 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/regex.h
+++ include/regex.h
@@ -44,12 +44,7 @@
 
 /* From #include <sys/cdefs.h> */
 #if !defined(__P)
-#  if defined(USE_PROTOTYPES) || defined(_WIN32)
 #	define __P(args)       args
-#  else
-#	define __P(args)       ()
-#	define const		/**/
-#  endif
 #endif
 #ifdef __cplusplus
 #  define __BEGIN_DECLS   extern "C" {
