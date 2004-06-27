51504774c3e2ad581072d156a60ec28e6aa8a25c
Sean Morrison <brlcad@users.sourceforge.net>
2004-06-27 19:33:09 +0000

    Progress?  since trepan's system sqrt was actually faster, i have it try a little harder to be more intelligent.  Added more sqrt and inverse sqrt routines and then an optimize() routine to automatically detect which is the fastest on demand.  Default is system sqrtf().
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7181 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -19,8 +19,15 @@
 #ifndef __MATHUTILS_H__
 #define __MATHUTILS_H__
 
+/* common header comes first */
+#include "common.h"
+
 /* system headers */
 #include <math.h>
+#include <iostream>
+
+/* common headers */
+#include "TimeKeeper.h"
 
 
 #define FP_BITS(fp) (*(unsigned long *)&(fp))
@@ -86,12 +93,28 @@ inline unsigned long FP_NORM_TO_BYTE3(float p)
 }
 
 
+/** The math_util class contains general routines for common
+ * mathematical functions that should be used with care.  The
+ * precision and/or accuracy of the routine below are not guaranteed,
+ * though the implementation should useful for fast estimates where
+ * accuracy is not a stringent concern.
+ *
+ * For the square root and inverse square root routines below, they
+ * are 
+
+ */
 class math_util {
 
 private:
-  // table of precomputed square root values
-  static unsigned int fast_sqrt_table[0x10000];  
-  static bool built;
+  /** table of precomputed square root values */
+  static unsigned int _fast_sqrt_table[0x10000];  
+  /** keep track of whether the table was precomputed yet */
+  static bool _built_fast_sqrt_table;
+
+  /** table of random floats for performance testing */
+  static float _random_floats[0x10000];
+  /** keep tracke of whether the random floats are set yet */
+  static bool _built_random_floats;
 
   typedef union FastSqrtUnion
   {
@@ -114,30 +137,76 @@ protected:
 	
       // Take the square root then strip the first 7 bits of
       //  the mantissa into the table
-      fast_sqrt_table[i + 0x8000] = (s.i & 0x7FFFFF);
+      _fast_sqrt_table[i + 0x8000] = (s.i & 0x7FFFFF);
 	
       // Repeat the process, this time with an exponent of 1, 
       //  stored as 128
       s.i = (i << 8) | (0x80 << 23);
       s.f = (float)sqrt(s.f);
 	
-      fast_sqrt_table[i] = (s.i & 0x7FFFFF);
+      _fast_sqrt_table[i] = (s.i & 0x7FFFFF);
     }
   }
 
-public:
+  static void build_random_floats()
+  {
+    unsigned int i;
+    bzfsrand(0); // ensure consistent seed and same rand value order
+    for (i=0; i < 0x10000; i++) {
+      _random_floats[i] = bzfrand();
+    }
+  }
 
-  /** nvidias fast square root routine is a table-based solution that
-   * trades off memory utilization for performance.  it's not
-   * necessarily a cache friendly method, but gives impressive results
-   * for common use.
+  /* INVERSE SQUARE ROOT */
+
+  /** system implementation of floating point square root estimate 
    */
-  static inline float fastsqrt(float n)
+  static inline float fastinvsqrt0(float n)
+  {
+    return 1.0f / sqrtf(n);
+  }
+
+  /** software estimate of inverse square root 
+   */
+  static inline float fastinvsqrt1(float n)
+  {
+    return 1.0f / fastsqrt1(n);
+  }
+
+  /** hardware instruction based inverse square root estimate 
+   */
+  static inline float fastinvsqrt2 (float n)
+  {
+#if defined(__APPLE__)
+    float result;
+    asm ( "frsqrte %0, %1" : /*OUT*/ "=f" (result) : /*IN*/ "f" (n) );
+    return result;
+#else
+    return 1.0f / sqrtf(n);
+#endif
+  }
+
+
+  /* SQUARE ROOT */
+
+  /** system implementation of floating point square root 
+   */
+  static inline float fastsqrt0(float n)
+  {
+    return sqrtf(n);
+  }
+
+  /** software estimate replacement -- this is nvidias fast square
+   * root routine is a table-based solution that trades off memory
+   * utilization for performance.  it's not necessarily a cache
+   * friendly method, but gives impressive results for common use.
+   */
+  static inline float fastsqrt1(float n)
   {
     // make sure the table is built
-    if (!built) {
+    if (!_built_fast_sqrt_table) {
       build_sqrt_table();
-      built = true;
+      _built_fast_sqrt_table = true;
     }
     
     // check for square root of 0
@@ -145,15 +214,126 @@ public:
       return 0.0;
     }
     
-    FP_BITS(n) = fast_sqrt_table[(FP_BITS(n) >> 8) & 0xFFFF] | ((((FP_BITS(n) - 0x3F800000) >> 1) + 0x3F800000) & 0x7F800000);
+    FP_BITS(n) = _fast_sqrt_table[(FP_BITS(n) >> 8) & 0xFFFF] | ((((FP_BITS(n) - 0x3F800000) >> 1) + 0x3F800000) & 0x7F800000);
     
     return n;
   }
-  
+
+  /** hardware instruction based square root estimate 
+   */
+  static inline float fastsqrt2(float n)
+  {
+#if defined(__APPLE__)
+    return 1.0f / fastinvsqrt2(n);
+#else
+    return sqrtf(n);
+#endif
+  }
+
+
+public:
+
+  /** function pointer to the fastinvsqrt routine that performs best
+   * on this system.
+   */
+  static float (*fastinvsqrt)(float);
+
+  /** function pointer to the fastsqrt routine that performs best on
+   * this system.
+   */
+  static float (*fastsqrt)(float);
+
+  /** optimize usage of square root and inverse square root at runtime
+   * to use the fastest available.  A quick timed test is performed on
+   * each of the routines and the function pointers are set to the
+   * fastest.  There is no consideration for tolerance being taken
+   * into account -- they are all assumed to be insufficient if
+   * accuracy is required.
+   */
+  static void optimize() 
+  {
+    /* array of function pointers for testing */
+    float (*mathTest[6])(float) = {fastsqrt0, fastsqrt1, fastsqrt0,
+				   fastinvsqrt0, fastinvsqrt1, fastinvsqrt0};
+    const char *label[6] = {"system square root", "nvidia square root estimate", "hardware-based square root",
+			    "system inverse square root", "nvidia inverse square root estimate", "harware-based inverse square root"};
+
+    std::cout << "Optimizing math routines..." << std::endl;
+
+    if (!_built_random_floats) {
+      std::cout << "...building random float table" << std::endl;
+      build_random_floats();
+      _built_random_floats = true;
+    }
+
+    /* minimize cache effect by preloading */
+    double sum = 0.0;
+    for (unsigned int fl = 0; fl < 0x10000; fl++) {
+      sum += _random_floats[fl];
+    }
+    // should always be false, but compiler shouldn't know that
+    if (sum < -1.0f) {
+      std::cerr << "ERROR: should not have reached here in MathUtils.h" << std::endl;
+      exit(1);
+    }
+
+    /* test math routine candidates (reverse order) */
+    unsigned int mostIterations = 0;
+    unsigned int bestIndex = 0;
+    for (int i = 5; i >= 0; i--) {
+      unsigned int iterations = 0; // how many test iterations were performed
+      sum = 0.0; // make sure optimizer doesn't optimize us away
+
+      if (i == 2) {
+	std::cout << "...testing square root" << std::flush;
+      } else if (i == 5) {
+	std::cout << "...testing inverse square root" << std::flush;
+      }
+
+      // test for half a second per function
+      TimeKeeper t = TimeKeeper::getCurrent();
+      do {
+	for (unsigned int f = 0; f < 0x10000; f++) {
+	  sum += mathTest[i](_random_floats[f]);
+	}
+	iterations++;
+      } while (TimeKeeper::getCurrent() - t < 0.5f);
+      std::cout << " ... " << iterations;
+
+      // should always be false, but compiler shouldn't know that
+      if (sum < -1.0f) {
+	std::cerr << "ERROR: should not have reached here in MathUtils.h" << std::endl;
+	exit(1);
+      }
+
+      if (iterations > mostIterations) {
+	mostIterations = iterations;
+	bestIndex = i;
+      }
+
+      if (i == 0) {
+	fastsqrt = mathTest[bestIndex];
+	std::cout << std::endl << "...using " << label[bestIndex] << std::endl;
+	bestIndex = 3;
+      } else if (i == 3) {
+	fastinvsqrt = mathTest[bestIndex];
+	std::cout << std::endl << "...using " << label[bestIndex] << std::endl;
+	bestIndex = 0;
+      }
+    }
+    std::cout << "...done optimizing math routines." << std::endl;
+    return;
+  }
+
 };
 
-unsigned int math_util::fast_sqrt_table[0x10000] = {0};
-bool math_util::built = false;
+/* static initializers */
+unsigned int math_util::_fast_sqrt_table[0x10000] = {0};
+bool math_util::_built_fast_sqrt_table = false;
+float math_util::_random_floats[0x10000] = {0};
+bool math_util::_built_random_floats = false;
+float (*math_util::fastinvsqrt)(float) = math_util::fastinvsqrt0;
+float (*math_util::fastsqrt)(float) = math_util::fastsqrt0;
 
 #endif  /* __MATHUTILS_H__ */
 
