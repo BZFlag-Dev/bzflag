328ff9b6843ad1c1c30c5d87f35f783a56149fae
Tim Riker <timriker@users.sourceforge.net>
2004-06-30 22:33:31 +0000

    start of native php support
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7214 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/bzlist.php
+++ misc/bzlist.php
@@ -21,11 +21,96 @@
 $listserver = 'http://db.bzflag.org/db/?action=LIST';
 $bzfquery = './bzfquery.pl';
 
+$title="BZFlag Server List";
+
+if ($_GET["hostport"]) {
+  $title .= " for " .$_GET["hostport"];
+}
+
+function query ($hostport) {
+  list($host, $port) = split(":", $hostport, 2);
+  $protocol = 'tcp';
+  $get_prot = getprotobyname($protocol);
+  if ($get_prot == -1) {
+     // if nothing found, returns -1
+     echo 'Invalid Protocol';
+     return;
+  }
+  if (!ctype_digit($port)) {
+    $port = getservbyname($port, 'tcp');
+  }
+  $ip = gethostbyname($host);
+  echo "$host:$port:$ip";
+  $fp = fsockopen($host, $port, $errno, $errstr, 5);
+  if (!$fp) {
+    echo "$errstr ($errno)<br>\n";
+    return;
+  }
+  $buffer=fread($fp, 9);
+  //var_dump($buffer);
+  # parse reply
+  $array = unpack("a4magic/a4protocol/Cid", $buffer);
+  //var_dump($array);
+  $magic = $array['magic'];
+  $protocol = $array['protocol'];
+  $id = $array['id'];
+  echo "$magic $protocol $id\n";
+  if ($magic != "BZFS") {
+    echo "not a bzflag server";
+    fclose($fp);
+    return;
+  }
+  if ($protocol != "1910") {
+    echo "incompatible version";
+    fclose($fp);
+    return;
+  }
+  $request = pack("n2", 0, 0x7167);
+  //var_dump($request);
+  fwrite($fp, $request);
+  $buffer=fread($fp, 40);
+  //var_dump($buffer);
+  $array += unpack("nlen/ncode/nstyle/nmaxPlayers/nmaxShots/nrogueSize/nredSize/ngreenSize/nblueSize/npurpleSize/nrogueMax/nredMax/ngreenMax/nblueMax/npurpleMax/nshakeWins/nshakeTimeout/nmaxPlayerScore/nmaxTeamScore/nmaxTime", $buffer);
+  var_dump($array);
+
+  echo "style:";
+  if ($array['style'] & 0x0001) echo " CTF";
+  if ($array['style'] & 0x0002) echo " flags";
+  if ($array['style'] & 0x0004) echo " rogues";
+  if ($array['style'] & 0x0008) echo " jumping";
+  if ($array['style'] & 0x0010) echo " inertia";
+  if ($array['style'] & 0x0020) echo " ricochet";
+  if ($array['style'] & 0x0040) echo " shaking";
+  if ($array['style'] & 0x0080) echo " antidote";
+  if ($array['style'] & 0x0100) echo " time-sync";
+  if ($array['style'] & 0x0200) echo " rabbit-hunt";
+  echo "\n";
+  echo "maxPlayers: " . $array['maxPlayers'] . "\n";
+  echo "maxShots: " . $array['maxShots'] . "\n";
+  echo "team sizes: " . $array['rogueSize'] . " " .
+      $array['redSize'] . " " . $array['greenSize'] . " " .
+      $array['blueSize'] . " " . $array['purpleSize'] .
+      " (rogue red green blue purple)\n";
+  echo  "max sizes:  " . $array['rogueMax'] . " " .
+      $array['redMax'] . " " . $array['greenMax'] . " " .
+      $array['blueMax'] . " " . $array['purpleMax'] . "\n";
+  if ($array['style'] & 0x0040) {
+    echo "wins to shake bad flag: " . $array['shakeWins'] . "\n";
+    echo "time to shake bad flag: " . $array['shakeTimeout'] / 10 . "\n";
+  }
+  echo "max player score: " . $array['maxPlayerScore'] . "\n";
+  echo "max team score: " . $array['maxTeamScore'] . "\n";
+  echo "max time: " . $maxTime / 10 . "\n";
+
+  fclose($fp);
+  return;
+}
+
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>temp</title>
+<title><? echo $title; ?></title>
 </head>
 <body>
 <?
@@ -33,11 +118,14 @@ if($_GET["hostport"]) {
   ?>
   <table border=0 cellpadding=0 cellspacing=0>
     <tr>
-      <td><? print "$_GET[hostport] Stats:"; ?></td>
+      <td><? echo "$_GET[hostport] Stats:"; ?></td>
     </tr>
     <tr><td>&nbsp;</td></tr>
     <tr>
-      <td><pre><? system("$bzfquery $hostport"); ?></pre></td>
+      <td>
+	<pre><? query($hostport); ?></pre>
+	<pre><? system("$bzfquery $hostport"); ?></pre>
+      </td>
     </tr>
   </table>
   <?
@@ -60,21 +148,21 @@ if ($fp) {
     $count = count($array);
     $LINK = "<a href=\"$_SERVER[PHP_SELF]?hostport=$array[0]\">$array[0]</a>";
     if (($array[0])) {
-      print "<tr>";
-      print "<td>$LINK</td>";
-      print "<td>$array[3]</td>";
+      echo "<tr>";
+      echo "<td>$LINK</td>";
+      echo "<td>$array[3]</td>";
       unset($array[0], $array[1], $array[2], $array[3]);
       foreach ( $array as $line ) {
 	$description .= "$line ";
       }
       //$bzdesc = substr("$description", 0, 50);
-      //print "<td>$bzdesc</td>";
-      print "<td>$description</td>";
+      //echo "<td>$bzdesc</td>";
+      echo "<td>$description</td>";
       unset($description);
-      print "</tr>\n";
+      echo "</tr>\n";
     }
   }
-  print "</table>\n";
+  echo "</table>\n";
   fclose ($fp);
 } ?>
 
