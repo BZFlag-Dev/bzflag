597866c2daf9f9d31e5b0ee42fa6b31e90b40946
trepan <trepan@users.sourceforge.net>
2004-08-01 16:16:38 +0000

    copyright
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7574 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/scene/Octree.h
+++ src/scene/Octree.h
@@ -1,3 +1,14 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #ifndef __OCTREE_H__
 #define __OCTREE_H__
@@ -97,3 +108,11 @@ class Octree {
 
 
 #endif // __OCTREE_H__
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
