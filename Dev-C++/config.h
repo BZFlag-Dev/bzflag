00b804c1ecc58c94d74e71fde9eda7718a9388a3
Ian Agar <lan56@users.sourceforge.net>
2004-08-04 18:12:01 +0000

    remove engine.c because it is not to be compiled, but instead #included
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7631 08b3d480-bf2c-0410-a26f-811ee3361c24

--- Dev-C++/config.h
+++ Dev-C++/config.h
@@ -16,7 +16,7 @@
 #define _DEVCPP_CONFIG
 
 /* Building regex */
-//#define BUILD_REGEX 1 //FIXME: it can't compile engine.c; undeclared types
+#define BUILD_REGEX 1
 
 /* Time Bomb expiration */
 /* #undef TIME_BOMB */
