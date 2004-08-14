652c610b06b0cff8e61c97f41662a100034c1503
Tim Riker <timriker@users.sourceforge.net>
2004-08-14 01:00:13 +0000

    ws script
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7761 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ misc/ws.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.dsp -o -name Makefile.am -o -name README\* -o -name \*.fmt| sort`
+for file in $files ; do
+ sed -i -e 's/[[:space:]]*$//' $file
+done
+grep -Irsl '[[:space:]][[:space:]]*$' . | grep -v Makefile$
