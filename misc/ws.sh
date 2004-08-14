44c8fd81bcd08dcb0223bd17feb0e828018daf17
Tim Riker <timriker@users.sourceforge.net>
2004-08-14 05:22:42 +0000

    loath M$
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7767 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/ws.sh
+++ misc/ws.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.dsp -o -name Makefile.am -o -name README\* | sort`
+# do NOT strip ws from the dsp files. loath M$
+files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name Makefile.am -o -name README\* | sort`
 for file in $files ; do
  sed -i -e 's/[[:space:]]*$//' $file
 done
