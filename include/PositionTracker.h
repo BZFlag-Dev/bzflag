ca34e2fd22a772171980405aedbba6953b3a0615
Sean Morrison <brlcad@users.sourceforge.net>
2004-08-23 06:33:44 +0000

    commit an updated position tracker before more ws commits cause more conflicts.. this revision adds support for waypoints and distance computation through those waypoints.  also adds ability to have objects tracked by numeric id or string.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7849 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/PositionTracker.h
+++ include/PositionTracker.h
@@ -26,16 +26,21 @@
 /** PositionTracker keeps track of where things are in a 3D Cartesian
  * coordinate space.  It can give introspective information like where
  * things are and how close other tracked objects are.  Objects may be
- * grouped into separate categories for subsearching.
+ * grouped into separate categories for subsearching.  All objects are
+ * labeled with either a string or numeric identifier.
  */
 class PositionTracker
 {
 private:
 
-  /** something that we are tracking
+  /** something that we are tracking (uniquely identified by either an
+   * integer or a string)
    */
+  typedef enum idtype { UNSET_ID, INT_ID, STR_ID } idtype_t;
   typedef struct item {
-    std::string id;
+    idtype_t id;
+    long int intID;
+    std::string strID;
     TimeKeeper lastUpdate;
     double position[3]; // position in Cartesian 3-space coodinates
     bool forgotten;
@@ -47,7 +52,11 @@ private:
    *  into some provided group name.  using that group name (which is
    *  "" if not provided), we get back a set of tracked objects.
    */
-  std::map <std::string, TrackedItemVector> _trackedItem;
+  std::map<std::string, TrackedItemVector> _trackedItem;
+
+  /** waypoint distance matrix
+   */
+  std::map<std::pair<unsigned short int, unsigned short int>, double> _waypointDistance;
 
 protected:
 
@@ -62,6 +71,7 @@ public:
    * token is returned that must be used for subsequent updates.
    */
   unsigned short track(const std::string id, std::string group = std::string(""));
+  unsigned short track(long int id, std::string group = std::string(""));
 
   /** update the position of a tracked item.  returns truthfully
    * whether the update could be performed (i.e. whether the id
@@ -69,15 +79,38 @@ public:
    * validation.
    */
   bool update(unsigned short int token, const std::string id, const double position[3], std::string group = std::string(""));
+  bool update(unsigned short int token, const std::string id, const float position[3], std::string group = std::string(""));
+  bool update(unsigned short int token, long int id, const double position[3], std::string group = std::string(""));
+  bool update(unsigned short int token, long int id, const float position[3], std::string group = std::string(""));
+
+  /** add a waypoint to the tracker so that it can calculate shortest
+   * paths better.  A negative distance will cause the real distance
+   * to be computed and stored, otherwise a non-negative distance will
+   * indicate how "far" the two points are.  Setting the distance to 
+   * zero or near zero is akin to a teleporter.
+   */
+  bool addWaypoint(const double from[3], const double to[3], double distance=-1.0);
+  bool addWaypoint(const float from[3], const float to[3], double distance=-1.0);
 
   /** stop tracking something if it was being tracked.  the token and
    * string id are both required for validation.
    */
   bool forget(unsigned short int token, const std::string id, std::string group = std::string(""));
+  bool forget(unsigned short int token, long int id, std::string group = std::string(""));
 
-  /** compute the distance between two tracked items given their tokens.
+  /** compute the simple distance between two tracked items given their tokens.
    */
-  double distanceBetween(unsigned short int from, unsigned short int to, std::string fromGroup=std::string(""), std::string toGroup=std::string("")) const;
+  double distanceBetween(unsigned short int fromToken, unsigned short int toToken, std::string fromGroup=std::string(""), std::string toGroup=std::string("")) const;
+
+  /** compute the shortest distance between two tracked items,
+   * utilizing available waypoint shortcuts.
+   */
+  double waypointDistance(unsigned short int fromToken, unsigned short int toToken, std::string fromGroup=std::string(""), std::string toGroup=std::string("")) const;
+
+  /** returns a count of how many objects are being tracked in a
+   * particular group.
+   */
+  unsigned short int trackedCount(std::string group = std::string("")) const;
 
 };
 
