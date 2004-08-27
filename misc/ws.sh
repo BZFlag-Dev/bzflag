1d9f77ab08666e9ecd4077f3fe931dc98f5d62b1
Tim Riker <timriker@users.sourceforge.net>
2004-08-27 06:13:10 +0000

    might as well convert 8 spaces to tab too
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7911 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/ws.sh
+++ misc/ws.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name Makefile.am -o -name README\* -o -name \*.dsp -o -name \*.fmt | sort`
+files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.c -o -name Makefile.am | sort`
+# convert 8 spaces to tab
+for file in $files ; do
+ sed -i -e 's/[ ][ ][ ][ ][ ][ ][ ][ ]$/\t/' $file
+done
+files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.c -o -name Makefile.am -o -name README\* -o -name \*.dsp -o -name \*.fmt | sort`
+# remove trailing whitespace and convert spacetab to tab
 for file in $files ; do
  sed -i -e 's/[[:space:]]*$//' -e 's/ \t/\t/' $file
 done
+echo files with trailing whitespace:
 grep -Irsl '[[:space:]][[:space:]]*$' . | grep -v Makefile$
