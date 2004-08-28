317e0ab461310f790eb9b58284acb3bd2c68268d
Sean Morrison <brlcad@users.sourceforge.net>
2004-08-28 14:23:34 +0000

    keys that envoke a modal state change should not be open -- make freezing key require shift modifier.  should also give some sort of notification if the motion is frozen.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@7929 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/playing.cxx
+++ src/bzflag/playing.cxx
@@ -757,9 +757,12 @@ static void		doKeyPlaying(const BzfKeyEvent& key, bool pressed)
   bool haveBinding = doKeyCommon(key, pressed);
 
 #if defined(FREEZING)
-  if (key.ascii == '`' && pressed && !haveBinding) {
+  if (key.ascii == '`' && pressed && !haveBinding && key.shift)) {
     // toggle motion freeze
     motionFreeze = !motionFreeze;
+    if (motionFreeze) {
+      addMessage(NULL, "The tank's motion is now frozen! .. Press Shift+` to unfreeze");
+    }
     return;
   }
 #endif
