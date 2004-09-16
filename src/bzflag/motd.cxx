c5fe207d0216d6a12ebddc23124f42a8a0682c17
Jeff Myers <jeffm2501@users.sourceforge.net>
2004-09-16 16:00:53 +0000

    add in motd files
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@8212 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/motd.cxx
@@ -0,0 +1,92 @@
+/* bzflag
+* Copyright (c) 1993 - 2004 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#ifdef _MSC_VER
+#pragma warning( 4: 4786)
+#endif
+
+#include "motd.h"
+
+MessageOfTheDay::MessageOfTheDay()
+{
+#ifdef HAVE_CURL
+	easyHandle = curl_easy_init();
+	if (!easyHandle) {
+		std::cout << "Something wrong with CURL" << std::endl;
+		return;
+	}
+	CURLcode result;
+	result = curl_easy_setopt(easyHandle, CURLOPT_WRITEFUNCTION, writeFunction);
+	if (result)
+		std::cout << "Something wrong with CURL; Error: " << result << std::endl;
+	result = curl_easy_setopt(easyHandle, CURLOPT_WRITEDATA, this);
+	if (result)
+		std::cout << "Something wrong with CURL; Error: " << result << std::endl;
+#endif
+}
+
+MessageOfTheDay::~MessageOfTheDay()
+{
+#ifdef HAVE_CURL
+	curl_easy_cleanup(easyHandle);
+#endif
+}
+
+#ifdef HAVE_CURL
+void MessageOfTheDay::collectData(char* ptr, int len)
+{
+	std::string readData(ptr, 0, len);
+	data += readData;
+}
+
+size_t MessageOfTheDay::writeFunction(void *ptr, size_t size, size_t nmemb,
+																		void *stream)
+{
+	int len = size * nmemb;
+	((MasterBanList *)stream)->collectData((char *)ptr, len);
+	return len;
+}
+#endif
+
+const std::string& MessageOfTheDay::get ( const std::string URL )
+{
+	data = "";
+	// get all up on the internet and go get the thing
+#ifdef HAVE_CURL
+	CURLcode result;
+	if (!easyHandle) {
+		data = "";
+		return data;
+	}
+	result = curl_easy_setopt(easyHandle, CURLOPT_URL, URL.c_str());
+	if (result) {
+		std::cout << "Something wrong with CURL; Error: " << result << std::endl;
+	}
+	result = curl_easy_perform(easyHandle);
+
+	if (result == (CURLcode)CURLOPT_ERRORBUFFER) {
+		std::cout << "Error: server reported: " << data << std::endl;
+		data = "";
+		return data;
+	}
+	if (result) {
+		std::cout << "Something wrong with CURL; Error: " << result << std::endl;
+		data = "";
+		return data;
+	}
+	result = curl_easy_setopt(easyHandle, CURLOPT_URL, NULL);
+	if (result) {
+		std::cout << "Something wrong with CURL; Error: " << result << std::endl;
+	}
+#endif
+	return data;
+}
---
+++ src/bzflag/motd.h
@@ -0,0 +1,42 @@
+/* bzflag
+* Copyright (c) 1993 - 2004 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+
+#ifndef __MOTD_H__
+#define __MOTD_H__
+
+// bzflag global header
+#include "global.h"
+
+// system headers
+#include <string>
+#ifdef HAVE_CURL
+#include <curl/curl.h>
+#endif
+
+class MessageOfTheDay {
+public:
+	MessageOfTheDay();
+	~MessageOfTheDay();
+
+	const std::string& get ( const std::string URL );
+protected:
+	std::string	data;
+private:
+#ifdef HAVE_CURL
+	void collectData(char* ptr, int len);
+	static size_t writeFunction(void *ptr, size_t size, size_t nmemb,
+		void *stream);
+	CURL *easyHandle;
+#endif
+};
+#endif //__MOTD_H__
