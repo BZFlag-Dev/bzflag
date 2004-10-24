3aeb6b429e5f0a5a065d96dd7b391745a405590b
trepan <trepan@users.sourceforge.net>
2004-10-24 22:23:24 +0000

    more new files
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@8611 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzfs/CustomPhysicsDriver.h
@@ -0,0 +1,42 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __CUSTOM_PHYSICS_DRIVER_H__
+#define __CUSTOM_PHYSICS_DRIVER_H__
+
+/* interface header */
+#include "WorldFileObject.h"
+
+/* common headers */
+#include "PhysicsDriver.h"
+
+class CustomPhysicsDriver : public WorldFileObject {
+  public:
+    CustomPhysicsDriver();
+    ~CustomPhysicsDriver();
+    virtual bool read(const char *cmd, std::istream& input);
+    virtual void write(WorldInfo*) const;
+    bool writeImmediately() { return true; }
+
+  private:
+    mutable PhysicsDriver* driver;
+};
+
+#endif  /* __CUSTOM_PHYSICS_DRIVER_H__ */
+
+// Local variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
