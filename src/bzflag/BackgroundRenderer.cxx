e15a4bc2183f6ad18ffb832cc7c85059fa32145b
Jeff Myers <jeffm2501@users.sourceforge.net>
2004-10-30 00:00:07 +0000

    don't let the cat out of the bag!
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@8709 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/BackgroundRenderer.cxx
+++ src/bzflag/BackgroundRenderer.cxx
@@ -104,10 +104,6 @@ BackgroundRenderer::BackgroundRenderer(const SceneRenderer&) :
     if (userTextures[0].size())
       groundTextureID = tm.getTextureID( userTextures[0].c_str(), false );
 
-	if (groundTextureID < 0)
-		groundTextureID = tm.getTextureID( "weather/snowground", true );
-
-
     if (groundTextureID < 0)
       groundTextureID = tm.getTextureID( BZDB.get("stdGroundTexture").c_str(), true );
 
