190c75b0cd6b6887002210fe36a547e97af423e6
Tim Riker <timriker@users.sourceforge.net>
2004-11-08 07:38:41 +0000

    use perl, grep first, lookout!
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@8814 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/ws.sh
+++ misc/ws.sh
@@ -1,13 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+# cleanup whitespace issues
+# could use new sed, but not everybody has that yet
+# sed -i -e 's/search/replace/g' filename1 ...
+# perl -pi -e 's/search/replace/g;' filename1 ...
+# grep them first to not touch the file date/time
 files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.c -o -name Makefile.am | sort`
 # convert 8 spaces to tab
 for file in $files ; do
- sed -i -e 's/[ ][ ][ ][ ][ ][ ][ ][ ]$/\t/' $file
+ # don't actually include 8 spaces or they might get replaced. ;-)
+ # that's a space in the []
+ if grep -q '[ ]       ' $file ; then
+  #sed -i -e 's/[ ]       /\t/g' $file
+  perl -pi -e 's/[ ]       /\t/g' $file
+ fi
 done
 files=`find . -name \*.cxx -o -name \*.h -o -name \*.cpp -o -name \*.c -o -name Makefile.am -o -name README\* -o -name \*.dsp -o -name \*.fmt | sort`
 # remove trailing whitespace and convert spacetab to tab
 for file in $files ; do
- sed -i -e 's/[[:space:]]*$//' -e 's/ \t/\t/' $file
+ # that's a tab in the []
+ if grep -q ' [	]' $file ; then
+  #sed -i -e 's/ \t/\t/g' $file
+  perl -pi -e 's/ *\t/\t/g' $file
+ fi
+ if grep -q '[[:space:]][[:space:]]*$' $file ; then
+  #sed -i -e 's/[[:space:]]*$//' $file
+  perl -pi -e 's/\s*$/\n/' $file
+ fi
 done
 echo files with trailing whitespace:
 grep -Irsl '[[:space:]][[:space:]]*$' . | grep -v Makefile$
