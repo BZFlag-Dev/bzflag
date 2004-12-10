2b16ea990a0fc2d8bb76775263c67de9d8718ed9
Sean Morrison <brlcad@users.sourceforge.net>
2004-12-10 03:41:29 +0000

    new compilation unit for creating/destructing startup info objects.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@9179 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/game/StartupInfo.cxx
@@ -0,0 +1,62 @@
+/* bzflag
+ * Copyright (c) 1993 - 2004 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* interface header */
+#include "StartupInfo.h"
+
+/* system implementation headers */
+#include <string.h>
+
+/* common implementation headers */
+#include "Protocol.h"
+
+
+StartupInfo::StartupInfo() : hasConfiguration(false),
+			     autoConnect(false),
+			     serverPort(ServerPort),
+			     team(AutomaticTeam),
+			     listServerURL(DefaultListServerURL),
+			     listServerPort(ServerPort + 1)
+{
+  strcpy(serverName, "");
+  strcpy(callsign, "");
+  strcpy(password, "");
+  strcpy(email, "default");
+  joystickName = "joystick";
+  joystick = false;
+}
+
+StartupInfo::~StartupInfo()
+{
+  hasConfiguration = false;
+  autoConnect = false;
+  memset(serverName, 0, 80);
+  serverPort = -1;
+  useUDPconnection = false;
+  team = NoTeam;
+  memset(callsign, 0, CallSignLen);
+  memset(password, 0, PasswordLen);
+  memset(token, 0, TokenLen);
+  memset(email, 0, EmailLen);
+  listServerURL = "";
+  listServerPort = 0;
+  joystick = false;
+  joystickName = "";
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
