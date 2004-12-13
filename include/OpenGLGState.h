faefc44391a390f42dbf85fae0b6ae623a08fda9
Sean Morrison <brlcad@users.sourceforge.net>
2004-12-13 03:45:12 +0000

    add a callback wrapper function to initContext
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@9240 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/OpenGLGState.h
+++ include/OpenGLGState.h
@@ -175,6 +175,12 @@ class OpenGLGStateBuilder {
 };
 
 
+inline void		initContextCB(void *)
+{
+  return OpenGLGState::initContext();
+}
+
+
 #endif // BZF_OPENGL_GSTATE_H
 
 // Local Variables: ***
