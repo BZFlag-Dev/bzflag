86c72d631ac71f844c7d2893735ccdf9521c0aa4
Frank Thilo <chestal@users.sourceforge.net>
2004-12-29 00:56:08 +0000

    speedTolerance adjustment for handicapped mode needs to be squared
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@9433 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -3231,7 +3231,7 @@ static void adjustTolerances()
 {
   // check for handicap adjustment
   if ((clOptions->gameStyle & HandicapGameStyle) != 0) {
-    speedTolerance *= HandicapSpeedAdj;
+    speedTolerance *= HandicapSpeedAdj * HandicapSpeedAdj;
   }
 
   // check for physics driver disabling
