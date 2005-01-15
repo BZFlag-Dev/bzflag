3ef212be53feba62ead080705fa9d72da5606231
Sean Morrison <brlcad@users.sourceforge.net>
2005-01-15 23:16:46 +0000

    the msgtimeupdate patch was protocol breaking...
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@9814 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -19,7 +19,7 @@
 
 // opaque version number increments on protocol incompatibility
 #ifndef BZ_PROTO_VERSION
-#define BZ_PROTO_VERSION	"0024"
+#define BZ_PROTO_VERSION	"0025"
 #endif
 
 // version numbers - also update:
@@ -39,7 +39,7 @@
 #endif
 
 #ifndef BZ_REV
-#define BZ_REV			39
+#define BZ_REV			40
 #endif
 
 // DEVEL | STABLE | MAINT
