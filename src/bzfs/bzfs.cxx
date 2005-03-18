151f8b472e4afcacdd5d6cb0c5da8d78045482d8
Sean Morrison <brlcad@users.sourceforge.net>
2005-03-18 22:03:29 +0000

    don't prevent players that don't have TALK from sending to the admin group
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10261 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -1525,9 +1525,12 @@ void sendFilteredMessage(int sendingPlayer, PlayerId recipientPlayer, const char
     GameKeeper::Player *recipientData = GameKeeper::Player::getPlayerByIndex(recipientPlayer);
 
     // don't care if they're real, just care if they're an admin
-    if (recipientData && recipientData->accessInfo.isAdmin()) {
-      sendMessage(sendingPlayer, recipientPlayer, msg);
-      return;
+    if (recipientData) {
+      if (recipientData->accessInfo.isAdmin() || 
+	  recipientPlayer == AdminPlayers) {
+	sendMessage(sendingPlayer, recipientPlayer, msg);
+	return;
+      }
     }
 
     sendMessage(ServerPlayer, sendingPlayer, "We're sorry, your not allowed to talk!");
