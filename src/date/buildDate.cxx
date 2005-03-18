fd4b5aafec0bc71219f7b92ddbe205a537e01099
Tim Riker <timriker@users.sourceforge.net>
2005-03-18 09:04:55 +0000

    2.0.2
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10247 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -38,12 +38,12 @@
 #endif
 
 #ifndef BZ_REV
-#define BZ_REV			1
+#define BZ_REV			2
 #endif
 
 // DEVEL | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#define BZ_BUILD_TYPE		"DEVEL"
+#define BZ_BUILD_TYPE		"STABLE"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993 - 2005 Tim Riker";
