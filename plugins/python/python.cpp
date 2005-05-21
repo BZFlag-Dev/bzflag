227de69cc8f808809f503c80db41845fe2c43273
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2005-05-21 07:23:34 +0000

    well, maybe this is correct. who knows. time to go get clean.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10589 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/python/python.cpp
+++ plugins/python/python.cpp
@@ -11,10 +11,14 @@
  */
 
 #include "bzfsAPI.h"
+#include "OSFile.h"
 #include <Python.h>
 #include "PyBZFlag.h"
 
-Python::BZFlag *module_bzflag;
+static char *ReadFile (const char *filename);
+
+static Python::BZFlag *module_bzflag;
+static PyObject *global_dict;
 
 BZ_GET_PLUGIN_VERSION
 
@@ -26,11 +30,33 @@ bz_Load (const char *commandLine)
 	Py_Initialize ();
 
 	module_bzflag = new Python::BZFlag ();
+
+	char *buffer = ReadFile (commandLine);
+
+	PyCodeObject *code = (PyCodeObject *) Py_CompileString (buffer, commandLine, Py_file_input);
+
+	global_dict = PyDict_New ();
+	PyDict_SetItemString (global_dict, "__builtins__", PyEval_GetBuiltins ());
+	PyDict_SetItemString (global_dict, "__name__", PyString_FromString ("__main__"));
+	PyEval_EvalCode (code, global_dict, global_dict);
 }
 
 BZF_PLUGIN_CALL
 int
 bz_Unload (void)
 {
+	PyDict_Clear (global_dict);
+	Py_DECREF (global_dict);
 	Py_Finalize ();
+}
+
+static char *
+ReadFile (const char *filename)
+{
+	OSFile osf;
+	osf.open (filename, "r");
+	char *buffer = new char[osf.size ()];
+	osf.read (buffer, osf.size ());
+	osf.close ();
+	return buffer;
 }
