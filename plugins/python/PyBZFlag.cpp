5b54fadccc8c57ce34f77c0963cee48663a36997
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2005-05-21 02:38:09 +0000

    add the beginnings of the main module
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10582 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/python/PyBZFlag.cpp
@@ -0,0 +1,28 @@
+/* bzflag
+ * Copyright (c) 1993 - 2005 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include <Python.h>
+#include "PyBZFlag.h"
+
+namespace Python
+{
+
+static struct PyMethodDef methods[] =
+{
+};
+
+BZFlag::BZFlag ()
+{
+	module = Py_InitModule3 ("BZFlag", methods, NULL);
+}
+
+}
