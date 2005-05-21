5df112e8df94df55eb6bb5c9c40b9e7787949a8f
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2005-05-21 01:56:48 +0000

    add the beginnings of a python plugin
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10578 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/python/python.cpp
@@ -0,0 +1,33 @@
+/* bzflag
+ * Copyright (c) 1993 - 2005 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "bzfsAPI.h"
+
+BZF_PLUGIN_CALL
+int
+bz_Load (const char *commandLine)
+{
+}
+
+BZF_PLUGIN_CALL
+int
+bz_Unload (void)
+{
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
