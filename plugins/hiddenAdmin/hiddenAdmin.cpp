60b75cbacbce0bdeb3ac092f59e9d04ca1cfbc71
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-05-23 01:06:19 +0000

    new plugin to hide addmin users ( they don't send out the "admin" bit )
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10634 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/hiddenAdmin/hiddenAdmin.cpp
@@ -0,0 +1,74 @@
+// hiddenAdmin.cpp : Defines the entry point for the DLL application.
+//
+
+
+#include "bzfsAPI.h"
+#include <string>
+#include <map>
+
+BZ_GET_PLUGIN_VERSION
+
+// event handler callback
+
+class HiddenAdmin : public bz_EventHandler
+{
+public:
+	HiddenAdmin();
+	virtual ~HiddenAdmin();
+
+	virtual void process ( bz_EventData *eventData );
+
+	virtual bool autoDelete ( void ) { return false;} // this will be used for more then one event
+protected:
+};
+
+HiddenAdmin	hiddenAdmin;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* commandLine )
+{
+	bz_debugMessage(4,"HiddenAdmin plugin loaded");
+
+	bz_registerEvent(bz_eGetPlayerInfoEvent,BZ_ALL_USERS,&hiddenAdmin);
+
+	return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+	bz_removeEvent(bz_eGetPlayerInfoEvent,BZ_ALL_USERS,&hiddenAdmin);
+
+	bz_debugMessage(4,"HiddenAdmin plugin unloaded");
+	return 0;
+}
+
+// ----------------- SpreeTracker-----------------
+
+/*typedef struct 
+{
+int playerID;
+std::string callsign;
+double		startTime;
+double		lastUpdateTime;
+int			spreeTotal;
+}trPlayerHistoryRecord;
+
+std::map<int, trPlayerHistoryRecord > playerList; */
+
+HiddenAdmin::HiddenAdmin()
+{
+}
+
+HiddenAdmin::~HiddenAdmin()
+{
+}
+
+void HiddenAdmin::process ( bz_EventData *eventData )
+{
+	if (eventData->eventType != bz_eGetPlayerInfoEvent)
+		return;
+
+	bz_GetPlayerInfoEventData	* infoData = (bz_GetPlayerInfoEventData*)eventData;
+
+	infoData->admin = false;
+}
+
