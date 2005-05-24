ba67cc0e06efab4607944276757c552632f17568
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2005-05-24 01:24:31 +0000

    comments!
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10662 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/python/BZReactor.py
+++ plugins/python/BZReactor.py
@@ -17,9 +17,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
-import sys
-print sys.path
-
 from twisted.internet import selectreactor
 import BZFlag, BZFlag.Event
 
