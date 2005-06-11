00e7c367b1bfb73ca57e3ecddb5fedba0daddf53
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-06-11 19:36:41 +0000

    add in forgoten files for message packing.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10829 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/messages.h
@@ -0,0 +1,56 @@
+/* bzflag
+* Copyright (c) 1993 - 2005 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/*
+* Communication protocol constants
+*/
+
+#ifndef	BZF_MESSAGES_H
+#define	BZF_MESSAGES_H
+
+#include "common.h"
+
+#define _CallSignLen		32		// including terminating NUL
+#define _PasswordLen		32		// including terminating NUL
+#define _EmailLen				128		// including terminating NUL
+#define _TokenLen				22		// opaque string (now int(10)) and terminating NUL
+#define _VersionLen			60		// including terminating NUL
+#define _MessageLen			128		// including terminating NUL
+
+class PlayerAddMessage
+{
+public:
+	PlayerAddMessage();
+
+	bool unpack ( void* buf );
+	void* pack ( void* buf );
+
+	int playerID;
+	int team;
+	int type;
+	int wins;
+	int losses;
+	int	tks;
+	std::string callsign;
+	std::string email;
+};
+
+#endif // BZF_MESSAGES_H
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
---
+++ src/common/messages.cxx
@@ -0,0 +1,103 @@
+
+/* bzflag
+* Copyright (c) 1993 - 2005 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#include <sstream>
+#include <string>
+
+#include <stdio.h>
+#include <string.h>
+#include "config.h"
+
+#include "messages.h"
+#include "Pack.h"
+
+
+PlayerAddMessage::PlayerAddMessage()
+{
+	playerID = -1;
+	team = -1;
+	type = -1;
+	wins = 0;
+	losses = 0;
+	tks = 0;
+}
+
+bool PlayerAddMessage::unpack ( void* buf )
+{
+	unsigned char id;
+	uint16_t _team, _type, _wins, _losses, _tks;
+	char _callsign[_CallSignLen] = {0};
+	char _email[_EmailLen] = {0};
+
+	buf = nboUnpackUByte(buf, id);
+	buf = nboUnpackUShort(buf, _type);
+	buf = nboUnpackUShort(buf, _team);
+	buf = nboUnpackUShort(buf, _wins);
+	buf = nboUnpackUShort(buf, _losses);
+	buf = nboUnpackUShort(buf, _tks);
+	buf = nboUnpackString(buf, _callsign, _CallSignLen);
+	buf = nboUnpackString(buf, _email, _EmailLen);
+
+	playerID = id;
+	team = _team;
+	type = _type;
+	wins = _wins;
+	losses = _losses;
+	tks = _tks;
+
+	callsign = _callsign;
+	email = _email;
+
+	return true;
+}
+
+void*  PlayerAddMessage::pack ( void* buf )
+{
+	uint16_t _team, _type, _wins, _losses, _tks;
+	char _callsign[_CallSignLen] = {0};
+	char _email[_EmailLen] = {0};
+
+	unsigned char id;
+
+	id = playerID;
+	_team = team;
+	_type = type;
+	_wins = wins;
+	_losses = losses;
+	_tks = tks;
+
+	strncpy(_callsign,callsign.c_str(),callsign.size() > _CallSignLen-1 ? _CallSignLen-1 :  callsign.size());
+	strncpy(_email,email.c_str(),email.size() > _EmailLen-1 ? _EmailLen-1 :  email.size());
+
+	buf = nboPackUByte(buf, id);
+	buf = nboPackUShort(buf, _type);
+	buf = nboPackUShort(buf, _type);
+	buf = nboPackUShort(buf, _team);
+	buf = nboPackUShort(buf, _wins);
+	buf = nboPackUShort(buf, _losses);
+	buf = nboPackUShort(buf, _tks);
+	buf = nboPackString(buf, _callsign, _CallSignLen);
+	buf = nboPackString(buf, _email, _EmailLen);
+
+	return buf;
+}								
+
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
