e9de555e98b0edf9216adcfc3c8c04316f1e3066
Alfredo Tupone <atupone@users.sourceforge.net>
2005-06-13 16:50:13 +0000

    Removed warning=all for people sanity ... and mine
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10881 08b3d480-bf2c-0410-a26f-811ee3361c24

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # script to prepare bzflag sources
-autoreconf --install --force --warning=all || exit 1
+autoreconf --install --force || exit 1
 
 if [ -z "$1" ] ; then
  echo BZFlag sources are now prepared. To build here, run:
