4f9e207e52acc1500557c5cc1c3c68a6a8591aaa
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-06-13 20:53:54 +0000

    plugin to make spawns shift up some distance into the air ( falling tanks )
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10885 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/airspawn/airspawn.cpp
@@ -0,0 +1,77 @@
+// airspawn.cpp : Defines the entry point for the DLL application.
+//
+
+
+#include "bzfsAPI.h"
+#include <string>
+#include <map>
+
+BZ_GET_PLUGIN_VERSION
+
+// event handler callback
+
+class airspawn : public bz_EventHandler
+{
+public:
+	airspawn();
+	virtual ~airspawn();
+
+	virtual void process ( bz_EventData *eventData );
+
+	virtual bool autoDelete ( void ) { return false;} // this will be used for more then one event
+	
+	float spawnRange;
+};
+
+airspawn	airspawnHandaler;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* commandLine )
+{
+	bz_debugMessage(4,"airspawn plugin loaded");
+
+	float range = 0;
+	if ( commandLine )
+		range = (float)atof(commandLine);
+	if ( range < 0.001f )
+		range = 10.0f;
+	
+	airspawnHandaler.spawnRange = range;
+	bz_registerEvent(bz_eGetPlayerSpawnPosEvent,BZ_ALL_USERS,&airspawnHandaler);
+	return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+	bz_removeEvent(bz_eGetPlayerSpawnPosEvent,BZ_ALL_USERS,&airspawnHandaler);
+	bz_debugMessage(4,"airspawn plugin unloaded");
+	return 0;
+}
+
+airspawn::airspawn()
+{
+}
+
+airspawn::~airspawn()
+{
+}
+
+void airspawn::process ( bz_EventData *eventData )
+{
+	switch (eventData->eventType)
+	{
+	default:
+		// really WTF!!!!
+		break;
+
+	case bz_eGetPlayerSpawnPosEvent:
+		{
+			bz_GetPlayerSpawnPosEventData *spawn = (bz_GetPlayerSpawnPosEventData*)eventData;
+
+			float randPos = rand()/(float)RAND_MAX;
+			spawn->pos[2] += randPos * spawnRange;
+			spawn->handled = true;
+		}
+		break;
+	}
+}
+
