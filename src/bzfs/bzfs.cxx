1360ef3972cde21ca57b06076830ce373868f954
Frank Evers <pimpinella@users.sourceforge.net>
2005-07-05 18:07:46 +0000

    this fixes a server crash if an occupied slot is present why performing /countdown. Not really nice (yes, i know it) at least it works though. ~pw ;)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@10957 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -2387,9 +2387,23 @@ void removePlayer(int playerIndex, const char *reason, bool notify)
   // first shutdown connection
 
   GameKeeper::Player *playerData
-		      = GameKeeper::Player::getPlayerByIndex(playerIndex);
-  if (!playerData)
-    return;
+		      = GameKeeper::Player::getValidPlayerByIndex(playerIndex);
+  if (!playerData) {
+		playerData = GameKeeper::Player::getPlayerByIndex(playerIndex);
+		if (!playerData)
+			return;
+		else {
+			std::string timeStamp = TimeKeeper::timestamp();
+      DEBUG1("Player %s [%d] removed at %s: %s\n",
+	      playerData->player.getCallSign(),
+	      playerIndex, timeStamp.c_str(), reason);
+			DEBUG1("Might have been empty slot");
+			playerData->netHandler->closing();
+			playerData->close();
+			return;
+		}
+	}
+			
 
   // call any on part events
   bz_PlayerJoinPartEventData partEventData;
