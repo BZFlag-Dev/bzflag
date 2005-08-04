e2101079a193e202232a6debd78eeeb36815e872
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-08-04 16:55:42 +0000

    remove comments that are not needed anymore
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11200 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/bzfsAPI.h
+++ include/bzfsAPI.h
@@ -50,15 +50,15 @@ typedef enum
 	bz_ePlayerJoinEvent,
 	bz_ePlayerPartEvent,
 	bz_eChatMessageEvent,
-	bz_eUnknownSlashCommand,		// will not take a team
+	bz_eUnknownSlashCommand,
 	bz_eGetPlayerSpawnPosEvent,
-	bz_eGetAutoTeamEvent,			// will not take a team
-	bz_eAllowPlayer,				// will not take a team
-	bz_eTickEvent,					// will not take a team
-	bz_eGenerateWorldEvent,			// will not take a team
-	bz_eGetPlayerInfoEvent,			// will not take a team
+	bz_eGetAutoTeamEvent,
+	bz_eAllowPlayer,
+	bz_eTickEvent,
+	bz_eGenerateWorldEvent,
+	bz_eGetPlayerInfoEvent,
 	bz_eAllowSpawn,
-	bz_eListServerUpdateEvent		// will not take a team
+	bz_eListServerUpdateEvent
 }bz_eEventType;
 
 #define BZ_ALL_USERS	-1
