a67a9e85fbaf178c1679f8c2760d3863ab5658cb
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-08-12 22:02:53 +0000

    add /killall
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11301 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/killall/ReadMe.txt
@@ -0,0 +1,9 @@
+========================================================================
+    DYNAMIC LINK LIBRARY : killall Project Overview
+========================================================================
+
+This is the SAMPLE_PLUGIN. It makes a killall DESCRIPTION
+
+Replace everyone in every file in here the term SAMPLE_PLUGIN with the name of your project
+open then .sln and .vcproj files in a text editor and do a search and replace
+
---
+++ plugins/killall/killall.cpp
@@ -0,0 +1,54 @@
+// killall.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+
+BZ_GET_PLUGIN_VERSION
+
+
+class KillAll : public bz_CustomSlashCommandHandler
+{
+public:
+	virtual bool handle ( int playerID, bzApiString command, bzApiString message, bzAPIStringList *params )
+	{
+		bz_PlayerRecord *player = bz_getPlayerByIndex(playerID);
+		if (!player)
+			return true;
+
+		if ( !player->admin )
+		{
+			bz_sendTextMessage(BZ_SERVER,playerID,"you do not have permisions to run /killall");
+			return true;
+		}
+
+		std::string msg = player->callsign.c_str();
+		msg += " has killed everyone";
+
+		bz_sendTextMessage(BZ_SERVER,BZ_ALL_USERS,msg.c_str());
+
+		bzAPIIntList *playerList = bz_newIntList();
+
+		bz_getPlayerIndexList ( playerList );
+
+		for ( unsigned int i = 0; i < playerList->size(); i++ )
+			bz_killPlayer(playerList->get(i),false);
+
+		return true;
+	}
+};
+
+KillAll	killall;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* commandLine )
+{
+	bz_debugMessage(4,"killall plugin loaded");
+	bz_registerCustomSlashCommand ( "killall", &killall );
+	return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+	bz_removeCustomSlashCommand ( "killall" );
+	bz_debugMessage(4,"killall plugin unloaded");
+	return 0;
+}
