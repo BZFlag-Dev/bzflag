ef046ff6304dd922fc7f57f89c7fb56a8ca37dce
Jeff Myers <jeffm2501@users.sourceforge.net>
2005-08-13 19:48:21 +0000

    add script to help automate new plug-in creation ( thanks bear person)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11312 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/newplug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Rename SAMPLE_PLUGIN here
+SAMPLE_NAME=SAMPLE_PLUGIN
+#
+if [ $# -lt 1 ] ;then
+ echo "syntax: $0 <new plugin name>"
+  exit 1
+fi
+#
+# copy
+cp -a ./$SAMPLE_NAME ./$1
+rm -r $1/CVS # Don't copy junk
+#
+# replace $SAMPLE_NAME within files
+find $1 -type f -exec sed -i "s/$SAMPLE_NAME/$1/g" '{}' \;
+#
+# replace within filenames
+for file in $1/*$SAMPLE_NAME* ;do
+ mv $file ${file//$SAMPLE_NAME/$1}
+done
