90c3c54ce07b585d420a161b12eb5a7c12f79f1f
trepan <trepan@users.sourceforge.net>
2005-08-15 11:21:50 +0000

    move defineTeamWorld() and defineRandomWorld() into a new file
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11339 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzfs/WorldGenerators.h
@@ -0,0 +1,29 @@
+/* bzflag
+ * Copyright (c) 1993 - 2005 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __WORLD_GENERATORS_H__
+#define __WORLD_GENERATORS_H__
+
+class WorldInfo;
+
+extern WorldInfo *defineTeamWorld();
+extern WorldInfo *defineRandomWorld();
+
+#endif // __WORLD_GENERATORS_H__
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
