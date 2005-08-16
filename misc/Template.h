f6b806717c569ed3e7a02da4cfe08fdae2cdcf3f
trepan <trepan@users.sourceforge.net>
2005-08-16 07:24:04 +0000

    source file templates
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11371 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ misc/Template.h
@@ -0,0 +1,41 @@
+/* bzflag
+ * Copyright (c) 1993 - 2005 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+ 
+//  Template.h
+//    A short description of the module's function
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+
+#include "common.h"
+
+// system headers
+
+// common headers
+
+// local headers
+
+
+
+#endif // TEMPLATE_H
+
+
+/*
+ * Local Variables: ***
+ * mode:C++ ***
+ * tab-width: 8 ***
+ * c-basic-offset: 2 ***
+ * indent-tabs-mode: t ***
+ * End: ***
+ * ex: shiftwidth=2 tabstop=8
+ */
