807749644ab3e5e36460b9c29e1fece24656887e
Tim Riker <timriker@users.sourceforge.net>
2005-08-17 19:40:22 +0000

    utf8
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11398 08b3d480-bf2c-0410-a26f-811ee3361c24

--- tools/modeltool/Q3BSP.cxx
+++ tools/modeltool/Q3BSP.cxx
@@ -4,7 +4,7 @@ This source file is part of OGRE
 (Object-oriented Graphics Rendering Engine)
 For the latest info, see http://www.ogre3d.org/
 
-Copyright © 2000-2002 The OGRE Team
+Copyright Â© 2000-2002 The OGRE Team
 Also see acknowledgements in Readme.html
 
 This program is free software; you can redistribute it and/or modify it under
