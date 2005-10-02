b0d8d1fb6e8b85876fdc736fde4c962187c82bcc
Bryan Jennings <bryjen@users.sourceforge.net>
2005-10-02 01:09:14 +0000

    mark 2.0.5 as under DEVELopment
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11732 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -45,7 +45,7 @@
 
 // DEVEL | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"STABLE"
+#  define BZ_BUILD_TYPE		"DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993 - 2005 Tim Riker";
