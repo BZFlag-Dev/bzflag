cb38e1874c59b413571bb28401421878a013f102
trepan <trepan@users.sourceforge.net>
2005-10-04 23:59:11 +0000

    warning
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11769 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -1933,7 +1933,7 @@ void removePlayer(int playerIndex, const char *reason, bool notify)
   if (reason)
     partEventData.reason = reason;
 
-  if (partEventData.team != NoTeam)	// don't give events if we don't have a real player slot
+  if ((TeamColor)partEventData.team != NoTeam)	// don't give events if we don't have a real player slot
 	worldEventManager.callEvents(bz_ePlayerPartEvent,&partEventData);
 
   if (notify) {
