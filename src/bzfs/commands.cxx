4b1ac1692b7a5c2992be93a0b0d287c29ef93b84
Alfredo Tupone <atupone@users.sourceforge.net>
2005-10-17 06:31:10 +0000

    Don't leak password: can be bad for this server, but good for other. Don't give away to all admins.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@11848 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/commands.cxx
+++ src/bzfs/commands.cxx
@@ -900,12 +900,10 @@ bool PasswordCommand::operator() (const char	 *message,
       sendMessage(ServerPlayer, t, "You are now an administrator!");
     } else {
       sendMessage(ServerPlayer, t, "Wrong Password!");
-	  std::string temp;
-	  temp = playerData->player.getCallSign() + std::string("  has tried to become administrator with password \"");
-	  if(message + 10 != NULL)
-		temp += message + 10;
-	  temp += "\"";
-	  sendMessage(ServerPlayer, AdminPlayers, temp.c_str());
+      std::string temp;
+      temp = playerData->player.getCallSign()
+	+ std::string(" has tried to become administrator with bad password");
+      sendMessage(ServerPlayer, AdminPlayers, temp.c_str());
     }
   }
   return true;
