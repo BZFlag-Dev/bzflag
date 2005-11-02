577962f476219b4143eba9c2aae3141001c8b87f
Alfredo Tupone <atupone@users.sourceforge.net>
2005-11-02 23:14:26 +0000

    handicap server side
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@12020 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -21,7 +21,7 @@
 
 // opaque version number increments on protocol incompatibility
 #ifndef BZ_PROTO_VERSION
-#define BZ_PROTO_VERSION	"0030"
+#define BZ_PROTO_VERSION	"0031"
 #endif
 
 // version numbers - also update:
@@ -40,7 +40,7 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		2
+#  define BZ_REV		3
 #endif
 
 // DEVEL | STABLE | MAINT
