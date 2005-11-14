7a5a9d9211e357d84e958fef41900158466b6178
trepan <trepan@users.sourceforge.net>
2005-11-14 11:16:28 +0000

    hack cleansing
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12071 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/common/AutoCompleter.cxx
+++ src/common/AutoCompleter.cxx
@@ -130,8 +130,7 @@ std::string AutoCompleter::complete(const std::string& str, std::string* matches
   // FIXME: hack to allow the auto-completion to work with old /clientquery
   const char* hackCmd = "/clientquery";
   const unsigned int hackLen = strlen(hackCmd);
-  const bool hack = (head.size() > hackLen) &&
-                    (strncasecmp(head.c_str(), hackCmd, hackLen) == 0);
+  const bool hack = (strncasecmp(head.c_str(), hackCmd, hackLen) == 0);
                     
   // return the largest common prefix without any spaces
   const int minLen = first->word.size() < last->word.size() ?
