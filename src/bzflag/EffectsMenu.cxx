e0c46d0f1e9594cb0b6a6a984762baa17ff8810d
Ian Agar <lan56@users.sourceforge.net>
2005-11-15 02:37:12 +0000

    make font smaller as it would run well into the console before
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@12078 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/EffectsMenu.cxx
+++ src/bzflag/EffectsMenu.cxx
@@ -276,7 +276,7 @@ void EffectsMenu::resize(int _width, int _height)
 
   // use a big font for title, smaller font for the rest
   const float titleFontSize = (float)_height / 15.0f;
-  const float fontSize = (float)_height / 45.0f;
+  const float fontSize = (float)_height / 65.0f;
   FontManager &fm = FontManager::instance();
 
   // reposition title
