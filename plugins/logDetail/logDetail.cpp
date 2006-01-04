5dc323520b73554e90cfcb017a1c10a254a82fb3
Bernt Hansen <bthansen@users.sourceforge.net>
2006-01-04 02:51:31 +0000

    Add copyright header
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12255 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/logDetail/logDetail.cpp
+++ plugins/logDetail/logDetail.cpp
@@ -1,4 +1,16 @@
-// logDetail.cpp : Defines the entry point for the DLL application.
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+// logDetail.cpp : Plugin module for logging server events to stdout
 //
 
 #include <iostream>
