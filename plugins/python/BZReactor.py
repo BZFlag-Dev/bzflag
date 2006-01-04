7937d06d2bc99defb3c97b96776bd019572a66ef
David Trowbridge <davidtrowbridge@users.sourceforge.net>
2006-01-04 02:33:23 +0000

    assign copyright on python examples
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@12254 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/python/BZReactor.py
+++ plugins/python/BZReactor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2005-2006 David Trowbridge
+# Copyright (C) 1993-2006 Tim Riker
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
