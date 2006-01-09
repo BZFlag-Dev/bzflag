5f93ee9d901c59cc6438727258d12c222c46f6ab
Daniel Remenak <dtremenak@users.sourceforge.net>
2006-01-09 08:17:26 +0000

    more ws
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@12291 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/Permissions.cxx
+++ src/bzfs/Permissions.cxx
@@ -233,11 +233,11 @@ bool PlayerAccessInfo::hasPerm(PlayerAccessInfo::AccessPerm right) const
     return true;
 
   bool isAllowed = false;
-  for (std::vector<std::string>::const_iterator itr=groups.begin(); itr!=groups.end(); ++itr) {
+  for (std::vector<std::string>::const_iterator itr = groups.begin(); itr != groups.end(); ++itr) {
     PlayerAccessMap::iterator group = groupAccess.find(*itr);
-    if (group != groupAccess.end()){
+    if (group != groupAccess.end()) {
       if (group->second.explicitDenys.test(right))
-      return false;
+	return false;
       else if (group->second.explicitAllows.test(right))
 	isAllowed = true;
     }
