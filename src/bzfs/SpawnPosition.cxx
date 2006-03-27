62aa815aa36b52a59e0092ce99fdbfd46de4d746
Sean Morrison <brlcad@users.sourceforge.net>
2006-03-27 22:53:36 +0000

    blessed radians, it is a 20 degree arc
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12633 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/SpawnPosition.cxx
+++ src/bzfs/SpawnPosition.cxx
@@ -186,7 +186,7 @@ const bool SpawnPosition::isFacing(const float *enemyPos, const float enemyAzimu
 const bool SpawnPosition::isImminentlyDangerous() const
 {
   GameKeeper::Player *playerData;
-  float twentyDegrees = (float)(M_PI / 9.0); /* +- 20 degrees, i.e. 40 degree arc */
+  float twentyDegrees = (float)(M_PI / 9.0); /* +- 10 degrees, i.e. 20 degree arc */
   for (int i = 0; i < curMaxPlayers; i++) {
     playerData = GameKeeper::Player::getPlayerByIndex(i);
     if (!playerData)
