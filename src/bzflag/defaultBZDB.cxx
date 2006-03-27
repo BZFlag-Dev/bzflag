d5477b37808621321e9c99efcbd7f9aeae31c29e
Sean Morrison <brlcad@users.sourceforge.net>
2006-03-27 23:41:36 +0000

    add hideEmails to the default list of settable variables, ScoreboardRenderer already uses it but required manual addition to config file.  default to false (i.e. display e-mails)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12636 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/defaultBZDB.cxx
+++ src/bzflag/defaultBZDB.cxx
@@ -162,7 +162,8 @@ DefaultDBItem	defaultDBItems[] = {
   { "httpTimeout",		"15",			true,	StateDatabase::ReadWrite,	NULL },
 
   // hud drawing
-  { "hudGUIBorderOpacityFactor","0.75",			true,	StateDatabase::ReadWrite,	NULL }
+  { "hudGUIBorderOpacityFactor","0.75",			true,	StateDatabase::ReadWrite,	NULL },
+  { "hideEmails",		"0",			true,	StateDatabase::ReadWrite,	NULL }
 };
 
 
