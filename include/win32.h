4f3c88bc9232be43a873a682a2075f78f22390fb
Sean Morrison <brlcad@users.sourceforge.net>
2006-03-27 02:03:31 +0000

    commit from v2_0branch, provide a std::isnan
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@12608 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/win32.h
+++ include/win32.h
@@ -72,6 +72,11 @@ typedef unsigned int	uint32_t;
 
 #  define PATH_MAX	MAX_PATH
 
+  namespace std {
+    template<typename _Tp>
+    int isnan(_Tp __f) { return _isnan((double)__f); }
+  }
+
 #endif // _MSC_VER
 #endif // __WIN32_H__
 
