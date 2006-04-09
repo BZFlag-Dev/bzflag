5a11e788886583252fe6cbb6d6e4646c12147065
Jeff Myers <jeffm2501@users.sourceforge.net>
2006-04-09 20:27:13 +0000

    set version to 2.0.6 release candidate while instealler and build testing happens.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12743 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -40,12 +40,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		5
+#  define BZ_REV		6
 #endif
 
 // DEVEL | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"DEVEL"
+#  define BZ_BUILD_TYPE		"RC1"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993 - 2006 Tim Riker";
