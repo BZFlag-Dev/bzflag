d8b0adf9b4234d466e7f676ca68cdf98e433105a
Tim Riker <timriker@users.sourceforge.net>
2006-04-17 20:11:11 +0000

    kfreebsd
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@12837 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/network.h
+++ include/network.h
@@ -64,7 +64,7 @@
 #  define AddrLen		unsigned int
 /* setsockopt incorrectly prototypes the 4th arg without const. */
 #  define SSOType		void*
-#elif defined(BSD) || defined(sun)
+#elif defined(BSD) || defined(sun) || defined(__GLIBC__)
 #  define AddrLen		socklen_t
 #elif defined (__APPLE__)
 #  include <AvailabilityMacros.h>
