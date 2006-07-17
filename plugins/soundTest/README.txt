6c5070260078edd497ad1bf56b34e7b506008471
Jeff Myers <jeffm2501@users.sourceforge.net>
2006-07-17 03:00:43 +0000

    implement /sound as a plugin to test the API call
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13063 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/soundTest/README.txt
@@ -0,0 +1,11 @@
+========================================================================
+    DYNAMIC LINK LIBRARY : soundTest Project Overview
+========================================================================
+
+This is the soundTest. It makes a soundTest_DESCRIPTION
+
+Replace every occurance of the term soundTest in every file in here
+with the name of your project.  Also open .sln and .vcproj files in a
+text editor and do a search and replace.  Then write a short readme with
+usage instructions or other details in this file.
+
---
+++ plugins/soundTest/soundTest.cpp
@@ -0,0 +1,57 @@
+// soundTest.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+
+class SoundCommand : public bz_CustomSlashCommandHandler
+{
+public:
+	virtual ~SoundCommand(){};
+	virtual bool handle ( int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList *params );
+};
+
+SoundCommand soundCommand;
+
+BZ_GET_PLUGIN_VERSION
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"soundTest plugin loaded");
+  bz_registerCustomSlashCommand("sound",&soundCommand);
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_debugMessage(4,"soundTest plugin unloaded");
+  bz_removeCustomSlashCommand("sound");
+  return 0;
+}
+
+bool SoundCommand::handle ( int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList *params )
+{
+	if (!bz_hasPerm(playerID,bz_perm_setAll))
+	{
+		bz_sendTextMessage(BZ_SERVER,playerID,"You do not have permission to play sounds (SETALL)");
+		return true;
+	}
+
+	if (message.size() <= 0) 
+	{
+		bz_sendTextMessage(BZ_SERVER, playerID, "Missing sound file name");
+		return true;
+	}
+
+	bz_sendPlayCustomLocalSound(playerID,message.c_str());
+	return true;
+}
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
