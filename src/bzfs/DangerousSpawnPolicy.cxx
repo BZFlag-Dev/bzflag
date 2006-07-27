0cb5d5e328ddaed213ae2933f3d0051d6b071f57
Sean Morrison <brlcad@users.sourceforge.net>
2006-07-27 23:34:40 +0000

    initial implementation of a new 'dangerous' spawn policy that attempts to drop players into some sort of imminently dangerous position or at least close to some enemy while otherwise behaving like the default spawn policy in terms of placement
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13170 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzfs/DangerousSpawnPolicy.cxx
@@ -0,0 +1,162 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* interface header */
+#include "DangerousSpawnPolicy.h"
+
+/* common headers */
+#include "GameKeeper.h"
+#include "TimeKeeper.h"
+#include "PlayerInfo.h"
+#include "StateDatabase.h"
+#include "BZDBCache.h"
+
+/* server headers */
+#include "bzfs.h"
+#include "DropGeometry.h"
+
+
+DangerousSpawnPolicy::DangerousSpawnPolicy()
+{
+}
+
+DangerousSpawnPolicy::~DangerousSpawnPolicy()
+{
+}
+
+void DangerousSpawnPolicy::getPosition(float pos[3], int playerId, bool onGroundOnly, bool notNearEdges)
+{
+  /* the player is coming to life, depending on who they are an what
+   * style map/configuration is being played determines how they will
+   * spawn.
+   */
+
+  GameKeeper::Player *playerData
+    = GameKeeper::Player::getPlayerByIndex(playerId);
+  if (!playerData)
+    return;
+
+  const PlayerInfo& pi = playerData->player;
+  TeamColor t = pi.getTeam();
+
+  if (!BZDB.isTrue("freeCtfSpawns") &&
+      playerData->player.shouldRestartAtBase() &&
+      (t >= RedTeam) && (t <= PurpleTeam) &&
+      (bases.find(t) != bases.end())) {
+
+    /* if the player needs to spawn on a base, select a random
+     * position on one of their team's available bases.
+     */
+
+    TeamBases &teamBases = bases[t];
+    const TeamBase &base = teamBases.getRandomBase((int)(bzfrand() * 100));
+    base.getRandomPosition(pos[0], pos[1], pos[2]);
+    playerData->player.setRestartOnBase(false);
+
+  } else {
+    /* *** "dangerous" spawn position selection occurs below here. ***
+     *
+     * Basic idea is to try to pick a dangerous spawn location,
+     * hopefully one that is imminently dangerous.  if not, hopefuly
+     * one that is just close to an enemy.  otherwise, just toss them
+     * in.
+     */
+
+    const float tankRadius = BZDBCache::tankRadius;
+    const float size = BZDBCache::worldSize;
+    const float maxHeight = world->getMaxWorldHeight();
+
+    // keep track of how much time we spend searching for a location
+    TimeKeeper start = TimeKeeper::getCurrent();
+
+    int tries = 0;
+    float minProximity = size / BZDB.eval("_spawnSafeSRMod");
+    float bestDist = -1.0f;
+    bool foundspot = false;
+    while (!foundspot) {
+      if (!world->getPlayerSpawnPoint(&pi, testPos)) {
+	if (notNearEdges) {
+	  // don't spawn close to map edges in CTF mode
+	  testPos[0] = ((float)bzfrand() - 0.5f) * size * 0.5f;
+	  testPos[1] = ((float)bzfrand() - 0.5f) * size * 0.5f;
+	} else {
+	  testPos[0] = ((float)bzfrand() - 0.5f) * (size - 2.0f * tankRadius);
+	  testPos[1] = ((float)bzfrand() - 0.5f) * (size - 2.0f * tankRadius);
+	}
+	testPos[2] = onGroundOnly ? 0.0f : ((float)bzfrand() * maxHeight);
+      }
+      tries++;
+
+      const float waterLevel = world->getWaterLevel();
+      float minZ = 0.0f;
+      if (waterLevel > minZ) {
+	minZ = waterLevel;
+      }
+      float maxZ = maxHeight;
+      if (onGroundOnly) {
+	maxZ = 0.0f;
+      }
+
+      if (DropGeometry::dropPlayer(testPos, minZ, maxZ)) {
+	foundspot = true;
+      }
+
+      // check every now and then if we have already used up 10ms of time
+      if (tries >= 50) {
+	tries = 0;
+	if (TimeKeeper::getCurrent() - start > BZDB.eval("_spawnMaxCompTime")) {
+	  if (bestDist < 0.0f) { // haven't found a single spot
+	    //Just drop the sucka in, and pray
+	    pos[0] = testPos[0];
+	    pos[1] = testPos[1];
+	    pos[2] = maxHeight;
+	    DEBUG1("Warning: DangerousSpawnPolicy ran out of time, just dropping the sucker in\n");
+	  }
+	  break;
+	}
+      }
+
+      // check if spot is dangerous enough
+      bool dangerous = isImminentlyDangerous(testPos);
+      if (foundspot && !dangerous) {
+	float enemyAngle;
+	float dist = enemyProximityCheck(enemyAngle);
+	if (dist < bestDist) { // best so far
+	  bestDist = dist;
+	  pos[0] = testPos[0];
+	  pos[1] = testPos[1];
+	  pos[2] = testPos[2];
+	}
+	if (bestDist < minProximity) { // close enough, stop looking
+	  foundspot = true;
+	}
+	minProximity *= 1.01f; // relax requirements a little
+      } else if (dangerous) {
+	foundspot = true;
+      }
+    }
+  }
+}
+
+void DangerousSpawnPolicy::getAzimuth(float &azimuth)
+{
+  azimuth = (float)(bzfrand() * 2.0 * M_PI);
+}
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/bzfs/DangerousSpawnPolicy.h
@@ -0,0 +1,53 @@
+/* bzflag
+ * Copyright (c) 1993 - 2006 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef __DANGEROUSSPAWNPOLICY_H__
+#define __DANGEROUSSPAWNPOLICY_H__
+
+#include "common.h"
+
+/* common interface headers */
+#include "DefaultSpawnPolicy.h"
+
+
+/** a DangerousSpawnPolicy is a SpawnPolicy that just generates a mostly
+ *  dangerous (yet hopefully still valid) position and drops you there.
+ */
+class DangerousSpawnPolicy : public DefaultSpawnPolicy 
+{
+
+public:
+  DangerousSpawnPolicy();
+  virtual ~DangerousSpawnPolicy();
+
+  virtual const char *Name() {
+    return "Dangerous";
+  }
+
+  virtual void getPosition(float pos[3], int playerId, bool onGroundOnly, bool notNearEdges);
+  virtual void getAzimuth(float &azimuth);
+
+private:
+  /* internal use */
+  TeamColor   team;
+  float       testPos[3];
+};
+
+#endif  /*__DANGEROUSSPAWNPOLICY_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
