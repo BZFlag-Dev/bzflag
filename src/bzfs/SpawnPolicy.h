c73368b975050e0f2bd72b20d3fc6f73ddab2b41
Sean Morrison <brlcad@users.sourceforge.net>
2006-07-27 20:51:53 +0000

    refactor SpawnPolicy into an abstract base class where the implementation must provide a name, new spawn positions, and spawn directions/azimuth.  keep the wonderfully named isImminentlyDangerous() as well as isFacing() and distanceFrom() funcs as protected but rework them to function stateless by passing in a spawn candidate position
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13157 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/SpawnPolicy.h
+++ src/bzfs/SpawnPolicy.h
@@ -15,9 +15,6 @@
 
 #include "common.h"
 
-/* common interface headers */
-#include "global.h"  /* for TeamColor */
-
 
 /** a SpawnPolicy is used to determine a new SpawnPosition.  Policies
  *  are defined to describe various spawning behaviors such as purely
@@ -32,24 +29,16 @@ public:
   SpawnPolicy();
   virtual ~SpawnPolicy();
 
-  virtual void getPosition(float pos[3], int playerId, bool onGroundOnly, bool notNearEdges);
-  virtual void getAzimuth(float &azimuth);
+  virtual const char *Name() = 0;
+
+  virtual void getPosition(float pos[3], int playerId, bool onGroundOnly, bool notNearEdges) = 0;
+  virtual void getAzimuth(float &azimuth) = 0;
   
 protected:
-  virtual bool isImminentlyDangerous() const;
+  virtual bool isFacing(const float *selfPos, const float *enemyPos, const float enemyAzimuth, const float deviation) const;
+  virtual float distanceFrom(const float *pos, const float *farPos) const;
+  virtual bool isImminentlyDangerous(const float *selfPos) const;
 
-private:  
-  float enemyProximityCheck(float &enemyAngle) const;
-  float distanceFrom(const float *farPos) const;
-  bool  isFacing(const float *enemyPos, const float enemyAzimuth, const float deviation) const;
-
-  /* temp, internal use */
-  TeamColor   team;
-  float       testPos[3];
-  
-  float safeSWRadius;
-  float safeSRRadius;
-  float safeDistance;
 };
 
 #endif  /*__SPAWNPOLICY_H__ */
