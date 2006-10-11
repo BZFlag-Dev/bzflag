eb207a45b965eef722610acc77cd3a5e813b2aaa
Thomas Stauer <t-s@users.sourceforge.net>
2006-10-11 08:39:04 +0000

    Do not put commas at the end of enums
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13313 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/global.h
+++ include/global.h
@@ -92,7 +92,7 @@ enum GameOptions {
   ShakableGameStyle =	 0x0040, // can drop bad flags
   AntidoteGameStyle =	 0x0080, // anti-bad flags
   HandicapGameStyle =	 0x0100, // handicap players based on score (eek! was TimeSyncGameStyle)
-  FreezeTagGameStyle =   0x0200, // collisions freeze player farther from base
+  FreezeTagGameStyle =   0x0200 // collisions freeze player farther from base
   // add here before reusing old ones above
 };
 
