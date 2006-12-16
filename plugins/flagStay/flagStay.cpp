ba6606c7078c3ad4cab52856efdf29728bcbf1ec
Jeff Myers <jeffm2501@users.sourceforge.net>
2006-12-16 20:23:42 +0000

    update FlagStay pluing for the new API in HEAD
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13495 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/flagStay/flagStay.cpp
@@ -0,0 +1,233 @@
+// flagStay.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+#include <string>
+#include <vector>
+#include <map>
+#include <math.h>
+
+BZ_GET_PLUGIN_VERSION
+
+class FlagStayZoneHandler : public bz_CustomMapObjectHandler
+{
+public:
+	virtual bool handle ( bz_ApiString object, bz_CustomMapObjectInfo *data );
+};
+
+FlagStayZoneHandler	flagStayZoneHandler;
+
+class EventHandler : public bz_EventHandler
+{
+public:
+	virtual void process ( bz_EventData *eventData );
+};
+
+EventHandler eventHandler;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"flagStay plugin loaded");
+  bz_registerCustomMapObject("FLAGSTAYZONE",&flagStayZoneHandler);
+
+  bz_registerEvent(bz_ePlayerUpdateEvent,&eventHandler);
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_removeEvent(bz_ePlayerUpdateEvent,&eventHandler);
+  bz_removeCustomMapObject("FLAGSTAYZONE");
+  bz_debugMessage(4,"flagStay plugin unloaded");
+  return 0;
+}
+
+
+class FlagStayZone
+{
+public:
+	FlagStayZone()
+	{
+		box = false;
+		xMax = xMin = yMax = yMin = zMax = zMin = rad = 0;
+	}
+
+	bool box;
+	float xMax,xMin,yMax,yMin,zMax,zMin;
+	float rad;
+
+	std::string message;
+	bool pointIn ( float pos[3] )
+	{
+		if ( box )
+		{
+			if ( pos[0] > xMax || pos[0] < xMin )
+				return false;
+
+			if ( pos[1] > yMax || pos[1] < yMin )
+				return false;
+
+			if ( pos[2] > zMax || pos[2] < zMin )
+				return false;
+		}
+		else
+		{
+			float vec[3];
+			vec[0] = pos[0]-xMax;
+			vec[1] = pos[1]-yMax;
+			vec[2] = pos[2]-zMax;
+
+			float dist = sqrt(vec[0]*vec[0]+vec[1]*vec[1]);
+			if ( dist > rad)
+				return false;
+
+			if ( pos[2] > zMax || pos[2] < zMin )
+				return false;
+
+		}
+		return true;
+	}
+
+	bool checkFlag ( const char* flag )
+	{
+		for ( unsigned int i = 0; i < flagList.size(); i++ )
+		{
+			if ( flagList[i] == flag )
+				return true;
+		}
+		return false;
+	}
+
+	std::vector<std::string> flagList;
+};
+
+std::vector <FlagStayZone> zoneList;
+
+bool FlagStayZoneHandler::handle ( bz_ApiString object, bz_CustomMapObjectInfo *data )
+{
+	if (object != "FLAGSTAYZONE" || !data)
+		return false;
+
+	FlagStayZone newZone;
+
+	// parse all the chunks
+	for ( unsigned int i = 0; i < data->data.size(); i++ )
+	{
+		std::string line = data->data.get(i).c_str();
+
+		bz_APIStringList *nubs = bz_newStringList();
+		nubs->tokenize(line.c_str()," ",0,true);
+		
+		if ( nubs->size() > 0)
+		{
+			std::string key = bz_toupper(nubs->get(0).c_str());
+
+			if ( key == "BBOX" && nubs->size() > 6)
+			{
+				newZone.box = true;
+				newZone.xMin = (float)atof(nubs->get(1).c_str());
+				newZone.xMax = (float)atof(nubs->get(2).c_str());
+				newZone.yMin = (float)atof(nubs->get(3).c_str());
+				newZone.yMax = (float)atof(nubs->get(4).c_str());
+				newZone.zMin = (float)atof(nubs->get(5).c_str());
+				newZone.zMax = (float)atof(nubs->get(6).c_str());
+			}
+			else if ( key == "CYLINDER" && nubs->size() > 5)
+			{
+				newZone.box = false;
+				newZone.rad = (float)atof(nubs->get(5).c_str());
+				newZone.xMax =(float)atof(nubs->get(1).c_str());
+				newZone.yMax =(float)atof(nubs->get(2).c_str());
+				newZone.zMin =(float)atof(nubs->get(3).c_str());
+				newZone.zMax =(float)atof(nubs->get(4).c_str());
+			}
+			else if ( key == "FLAG" && nubs->size() > 1)
+			{
+				std::string flag = nubs->get(1).c_str();
+				newZone.flagList.push_back(flag);
+			}
+			else if ( key == "MESSAGE" && nubs->size() > 1 )
+			{
+				newZone.message = nubs->get(1).c_str();
+			}
+		}
+		bz_deleteStringList(nubs);
+	}
+	zoneList.push_back(newZone);
+	return true;
+}
+
+std::map<int,int>	playeIDToZoneMap;
+
+void EventHandler::process ( bz_EventData *eventData )
+{
+	float pos[3] = {0};
+
+	int playerID = -1;
+
+	switch (eventData->eventType)
+	{
+	case bz_ePlayerUpdateEvent:
+		pos[0] = ((bz_PlayerUpdateEventData_V1*)eventData)->pos[0];
+		pos[1] = ((bz_PlayerUpdateEventData_V1*)eventData)->pos[1];
+		pos[2] = ((bz_PlayerUpdateEventData_V1*)eventData)->pos[2];
+		playerID = ((bz_PlayerUpdateEventData_V1*)eventData)->player;
+		break;
+
+	case bz_eShotFiredEvent:
+		pos[0] = ((bz_ShotFiredEventData_V1*)eventData)->pos[0];
+		pos[1] = ((bz_ShotFiredEventData_V1*)eventData)->pos[1];
+		pos[2] = ((bz_ShotFiredEventData_V1*)eventData)->pos[2];
+		playerID = ((bz_ShotFiredEventData_V1*)eventData)->player;
+		break;
+
+	default:
+		return;
+	}
+
+	const char* flagAbrev = bz_getPlayerFlag(playerID);
+	if (!flagAbrev)
+		return;
+
+	std::vector<FlagStayZone*> validZones;
+
+	// check and see if a zone cares about the current flag
+	for ( unsigned int i = 0; i < zoneList.size(); i++ )
+	{
+		if ( zoneList[i].checkFlag(flagAbrev) )
+			validZones.push_back(&zoneList[i]);
+	}
+	
+        // Check each zone for this flag to see if we are in one
+	bool insideOne = false;
+	for ( unsigned int i = 0; i < validZones.size(); i++ )
+	{
+		if ( validZones[i]->pointIn(pos) )
+		{
+			insideOne = true;
+			playeIDToZoneMap[playerID] = i;
+		}
+	}
+
+        // if they have taken the flag out of a zone, pop it.
+	if (!insideOne && validZones.size() > 0)
+	{
+		int lastZone = -1;
+		if ( playeIDToZoneMap.find(playerID) != playeIDToZoneMap.end() )
+			lastZone = playeIDToZoneMap[playerID];
+		bz_removePlayerFlag(playerID);
+		if (lastZone != -1 && zoneList[lastZone].message.size())
+			bz_sendTextMessage(BZ_SERVER,playerID,zoneList[lastZone].message.c_str());
+	}
+}
+
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
