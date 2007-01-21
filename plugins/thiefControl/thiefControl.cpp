f37f976d7239b100021720890c57faf592b0456f
Bernt Hansen <bthansen@users.sourceforge.net>
2007-01-21 16:06:47 +0000

    Add theifControl plugin
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13576 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/thiefControl/thiefControl.cpp
@@ -0,0 +1,62 @@
+// thiefControl.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+
+BZ_GET_PLUGIN_VERSION
+
+class thiefControl : public bz_EventHandler
+{
+public:
+  thiefControl() {} ;
+  virtual ~thiefControl() {};
+  virtual void process( bz_EventData *eventData );
+};
+
+thiefControl thiefHandler;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"thiefControl plugin loaded");
+  bz_registerEvent(bz_eFlagTransferredEvent, &thiefHandler);
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_removeEvent(bz_eFlagTransferredEvent, &thiefHandler);
+  bz_debugMessage(4,"thiefControl plugin unloaded");
+  return 0;
+}
+
+void thiefControl::process( bz_EventData *eventData )
+{
+  bz_FlagTransferredEventData_V1 *data = (bz_FlagTransferredEventData_V1 *) eventData;
+  bz_BasePlayerRecord *playerFrom = bz_getPlayerByIndex(data->fromPlayerID);
+  bz_BasePlayerRecord *playerTo = bz_getPlayerByIndex(data->toPlayerID);
+
+  if (eventData) {
+    switch (eventData->eventType) {
+    case bz_eFlagTransferredEvent:
+      
+      if (playerFrom && playerTo) {
+	if ((playerTo->team != eRogueTeam || bz_getGameType() == eRabbitGame) && playerFrom->team == playerTo->team) {
+	  data->action = data->DropThief;
+	  bz_sendTextMessage(BZ_SERVER, data->toPlayerID, "Flag dropped. Don't steal from teammates!");
+	}
+      }
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
