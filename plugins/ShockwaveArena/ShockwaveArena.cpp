305f94d7f113c997d95e1b0b8c88b4631c2985f7
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-01-27 02:34:31 +0000

    Shockwaves all the time
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13590 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/ShockwaveArena/ShockwaveArena.cpp
@@ -0,0 +1,46 @@
+// ShockwaveArena.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+
+BZ_GET_PLUGIN_VERSION
+
+class Handler : public bz_EventHandler
+{
+public:
+	virtual void process ( bz_EventData *eventData )
+	{
+		if (eventData->eventType == bz_eFlagGrabbedEvent)
+		{
+			bz_FlagGrabbedEventData_V1 *data = (bz_FlagGrabbedEventData_V1*)eventData;
+			data->shotType = eShockWaveShot;
+		}
+
+		if (eventData->eventType == bz_ePlayerSpawnEvent)
+		{
+			bz_PlayerSpawnEventData_V1 *data = (bz_PlayerSpawnEventData_V1*)eventData;
+			bz_setPlayerShotType(data->playerID,eShockWaveShot);
+		}
+	}
+};
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"ShockwaveArena plugin loaded");
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_debugMessage(4,"ShockwaveArena plugin unloaded");
+  return 0;
+}
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
