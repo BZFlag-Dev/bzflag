5f53b3a8fe8fbf6cc1f4b6ef37f1f88320b8fd1d
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-01-27 18:59:32 +0000

    remove ShockwaveArena, replace it with weaponArena. weaponArena lets you specify the weapon, or get a random one by default.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13603 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/weaponArena/README.txt
+++ plugins/weaponArena/README.txt
@@ -1,10 +1,10 @@
 ========================================================================
-    DYNAMIC LINK LIBRARY : ShockwaveArena Project Overview
+    DYNAMIC LINK LIBRARY : weaponArena Project Overview
 ========================================================================
 
-This is the ShockwaveArena. It makes a ShockwaveArena_DESCRIPTION
+This is the weaponArena. It makes a weaponArena_DESCRIPTION
 
-Replace every occurance of the term ShockwaveArena in every file in here
+Replace every occurance of the term weaponArena in every file in here
 with the name of your project.  Also open .sln and .vcproj files in a
 text editor and do a search and replace.  Then write a short readme with
 usage instructions or other details in this file.
--- plugins/weaponArena/weaponArena.cpp
+++ plugins/weaponArena/weaponArena.cpp
@@ -1,44 +1,74 @@
-// ShockwaveArena.cpp : Defines the entry point for the DLL application.
+// weaponArena.cpp : Defines the entry point for the DLL application.
 //
 
 #include "bzfsAPI.h"
 
 BZ_GET_PLUGIN_VERSION
 
+bool randomShots = false;
+bz_eShotType shotType = eNoShot;
+
 class Handler : public bz_EventHandler
 {
 public:
 	virtual void process ( bz_EventData *eventData )
 	{
+		if (randomShots)
+			shotType = (bz_eShotType)((rand()%eLastShotType-1)+1);
+
+		if (shotType == eNoShot)
+			return;
+
 		if (eventData->eventType == bz_eFlagGrabbedEvent)
 		{
 			bz_FlagGrabbedEventData_V1 *data = (bz_FlagGrabbedEventData_V1*)eventData;
-			data->shotType = eShockWaveShot;
+			data->shotType = shotType;
 		}
 
 		if (eventData->eventType == bz_ePlayerSpawnEvent)
 		{
 			bz_PlayerSpawnEventData_V1 *data = (bz_PlayerSpawnEventData_V1*)eventData;
-			bz_setPlayerShotType(data->playerID,eShockWaveShot);
+			bz_setPlayerShotType(data->playerID,shotType);
 		}
 
 		if (eventData->eventType == bz_eFlagDroppedEvent)
 		{
 			bz_FlagDroppedEvenData_V1 *data = (bz_FlagDroppedEvenData_V1*)eventData;
-			bz_setPlayerShotType(data->playerID,eShockWaveShot);
+			bz_setPlayerShotType(data->playerID,shotType);
 		}
-		
 	}
 };
 
 Handler handler;
 
-BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+BZF_PLUGIN_CALL int bz_Load ( const char* commandLine )
 {
 	bz_registerEvent(bz_eFlagGrabbedEvent,&handler);
 	bz_registerEvent(bz_ePlayerSpawnEvent,&handler);
 	bz_registerEvent(bz_eFlagDroppedEvent,&handler);
-	bz_debugMessage(4,"ShockwaveArena plugin loaded");
+	bz_debugMessage(4,"weaponArena plugin loaded");
+
+	randomShots = true;
+	if (commandLine && strlen(commandLine))
+	{
+		shotType = eNoShot;
+		std::string flag = commandLine;
+		if ( flag == "SW" )
+			shotType = eShockWaveShot;
+		else if ( flag == "LZ")
+			shotType = eLaserShot;
+		else if ( flag == "GM")
+			shotType = eGMShot;
+		else if ( flag == "SB")
+			shotType = eSuperShot;
+		else if ( flag == "IB")
+			shotType = eInvisibleShot;
+		else if ( flag == "SS")
+			shotType = eStandardShot;
+
+		randomShots = false;
+	}
+
   return 0;
 }
 
@@ -47,7 +77,7 @@ BZF_PLUGIN_CALL int bz_Unload ( void )
 	bz_removeEvent(bz_eFlagGrabbedEvent,&handler);
 	bz_removeEvent(bz_ePlayerSpawnEvent,&handler);
 	bz_removeEvent(bz_eFlagDroppedEvent,&handler);
-	bz_debugMessage(4,"ShockwaveArena plugin unloaded");
+	bz_debugMessage(4,"weaponArena plugin unloaded");
   return 0;
 }
 
