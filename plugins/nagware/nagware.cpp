222434e9ba65da15dbfeb2552150201004633a17
Bernt Hansen <bthansen@users.sourceforge.net>
2007-02-12 22:15:13 +0000

    Typo - ints don't sing.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13693 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/nagware/nagware.cpp
+++ plugins/nagware/nagware.cpp
@@ -120,7 +120,7 @@ void sendNagMessage (int who, std::string *msg ){
   std::string fullMsg = *msg + Config.msgSuffix;
   unsigned int idx=0, x;
 
-  while ((x = (unsinged int)fullMsg.find("\\n", idx)) != std::string::npos){
+  while ((x = (unsigned int)fullMsg.find("\\n", idx)) != std::string::npos){
     bz_sendTextMessage(BZ_SERVER, who, fullMsg.substr(idx, x-idx).c_str());
     idx = x+2;
   }
