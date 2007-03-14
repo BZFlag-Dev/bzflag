11458917782cb2540979f8cec010ba0f669b721c
Bernt Hansen <bthansen@users.sourceforge.net>
2007-03-14 21:22:18 +0000

    Make section and key names ignore case Add some usage documentation to the header file
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13842 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/PluginConfig.h
+++ plugins/plugin_utils/PluginConfig.h
@@ -19,6 +19,28 @@ using namespace std;
 
 /*
  * PluginConfig - INI style configuration file parser class
+ *
+ * Reads a configuration file like this:
+ *
+ * -- myconfig.txt --
+ * [Section]
+ *    SomeKey = Some value goes here
+ *    key2    =    Some other value
+ * [ AnotherSection ]
+ *    Key3 = 4
+ * ------------------
+ * Then you parse it as follows:
+ *  config = PluginConfig("myconfig.txt");
+ *  config.item("Section", "SomeKey") returns "Some value goes here"
+ *  config.item("Section", "key2")    returns "Some other value"
+ *  config.item("anotherSection", "Key3") returns "4"
+ *  config.item("missingSection", "Anything") returns ""
+ *
+ * Section and Key strings are not case sensitive but value strings are.
+ * Sections, keys, and values have leading and trailing whitespace stripped.
+ * Nonexistent keys return the empty string
+ *
+ * Debug level 4 (-dddd) provides information about the parsing process
  */
 
 class PluginConfig {
