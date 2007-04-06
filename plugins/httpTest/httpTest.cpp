584f9551f7ad7eae4a58b720b9204a397a35c791
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-04-06 02:23:24 +0000

    only grab the connection if it's a HTTP get.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13989 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/httpTest/httpTest.cpp
+++ plugins/httpTest/httpTest.cpp
@@ -10,48 +10,48 @@ BZ_GET_PLUGIN_VERSION
 class HTTPHandler : public bz_NonPlayerConnectionHandler
 {
 public:
-	virtual void pending ( int connectionID, void *data, unsigned int size )
+  virtual void pending ( int connectionID, void *data, unsigned int size )
+  {
+    std::string oldData;
+
+    if ( pendingData.find(connectionID) != pendingData.end() )
+      oldData = pendingData[connectionID];
+
+    char *str = (char*)malloc(size+1);
+    memcpy(str,data,size);
+    str[size] = 0;
+
+    oldData += str;
+    free(str);
+
+    pendingData[connectionID] = oldData;
+
+    if ( strstr(oldData.c_str(),"\r\n\r\n") )
+    {
+      // it's done, lets parse it
+
+      std::vector<std::string> items = tokenize(oldData,std::string ("\r\n"),0,false);
+      for (unsigned int i = 0; i < items.size(); i++ )
+      {
+	std::string item = items[i];
+
+	std::vector<std::string> chunks = tokenize(item,std::string(" "),0,true);
+
+	if (chunks.size() && tolower(chunks[0]) == "get")
 	{
-		std::string oldData;
-
-		if ( pendingData.find(connectionID) != pendingData.end() )
-			oldData = pendingData[connectionID];
-
-		char *str = (char*)malloc(size+1);
-		memcpy(str,data,size);
-		str[size] = 0;
-
-		oldData += str;
-		free(str);
-
-		pendingData[connectionID] = oldData;
-
-		if ( strstr(oldData.c_str(),"\r\n\r\n") )
-		{
-			// it's done, lets parse it
-
-			std::vector<std::string> items = tokenize(oldData,std::string ("\r\n"),0,false);
-			for (unsigned int i = 0; i < items.size(); i++ )
-			{
-				std::string item = items[i];
-
-				std::vector<std::string> chunks = tokenize(item,std::string(" "),0,true);
-
-				if (chunks.size() && tolower(chunks[0]) == "get")
-				{
-					std::string returnPage = "This Is data from HTTP via BZFS\r\n\r\n";
-					bz_sendNonPlayerData(connectionID,returnPage.c_str(),(unsigned int)returnPage.size());
-				}
-			}
-			// we are done with it
-			bz_removeNonPlayerConnectionHandler(connectionID,this);
-			//bz_disconectNonPlayerConnection(connectionID);
-
-			pendingData.erase(pendingData.find(connectionID));
-		}
+	  std::string returnPage = "This Is data from HTTP via BZFS\r\n\r\n";
+	  bz_sendNonPlayerData(connectionID,returnPage.c_str(),(unsigned int)returnPage.size());
 	}
+      }
+      // we are done with it
+      bz_removeNonPlayerConnectionHandler(connectionID,this);
+      //bz_disconectNonPlayerConnection(connectionID);
 
-	std::map<int,std::string>	pendingData;
+      pendingData.erase(pendingData.find(connectionID));
+    }
+  }
+
+  std::map<int,std::string>	pendingData;
 };
 
 HTTPHandler http;
@@ -59,13 +59,16 @@ HTTPHandler http;
 class NewConnectionHandler : public bz_EventHandler
 {
 public:
-	virtual void process ( bz_EventData *eventData )
-	{
-		bz_NewNonPlayerConnectionEventData_V1 *data = (bz_NewNonPlayerConnectionEventData_V1*)eventData;
+  virtual void process ( bz_EventData *eventData )
+  {
+    bz_NewNonPlayerConnectionEventData_V1 *data = (bz_NewNonPlayerConnectionEventData_V1*)eventData;
 
-		bz_registerNonPlayerConnectionHandler(data->connectionID,&http);
-		http.pending(data->connectionID,data->data,data->size);
-	}
+    if ( data->size > 3 && strncmp((char*)data->data,"GET",3) == 0 )
+    {
+      bz_registerNonPlayerConnectionHandler(data->connectionID,&http);
+      http.pending(data->connectionID,data->data,data->size);
+    }
+  }
 };
 
 NewConnectionHandler newConnect; 
