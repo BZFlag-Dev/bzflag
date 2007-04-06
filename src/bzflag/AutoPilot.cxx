8b9ddf7d11b39e623119217912015e0376aaa994
Daniel Remenak <dtremenak@users.sourceforge.net>
2007-04-06 19:08:16 +0000

    add standard indentation block for emacs and vi
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@13994 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/AutoPilot.cxx
+++ src/bzflag/AutoPilot.cxx
@@ -771,3 +771,11 @@ void	doAutoPilot(float &rotation, float &speed)
 
   fireAtTank();
 }
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
