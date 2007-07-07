c03704a9ca53b1840a85ca341fa4bc52597608f8
Jørgen Pedersen Tjernø <daxxar@users.sourceforge.net>
2007-07-07 12:44:06 +0000

    Whoops, forgot this important file (the messagefactory). AdvancedRobot is empty, will be filled eventually. ;-)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@14471 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzrobots/AdvancedRobot.h
@@ -0,0 +1,25 @@
+/* bzflag
+ * Copyright (c) 1993 - 2007 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+ * AdvancedRobot: A class for the simulation and implementation of an AdvancedRobot!
+ */
+
+#ifndef	BZF_ADVANCED_ROBOT_H
+#define	BZF_ADVANCED_ROBOT_H
+
+class AdvancedRobot
+{
+    public:
+};
+
+#endif
