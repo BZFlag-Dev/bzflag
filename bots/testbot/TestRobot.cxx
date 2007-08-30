fbd50d0d8ae16e36e5fef182a98f167a7508544d
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-08-30 20:13:34 +0000

    testbot gets it's own dir in /bots now
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15589 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ bots/testbot/TestRobot.cxx
@@ -0,0 +1,42 @@
+#include "TestRobot.h"
+#include "TimeKeeper.h"
+#include "Logger.h"
+
+extern "C" {
+    BZAdvancedRobot *create() { return new TestRobot(); }
+    void destroy(BZAdvancedRobot *robot) { delete robot; }
+}
+
+void TestRobot::initialize()
+{
+    setCompatability(false);
+}
+
+void TestRobot::update()
+{
+    setFire();
+    setAhead(20);
+    do {
+        if (getGunHeat() <= 0)
+            setFire();
+        execute();
+    } while (getDistanceRemaining() > 0);
+
+    setTurnLeft(90);
+    do {
+        execute();
+    } while (getTurnRemaining() > 0);
+}
+
+void TestRobot::onHitWall(const HitWallEvent &/*hwe*/)
+{
+    FRONTENDLOGGER << "Got a hitWallEvent, stopping!" << std::endl;
+    setAhead(0);
+}
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
