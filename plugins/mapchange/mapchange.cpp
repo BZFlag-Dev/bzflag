acae5b380d2f643d2fbbc7f3e5317f32741ea654
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-09-05 17:02:03 +0000

    tabtastic!
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15701 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/mapchange/mapchange.cpp
+++ plugins/mapchange/mapchange.cpp
@@ -8,162 +8,173 @@
 class Game
 {
 public:
-	bool hasBeenPlayed;
+  bool hasBeenPlayed;
 
-	std::string mapFile;
-	std::string publicText;
-	Game()
-	{
-		hasBeenPlayed = false;
-	}
+  std::string mapFile;
+  std::string publicText;
+  Game()
+  {
+    hasBeenPlayed = false;
+  }
 };
 
 typedef enum
 {
-	eTimedGame,
-	eMaxKillScore,
-	eMaxCapScore,
-	eNoPlayers
+  eTimedGame,
+  eMaxKillScore,
+  eMaxCapScore,
+  eNoPlayers
 }EndCond;
 
-/*typedef enum
+typedef enum
 {
-	eLoopInf,
-	eRandomInf,
-	eRandomOnce,
-	e
-}CycleMode; */
+  eLoopInf,
+  eRandomInf,
+  eRandomOnce,
+  eNoLoop
+}CycleMode; 
 
-EndCond				endCond;
-double				timeLimit;
-int					scoreCapLimit;
-double				startTime;
-int					currentIndex;
+EndCond		    endCond;
+double		    timeLimit;
+int		    scoreCapLimit;
+double		    startTime;
+int		    currentIndex;
 
-std::vector<Game>	gameList;
-bool				loop;
+std::vector<Game>   gameList;
+CycleMode	    cycleMode;
 
 class MapChangeEventHandler : public bz_EventHandler
 {
 public:
-	virtual void process ( bz_EventData *eventData );
+  virtual void process ( bz_EventData *eventData );
 };
 
 MapChangeEventHandler handler;
 
-EndCond condfromString ( const std::string &str )
+EndCond condFromString ( const std::string &str )
 {
-	if ( str == "timed")
-		return eTimedGame;
-	else if ( str == "maxkill")
-		return eMaxKillScore;
-	else if ( str == "maxcap")
-		return eMaxCapScore;
-	else if ( str == "empty")
-		return eNoPlayers;
+  if ( str == "timed")
+    return eTimedGame;
+  else if ( str == "maxkill")
+    return eMaxKillScore;
+  else if ( str == "maxcap")
+    return eMaxCapScore;
+  else if ( str == "empty")
+    return eNoPlayers;
 
-	return eTimedGame;
+  return eTimedGame;
+}
+
+CycleMode cycleFromString ( const std::string &str )
+{
+  if ( str == "loop")
+    return eLoopInf;
+  else if ( str == "random")
+    return eRandomInf;
+  else if ( str == "onerand")
+    return eRandomOnce;
+
+  return eNoLoop;
 }
 
 bool loadGamesFromFile ( const char* config )
 {
-	FILE *fp = fopen(config,"rb");
-	if(!fp)
-		return false;
-	fseek(fp,SEEK_END,0);
+  FILE *fp = fopen(config,"rb");
+  if(!fp)
+    return false;
+  fseek(fp,SEEK_END,0);
 
-	std::string text;
-	unsigned int size = ftell(fp);
-	fseek(fp,SEEK_SET,0);
+  std::string text;
+  unsigned int size = ftell(fp);
+  fseek(fp,SEEK_SET,0);
 
-	char *temp = (char*)malloc(size+1);
-	fread(temp,size,1,fp);
-	fclose(fp);
-	temp[size] = 0;
-	text = temp;
-	free(temp);
+  char *temp = (char*)malloc(size+1);
+  fread(temp,size,1,fp);
+  fclose(fp);
+  temp[size] = 0;
+  text = temp;
+  free(temp);
 
-	endCond = eTimedGame;
-	timeLimit = 30.0*60.0;
-	scoreCapLimit = 10;
+  endCond = eTimedGame;
+  timeLimit = 30.0*60.0;
+  scoreCapLimit = 10;
 
-	startTime = -1;
-	currentIndex = -1;
-	loop = true;
+  startTime = -1;
+  currentIndex = -1;
+  cycleMode = eLoopInf;
 
-	std::vector<std::string> lines = tokenize(text,std::string("\r\n"),0,false);
+  std::vector<std::string> lines = tokenize(text,std::string("\r\n"),0,false);
 
-	for ( unsigned int i = 0; i < (unsigned int)lines.size(); i++ )
+  for ( unsigned int i = 0; i < (unsigned int)lines.size(); i++ )
+  {
+    std::string line = lines[i];
+
+    std::vector<std::string> params = tokenize(line,std::string(","),0,true);
+
+    if (params.size())
+    {
+      if (tolower(params[0]) == "mode")
+      {
+	if ( params.size() > 1 )
+		endCond = condFromString(tolower(params[1]));
+	if ( params.size() > 2 )
 	{
-		std::string line = lines[i];
+	  if (endCond == eTimedGame )
+	    timeLimit = fabs(atof(params[2].c_str()));
+	  else
+	    scoreCapLimit = atoi(params[2].c_str());
 
-		std::vector<std::string> params = tokenize(line,std::string(","),0,true);
+	  if (timeLimit == 0.0)
+	    timeLimit = 30;
 
-		if (params.size())
-		{
-			if (tolower(params[0]) == "mode")
-			{
-				if ( params.size() > 1 )
-					endCond = condfromString(tolower(params[1]));
-				if ( params.size() > 2 )
-				{
-					if (endCond == eTimedGame )
-						timeLimit = fabs(atof(params[2].c_str()));
-					else
-						scoreCapLimit = atoi(params[2].c_str());
-
-					if (timeLimit == 0.0)
-						timeLimit = 30;
-
-					if ( scoreCapLimit <= 0 )
-						scoreCapLimit = 10;
-
-				}
-
-				if ( params.size() > 3)
-					loop = tolower(params[3]) == "loop";
-			}
-			else
-			{
-				Game	game;
-				game.mapFile = params[0];
-				if ( params.size() > 1 )
-					game.publicText = params[1];
-
-				gameList.push_back(game);
-			}
-		}
+	  if ( scoreCapLimit <= 0 )
+	    scoreCapLimit = 10;
 	}
-	return gameList.size();
+
+	if ( params.size() > 3)
+	  cycleMode = cycleFromString(tolower(params[3]));
+      }
+      else
+      {
+	Game  game;
+	game.mapFile = params[0];
+	if ( params.size() > 1 )
+	  game.publicText = params[1];
+
+	gameList.push_back(game);
+      }
+    }
+  }
+  return gameList.size();
 }
 
 BZ_GET_PLUGIN_VERSION
 
 BZF_PLUGIN_CALL int bz_Load ( const char* commandLine )
 {
-	if(!commandLine || !strlen(commandLine))
-	{
-		bz_debugMessage(0,"mapchange plugin requires a config file as a param and will not load");
-		return -1;
-	}
+  if(!commandLine || !strlen(commandLine))
+  {
+    bz_debugMessage(0,"mapchange plugin requires a config file as a param and will not load");
+    return -1;
+  }
 
-	if(!loadGamesFromFile(commandLine))
-	{
-		bz_debugMessage(0,"mapchange plugin config file failure, aborting load");
-		return -1;
-	}
+  if(!loadGamesFromFile(commandLine))
+  {
+    bz_debugMessage(0,"mapchange plugin config file failure, aborting load");
+    return -1;
+  }
 
-	bz_debugMessage(4,"mapchange plugin loaded");
+  bz_debugMessage(4,"mapchange plugin loaded");
 
-	bz_APIIntList *players = bz_getPlayerIndexList();
-	if ( players->size() )
-		startTime = bz_getCurrentTime();
+  bz_APIIntList *players = bz_getPlayerIndexList();
+  if ( players->size() )
+    startTime = bz_getCurrentTime();
 
-	bz_registerEvent ( bz_ePlayerJoinEvent, &handler );
-	bz_registerEvent ( bz_ePlayerDieEvent, &handler );
-	bz_registerEvent ( bz_ePlayerPartEvent, &handler );
-	bz_registerEvent ( bz_eCaptureEvent, &handler );
-	bz_registerEvent ( bz_eGetWorldEvent, &handler );
+  bz_registerEvent ( bz_ePlayerJoinEvent, &handler );
+  bz_registerEvent ( bz_ePlayerDieEvent, &handler );
+  bz_registerEvent ( bz_ePlayerPartEvent, &handler );
+  bz_registerEvent ( bz_eCaptureEvent, &handler );
+  bz_registerEvent ( bz_eGetWorldEvent, &handler );
 
   return 0;
 }
@@ -182,9 +193,18 @@ BZF_PLUGIN_CALL int bz_Unload ( void )
 
 void MapChangeEventHandler::process ( bz_EventData *eventData )
 {
-	if (!eventData)
-		return;
+  if (!eventData)
+    return;
 
+  switch(eventData->eventType)
+  {
+  case bz_ePlayerJoinEvent:
+  case bz_ePlayerDieEvent:
+  case bz_ePlayerPartEvent:
+  case bz_eCaptureEvent:
+  case bz_eGetWorldEvent:
+    break;
+ }
 }
 
 // Local Variables: ***
