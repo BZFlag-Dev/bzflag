a87027c9db90d53dd033d71ae15f5bfaa5475970
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-09-07 17:16:47 +0000

    make the std::string versions inline so they don't get double defined when linked
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15769 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/plugin_files.h
+++ plugins/plugin_utils/plugin_files.h
@@ -19,28 +19,28 @@
 #include "bzfsAPI.h"
 
 std::vector<std::string> getFilesInDir ( const char* dir, const char* filter = "*.*", bool recursive = false );
-std::vector<std::string> getFilesInDir ( const std::string &dir, const char* filter = "*.*", bool recursive = false ){return getFilesInDir(dir.c_str(),filter,recursive);}
+inline std::vector<std::string> getFilesInDir ( const std::string &dir, const char* filter = "*.*", bool recursive = false ){return getFilesInDir(dir.c_str(),filter,recursive);}
 
 std::string getFileDir ( const char* file );
-std::string getFileDir ( const std::string &file ){return getFileDir(file.c_str());}
+inline std::string getFileDir ( const std::string &file ){return getFileDir(file.c_str());}
 
 std::string getPathForOS ( const char* file );
-std::string getPathForOS ( const std::string &file ){return getPathForOS(file.c_str());}
+inline std::string getPathForOS ( const std::string &file ){return getPathForOS(file.c_str());}
 
 std::string getFileExtension ( const char* file );
-std::string getFileExtension ( const std::string &file ){return getFileExtension(file.c_str());}
+inline std::string getFileExtension ( const std::string &file ){return getFileExtension(file.c_str());}
 
 std::string getFileTitle ( const char* file );
-std::string getFileTitle ( const std::string &file ){return getFileTitle(file.c_str());}
+inline std::string getFileTitle ( const std::string &file ){return getFileTitle(file.c_str());}
 
 std::string getFileText ( const char* file );
-std::string getFileText ( const std::string &file ){return getFileText(file.c_str());}
+inline std::string getFileText ( const std::string &file ){return getFileText(file.c_str());}
 
 std::vector<std::string> getFileTextLines ( const char* file );
-std::vector<std::string> getFileTextLines ( const std::string &file ){return getFileTextLines(file.c_str());}
+inline std::vector<std::string> getFileTextLines ( const std::string &file ){return getFileTextLines(file.c_str());}
 
 unsigned int getFileLen ( const char* file );
-unsigned int getFileLen ( std::string &file ){return getFileLen(file.c_str());}
+inline unsigned int getFileLen ( std::string &file ){return getFileLen(file.c_str());}
 
 
 #endif //_PLUGIN_FILES_H_
