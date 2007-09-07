17fa9ddd2df438dcaa1afe65f752f44b93c7bd59
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-09-07 22:58:56 +0000

    do some basic directory listing
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15796 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/httpTest/httpTest.cpp
+++ plugins/httpTest/httpTest.cpp
@@ -5,6 +5,7 @@
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
 #include "plugin_HTTP.h"
+#include "plugin_files.h"
 
 BZ_GET_PLUGIN_VERSION
 
@@ -16,7 +17,10 @@ public:
   virtual bool acceptURL ( const char *url );
   virtual void getURLData ( const char* url, int requestID, const URLParams &paramaters, bool get = true );
 
+  virtual void generateDirpage ( const std::string& url, int requestID );
+
   std::string dir;
+  bool dirBrowsing;
 };
 
 HTTPServer httpServer("httpTest");
@@ -25,6 +29,7 @@ BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
 {
   bz_debugMessage(4,"httpTest plugin loaded");
   httpServer.startupHTTP();
+  httpServer.dirBrowsing = true;
   return 0;
 }
 
@@ -44,10 +49,68 @@ bool HTTPServer::acceptURL ( const char *url )
   return true;
 }
 
+std::string convertPathToURL ( const std::string &file )  // ensures all the delims are constant
+{
+  std::string delim;
+  delim += "/";
+  return replace_all(file,"\\",delim);
+}
+
+
+void HTTPServer::generateDirpage ( const std::string &url, int requestID )
+{
+  std::string pathURL = url;
+  if ( pathURL.size() )
+    pathURL = url.c_str()+1;
+
+  if (pathURL.size() && *(pathURL.end()-1) == '/')
+    pathURL.erase(pathURL.end()-1);
+
+  std::string requestedDir = dir + pathURL;
+  std::vector<std::string> dirs = getDirsInDir(requestedDir);
+
+  std::string page;
+  page = "<HTML><HEAD></HEAD><BODY>\n<BR> The URL was " + url + "<BR>\n";
+
+  page = "BZFS HTML Test, dir browsing<BR>\n";
+
+  std::string serverAddress = getBaseServerURL();
+  serverAddress.erase(serverAddress.end()-1);
+
+  std::string baseURL;
+  if ( url.size() > 1)
+    baseURL = url.c_str()+1;
+
+  for (int i = 0; i < (int)dirs.size(); i++ )
+  {
+    std::string returnedDir = dirs[i];
+    std::string dirURL = convertPathToURL(returnedDir.c_str()).c_str()+requestedDir.size()+1;
+
+    std::string fullURL;
+    fullURL += baseURL + dirURL;
+    page += format("<A href=\"%s/\">%s/</a><BR>\n",fullURL.c_str(),dirURL.c_str());
+  }
+
+  std::vector<std::string> files = getFilesInDir(requestedDir);
+  for (int i = 0; i < (int)files.size(); i++ )
+  {
+    std::string fileURL = convertPathToURL(files[i].c_str()).c_str()+dir.size();
+    std::string fullURL;
+    fullURL += baseURL + fileURL;
+    page += format("<A href=\"%s\">%s</a><BR>\n",fullURL.c_str(),fileURL.c_str());
+  }
+
+  page += "<BR></body></HTML>";
+
+  setURLDocType(eHTML,requestID);
+  setURLDataSize ( (unsigned int)page.size(), requestID );
+  setURLData ( page.c_str(), requestID );
+}
+
 void HTTPServer::getURLData ( const char* url, int requestID, const std::map<std::string,std::string> &paramaters, bool get )
 {
   std::string URL = url;
-  if ( !get || URL.size() < 2 )
+  if ( !get || tolower(URL) == "status" )
   {
     std::string crapPage = "This Is data from HTTP via BZFS\r\n";
 
@@ -75,8 +138,16 @@ void HTTPServer::getURLData ( const char* url, int requestID, const std::map<std
   }
   else
   {
-    if (*(URL.end()-1) == '/')
-      URL += "index.html";
+    if (*(URL.end()-1) == '/' || URL.size()< 2)
+    {
+      if (dirBrowsing)
+      {
+	generateDirpage(URL,requestID);
+	return;
+      }
+      else
+       URL += "index.html";
+    }
 
     std::string path = dir + URL;
     FILE *fp = fopen(path.c_str(),"rb");
