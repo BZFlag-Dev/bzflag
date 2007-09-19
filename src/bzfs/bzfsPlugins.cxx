f5a5aca2a14c863e9bd4f6856c093490e04ddae7
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-09-19 22:20:11 +0000

    change load an unload to check for the PLUGINS perm not just admin status, some admins are stupid ;)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15924 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfsPlugins.cxx
+++ src/bzfs/bzfsPlugins.cxx
@@ -393,7 +393,7 @@ public:
 			return true;
 		}
 
-		if ( !record.admin )
+		if (!record.hasPerm("PLUGINS"))
 		{
 			bz_sendTextMessage(BZ_SERVER,playerID,"You do not have permission to (un)load plug-ins.");
 			return true;
