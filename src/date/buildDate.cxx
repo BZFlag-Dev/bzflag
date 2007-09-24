07545b3bcee95beed6ffee3c75979e02e5e9c866
Daniel Remenak <dtremenak@users.sourceforge.net>
2007-09-24 15:38:55 +0000

    previous commit requires protocol bump
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@15953 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -21,7 +21,7 @@
 
 // opaque version number increments on protocol incompatibility
 #ifndef BZ_PROTO_VERSION
-#  define BZ_PROTO_VERSION	"0056"
+#  define BZ_PROTO_VERSION	"0057"
 #endif
 // ditto for bzrobots
 #ifndef BZROBOTS_PROTO_VERSION
@@ -42,7 +42,7 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		15
+#  define BZ_REV		16
 #endif
 
 // DEVEL | STABLE | MAINT
