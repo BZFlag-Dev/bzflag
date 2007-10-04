8a46944653f265137fb1e7464e1118b8abbe98fe
Alfredo Tupone <atupone@users.sourceforge.net>
2007-10-04 20:53:08 +0000

    Mark it rc3
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@16009 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -45,7 +45,7 @@
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"RC2"
+#  define BZ_BUILD_TYPE		"RC3"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993 - 2007 Tim Riker";
