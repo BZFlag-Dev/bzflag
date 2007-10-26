68fc437cb02df93200b1294e645aebf32fcaf4c7
Bernt Hansen <bthansen@users.sourceforge.net>
2007-10-26 19:51:26 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16109 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/thiefControl/thiefControl.cpp
+++ plugins/thiefControl/thiefControl.cpp
@@ -5,39 +5,39 @@
 
 BZ_GET_PLUGIN_VERSION
 
-class ThiefControl : public bz_EventHandler
+class ThiefControl:public bz_EventHandler
 {
 public:
-  ThiefControl() {} ;
+  ThiefControl() {};
   virtual ~ThiefControl() {};
-  virtual void process( bz_EventData *eventData );
+  virtual void process(bz_EventData * eventData);
 };
 
 ThiefControl thiefHandler;
 
-BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+BZF_PLUGIN_CALL int bz_Load(const char * /*commandLine */ )
 {
-  bz_debugMessage(4,"thiefControl plugin loaded");
+  bz_debugMessage(4, "thiefControl plugin loaded");
   bz_registerEvent(bz_eFlagTransferredEvent, &thiefHandler);
   return 0;
 }
 
-BZF_PLUGIN_CALL int bz_Unload ( void )
+BZF_PLUGIN_CALL int bz_Unload(void)
 {
   bz_removeEvent(bz_eFlagTransferredEvent, &thiefHandler);
-  bz_debugMessage(4,"thiefControl plugin unloaded");
+  bz_debugMessage(4, "thiefControl plugin unloaded");
   return 0;
 }
 
-void ThiefControl::process( bz_EventData *eventData )
+void ThiefControl::process(bz_EventData * eventData)
 {
   bz_FlagTransferredEventData_V1 *data = (bz_FlagTransferredEventData_V1 *) eventData;
   const std::string noStealMsg = "Flag dropped. Don't steal from teammates!";
 
-  if(!data)
+  if (!data)
     return;
 
-  if(data->eventType != bz_eFlagTransferredEvent)
+  if (data->eventType != bz_eFlagTransferredEvent)
     return;
 
   bz_BasePlayerRecord *playerFrom = bz_getPlayerByIndex(data->fromPlayerID);
@@ -47,72 +47,54 @@ void ThiefControl::process( bz_EventData *eventData )
 
   bz_BasePlayerRecord *playerTo = bz_getPlayerByIndex(data->toPlayerID);
 
-  if(!playerTo) {
+  if (!playerTo) {
     bz_freePlayerRecord(playerFrom);
     return;
   }
 
+  switch (bz_getGameType()) {
 
-  switch(bz_getGameType()) {
-
-    case eTeamFFAGame:
-    {
-      if(playerTo->team == playerFrom->team &&
-	 playerTo->team != eRogueTeam) {
-
-	data->action = data->DropThief;
-	bz_sendTextMessage(BZ_SERVER, data->toPlayerID, noStealMsg.c_str());
-
-      }
-
+  case eTeamFFAGame:
+    if (playerTo->team == playerFrom->team && playerTo->team != eRogueTeam) {
+      data->action = data->DropThief;
+      bz_sendTextMessage(BZ_SERVER, data->toPlayerID, noStealMsg.c_str());
     }
-      break;
+    break;
 
-
-    case eClassicCTFGame:
+  case eClassicCTFGame:
     {
-
       // Allow teammates to steal team flags
       // This will allow someone to steal a team flag in order
       // to possibly capture it faster.
       bool allowTransfer = false;
 
-      if(playerTo->team == playerFrom->team &&
-	 playerTo->team != eRogueTeam) {
-
+      if (playerTo->team == playerFrom->team && playerTo->team != eRogueTeam) {
 	std::string flagT = data->flagType;
 
 	// Allow theft of team flags only
 	allowTransfer = (flagT == "R*" || flagT == "G*" || flagT == "B*" || flagT == "P*");
 
-	if(!allowTransfer) {
+	if (!allowTransfer) {
 	  data->action = data->DropThief;
 	  bz_sendTextMessage(BZ_SERVER, data->toPlayerID, noStealMsg.c_str());
 	}
       }
     }
-      break;
+    break;
 
+  case eOpenFFAGame:
+    break;
 
-    case eOpenFFAGame:
-    {
+  case eRabbitGame:
+    if (playerTo->team == playerFrom->team) {
+
+      data->action = data->DropThief;
+      bz_sendTextMessage(BZ_SERVER, data->toPlayerID, noStealMsg.c_str());
     }
-      break;
+    break;
 
-
-    case eRabbitGame:
-    {
-      if(playerTo->team == playerFrom->team) {
-
-	data->action = data->DropThief;
-	bz_sendTextMessage(BZ_SERVER, data->toPlayerID, noStealMsg.c_str());
-      }
-    }
-      break;
-
-
-    default:
-      break;
+  default:
+    break;
 
   }
 
