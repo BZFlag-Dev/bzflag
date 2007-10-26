00f88914763ef701363926d27155e179e8a8fca1
Bernt Hansen <bthansen@users.sourceforge.net>
2007-10-26 19:50:59 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16106 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/regFlag/regFlag.cpp
+++ plugins/regFlag/regFlag.cpp
@@ -6,46 +6,45 @@
 
 BZ_GET_PLUGIN_VERSION
 
-class RegFlag : public bz_EventHandler
+class RegFlag:public bz_EventHandler
 {
 public:
-  virtual void process ( bz_EventData *eventData );
+  virtual void process(bz_EventData * eventData);
 };
 
 RegFlag RegFlagHandler;
 
-BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+BZF_PLUGIN_CALL int bz_Load(const char * /*commandLine */ )
 {
-  bz_debugMessage(4,"regFlag plugin loaded");
+  bz_debugMessage(4, "regFlag plugin loaded");
   bz_registerEvent(bz_ePlayerUpdateEvent, &RegFlagHandler);
   return 0;
 }
 
-BZF_PLUGIN_CALL int bz_Unload ( void )
+BZF_PLUGIN_CALL int bz_Unload(void)
 {
-  bz_debugMessage(4,"regFlag plugin unloaded");
+  bz_debugMessage(4, "regFlag plugin unloaded");
   bz_removeEvent(bz_ePlayerUpdateEvent, &RegFlagHandler);
   return 0;
 }
 
-void RegFlag::process ( bz_EventData *eventData )
+void RegFlag::process(bz_EventData * eventData)
 {
   bz_BasePlayerRecord *player = NULL;
   int playerID = -1;
 
-  switch (eventData->eventType)
-    {
-    case bz_ePlayerUpdateEvent:
-      playerID = ((bz_PlayerUpdateEventData_V1 *)eventData)->player;
-      break;
+  switch (eventData->eventType) {
+  case bz_ePlayerUpdateEvent:
+    playerID = ((bz_PlayerUpdateEventData_V1 *) eventData)->player;
+    break;
 
-    case bz_eShotFiredEvent:
-      playerID = ((bz_ShotFiredEventData_V1 *)eventData)->player;
-      break;
+  case bz_eShotFiredEvent:
+    playerID = ((bz_ShotFiredEventData_V1 *) eventData)->player;
+    break;
 
-    default:
-      return;
-    }
+  default:
+    return;
+  }
 
   player = bz_getPlayerByIndex(playerID);
   if (!player)
@@ -56,12 +55,12 @@ void RegFlag::process ( bz_EventData *eventData )
   }
   bz_freePlayerRecord(player);
 
-  const char* flagAbrev = bz_getPlayerFlag(playerID);
+  const char *flagAbrev = bz_getPlayerFlag(playerID);
   if (!flagAbrev)
     return;
 
   bz_removePlayerFlag(playerID);
-  bz_sendTextMessage(BZ_SERVER,playerID, "Flags are for registered players only");
+  bz_sendTextMessage(BZ_SERVER, playerID, "Flags are for registered players only");
 
 }
 
@@ -72,4 +71,3 @@ void RegFlag::process ( bz_EventData *eventData )
 // indent-tabs-mode: t ***
 // End: ***
 // ex: shiftwidth=2 tabstop=8
-
