45fedb5376c9edbf8773bd368fa805cec8dac251
Joshua Bodine <macsforme@users.sourceforge.net>
2007-11-16 00:37:57 +0000

    All hail BZFlag 2.0.11! ;)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@16210 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -40,12 +40,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		10
+#  define BZ_REV		11
 #endif
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"MAINT"
+#  define BZ_BUILD_TYPE		"DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993 - 2007 Tim Riker";
