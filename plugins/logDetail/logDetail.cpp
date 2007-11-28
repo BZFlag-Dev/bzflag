9f87e71f6a3a7af76f50c8fb112fb12890e560b7
Bernt Hansen <bthansen@users.sourceforge.net>
2007-11-28 01:39:24 +0000

    Log the filtered message not the raw text when messages are filtered.
    By default the chat log records raw text so we don't need a duplicate message.
    
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16254 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/logDetail/logDetail.cpp
+++ plugins/logDetail/logDetail.cpp
@@ -132,7 +132,7 @@ void LogDetail::process(bz_EventData *eventData)
     case bz_eMessageFilteredEvent:
       bz_debugMessagef(0, "MSG-FILTERED %s %s",
 		       displayCallsign(filteredData->player).c_str(),
-		       filteredData->rawMessage.c_str());
+		       filteredData->filteredMessage.c_str());
       break;
     case bz_eServerMsgEvent:
       if ((serverMsgData->to == BZ_ALLUSERS) and (serverMsgData->team == eNoTeam)) {
