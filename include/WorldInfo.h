8110ae4ffd29163f8e2b47d09d1001705a0e28dc
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-12-18 03:34:11 +0000

    start a world info object.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16340 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/WorldInfo.h
@@ -0,0 +1,52 @@
+/* bzflag
+ * Copyright (c) 1993 - 2007 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* WorldInfo:
+ *  Human readable information about the map
+ */
+
+#ifndef	BZF_WORLD_INFO_H
+#define	BZF_WORLD_INFO_H
+
+#include "common.h"
+#include "BufferedNetworkMessage.h"
+
+#include <string>
+
+class WorldInfo
+{
+public:
+  
+  class WorldInfoItem
+  {
+  public:
+    std::string key;
+    std::string value;
+  };
+
+  std::vector<WorldInfoItem> items;
+
+  void clear ( void );
+
+  void read ( )
+};
+
+
+#endif // BZF_WORLD_INFO_H
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
