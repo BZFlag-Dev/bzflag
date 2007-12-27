ea97f49cd65010c747f26cdfea12ce0d9e2dc4aa
Jeff Myers <jeffm2501@users.sourceforge.net>
2007-12-27 18:33:13 +0000

    add implementation files for WorldContentInfo
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16373 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/game/WorldContentInfo.cxx
@@ -0,0 +1,41 @@
+/* bzflag
+ * Copyright (c) 1993 - 2007 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* WorldContentInfo:
+ *  Human readable information about the map
+ */
+
+#include "WorldContentInfo.h"
+
+
+void WorldInfo::clear ( void )
+{
+
+}
+
+void  WorldInfo::send ( NetHandler *handler )
+{
+
+}
+
+void  WorldInfo::receive ( BufferedNetworkMessage *msg )
+{
+}
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
