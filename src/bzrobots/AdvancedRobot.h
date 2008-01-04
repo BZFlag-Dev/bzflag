6ec22501c5b1aea0297d81e4eafc2af7adc8b0eb
Sean Morrison <brlcad@users.sourceforge.net>
2008-01-04 18:01:46 +0000

    cleanup header, doxygenify
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16405 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzrobots/AdvancedRobot.h
+++ src/bzrobots/AdvancedRobot.h
@@ -10,44 +10,47 @@
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-/*
- * AdvancedRobot: A class for the simulation and implementation of mostly
- * RoboCode-compliant AdvancedRobot (see BZRobot for the "normal" one)
- */
+#ifndef __ADVANCEDROBOT_H__
+#define __ADVANCEDROBOT_H__
 
-#ifndef BZROBOTS_ADVANCEDROBOT_H
-#define BZROBOTS_ADVANCEDROBOT_H
-
+/* interface header */
 #include "BZAdvancedRobot.h"
 
-class AdvancedRobot :public BZAdvancedRobot
+
+/**
+ * AdvancedRobot: A class for the simulation and implementation of
+ * mostly RoboCode-compliant AdvancedRobot (see BZRobot for the
+ * "normal" one).  AdvancedRobots behave slightly differently.  The
+ * implementor defines run(), including the while-loop.
+ */
+class AdvancedRobot : public BZAdvancedRobot
 {
-  protected:
-    double getGunTurnRemaining();
-    double getRadarTurnRemaining();
-    bool isAdjustGunForRobotTurn();
-    bool isAdjustRadarForRobotTurn();
-    void setBack(double distance);
-    void setFire(double power);
-    // TODO: Implement 'Bullet setFireBullet(double power)'?
-    void setMaxTurnRate(double maxTurnRate); // This needs to divide by the max, so we get something between 0 and 1, then pass to setTurnRate
-    void setMaxSpeed(double maxSpeed); // This needs to divide by the max, so we get something between 0 and 1, then pass to setSpeed
-    void setTurnGunLeft(double degrees);
-    void setTurnGunRight(double degrees);
-    void setTurnRadarLeft(double degrees);
-    void setTurnRadarRight(double degrees);
-    void setTurnRight(double degrees);
-  public:
-    /* AdvancedRobots behave slightly differently. The implementor defines run, including the while-loop. */
-    void update() {}
-    void initialize() {}
-    virtual void run() = 0;
+protected:
+  double getGunTurnRemaining();
+  double getRadarTurnRemaining();
+  bool isAdjustGunForRobotTurn();
+  bool isAdjustRadarForRobotTurn();
+  void setBack(double distance);
+  void setFire(double power);
+  // TODO: Implement 'Bullet setFireBullet(double power)'?
+  void setMaxTurnRate(double maxTurnRate); // This needs to divide by the max, so we get something between 0 and 1, then pass to setTurnRate
+  void setMaxSpeed(double maxSpeed); // This needs to divide by the max, so we get something between 0 and 1, then pass to setSpeed
+  void setTurnGunLeft(double degrees);
+  void setTurnGunRight(double degrees);
+  void setTurnRadarLeft(double degrees);
+  void setTurnRadarRight(double degrees);
+  void setTurnRight(double degrees);
+public:
+
+  void update() {}
+  void initialize() {}
+  virtual void run() = 0;
 };
 
-#endif
+#endif  /* __ADVANCEDROBOT_H__ */
 
 // Local Variables: ***
-// mode:C++ ***
+// mode: C++ ***
 // tab-width: 8 ***
 // c-basic-offset: 2 ***
 // indent-tabs-mode: t ***
