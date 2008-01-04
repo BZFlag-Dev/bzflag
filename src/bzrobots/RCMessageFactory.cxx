f023ac4aef2b84c093a0154394682fe3dcf3d609
Sean Morrison <brlcad@users.sourceforge.net>
2008-01-04 20:06:41 +0000

    add the missing comment headers
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16415 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzrobots/RCMessageFactory.cxx
+++ src/bzrobots/RCMessageFactory.cxx
@@ -1,5 +1,16 @@
-#include "RCMessageFactory.h"
+/* bzflag
+ * Copyright (c) 1993 - 2007 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
+#include "RCMessageFactory.h"
 
 #include "RCRequests.h"
 #include "RCReplies.h"
--- src/bzrobots/Tank.cxx
+++ src/bzrobots/Tank.cxx
@@ -1,3 +1,15 @@
+/* bzflag
+ * Copyright (c) 1993 - 2007 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named LICENSE that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include "Tank.h"
 
 #include "MessageUtilities.h"
