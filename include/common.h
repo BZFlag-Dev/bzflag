a3903b098aba6d93d15d3142103f95abc2e0d43d
Sean Morrison <brlcad@users.sourceforge.net>
2008-01-04 22:19:51 +0000

    add the same short-hand defines used by bzapi for defining stdarg function attributs, provide BZ_ATTR_12, 23, and 34.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16425 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/common.h
+++ include/common.h
@@ -314,6 +314,31 @@ typedef unsigned char	uint8_t;
 #endif
 #endif
 
+/**
+ * This is so we can use gcc's "format string vs arguments"-check
+ * for various printf-like functions, and still maintain compatability.
+ * Not tested on other platforms yet, but should work.
+ */
+#ifndef __attribute__
+/* This feature is available in gcc versions 2.5 and later.  */
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5)
+#  define __attribute__(Spec) /* empty */
+# endif
+/* The __-protected variants of `format' and `printf' attributes
+ *    are accepted by gcc versions 2.6.4 (effectively 2.7) and later.
+ */
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+#  define __format__ format
+#  define __printf__ printf
+# endif
+#endif
+
+/** shorthand defines to make the code cleaner. */
+#define BZ_ATTR_34 __attribute__ ((__format__ (__printf__, 3, 4)))
+#define BZ_ATTR_23 __attribute__ ((__format__ (__printf__, 2, 3)))
+#define BZ_ATTR_12 __attribute__ ((__format__ (__printf__, 1, 2)))
+
+
 #endif /* BZF_COMMON_H */
 
 
