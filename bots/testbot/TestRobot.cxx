529436cdb7cbd5cb712d8d37d5371ab05ea5d5a1
Sean Morrison <brlcad@users.sourceforge.net>
2008-01-06 22:40:31 +0000

    ws, add missing license header
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16448 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bots/testbot/TestRobot.cxx
+++ bots/testbot/TestRobot.cxx
@@ -1,38 +1,63 @@
+/* bzflag
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* interface header */
 #include "TestRobot.h"
+
+/* common implementation headers */
 #include "TimeKeeper.h"
+
+/* local implementation headers */
 #include "Logger.h"
 
+
 extern "C" {
-    BZAdvancedRobot *create() { return new TestRobot(); }
-    void destroy(BZAdvancedRobot *robot) { delete robot; }
+  BZAdvancedRobot *create()
+  {
+    return new TestRobot();
+  }
+  void destroy(BZAdvancedRobot *robot)
+  {
+    delete robot;
+  }
 }
 
 void TestRobot::initialize()
 {
-    setCompatability(false);
+  setCompatability(false);
 }
 
 void TestRobot::update()
 {
-    setFire();
-    setAhead(20);
-    do {
-        if (getGunHeat() <= 0)
-            setFire();
-        execute();
-    } while (getDistanceRemaining() > 0);
-
-    setTurnLeft(90);
-    do {
-        execute();
-    } while (getTurnRemaining() > 0);
+  setFire();
+  setAhead(20);
+  do {
+    if (getGunHeat() <= 0)
+      setFire();
+    execute();
+  } while (getDistanceRemaining() > 0);
+  
+  setTurnLeft(90);
+  do {
+    execute();
+  } while (getTurnRemaining() > 0);
 }
 
 void TestRobot::onHitWall(const HitWallEvent &/*hwe*/)
 {
-    FRONTENDLOGGER << "Got a hitWallEvent, stopping!" << std::endl;
-    setAhead(0);
+  FRONTENDLOGGER << "Got a hitWallEvent, stopping!" << std::endl;
+  setAhead(0);
 }
+
 // Local Variables: ***
 // mode: C++ ***
 // tab-width: 8 ***
