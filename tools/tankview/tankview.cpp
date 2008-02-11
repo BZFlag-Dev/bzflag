481dea651e00dd1114098615d89874af655d2708
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-02-11 22:18:08 +0000

    draw a view with a ground grid and the standard tank bounds.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16549 08b3d480-bf2c-0410-a26f-811ee3361c24

--- tools/tankview/tankview.cpp
+++ tools/tankview/tankview.cpp
@@ -2,6 +2,107 @@
 //
 
 #include "tankview.h"
+#include "Model.h"
+#include "config.h"
+
+// for the stupid debug openGLcount model uses
+#ifdef DEBUG
+int __beginendCount;
+#endif
+
+void drawGridAndBounds ( void )
+{
+  glDisable(GL_TEXTURE_2D);
+  glDisable(GL_LIGHTING);
+
+  glBegin(GL_LINES);
+  // axis markers
+  glColor4f(1,0,0,0.25f);
+  glVertex3f(0,0,0);
+  glVertex3f(1,0,0);
+
+  glColor4f(0,1,0,0.25f);
+  glVertex3f(0,0,0);
+  glVertex3f(0,1,0);
+
+  glColor4f(0,0,1,0.25f);
+  glVertex3f(0,0,0);
+  glVertex3f(0,0,1);
+
+  // grid
+  glColor4f(1,1,1,0.125f);
+  float grid = 10.0f;
+  float increment = 0.25f;
+
+  for( float i = -grid; i <= grid; i += increment )
+  {
+    glVertex3f(i,grid,0);
+    glVertex3f(i,-grid,0);
+    glVertex3f(grid,i,0);
+    glVertex3f(-grid,i,0);
+  }
+
+  // tank bbox
+  glColor4f(0,1,1,0.25f);
+  float width = 1.4f;
+  float height = 2.05f;
+  float front = 4.94f;
+  float back = -3.10f;
+
+  // front
+  glVertex3f(front,-width,0);
+  glVertex3f(front,width,0);
+  glVertex3f(front,width,0);
+  glVertex3f(front,width,height);
+  glVertex3f(front,-width,height);
+  glVertex3f(front,width,height);
+  glVertex3f(front,-width,0);
+  glVertex3f(front,-width,height);
+
+  // back
+  glVertex3f(back,-width,0);
+  glVertex3f(back,width,0);
+  glVertex3f(back,width,0);
+  glVertex3f(back,width,height);
+  glVertex3f(back,-width,height);
+  glVertex3f(back,width,height);
+  glVertex3f(back,-width,0);
+  glVertex3f(back,-width,height);
+
+  // sides
+  glVertex3f(back,-width,0);
+  glVertex3f(front,-width,0);
+
+  glVertex3f(back,width,0);
+  glVertex3f(front,width,0);
+
+  glVertex3f(back,-width,height);
+  glVertex3f(front,-width,height);
+
+  glVertex3f(back,width,height);
+  glVertex3f(front,width,height);
+  glEnd();
+}
+
+OBJModel base,turret,barrel,lTread,rTread;
+
+void loadModels ( void )
+{
+  barrel.read(std::string("/data/geometry/tank/std/barrel.obj"));
+  base.read(std::string("/data/geometry/tank/std/base.obj"));
+  turret.read(std::string("/data/geometry/tank/std/turret.obj"));
+  lTread.read(std::string("/data/geometry/tank/std/lcasing.obj"));
+  rTread.read(std::string("/data/geometry/tank/std/rcasing.obj"));
+}
+
+void drawModels ( void )
+{
+  base.draw();
+  lTread.draw();
+  rTread.draw();
+  turret.draw();
+  barrel.draw();
+}
 
 #ifdef _WIN32
 int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPInst, LPSTR lpCmdLine, int nShowCmd )
@@ -12,10 +113,26 @@ int main ( int argc, char* argv[] )
 #endif
   SimpleDisplay display;
 
+  SimpleDisplayCamera camera;
+
+  // load up the models
+  loadModels();
+
+  camera.rotateGlob(-90,1,0,0);
+  camera.moveLoc(0,0,-20);
+  camera.moveLoc(0,1.5f,0);
+
   while (display.update())
   {
     display.clear();
+    camera.applyView();
 
+    // bind a texture
+
+    drawGridAndBounds();
+    drawModels();
+
+    camera.removeView();
     display.flip();
   }
 }
