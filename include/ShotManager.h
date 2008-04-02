5f5a97474c7d5953f9aea1cd257cc375d03b5117
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-04-02 00:42:09 +0000

    ShotManager, that what manages shots. Today the server, tomorrow the World(where world == client)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@16890 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ include/ShotManager.h
@@ -0,0 +1,38 @@
+/* bzflag
+* Copyright (c) 1993 - 2008 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/*
+* ShotPath:
+*	Encapsulates the path a shot follows.  Most paths can
+*	be computed at the instant of firing (though they may
+*	terminate early because of a hit).  Some paths need
+*	to be updated continuously during flight.
+*
+* RemoteShotPath:
+*	A ShotPath acting as a proxy for a remote ShotPath.
+*	Created by a LocalPlayer on behalf of a RemotePlayer.
+*/
+
+#ifndef	__SHOTMANGER_H__
+#define	__SHOTMANGER_H__
+
+#include "common.h"
+
+#endif /* __SHOTMANAGERH__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
