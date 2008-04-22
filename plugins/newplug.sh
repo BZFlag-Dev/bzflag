1151a233146397f99a694b4b88df68a319c3c7d8
Sean Morrison <brlcad@users.sourceforge.net>
2008-04-22 07:31:56 +0000

    merge in the better script foo from trunk sans modifying the build system.  still make them do that step manually since it sucks on branch.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@17062 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/newplug.sh
+++ plugins/newplug.sh
@@ -1,26 +1,45 @@
-#!/bin/bash
+#!/bin/sh
+
+NARG="$#"
+ARGS="$*"
+ARG1="$1"
+PATH_TO_HERE="`dirname $0`"
+
 SAMPLE_PLUGIN=SAMPLE_PLUGIN
 
 # make sure user gives a plugin name
-if [ $# -lt 1 ] ;then
- echo "syntax: $0 <new plugin name>"
-  exit 1
+if [ $NARG -lt 1 ] ;then
+    echo "syntax: $0 <new_plugin_name>"
+    exit 1
+elif [ $NARG -gt 1 ] ;then
+    echo "syntax: $0 <new_plugin_name>"
+    exit 1
 fi
 
 # make sure it doesn't already exist
-if [ -d "./$1" ] ; then
-    echo "ERROR: $1 already exists"
+if [ -d "$PATH_TO_HERE/$ARG1" ] ; then
+    echo "ERROR: $ARG1 already exists, remove it or use a different name"
+    exit 1
+fi
+
+# make sure the sample exists
+if [ ! -d "$PATH_TO_HERE/$SAMPLE_PLUGIN" ] ; then
+    echo "ERROR: $SAMPLE_PLUGIN seems to be missing..."
     exit 1
 fi
 
 # copy the template
-#echo cp -pR ./$SAMPLE_PLUGIN ./$1
-cp -pR ./$SAMPLE_PLUGIN ./$1
+echo "cp -pR $PATH_TO_HERE/$SAMPLE_PLUGIN $PATH_TO_HERE/$1"
+cp -pR "$PATH_TO_HERE/$SAMPLE_PLUGIN" "$PATH_TO_HERE/$ARG1"
+if [ $? != 0 ] ; then
+    echo "ERROR: copy of $SAMPLE_PLUGIN failed"
+    exit 1
+fi
 
-# Don't copy junk
-if [ -d "$1/CVS" ] ; then
-    #echo "rm -r $1/CVS"
-    rm -rf $1/CVS
+# Don't copy CVS dir
+if [ -d "$PATH_TO_HERE/$ARG1/CVS" ] ; then
+    echo "rm -rf $PATH_TO_HERE/$ARG1/CVS"
+    rm -rf $PATH_TO_HERE/$ARG1/CVS
 fi
 
 # Don't copy .svn dir
@@ -30,16 +49,34 @@ if [ -d "$PATH_TO_HERE/$ARG1/.svn" ] ; then
 fi
 
 # replace $SAMPLE_PLUGIN within files
-#echo "find $1 -type f -exec perl -pi -e \"s/$SAMPLE_PLUGIN/$1/g\" '{}' \;"
-find $1 -type f -exec perl -pi -e "s/$SAMPLE_PLUGIN/$1/g" '{}' \;
+echo "find $ARG1 -type f -exec perl -pi -e \"s/$SAMPLE_PLUGIN/$ARG1/g\" '{}' \;"
+find $PATH_TO_HERE/$ARG1 -type f -exec perl -pi -e "s/$SAMPLE_PLUGIN/$ARG1/g" '{}' \;
+if [ $? != 0 ] ; then
+    echo "ERROR: find failed"
+    exit 1
+fi
 
 # rename files
-for file in $1/*$SAMPLE_PLUGIN* $1/.deps/*$SAMPLE_PLUGIN* ;do
- mv $file ${file//$SAMPLE_PLUGIN/$1}
+for file in $PATH_TO_HERE/$ARG1/*$SAMPLE_PLUGIN* ;do
+    echo "mv $file `echo $file | sed \"s/$SAMPLE_PLUGIN/$ARG1/\"`"
+    mv $file `echo $file | sed "s/$SAMPLE_PLUGIN/$ARG1/"`
+    if [ $? != 0 ] ; then
+	echo "mv $file `echo $file | sed s/$SAMPLE_PLUGIN/$ARG1/` failed"
+	exit 1
+    fi
 done
 
+# update the Makefile.am
+expression="s/(.*SAMPLE_PLUGIN.*)/\1\n\t$ARG1 \\\\/"
+echo "perl -pi -e '$expression' $PATH_TO_HERE/Makefile.am"
+perl -pi -e "$expression" "$PATH_TO_HERE/Makefile.am"
+if [ $? != 0 ] ; then
+    echo "ERROR: Update of Makefile.am failed"
+    exit 1
+fi
+
 echo "---"
-echo "New plug-in '$1' is ready."
+echo "New plug-in \"$ARG1\" is ready.  A directory for your plug-in was created."
 echo ""
 echo "To add $1 to the build system, you need to edit two files:"
 echo "  Edit plugins/Makefile.am and add a line for your plugin to the SUBDIRS list"
@@ -47,4 +84,6 @@ echo "  Edit configure.ac and add a line for the plugins/$1/Makefile near the en
 echo ""
 echo "You then need to rerun autogen.sh and configure just once to enable your"
 echo "new plugin with the build system."
+echo ""
+echo "Get started coding here: $PATH_TO_HERE/$ARG1"
 echo ""
