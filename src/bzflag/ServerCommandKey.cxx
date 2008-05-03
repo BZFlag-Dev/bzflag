05faa9e5db40f15c90ded3fddbbce16a5a093d8f
Daniel Remenak <dtremenak@users.sourceforge.net>
2008-05-03 08:15:55 +0000

    add missing emacs/vi formatting block.  it is totally cool that I noticed this from visual studio.  long live exTabSettings!
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17119 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/ServerCommandKey.cxx
+++ src/bzflag/ServerCommandKey.cxx
@@ -533,3 +533,11 @@ bool			ServerCommandKey::keyRelease(const BzfKeyEvent& key)
   }
   return keyPress(key);
 }
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
