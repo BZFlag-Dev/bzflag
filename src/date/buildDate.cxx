6f2328ed091edff77ce60132b0fdf95657896f21
Daniel Remenak <dtremenak@users.sourceforge.net>
2008-05-24 07:12:50 +0000

    numbers are cheap, bump the protocol for that bugfix.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17255 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -21,7 +21,7 @@
 
 // opaque version number increments on protocol incompatibility
 #ifndef BZ_PROTO_VERSION
-#  define BZ_PROTO_VERSION	"0061"
+#  define BZ_PROTO_VERSION	"0062"
 #endif
 // ditto for bzrobots
 #ifndef BZROBOTS_PROTO_VERSION
@@ -42,7 +42,7 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		03
+#  define BZ_REV		04
 #endif
 
 // DEVEL | STABLE | MAINT
