e41f25026702d631bc0c3c87d809143eba78a70a
Bryan Jennings <bryjen@users.sourceforge.net>
2008-05-24 19:46:30 +0000

    gcc warned about NULL in 'math'
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17266 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/bzUnicode.h
+++ include/bzUnicode.h
@@ -96,7 +96,7 @@ public:
     inline static std::wstring wideStringFromUTF8(const std::string& nstr)
     {
       std::wstring ret;
-      for (UTF8StringItr itr(nstr.c_str()); (*itr != NULL); ++itr)
+      for (UTF8StringItr itr(nstr.c_str()); (*itr != 0); ++itr)
 	ret += (wchar_t)(*itr);
       return ret;
     }
