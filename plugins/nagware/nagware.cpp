c1df8ab6632ecbc22524ebc336c0a01a6624ee20
Bernt Hansen <bthansen@users.sourceforge.net>
2008-06-01 02:30:07 +0000

    Quell compiler warnings about type conversions
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17349 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/nagware/nagware.cpp
+++ plugins/nagware/nagware.cpp
@@ -153,7 +153,7 @@ void tickEvent (float time)
 }
 
 
-void dispNagMsg (int who, char *label, MsgEnt *m){
+void dispNagMsg (int who, const char *label, MsgEnt *m){
   char msg[140];
 
   if (m->repeat)
@@ -269,7 +269,7 @@ void sendHelp (int who)
   bz_sendTextMessage(BZ_SERVER, who, "NAG commands: off, on, config, reload, list");
 }
 
-bool checkPerms (int playerID, char *nagCmd, const char *permName)
+bool checkPerms (int playerID, const char *nagCmd, const char *permName)
 {
   if (permName==NULL || *permName=='\0')
     permName = "NAG";
@@ -436,7 +436,7 @@ BZF_PLUGIN_CALL int bz_Unload (void)
  * Read Configuration file...
 */
 
-bool configError (char *msg, int linenum, int playerID, FILE *fp){
+bool configError (const char *msg, int linenum, int playerID, FILE *fp){
   char send[256];
   fclose (fp);
   sprintf (send, "+++ nagware config file error (%s) at line #%d", msg, linenum);
