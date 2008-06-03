d408b7ee48a99a0ae8fdba7a26863d1368d7cd5b
Harrison Reiser <bugq@users.sourceforge.net>
2008-06-03 17:09:28 +0000

    skeleton for webadmin plugin created
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17373 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/webadmin/README.txt
@@ -0,0 +1,6 @@
+========================================================================
+    DYNAMIC LINK LIBRARY : webadmin Project Overview
+========================================================================
+
+webadmin is a web-based interface for controlling many aspects of the
+BZFS.  It can be accessed through the game port at "/webadmin".
---
+++ plugins/webadmin/webadmin.cpp
@@ -0,0 +1,27 @@
+// webadmin.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+#include "plugin_utils.h"
+
+BZ_GET_PLUGIN_VERSION
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"webadmin plugin loaded");
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_debugMessage(4,"webadmin plugin unloaded");
+  return 0;
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
