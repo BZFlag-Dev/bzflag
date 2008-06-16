61a1c749d6bb149b1792dc6d9fdf3c7a02cb4fb1
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-06-16 18:12:14 +0000

    add simple plug-in to test shots( world weapon every 5 seconds )
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_99_shot_branch@17563 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/ShotTest/ShotTest.cpp
@@ -0,0 +1,67 @@
+// SAMPLE_PLUGIN.cpp : Defines the entry point for the DLL application.
+//
+
+#include "bzfsAPI.h"
+#include "plugin_utils.h"
+
+BZ_GET_PLUGIN_VERSION
+
+class ShotHandler : public bz_EventHandler
+{
+public:
+
+  double startTime;
+  double increment;
+
+  ShotHandler()
+  {
+    startTime = -1;
+    increment = 5.0;
+  }
+
+  void fireDamnitFire ( void )
+  {
+    if (!bz_anyPlayers())
+      return;
+
+    float origin[3] = {0,0,2};
+    bz_fireWorldWep(NULL,origin);
+  }
+
+  virtual void process ( bz_EventData *eventData )
+  {
+    double now = bz_getCurrentTime();
+    if ( now - startTime > increment )
+    {
+      fireDamnitFire();
+      startTime = now;
+    }
+  }
+};
+
+ShotHandler sh;
+
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"ShotTest plug-in loaded");
+
+  bz_registerEvent(bz_eTickEvent,&sh);
+  return 0;
+}
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_debugMessage(4,"ShotTest plug-in unloaded");
+
+  bz_removeEvent(bz_eTickEvent,&sh);
+
+  return 0;
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
