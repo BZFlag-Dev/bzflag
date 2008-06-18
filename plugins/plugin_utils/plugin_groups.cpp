ea51a3e3bc818862416abfc3caacd18f7436a565
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-06-18 18:27:14 +0000

    add plugin_groups to make searching for groups simpler
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17638 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ plugins/plugin_utils/plugin_groups.cpp
@@ -0,0 +1,109 @@
+/* bzflag
+* Copyright (c) 1993 - 2008 Tim Riker
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#include "plugin_groups.h"
+
+bool permInGroup ( const std::string &perm, bz_APIStringList* groupPerms )
+{
+  for (unsigned int i = 0; i < groupPerms->size(); i++)
+  {
+    if (strcasecmp(perm.c_str(),groupPerms->get(i).c_str()) == 0)
+      return true;
+  }
+  return false;
+}
+
+std::vector<std::string> findGroupsWithPerms( const std::vector<std::string> &perms )
+{
+  std::vector<std::string> groupsWithPerms;
+
+  bz_APIStringList* groupList = bz_getGroupList();
+
+  if (groupList)
+  {
+    for ( unsigned int i = 0; i < groupList->size();i++)
+    {
+      std::string groupName = groupList->get(i).c_str();
+
+      bz_APIStringList *groupPerms = bz_getGroupPerms(groupName.c_str());
+      if (groupPerms)
+      {
+	// see if any of the perms are NOT in this group.
+	bool hasOneWithNoPerm = false;
+	for (size_t p =0; p < perms.size(); p++)
+	{
+	  if (!permInGroup(perms[p],groupPerms))
+	    hasOneWithNoPerm = true;
+	}
+	bz_deleteStringList(groupPerms);
+
+	if (!hasOneWithNoPerm)
+	  groupsWithPerms.push_back(groupName);
+      }
+    }
+    bz_deleteStringList(groupList);
+  }
+
+  return groupsWithPerms;
+}
+
+std::vector<std::string> findGroupsWithPerm( const const char* perm )
+{
+  std::string name;
+  if(perm)
+    name = perm;
+  return findGroupsWithPerm(name);
+}
+
+std::vector<std::string> findGroupsWithPerm( const std::string &perm )
+{
+  std::vector<std::string> groupsWithPerms;
+
+  bz_APIStringList* groupList = bz_getGroupList();
+
+  if (groupList)
+  {
+    for ( unsigned int i = 0; i < groupList->size();i++)
+    {
+      std::string groupName = groupList->get(i).c_str();
+
+      bz_APIStringList *groupPerms = bz_getGroupPerms(groupName.c_str());
+      if (groupPerms)
+      {
+	if (permInGroup(perm,groupPerms))
+	  groupsWithPerms.push_back(groupName);
+
+	bz_deleteStringList(groupPerms);
+      }
+    }
+    bz_deleteStringList(groupList);
+  }
+  return groupsWithPerms;
+}
+
+std::vector<std::string> findGroupsWithAdmin( void )
+{
+  std::vector<std::string> perms;
+  perms.push_back(std::string(bz_perm_kick));
+  perms.push_back(std::string(bz_perm_ban));
+
+  return findGroupsWithPerms(perms);
+}
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
