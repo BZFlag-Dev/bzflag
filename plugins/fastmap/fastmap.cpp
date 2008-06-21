bbd8e304a85824b665480c766217046de93aa269
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-06-21 00:29:10 +0000

    upgrade fastmap to use new central HTTP vdir system. *NOTE* fastmap can not be used with webreport, webstats, or webadmin until they are upgraded to the new system as well.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17696 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/fastmap/fastmap.cpp
+++ plugins/fastmap/fastmap.cpp
@@ -1,109 +1,102 @@
-// webReport.cpp : Defines the entry point for the DLL application.
+// httpTest.cpp : Defines the entry point for the DLL application.
 //
 
+#include <map>
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
-#include "plugin_HTTP.h"
+#include "plugin_HTTPVDIR.h"
+#include <string>
+#include <sstream>
 
-class FastMap : public BZFSHTTPServer
+BZ_GET_PLUGIN_VERSION
+
+class Fastmap : public BZFSHTTPVDir, public bz_EventHandler
 {
 public:
-  FastMap(const char * plugInName);
+  Fastmap(): BZFSHTTPVDir(),mapData(NULL),mapDataSize(0){registerVDir();}
+  virtual ~Fastmap(){};
 
-  virtual bool acceptURL(const char *url) { return handler.mapData && handler.mapDataSize; }
-  virtual void getURLData(const char* url, int requestID, const URLParams &parameters, bool get = true);
+  virtual const char * getVDir ( void ){return "fastmap";}
 
-  class Handler : public bz_EventHandler
+  virtual bool handleRequest ( const HTTPRequest &request, HTTPReply &reply, int userID )
   {
-  public:
-    BZFSHTTPServer *server;
+    reply.returnCode = HTTPReply::e200OK;
+    reply.docType = HTTPReply::eOctetStream;
 
-    virtual ~Handler ( void )
+    if (mapData && mapDataSize)
+    {
+      reply.body.resize(mapDataSize);
+
+      std::stringstream stream(reply.body);
+      stream.write(mapData,(std::streamsize)mapDataSize);
+    }
+    else
+      reply.returnCode = HTTPReply::e404NotFound;
+
+   
+    return true;
+  }
+
+  virtual void process(bz_EventData * eventData)
+  {
+    if (eventData->eventType == bz_eWorldFinalized)
     {
       if (mapData)
 	free(mapData);
 
       mapData = NULL;
       mapDataSize = 0;
-    }
 
-    virtual void process(bz_EventData * eventData)
-    {
-      if (eventData->eventType == bz_eWorldFinalized)
+      if (!bz_getPublic() || bz_getClientWorldDownloadURL().size())
+	return;
+
+      mapDataSize = bz_getWorldCacheSize();
+      if (!mapDataSize)
+	return;
+
+      mapData = (char *) malloc(mapDataSize);
+      if (!mapData)
       {
-	if (mapData)
-	  free(mapData);
-
-	mapData = NULL;
 	mapDataSize = 0;
-
-	if (!server || !bz_getPublic() || bz_getClientWorldDownloadURL().size())
-	  return;
-
-	mapDataSize = bz_getWorldCacheSize();
-	if (!mapDataSize)
-	  return;
-
-	mapData = (char *) malloc(mapDataSize);
-	if (!mapData)
-	{
-	  mapDataSize = 0;
-	  return;
-	}
-
-	bz_getWorldCacheData((unsigned char*)mapData);
-
-	bz_debugMessagef(2, "FastMap: Running local HTTP server for maps using URL %s", server->getBaseServerURL());
-	bz_setClientWorldDownloadURL(server->getBaseServerURL());
+	return;
       }
-    }
-    char *mapData;
-    size_t mapDataSize;
-  };
 
-  Handler handler;
+      bz_getWorldCacheData((unsigned char*)mapData);
+
+      bz_debugMessagef(2, "FastMap: Running local HTTP server for maps using URL %s", getBaseURL().c_str());
+      bz_setClientWorldDownloadURL(getBaseURL().c_str());
+    }
+  }
+  char *mapData;
+  size_t mapDataSize;
 };
 
-FastMap fastmap("fastmap");
+Fastmap *server = NULL;
 
-BZ_GET_PLUGIN_VERSION
-
-BZF_PLUGIN_CALL int bz_Load(const char *commandLine)
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
 {
-  bz_registerEvent(bz_eWorldFinalized, &fastmap.handler);
+  bz_debugMessage(4,"Fastmap plugin loaded");
+  if(server)
+    delete(server);
 
-  bz_setclipFieldString("fastmap_index_description", "Download the map via HTTP");
-  fastmap.startupHTTP();
-  bz_debugMessage(4, "fastmap plugin loaded");
+  server = new Fastmap;
+
+  bz_registerEvent(bz_eWorldFinalized, server);
+
   return 0;
 }
 
 BZF_PLUGIN_CALL int bz_Unload ( void )
 {
-  bz_removeEvent(bz_eWorldFinalized, &fastmap.handler);
+  bz_removeEvent(bz_eWorldFinalized, server);
 
-  fastmap.shutdownHTTP();
-  bz_debugMessage(4, "fastmap plugin unloaded");
+  if(server)
+    delete(server);
+  server = NULL;
+
+  bz_debugMessage(4,"Fastmap plugin unloaded");
   return 0;
 }
-
-// template stuff
-// globals
-
-FastMap::FastMap(const char *plugInName): BZFSHTTPServer(plugInName)
-{
-  handler.mapData = NULL;
-  handler.mapDataSize = 0;
-  handler.server = this;
-}
-
-void FastMap::getURLData(const char* url, int requestID, const URLParams &parameters, bool get)
-{
-  setURLDocType(eOctetStream, requestID);
-  setURLDataSize(handler.mapDataSize, requestID);
-  setURLData(handler.mapData, requestID);
-}
-
 
 // Local Variables: ***
 // mode: C++ ***
