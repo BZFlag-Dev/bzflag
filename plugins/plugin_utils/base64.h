1055c32e4ebfef2c2bb3aa20967efb76f72215e4
Scott Wichser <blast007@users.sourceforge.net>
2008-06-26 06:34:52 +0000

    Add the "Local Variables" block and a newline at the end.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17866 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/base64.h
+++ plugins/plugin_utils/base64.h
@@ -167,3 +167,14 @@ namespace base64
 } // end namespace
 
 #endif
+
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
+
