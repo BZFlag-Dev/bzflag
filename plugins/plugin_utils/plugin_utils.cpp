306a15f54495249e34173771aa942124acb3658a
Bernt Hansen <bthansen@users.sourceforge.net>
2008-06-26 16:55:58 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17878 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/plugin_utils.cpp
+++ plugins/plugin_utils/plugin_utils.cpp
@@ -1,14 +1,14 @@
 /* bzflag
-* Copyright (c) 1993 - 2008 Tim Riker
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -36,9 +36,9 @@ std::string base64_decode(const std::string &text)
   return dest;
 }
 
-const char* bzu_GetTeamName( bz_eTeamType team )
+const char* bzu_GetTeamName(bz_eTeamType team)
 {
-  switch (team){
+  switch (team) {
 
   case eRedTeam:
     return "Red";
@@ -71,7 +71,7 @@ const char* bzu_GetTeamName( bz_eTeamType team )
   return "Unknown";
 }
 
-std::string printTime ( bz_Time *ts, const char* _timezone )
+std::string printTime(bz_Time *ts, const char* _timezone)
 {
   std::string time;
   appendTime(time,ts,_timezone);
@@ -80,37 +80,35 @@ std::string printTime ( bz_Time *ts, const char* _timezone )
 
 //Date: Mon, 23 Jun 2008 17:50:22 GMT
 
-void appendTime ( std::string & text, bz_Time *ts, const char* _timezone )
+void appendTime(std::string & text, bz_Time *ts, const char* _timezone)
 {
-  switch(ts->dayofweek)
-  {
-    case 1:
-      text += "Mon";
-      break;
-    case 2:
-      text += "Tue";
-      break;
-    case 3:
-      text += "Wed";
-      break;
-    case 4:
-      text += "Thu";
-      break;
-    case 5:
-      text += "Fri";
-      break;
-    case 6:
-      text += "Sat";
-      break;
-    case 0:
-      text += "Sun";
-      break;
+  switch(ts->dayofweek) {
+  case 1:
+    text += "Mon";
+    break;
+  case 2:
+    text += "Tue";
+    break;
+  case 3:
+    text += "Wed";
+    break;
+  case 4:
+    text += "Thu";
+    break;
+  case 5:
+    text += "Fri";
+    break;
+  case 6:
+    text += "Sat";
+    break;
+  case 0:
+    text += "Sun";
+    break;
   }
 
   text += format(", %d ",ts->day);
-  
-  switch(ts->month)
-  {
+
+  switch(ts->month) {
   case 0:
     text += "Jan";
     break;
@@ -147,7 +145,7 @@ void appendTime ( std::string & text, bz_Time *ts, const char* _timezone )
   case 11:
     text += "Dec";
     break;
- }
+  }
 
   text += format(" %d %d:%d:%d ",ts->year,ts->hour,ts->minute,ts->second);
   if (_timezone)
@@ -237,14 +235,15 @@ std::string format(const char* fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  char	temp[2048];
+  char temp[2048];
   vsprintf(temp,fmt, args);
   std::string result = temp;
   va_end(args);
   return result;
 }
 
-std::vector<std::string> tokenize(const std::string& in, const std::string &delims, const int maxTokens, const bool useQuotes, size_t offset)
+std::vector<std::string> tokenize(const std::string& in, const std::string &delims,
+				  const int maxTokens, const bool useQuotes, size_t offset)
 {
   std::vector<std::string> tokens;
   int numTokens = 0;
@@ -263,7 +262,7 @@ std::vector<std::string> tokenize(const std::string& in, const std::string &deli
     bool foundSlash = false;
 
     currentChar = (pos < in.size()) ? in[pos] : -1;
-    while ((currentChar != -1) && !tokenDone){
+    while ((currentChar != -1) && !tokenDone) {
 
       tokenDone = false;
 
@@ -272,48 +271,48 @@ std::vector<std::string> tokenize(const std::string& in, const std::string &deli
 	break; // breaks out of while loop
       }
 
-      if (!useQuotes){
+      if (!useQuotes) {
 	currentToken << char(currentChar);
       } else {
 
-	switch (currentChar){
-	  case '\\' : // found a backslash
-	    if (foundSlash){
-	      currentToken << char(currentChar);
-	      foundSlash = false;
-	    } else {
-	      foundSlash = true;
-	    }
-	    break;
-	  case '\"' : // found a quote
-	    if (foundSlash){ // found \"
-	      currentToken << char(currentChar);
-	      foundSlash = false;
-	    } else { // found unescaped "
-	      if (inQuote){ // exiting a quote
-		// finish off current token
-		tokenDone = true;
-		inQuote = false;
-		//slurp off one additional delimeter if possible
-		if (pos+1 < in.size() &&
-		  delims.find(in[pos+1]) != std::string::npos) {
-		    pos++;
-		  }
-
-	      } else { // entering a quote
-		// finish off current token
-		tokenDone = true;
-		inQuote = true;
-	      }
-	    }
-	    break;
-	  default:
-	    if (foundSlash){ // don't care about slashes except for above cases
-	      currentToken << '\\';
-	      foundSlash = false;
-	    }
+	switch (currentChar) {
+	case '\\' : // found a backslash
+	  if (foundSlash) {
 	    currentToken << char(currentChar);
-	    break;
+	    foundSlash = false;
+	  } else {
+	    foundSlash = true;
+	  }
+	  break;
+	case '\"' : // found a quote
+	  if (foundSlash) { // found \"
+	    currentToken << char(currentChar);
+	    foundSlash = false;
+	  } else { // found unescaped "
+	    if (inQuote) { // exiting a quote
+	      // finish off current token
+	      tokenDone = true;
+	      inQuote = false;
+	      //slurp off one additional delimeter if possible
+	      if (pos+1 < in.size() &&
+		  delims.find(in[pos+1]) != std::string::npos) {
+		pos++;
+	      }
+
+	    } else { // entering a quote
+	      // finish off current token
+	      tokenDone = true;
+	      inQuote = true;
+	    }
+	  }
+	  break;
+	default:
+	  if (foundSlash) { // don't care about slashes except for above cases
+	    currentToken << '\\';
+	    foundSlash = false;
+	  }
+	  currentToken << char(currentChar);
+	  break;
 	}
       }
 
@@ -391,7 +390,7 @@ std::string url_decode(const std::string &text)
   std::string destination;
 
   std::string::const_iterator itr = text.begin();
-  while ( itr != text.end() ) {
+  while (itr != text.end()) {
     if (*itr != '%') {
       destination += *itr++;
     } else {
@@ -411,7 +410,7 @@ std::string url_decode(const std::string &text)
 
       int val = 0;
       sscanf(hex,"%x",&val);
-      if ( val != 0 )
+      if (val != 0)
 	destination += (char)val;
       itr++;
     }
@@ -421,19 +420,14 @@ std::string url_decode(const std::string &text)
 
 size_t find_first_substr(const std::string &findin, const std::string findwhat, size_t offset)
 {
-  if (findwhat.size())
-  {
-    for ( size_t f = offset; f < findin.size(); f++ )
-    {
-      if (findin[f] == findwhat[0])
-      {
+  if (findwhat.size()) {
+    for (size_t f = offset; f < findin.size(); f++) {
+      if (findin[f] == findwhat[0]) {
 	size_t start = f;
-	for ( size_t w = 1; w < findwhat.size(); w++ )
-	{
-	  if ( f+w > findin.size())
+	for (size_t w = 1; w < findwhat.size(); w++) {
+	  if (f+w > findin.size())
 	    return std::string::npos;
-	  if (findin[f+w] != findwhat[w])
-	  {
+	  if (findin[f+w] != findwhat[w]) {
 	    f+=w;
 	    w = findwhat.size();
 	  }
@@ -446,12 +440,10 @@ size_t find_first_substr(const std::string &findin, const std::string findwhat, 
   return std::string::npos;
 }
 
-void trimLeadingWhitespace ( std::string &text )
+void trimLeadingWhitespace(std::string &text)
 {
-  for(size_t s = 0; s < text.size(); s++)
-  {
-    if (!isWhitespace(text[s]))
-    {
+  for(size_t s = 0; s < text.size(); s++) {
+    if (!isWhitespace(text[s])) {
       if (s)
 	text.erase(text.begin()+s-1);
       return;
@@ -459,7 +451,7 @@ void trimLeadingWhitespace ( std::string &text )
   }
 }
 
-std::string trimLeadingWhitespace ( const std::string &text )
+std::string trimLeadingWhitespace(const std::string &text)
 {
   std::string s = text;
   trimLeadingWhitespace(s);
