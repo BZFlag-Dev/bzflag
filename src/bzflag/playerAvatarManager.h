5acb9cf4727ff84ff00348b21bf2770de5220a80
Bernt Hansen <bthansen@users.sourceforge.net>
2008-06-29 22:15:47 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17944 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzflag/playerAvatarManager.h
+++ src/bzflag/playerAvatarManager.h
@@ -1,65 +1,63 @@
 /* bzflag
-* Copyright (c) 1993 - 2008 Tim Riker
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
-#ifndef	__PLAYER_AVATAR_MANAGER_H__
-#define	__PLAYER_AVATAR_MANAGER_H__
+#ifndef __PLAYER_AVATAR_MANAGER_H__
+#define __PLAYER_AVATAR_MANAGER_H__
 
 #include "common.h"
 #include "global.h"
 
-typedef enum
-{
+typedef enum {
   eNormal,
   eFat,
   eTiny,
   eThin,
   eThief
-}teAvatarScaleModes;
+} teAvatarScaleModes;
 
 class SceneNode;
 
 class PlayerAvatar
 {
 public:
-  virtual ~PlayerAvatar ( void ){};
+  virtual ~PlayerAvatar(void) {};
 
-  virtual void move ( const float pos[3], const float forward[3] ) = 0;
-  virtual void moveIDL ( const float plane[4] ) = 0;
-  virtual void movePause ( const float pos[3], float rad ) = 0;
+  virtual void move(const float pos[3], const float forward[3]) = 0;
+  virtual void moveIDL(const float plane[4]) = 0;
+  virtual void movePause(const float pos[3], float rad) = 0;
 
-  virtual void setTurnOffsets ( const float left, const float right ) = 0;
+  virtual void setTurnOffsets(const float left, const float right) = 0;
 
-  virtual void setScale ( const float scale[3] ) = 0;
-  virtual void setScale ( teAvatarScaleModes mode ) = 0;
+  virtual void setScale(const float scale[3]) = 0;
+  virtual void setScale(teAvatarScaleModes mode) = 0;
 
-  virtual void explode ( void ) = 0;
-  virtual void setVisualTeam (TeamColor visualTeam, const float color[4] ) = 0;
-  virtual void setColor ( const float color[4] ) = 0;
+  virtual void explode(void) = 0;
+  virtual void setVisualTeam(TeamColor visualTeam, const float color[4]) = 0;
+  virtual void setColor(const float color[4]) = 0;
 
-  virtual void setVisualMode ( bool inCockpit, bool showTreads ) = 0;
-  virtual void setAnimationValues ( float explodeParam, float jumpParam ) = 0;
+  virtual void setVisualMode(bool inCockpit, bool showTreads) = 0;
+  virtual void setAnimationValues(float explodeParam, float jumpParam) = 0;
 
-  virtual void setClippingPlane (  const float plane[4] ) = 0;
+  virtual void setClippingPlane( const float plane[4]) = 0;
 
-  virtual void renderRadar ( void ) = 0;
+  virtual void renderRadar(void) = 0;
 
-  virtual std::vector<SceneNode*> getSceneNodes ( void ) = 0;
-  virtual std::vector<SceneNode*> getIDLSceneNodes ( void ) = 0;
-  virtual std::vector<SceneNode*> getPauseSceneNodes ( void ) = 0;
+  virtual std::vector<SceneNode*> getSceneNodes(void) = 0;
+  virtual std::vector<SceneNode*> getIDLSceneNodes(void) = 0;
+  virtual std::vector<SceneNode*> getPauseSceneNodes(void) = 0;
 };
 
-PlayerAvatar * getPlayerAvatar ( int playerID, const float pos[3], const float forward[3]  ); 
-void  freePlayerAvatar ( PlayerAvatar *avatar );
-
+PlayerAvatar * getPlayerAvatar(int playerID, const float pos[3], const float forward[3]);
+void freePlayerAvatar(PlayerAvatar *avatar);
 
 #endif /* __PLAYER_AVATAR_MANAGER_H__ */
 
