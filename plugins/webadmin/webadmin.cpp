0386888437db1b3e91e3ba739af5f9edae251cfc
Harrison Reiser <bugq@users.sourceforge.net>
2008-07-01 05:40:38 +0000

    unfinished backend workings
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17959 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/webadmin/webadmin.cpp
+++ plugins/webadmin/webadmin.cpp
@@ -4,11 +4,13 @@
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
 #include "plugin_HTTP.h"
+#include <fstream>
+#include <cstring>
 
 class WebAdmin : public BZFSHTTPAuth, TemplateCallbackClass
 {
 public:
-	WebAdmin():BZFSHTTPAuth(){registerVDir();}
+	WebAdmin():BZFSHTTPAuth(),it(bzu_standardPerms.end()){registerVDir();}
 
 	virtual const char * getVDir ( void ){return "webadmin";}
 	virtual const char * getDescription ( void ){return "Server Administration (Login Required)";}
@@ -21,24 +23,23 @@ public:
   virtual void keyCallback (std::string &data, const std::string &key);
   virtual bool loopCallback (const std::string &key);
   virtual bool ifCallback (const std::string &key);
-  
+
 private:
+	std::map<std::string,std::string> tmplvars;
+	std::vector<std::string>::const_iterator it;
 };
 
 WebAdmin *webAdmin = NULL;
 
 BZ_GET_PLUGIN_VERSION
 
+
 BZF_PLUGIN_CALL int bz_Load(const char* commandLine)
 {
 	if(webAdmin)
 		delete(webAdmin);
 	webAdmin = new WebAdmin;
-
-  if (commandLine && strlen(commandLine))
-    webAdmin->init(commandLine);
-  else
-    webAdmin->init("./");
+	webAdmin->init(commandLine);
 
   bz_debugMessage(4,"webadmin plugin loaded");
   return 0;
@@ -54,18 +55,27 @@ BZF_PLUGIN_CALL int bz_Unload(void)
   return 0;
 }
 
-void WebAdmin::init(const char* tDir)
+void WebAdmin::init(const char* cmdln)
 {
-  if (tDir)
-    templateSystem.addSearchPath(tDir);
+  templateSystem.addSearchPath(cmdln ? cmdln : "./");
 
 	// level one has admin perms
 	addPermToLevel(1,"ADMIN");
-
+	
+	templateSystem.addIF("IsCurrentPage",this);
   templateSystem.addIF("Error",this);
-  templateSystem.addKey("Error",this);
 
-  templateSystem.setPluginName("WebReport", getBaseURL().c_str());
+  templateSystem.addKey("Error",this);
+	templateSystem.addKey("Callsign",this);
+	templateSystem.addKey("BannedUser",this);
+	templateSystem.addKey("PageName",this);
+	
+	templateSystem.addLoop("Navigation",this);
+	templateSystem.addLoop("Players",this);
+	templateSystem.addLoop("IPBanList",this);
+	templateSystem.addLoop("IDBanList",this);
+
+  templateSystem.setPluginName("webadmin", getBaseURL().c_str());
 
 	setupAuth();
 }
@@ -73,27 +83,34 @@ void WebAdmin::init(const char* tDir)
 // event hook for [$Something] in templates
 void WebAdmin::keyCallback (std::string &data, const std::string &key)
 {
+	std::map<std::string,std::string>::iterator pair = tmplvars.find(key);
+	if (pair != tmplvars.end()) data = pair->second;
 }
 
 // condition check for [*START] in templates
 bool WebAdmin::loopCallback (const std::string &key)
 {
+	if (key == "players") {
+		if (it == bzu_standardPerms.end()) return false;
+		tmplvars["callsign"] = *it++;
+		return true;
+	}
   return false;
 }
 
 // condition check for [?IF] in templates
 bool WebAdmin::ifCallback (const std::string &key)
 {
-  return false; 
+  return false;
 }
 
 bool WebAdmin::handleAuthedRequest ( int level, const HTTPRequest &request, HTTPReply &reply )
 {
   std::string &page = reply.body;
 
-	if (level == 1)
-		page = format("authenticated sessionID %d",request.sessionID);
-	else if ( level == VERIFIED)	// valid login but does not have admin groups
+	if (level == 1) {
+		page = format("vdir=%s, resource=%s, baseURL=%s", request.vdir.c_str(), request.resource.c_str(), request.baseURL.c_str());
+	} else if ( level == VERIFIED)	// valid login but does not have admin groups
 		page = format("Not authenticated(Verified) sessionID %d",request.sessionID);
 	else
 		page = format("Not authenticated sessionID %d",request.sessionID);
@@ -101,6 +118,7 @@ bool WebAdmin::handleAuthedRequest ( int level, const HTTPRequest &request, HTTP
 	reply.docType = HTTPReply::eHTML;
 	//templateSystem.processTemplate(reply.body, )
 
+	tmplvars.clear();
 	return true;
 }
 
