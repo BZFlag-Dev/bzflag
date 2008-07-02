edfaa1be860cc11df8120c4d9359d5f3e07f9ea8
Harrison Reiser <bugq@users.sourceforge.net>
2008-07-02 03:20:12 +0000

    theoretically functional kicking page
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17964 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/webadmin/webadmin.cpp
+++ plugins/webadmin/webadmin.cpp
@@ -10,14 +10,14 @@
 class WebAdmin : public BZFSHTTPAuth, TemplateCallbackClass
 {
 public:
-	WebAdmin():BZFSHTTPAuth(),it(bzu_standardPerms.end()){registerVDir();}
+  WebAdmin::WebAdmin():BZFSHTTPAuth(),it(NULL) {registerVDir();}
 
-	virtual const char * getVDir ( void ){return "webadmin";}
-	virtual const char * getDescription ( void ){return "Server Administration (Login Required)";}
+  virtual const char * getVDir ( void ){return "webadmin";}
+  virtual const char * getDescription ( void ){return "Server Administration (Login Required)";}
 
   void init (const char *tDir);
 
-	virtual bool handleAuthedRequest ( int level, const HTTPRequest &request, HTTPReply &reply );
+  virtual bool handleAuthedRequest ( int level, const HTTPRequest &request, HTTPReply &reply );
  
   // from TemplateCallbackClass
   virtual void keyCallback (std::string &data, const std::string &key);
@@ -25,21 +25,31 @@ public:
   virtual bool ifCallback (const std::string &key);
 
 private:
-	std::map<std::string,std::string> tmplvars;
-	std::vector<std::string>::const_iterator it;
+  typedef std::vector<std::string>::const_iterator loop_pos;
+  loop_pos *it;
+  std::map<std::string,std::string> tmplvars;
 };
 
 WebAdmin *webAdmin = NULL;
+
+//typedef void (WebAdmin::*controller)(const HTTPRequest &);
+//std::map<std::string,controller> controllers;
+const std::vector<std::string> pages = tokenize(
+    "main "
+    "banlist "
+    "helpmsg "
+    "group "
+  , std::string(" "), 0, false);
 
 BZ_GET_PLUGIN_VERSION
 
 
 BZF_PLUGIN_CALL int bz_Load(const char* commandLine)
 {
-	if(webAdmin)
-		delete(webAdmin);
-	webAdmin = new WebAdmin;
-	webAdmin->init(commandLine);
+  if(webAdmin)
+    delete(webAdmin);
+  webAdmin = new WebAdmin;
+  webAdmin->init(commandLine);
 
   bz_debugMessage(4,"webadmin plugin loaded");
   return 0;
@@ -47,9 +57,9 @@ BZF_PLUGIN_CALL int bz_Load(const char* commandLine)
 
 BZF_PLUGIN_CALL int bz_Unload(void)
 {
-	if(webAdmin)
-		delete(webAdmin);
-	webAdmin = NULL;
+  if(webAdmin)
+    delete(webAdmin);
+  webAdmin = NULL;
 
   bz_debugMessage(4,"webadmin plugin unloaded");
   return 0;
@@ -59,67 +69,99 @@ void WebAdmin::init(const char* cmdln)
 {
   templateSystem.addSearchPath(cmdln ? cmdln : "./");
 
-	// level one has admin perms
-	addPermToLevel(1,"ADMIN");
-	
-	templateSystem.addIF("IsCurrentPage",this);
+  // level one has admin perms
+  addPermToLevel(1,"ADMIN");
+  
+  templateSystem.addIF("IsCurrentPage",this);
   templateSystem.addIF("Error",this);
 
   templateSystem.addKey("Error",this);
-	templateSystem.addKey("Callsign",this);
-	templateSystem.addKey("BannedUser",this);
-	templateSystem.addKey("PageName",this);
-	
-	templateSystem.addLoop("Navigation",this);
-	templateSystem.addLoop("Players",this);
-	templateSystem.addLoop("IPBanList",this);
-	templateSystem.addLoop("IDBanList",this);
+  templateSystem.addKey("Callsign",this);
+  templateSystem.addKey("BannedUser",this);
+  templateSystem.addKey("PageName",this);
+  
+  templateSystem.addLoop("Navigation",this);
+  templateSystem.addLoop("Players",this);
+  templateSystem.addLoop("IPBanList",this);
+  templateSystem.addLoop("IDBanList",this);
 
   templateSystem.setPluginName("webadmin", getBaseURL().c_str());
 
-	setupAuth();
+  setupAuth();
 }
 
 // event hook for [$Something] in templates
 void WebAdmin::keyCallback (std::string &data, const std::string &key)
 {
-	std::map<std::string,std::string>::iterator pair = tmplvars.find(key);
-	if (pair != tmplvars.end()) data = pair->second;
+  const std::map<std::string,std::string>::iterator &pair = tmplvars.find(key);
+  if (pair != tmplvars.end()) data = pair->second;
 }
 
 // condition check for [*START] in templates
 bool WebAdmin::loopCallback (const std::string &key)
 {
-	if (key == "players") {
-		if (it == bzu_standardPerms.end()) return false;
-		tmplvars["callsign"] = *it++;
-		return true;
-	}
-  return false;
+  if (key == "players") {
+    if (!it) it = new loop_pos(bzu_standardPerms.begin());
+    else if (*it != bzu_standardPerms.end()) {
+      tmplvars["callsign"] = **it++;
+      return true;
+    }
+  } else if (key == "navigation") {
+    if (!it) it = new loop_pos(pages.begin());
+    else if (*it != pages.end()) {
+      tmplvars["pagename"] = **it++;
+      return true;
+    }
+  } else return false;
+  delete(it);
+  return it = NULL;
 }
 
 // condition check for [?IF] in templates
 bool WebAdmin::ifCallback (const std::string &key)
 {
+  if (key == "iscurrentpage")
+    return tmplvars["pagename"] == tmplvars["currentpage"];
   return false;
 }
 
 bool WebAdmin::handleAuthedRequest ( int level, const HTTPRequest &request, HTTPReply &reply )
 {
-  std::string &page = reply.body;
+  loop_pos page_iter;
+  size_t last;
+  std::string pagename = request.resource;
 
-	if (level == 1) {
-		page = format("vdir=%s, resource=%s, baseURL=%s", request.vdir.c_str(), request.resource.c_str(), request.baseURL.c_str());
-	} else if ( level == VERIFIED)	// valid login but does not have admin groups
-		page = format("Not authenticated(Verified) sessionID %d",request.sessionID);
-	else
-		page = format("Not authenticated sessionID %d",request.sessionID);
+  switch(level) {
+  case 1:
+  case VERIFIED:
+    last = pagename.size()-1;
+    if (pagename[last] == '/') pagename.erase(last);
+    if (pagename == "") pagename = "main";
+    // std::map<std::string,controller>::iterator pair = controllers.find(pagename);
+    page_iter = find(pages.begin(), pages.end(), pagename);
+    // if (pair != controllers.end()) {
+    if (page_iter != pages.end()) {
+      // if (pair->second) (this->*(pair->second))(request);
+      if (!templateSystem.processTemplateFile(reply.body, (pagename + ".tmpl").c_str())) {
+        reply.returnCode = HTTPReply::e500ServerError;
+        if (!templateSystem.processTemplateFile(reply.body, "505.tmpl"))
+          reply.body = format("Missing template: %s.tmpl", pagename.c_str());
+      }
+    } else {
+      reply.returnCode = HTTPReply::e404NotFound;
+      if (!templateSystem.processTemplateFile(reply.body, "404.tmpl"))
+        reply.body = format("No such resource: %s", pagename.c_str());
+    }
+    break;
+  //reply.body = format("Not authenticated(Verified) sessionID %d",request.sessionID);
+  default:
+    reply.body = format("Not authenticated sessionID %d",request.sessionID);
+  }
 
-	reply.docType = HTTPReply::eHTML;
-	//templateSystem.processTemplate(reply.body, )
+  reply.docType = HTTPReply::eHTML;
 
-	tmplvars.clear();
-	return true;
+  tmplvars.clear();
+  return true;
 }
 
 
