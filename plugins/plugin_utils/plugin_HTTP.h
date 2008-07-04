cc63dee56f8e0432bc57b9e2db8db56ca0f87d6b
Bernt Hansen <bthansen@users.sourceforge.net>
2008-07-04 14:50:05 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@17989 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/plugin_HTTP.h
+++ plugins/plugin_utils/plugin_HTTP.h
@@ -1,14 +1,14 @@
 /* bzflag
-* Copyright (c) 1993 - 2008 Tim Riker
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 // a base class for plugins that want to do HTTP
 #ifndef _PLUGIN_HTTP_H_
@@ -20,22 +20,22 @@
 #include "bzfsAPI.h"
 
 // called to get the code for a template variable
-typedef void (*TemplateKeyCallback) ( std::string &data, const std::string &key );
+typedef void (*TemplateKeyCallback)(std::string &data, const std::string &key);
 
 // called for logic statements, loops and if tests
 // called repeatedly for each loop to know if it should be done.
 // return true to do an instance of the loop
 // return false to stop and continue the template
 // for if test called to determine true or false
-typedef bool (*TemplateTestCallback) ( const std::string &key );
+typedef bool (*TemplateTestCallback)(const std::string &key);
 
 class TemplateCallbackClass
 {
 public:
-  virtual ~TemplateCallbackClass(){};
-  virtual void keyCallback ( std::string & /* data */, const std::string & /* key */ ){};
-  virtual bool loopCallback ( const std::string &/* key */ ){return false;}
-  virtual bool ifCallback ( const std::string &/* key */ ){return false;}
+  virtual ~TemplateCallbackClass() {};
+  virtual void keyCallback(std::string & /* data */, const std::string & /* key */) {};
+  virtual bool loopCallback(const std::string &/* key */) { return false; }
+  virtual bool ifCallback(const std::string &/* key */) { return false; }
 };
 
 class Templateiser : public TemplateCallbackClass
@@ -44,34 +44,34 @@ public:
   Templateiser();
   virtual ~Templateiser();
 
-  void addKey ( const char *key, TemplateKeyCallback callback );
-  void addKey ( const char *key, TemplateCallbackClass *callback );
-  void clearKey ( const char *key );
-  void flushKeys ( void );
+  void addKey(const char *key, TemplateKeyCallback callback);
+  void addKey(const char *key, TemplateCallbackClass *callback);
+  void clearKey(const char *key);
+  void flushKeys(void);
 
-  void addLoop ( const char *loop, TemplateTestCallback callback );
-  void addLoop ( const char *loop, TemplateCallbackClass *callback );
-  void clearLoop ( const char *loop );
-  void flushLoops ( void );
+  void addLoop(const char *loop, TemplateTestCallback callback);
+  void addLoop(const char *loop, TemplateCallbackClass *callback);
+  void clearLoop(const char *loop);
+  void flushLoops(void);
 
-  void addIF ( const char *name, TemplateTestCallback callback );
-  void addIF ( const char *name, TemplateCallbackClass *callback );
-  void clearIF ( const char *name );
-  void flushIFs ( void );
+  void addIF(const char *name, TemplateTestCallback callback);
+  void addIF(const char *name, TemplateCallbackClass *callback);
+  void clearIF(const char *name);
+  void flushIFs(void);
 
-  void processTemplate ( std::string &code, const std::string &templateText );
-  bool processTemplateFile ( std::string &code, const char *file );
+  void processTemplate(std::string &code, const std::string &templateText);
+  bool processTemplateFile(std::string &code, const char *file);
 
   // for the default template tokens
-  virtual void keyCallback ( std::string &data, const std::string &key );
-  virtual bool loopCallback ( const std::string &key );
-  virtual bool ifCallback ( const std::string &key );
+  virtual void keyCallback(std::string &data, const std::string &key);
+  virtual bool loopCallback(const std::string &key);
+  virtual bool ifCallback(const std::string &key);
 
-  void startTimer ( void );
-  void setPluginName ( const char* name, const char* URL );
+  void startTimer(void);
+  void setPluginName(const char* name, const char* URL);
 
-  void addSearchPath ( const char* path );
-  void flushSearchPaths ( void );
+  void addSearchPath(const char* path);
+  void flushSearchPaths(void);
 
 protected:
   typedef std::map<std::string,TemplateKeyCallback> KeyMap;
@@ -85,21 +85,26 @@ protected:
   ClassMap loopClassCallbacks;
   ClassMap ifClassCallbacks;
 
-  bool callKey ( std::string &data, const std::string &key );
-  bool callLoop ( const std::string &key );
-  bool callIF ( const std::string &key );
+  bool callKey(std::string &data, const std::string &key);
+  bool callLoop(const std::string &key);
+  bool callIF(const std::string &key);
 
-  void setDefaultTokens ( void );
+  void setDefaultTokens(void);
 
 private:
-  std::string::const_iterator readKey ( std::string &key, std::string::const_iterator inItr, const std::string &str );
-  std::string::const_iterator findNextTag ( const std::vector<std::string> &keys, std::string &endKey, std::string &code, std::string::const_iterator inItr, const std::string &str );
+  std::string::const_iterator readKey(std::string &key,
+				      std::string::const_iterator inItr,
+				      const std::string &str);
+  std::string::const_iterator findNextTag(const std::vector<std::string> &keys,
+					  std::string &endKey, std::string &code,
+					  std::string::const_iterator inItr,
+					  const std::string &str);
 
-  void processInclude ( std::string &code, std::string::const_iterator &inItr, const std::string &str );
-  void processComment ( std::string &code, std::string::const_iterator &inItr, const std::string &str );
-  void replaceVar ( std::string &code, std::string::const_iterator &itr, const std::string &str );
-  void processIF ( std::string &code, std::string::const_iterator &inItr, const std::string &str );
-  void processLoop ( std::string &code, std::string::const_iterator &inItr, const std::string &str );
+  void processInclude(std::string &code, std::string::const_iterator &inItr, const std::string &str);
+  void processComment(std::string &code, std::string::const_iterator &inItr, const std::string &str);
+  void replaceVar(std::string &code, std::string::const_iterator &itr, const std::string &str);
+  void processIF(std::string &code, std::string::const_iterator &inItr, const std::string &str);
+  void processLoop(std::string &code, std::string::const_iterator &inItr, const std::string &str);
 
   double startTime;
   std::string pluginName,baseURL;
@@ -107,8 +112,7 @@ private:
   std::vector<std::string> filePaths;
 };
 
-typedef enum 
-{
+typedef enum {
   eUnknown = 0,
   eHead,
   eGet,
@@ -124,7 +128,7 @@ typedef enum
 class HTTPRequest
 {
 public:
-  HTTPRequest() : requestID(-1), request(eUnknown){};
+  HTTPRequest() : requestID(-1), request(eUnknown) {};
 
   int		  sessionID;
   int		  requestID;
@@ -152,37 +156,35 @@ public:
   std::string username;
   std::string password;
 
-  bool getParam ( const char* param, std::string &val ) const; 
-  bool getParam ( const std::string &param, std::string &val ) const;
+  bool getParam(const char* param, std::string &val) const;
+  bool getParam(const std::string &param, std::string &val) const;
 
-  bool getParam ( const char* param, std::vector<std::string> &val ) const; 
-  bool getParam ( const std::string &param, std::vector<std::string> &val ) const;
+  bool getParam(const char* param, std::vector<std::string> &val) const;
+  bool getParam(const std::string &param, std::vector<std::string> &val) const;
 
 };
 
 class HTTPReply
 {
 public:
-  HTTPReply(): docType(eText), returnCode(e404NotFound), forceNoCache(true){};
+  HTTPReply(): docType(eText), returnCode(e404NotFound), forceNoCache(true) {};
 
-  typedef enum
-  {
+  typedef enum {
     eText,
     eOctetStream,
     eBinary,
     eHTML,
     eOther
-  }DocumentType;
+  } DocumentType;
 
-  typedef enum
-  {
+  typedef enum {
     e200OK,
     e301Redirect,
     e401Unauthorized,
     e403Forbiden,
     e404NotFound,
     e500ServerError
-  }ReturnCode;
+  } ReturnCode;
 
   DocumentType docType;
   ReturnCode  returnCode;
@@ -201,35 +203,34 @@ public:
   // content info
   std::string lastUpdateTime;
   std::string md5;
-  
+
   std::string redirectLoc;
 };
 
-class BZFSHTTP 
+class BZFSHTTP
 {
 public:
   BZFSHTTP();
   virtual ~BZFSHTTP();
 
-  typedef enum
-  {
+  typedef enum {
     e404 = 0, // not found
     e200,     // ok
     eDeffer
-  }PageStatus;
+  } PageStatus;
 
-  void registerVDir ( void );
+  void registerVDir(void);
 
-  virtual const char * getVDir ( void ) = 0;
-  virtual const char * getDescription ( void ){return "";}
-  virtual bool supportPut ( void ){return false;}
+  virtual const char * getVDir(void) = 0;
+  virtual const char * getDescription(void) { return ""; }
+  virtual bool supportPut(void) { return false; }
 
-  virtual bool handleRequest ( const HTTPRequest &request, HTTPReply &reply ) = 0;
+  virtual bool handleRequest(const HTTPRequest &request, HTTPReply &reply) = 0;
 
-  virtual bool resumeTask (  int /*requestID*/ ) {return true;}
+  virtual bool resumeTask(int /*requestID*/) {return true; }
 
 protected:
-  std::string getBaseURL ( void );
+  std::string getBaseURL(void);
 };
 
 #define UNAUTHENTICATED -1
@@ -239,28 +240,28 @@ class BZFSHTTPAuth : public BZFSHTTP
 {
 public:
   BZFSHTTPAuth();
-  virtual ~BZFSHTTPAuth(){};
+  virtual ~BZFSHTTPAuth() {};
 
   // called to init the auth system, call in the derived class constructor
-  void setupAuth ( void );
+  void setupAuth(void);
 
-  virtual const char * getVDir ( void ){return NULL;}
+  virtual const char * getVDir(void) { return NULL; }
 
   // do not overide these, they are used buy the auth system
   // use the 2 variants below
-  virtual bool handleRequest ( const HTTPRequest &request, HTTPReply &reply );
-  virtual bool resumeTask (  int requestID );
+  virtual bool handleRequest(const HTTPRequest &request, HTTPReply &reply);
+  virtual bool resumeTask(int requestID);
 
   // authed versions of the main callbacks
-  virtual bool handleAuthedRequest ( int level, const HTTPRequest &request, HTTPReply &reply ) = 0;
-  virtual bool resumeAuthedTask ( int /*requestID*/ ){return true;}
+  virtual bool handleAuthedRequest(int level, const HTTPRequest &request, HTTPReply &reply) = 0;
+  virtual bool resumeAuthedTask(int /*requestID*/) { return true; }
 
 protected:
   Templateiser	templateSystem;
 
   class TSURLCallback : public TemplateCallbackClass
   {
-  public: 
+  public:
     virtual void keyCallback(std::string &data, const std::string &key);
 
     std::string URL;
@@ -281,18 +282,17 @@ protected:
 
   std::vector<int> defferedAuthedRequests;
 
-  void addPermToLevel ( int level, const std::string &perm );
+  void addPermToLevel(int level, const std::string &perm);
 
-  int getLevelFromGroups (const std::vector<std::string> &groups );
+  int getLevelFromGroups(const std::vector<std::string> &groups);
 private:
-  void flushTasks ( void );
-  bool verifyToken ( const HTTPRequest &request, HTTPReply &reply );
+  void flushTasks(void);
+  bool verifyToken(const HTTPRequest &request, HTTPReply &reply);
 
-  typedef struct  
-  {
+  typedef struct {
     double time;
     int level;
-  }AuthInfo;
+  } AuthInfo;
 
   std::map<int,AuthInfo> authedSessions;
 };
