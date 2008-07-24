62b994fdfd7a32405b4a4db32513da658655821f
Jeff Myers <jeffm2501@users.sourceforge.net>
2008-07-24 04:40:48 +0000

    whitespace
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@18161 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/serverSideBotSample/serverSideBotSample.cpp
+++ plugins/serverSideBotSample/serverSideBotSample.cpp
@@ -11,25 +11,25 @@ BZ_GET_PLUGIN_VERSION
 class SimpleBotHandler : public bz_ServerSidePlayerHandler , bz_EventHandler
 {
 public:
-	SimpleBotHandler();
-	virtual void added ( int playerIndex );
-	virtual void removed ( void );
-	virtual void playerRemoved ( int playerIndex );
+  SimpleBotHandler();
+  virtual void added ( int playerIndex );
+  virtual void removed ( void );
+  virtual void playerRemoved ( int playerIndex );
 
-	virtual void flagUpdate ( int count, bz_FlagUpdateRecord **flagList );
-	virtual void playerUpdate ( bz_PlayerInfoUpdateRecord *playerRecord );
-	virtual void teamUpdate ( int count, bz_TeamInfoRecord **teamList );
+  virtual void flagUpdate ( int count, bz_FlagUpdateRecord **flagList );
+  virtual void playerUpdate ( bz_PlayerInfoUpdateRecord *playerRecord );
+  virtual void teamUpdate ( int count, bz_TeamInfoRecord **teamList );
 
-	virtual void process ( bz_EventData *eventData );
+  virtual void process ( bz_EventData *eventData );
 
-	virtual void playerSpawned ( int player, float pos[3], float rot );
+  virtual void playerSpawned ( int player, float pos[3], float rot );
 
 protected:
 
-	bool inGame;
-	float pos[3];
-	float rot;
-	float lastUpdateTime;
+  bool inGame;
+  float pos[3];
+  float rot;
+  float lastUpdateTime;
 };
 
 SimpleBotHandler	bot;
@@ -58,142 +58,142 @@ BZF_PLUGIN_CALL int bz_Unload ( void )
 
 SimpleBotHandler::SimpleBotHandler()
 {
-	inGame = false;
-	pos[0] = pos[1] = pos[2] = rot = 0;
+  inGame = false;
+  pos[0] = pos[1] = pos[2] = rot = 0;
 }
 
 void SimpleBotHandler::added ( int playerIndex )
 {
-	bz_debugMessage(3,"SimpleBotHandler::added");
-	std::string name = "dante_";
-	name += bz_format("%d",playerID);
-	setPlayerData(name.c_str(),NULL,"bot sample",eAutomaticTeam);
-	bz_registerEvent ( bz_ePlayerSpawnEvent, this );
-	bz_registerEvent ( bz_eTickEvent, this );
-	joinGame();
+  bz_debugMessage(3,"SimpleBotHandler::added");
+  std::string name = "dante_";
+  name += bz_format("%d",playerID);
+  setPlayerData(name.c_str(),NULL,"bot sample",eAutomaticTeam);
+  bz_registerEvent ( bz_ePlayerSpawnEvent, this );
+  bz_registerEvent ( bz_eTickEvent, this );
+  joinGame();
 
-	lastUpdateTime = (float)bz_getCurrentTime();
+  lastUpdateTime = (float)bz_getCurrentTime();
 }
 
 void SimpleBotHandler::playerSpawned ( int player, float _pos[3], float _rot )
 {
-	bz_debugMessage(3,"SimpleBotHandler::playerSpawned");
-	bz_debugMessage(2,bz_format("SimpleBotHandler::playerSpawned #%d x%f y%f z%f r%f",player,_pos[0],_pos[1],_pos[2],_rot));
+  bz_debugMessage(3,"SimpleBotHandler::playerSpawned");
+  bz_debugMessage(2,bz_format("SimpleBotHandler::playerSpawned #%d x%f y%f z%f r%f",player,_pos[0],_pos[1],_pos[2],_rot));
 
-	if (player == playerID)
-	{
-		inGame = true;
-		pos[0] = _pos[0];
-		pos[1] = _pos[1];
-		pos[2] = _pos[2];
-		rot = _rot;
-	}
-	else
-	{
-	}
+  if (player == playerID)
+  {
+    inGame = true;
+    pos[0] = _pos[0];
+    pos[1] = _pos[1];
+    pos[2] = _pos[2];
+    rot = _rot;
+  }
+  else
+  {
+  }
 }
 
 void SimpleBotHandler::removed ( void )
 {
-	bz_removeEvent ( bz_ePlayerSpawnEvent, this );
-	bz_debugMessage(3,"SimpleBotHandler::removed");
+  bz_removeEvent ( bz_ePlayerSpawnEvent, this );
+  bz_debugMessage(3,"SimpleBotHandler::removed");
 }
 
 void SimpleBotHandler::playerRemoved ( int playerIndex )
 {
-	bz_debugMessage(3,"SimpleBotHandler::playerRemoved");
+  bz_debugMessage(3,"SimpleBotHandler::playerRemoved");
 }
 
 void SimpleBotHandler::flagUpdate ( int count, bz_FlagUpdateRecord **flagList )
 {
-	bz_debugMessage(3,"SimpleBotHandler::flagUpdate");
+  bz_debugMessage(3,"SimpleBotHandler::flagUpdate");
 }
 
 void SimpleBotHandler::playerUpdate ( bz_PlayerInfoUpdateRecord *playerRecord )
 {
-	bz_debugMessage(3,"SimpleBotHandler::playerUpdate");
+  bz_debugMessage(3,"SimpleBotHandler::playerUpdate");
 }
 
 void SimpleBotHandler::teamUpdate ( int count, bz_TeamInfoRecord **teamList )
 {
-	bz_debugMessage(3,"SimpleBotHandler::teamUpdate");
+  bz_debugMessage(3,"SimpleBotHandler::teamUpdate");
 }
 
 void SimpleBotHandler::process ( bz_EventData *eventData )
 {
-	switch(eventData->eventType)
+  switch(eventData->eventType)
+  {
+  case bz_ePlayerSpawnEvent:
+    {
+      bz_debugMessage(3,"SimpleBotHandler::process bz_ePlayerSpawnEvent");
+
+      bz_PlayerSpawnEventData_V1	*spawnEvent = (bz_PlayerSpawnEventData_V1*)eventData;
+
+      if (spawnEvent->playerID == playerID)
+	break;
+
+      bz_BasePlayerRecord * player = bz_getPlayerByIndex ( spawnEvent->playerID );
+      std::string message;
+      message = bz_format("well, look who droped in...%s",player->callsign.c_str());
+      bz_sendTextMessage(playerID,BZ_ALLUSERS,message.c_str());
+      bz_freePlayerRecord(player);
+    }
+    break;
+
+  case bz_eTickEvent:
+    {
+      float thisTime = (float)bz_getCurrentTime();
+
+      // here we have to do an update
+      // move to the first player We Find
+      bz_APIIntList	*list = bz_getPlayerIndexList();
+
+      bz_BasePlayerRecord	*player = NULL;
+      for ( unsigned int i = 0; i < list->size(); i++)
+      {
+	if ( list->get(i) != playerID )
 	{
-		case bz_ePlayerSpawnEvent:
-			{
-				bz_debugMessage(3,"SimpleBotHandler::process bz_ePlayerSpawnEvent");
-
-				bz_PlayerSpawnEventData_V1	*spawnEvent = (bz_PlayerSpawnEventData_V1*)eventData;
-
-				if (spawnEvent->playerID == playerID)
-					break;
-
-				bz_BasePlayerRecord * player = bz_getPlayerByIndex ( spawnEvent->playerID );
-				std::string message;
-				message = bz_format("well, look who droped in...%s",player->callsign.c_str());
-				bz_sendTextMessage(playerID,BZ_ALLUSERS,message.c_str());
-				bz_freePlayerRecord(player);
-			}
-			break;
-
-		case bz_eTickEvent:
-			{
-				float thisTime = (float)bz_getCurrentTime();
-
-				// here we have to do an update
-				// move to the first player We Find
-				bz_APIIntList	*list = bz_getPlayerIndexList();
-
-				bz_BasePlayerRecord	*player = NULL;
-				for ( unsigned int i = 0; i < list->size(); i++)
-				{
-					if ( list->get(i) != playerID )
-					{
-						player = bz_getPlayerByIndex(list->get(i));
-						break;
-					}
-				}
-				if (player)
-				{
-					float vectorTo[3];
-
-					vectorTo[0] = player->currentState.pos[0] - pos[0];
-					vectorTo[1] = player->currentState.pos[1] - pos[1];
-					vectorTo[2] = player->currentState.pos[2] - pos[2];
-
-					float dist = sqrt(vectorTo[0]*vectorTo[0]+vectorTo[1]*vectorTo[1]+vectorTo[2]*vectorTo[2]);
-					vectorTo[0] /= dist;
-					vectorTo[1] /= dist;
-					vectorTo[2] /= dist;
-
-					float speed = 10.0f;
-					float timeDelta =	thisTime - lastUpdateTime;
-
-					pos[0] += vectorTo[0] * speed * timeDelta;
-					pos[1] += vectorTo[1] * speed * timeDelta;
-					pos[2] += vectorTo[2] * speed * timeDelta;
-				}
-
-				bz_PlayerUpdateState	myState;
-
-				myState.status = eAlive;
-				myState.pos[0] = pos[0];
-				myState.pos[1] = pos[1];
-				myState.pos[2] = pos[2];
-				myState.rotation = rot;
-				//updateState ( &myState );
-				bz_deleteIntList(list);
-
-				lastUpdateTime = thisTime;
-			}
-			break;
-		default:
-			return;
+	  player = bz_getPlayerByIndex(list->get(i));
+	  break;
 	}
+      }
+      if (player)
+      {
+	float vectorTo[3];
+
+	vectorTo[0] = player->currentState.pos[0] - pos[0];
+	vectorTo[1] = player->currentState.pos[1] - pos[1];
+	vectorTo[2] = player->currentState.pos[2] - pos[2];
+
+	float dist = sqrt(vectorTo[0]*vectorTo[0]+vectorTo[1]*vectorTo[1]+vectorTo[2]*vectorTo[2]);
+	vectorTo[0] /= dist;
+	vectorTo[1] /= dist;
+	vectorTo[2] /= dist;
+
+	float speed = 10.0f;
+	float timeDelta =	thisTime - lastUpdateTime;
+
+	pos[0] += vectorTo[0] * speed * timeDelta;
+	pos[1] += vectorTo[1] * speed * timeDelta;
+	pos[2] += vectorTo[2] * speed * timeDelta;
+      }
+
+      bz_PlayerUpdateState	myState;
+
+      myState.status = eAlive;
+      myState.pos[0] = pos[0];
+      myState.pos[1] = pos[1];
+      myState.pos[2] = pos[2];
+      myState.rotation = rot;
+      //updateState ( &myState );
+      bz_deleteIntList(list);
+
+      lastUpdateTime = thisTime;
+    }
+    break;
+  default:
+    return;
+  }
 }
 
 
