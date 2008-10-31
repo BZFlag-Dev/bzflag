6545c9e89b80525fcb10fd1754412c2cb2a41bf4
Sean Morrison <brlcad@users.sourceforge.net>
2008-10-31 18:17:41 +0000

    ws
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@18837 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/Reports.cxx
+++ src/bzfs/Reports.cxx
@@ -32,23 +32,23 @@ Reports::~Reports()
 
 }
 
-bool Reports::file ( const std::string &user, const std::string message )
+bool Reports::file(const std::string &user, const std::string message)
 {
   time_t now = time(NULL);
 
-  Report report(ctime(&now),user,message);
+  Report report(ctime(&now), user, message);
 
-  if (clOptions->reportFile.size()) {
+  if(clOptions->reportFile.size()) {
     std::ofstream ofs(clOptions->reportFile.c_str(), std::ios::out | std::ios::app);
     ofs << report.fileLine() << std::endl;
   }
 
-  if (clOptions->reportPipe.size() > 0) {
+  if(clOptions->reportPipe.size() > 0) {
     FILE* pipeWrite = popen(clOptions->reportPipe.c_str(), "w");
-    if (pipeWrite != NULL)
+    if(pipeWrite != NULL)
       fprintf(pipeWrite, "%s\n\n", report.fileLine().c_str());
     else
-      logDebugMessage(1,"Couldn't write report to the pipe\n");
+      logDebugMessage(1, "Couldn't write report to the pipe\n");
     pclose(pipeWrite);
   }
 
@@ -58,16 +58,16 @@ bool Reports::file ( const std::string &user, const std::string message )
   const unsigned int wordsize = words.size();
   std::string temp2;
 
-  while (cur != wordsize) {
+  while(cur != wordsize) {
     temp2.clear();
-    while (cur != wordsize && (temp2.size() + words[cur].size() + 1 ) < (unsigned) MessageLen) {
+    while(cur != wordsize &&(temp2.size() + words[cur].size() + 1) <(unsigned) MessageLen) {
 	temp2 += words[cur] + " ";
 	++cur;
     }
-    sendMessage (ServerPlayer, AdminPlayers, temp2.c_str());
+    sendMessage(ServerPlayer, AdminPlayers, temp2.c_str());
   }
 
-  logDebugMessage(1,"A report from %s has been filed (time: %s).\n", report.from.c_str(), report.time.c_str());
+  logDebugMessage(1, "A report from %s has been filed(time: %s).\n", report.from.c_str(), report.time.c_str());
 
   // Notify plugins of the report filed
   bz_ReportFiledEventData_V1 reportData;
@@ -78,14 +78,14 @@ bool Reports::file ( const std::string &user, const std::string message )
   return true;
 }
 
-size_t Reports::getLines (std::vector<std::string> &lines, const char* p )
+size_t Reports::getLines(std::vector<std::string> &lines, const char* p)
 {
   lines.clear();
-  if (clOptions->reportFile.size() > 0) 
+  if(clOptions->reportFile.size() > 0) 
     return 0;
 
   std::ifstream ifs(clOptions->reportFile.c_str(), std::ios::in);
-  if (ifs.fail())
+  if(ifs.fail())
     return 0;
 
   std::string pattern = "*";
@@ -95,10 +95,10 @@ size_t Reports::getLines (std::vector<std::string> &lines, const char* p )
   // assumes that empty lines separate the reports
 
   bool done = false;
-  while (!done) {
+  while(!done) {
     std::string line;
     done = std::getline(ifs, line) == NULL;
-    if (line.size()) {
+    if(line.size()) {
       Report report(line);
 
       if(report.match(pattern))
@@ -108,28 +108,28 @@ size_t Reports::getLines (std::vector<std::string> &lines, const char* p )
   return lines.size();
 }
 
-size_t Reports::count ( void )
+size_t Reports::count(void)
 {
   std::ifstream ifs(clOptions->reportFile.c_str(), std::ios::in);
-  if (ifs.fail())
+  if(ifs.fail())
     return 0;
 
   size_t s = 0;
 
   bool done = false;
-  while (!done) {
+  while(!done) {
     std::string line;
     done = std::getline(ifs, line) == NULL;
-    if (line.size())
+    if(line.size())
       s++;
   }
   return s;
 }
 
-bool Reports::clear ( void )
+bool Reports::clear(void)
 {
   // just blast out the file with a single newline
-  if (!clOptions->reportFile.size()) {
+  if(!clOptions->reportFile.size()) {
     std::ofstream ofs(clOptions->reportFile.c_str(), std::ios::out);
     ofs << std::endl;
     return true;
@@ -138,28 +138,28 @@ bool Reports::clear ( void )
   return false;
 }
 
-bool Reports::clear ( size_t index )
+bool Reports::clear(size_t index)
 {
-  if (!clOptions->reportFile.size()) 
+  if(!clOptions->reportFile.size()) 
     return false;
 
   std::vector<Report> reports;
 
   std::ifstream ifs(clOptions->reportFile.c_str(), std::ios::in);
-  if (ifs.fail())
+  if(ifs.fail())
     return false;
 
   // read em all in
   bool done = false;
-  while (!done) {
+  while(!done) {
     std::string line;
     done = std::getline(ifs, line) == NULL;
-    if (line.size())
+    if(line.size())
       reports.push_back(Report(line));
   }
   ifs.close();
 
-  if (index >= reports.size())
+  if(index >= reports.size())
     return false;
 
   reports.erase(reports.begin()+index);
@@ -167,32 +167,32 @@ bool Reports::clear ( size_t index )
   std::ofstream ofs(clOptions->reportFile.c_str(), std::ios::out);
 
   std::vector<Report>::iterator itr = reports.begin();
-  while (itr != reports.end())
-  for (size_t r = 0; r < reports.size(); r++)
-    ofs << (itr++)->fileLine() << std::endl;
+  while(itr != reports.end())
+  for(size_t r = 0; r < reports.size(); r++)
+    ofs <<(itr++)->fileLine() << std::endl;
 
   return true;
 }
 
-Reports::Report Reports::get ( size_t index )
+Reports::Report Reports::get(size_t index)
 {
-  if (!clOptions->reportFile.size()) 
+  if(!clOptions->reportFile.size()) 
     return Report();
 
   std::list<Report> reports;
 
   std::ifstream ifs(clOptions->reportFile.c_str(), std::ios::in);
-  if (ifs.fail())
+  if(ifs.fail())
     return Report();
 
   size_t s = 0;
   // read em all in
   bool done = false;
-  while (!done) {
+  while(!done) {
     std::string line;
     done = std::getline(ifs, line) == NULL;
-    if (line.size()) {
-      if (s == index)
+    if(line.size()) {
+      if(s == index)
 	return Report(line);
       s++;
     }
@@ -204,17 +204,17 @@ Reports::Report Reports::get ( size_t index )
 
 Reports::Report::Report(const char* t, const std::string &f, const std::string & m)
 {
-  if (t)
+  if(t)
     time = t;
 
   from = f;
   message = m;
 }
 
-bool Reports::Report::fill ( const std::string &line )
+bool Reports::Report::fill(const std::string &line)
 {
-  std::vector<std::string> parts = TextUtils::tokenize(line,std::string(":"),3,false);
-  if (parts.size() >= 4) {
+  std::vector<std::string> parts = TextUtils::tokenize(line, std::string(":"), 3, false);
+  if(parts.size() >= 4) {
     time = parts[0];
     from = parts[2];
     message = parts[3];
@@ -224,22 +224,22 @@ bool Reports::Report::fill ( const std::string &line )
   return time.size() > 0;
 }
 
-std::string Reports::Report::fileLine( void )
+std::string Reports::Report::fileLine(void)
 {
   return time + ":Reported by :" + from + ":" + message;
 }
 
-bool Reports::Report::matchName ( const std::string pattern )
+bool Reports::Report::matchName(const std::string pattern)
 {
   return glob_match(pattern, TextUtils::toupper(from));
 }
 
-bool Reports::Report::matchMessage ( const std::string pattern )
+bool Reports::Report::matchMessage(const std::string pattern)
 {
   return glob_match(pattern, TextUtils::toupper(message));
 }
 
-bool Reports::Report::match ( const std::string pattern )
+bool Reports::Report::match(const std::string pattern)
 {
   return matchMessage(pattern)|| matchName(pattern);
 }
