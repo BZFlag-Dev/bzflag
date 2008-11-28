168a76cb5c0c303da06b028b69f19f03857e130a
trepan <trepan@users.sourceforge.net>
2008-11-28 04:32:52 +0000

    * added missing files, thanks delusional
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@18988 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/DynamicWorldText.cxx
@@ -0,0 +1,148 @@
+/* bzflag
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "common.h"
+
+/* interface header */
+#include "DynamicWorldText.h"
+
+/* system headers */
+#include <string>
+using std::string;
+
+/* common headers */
+#include "WorldText.h"
+#include "TextSceneNode.h"
+#include "SceneRenderer.h"
+
+/* local headers */
+#include "World.h"
+
+
+DynamicWorldText DynamicWorldText::bzdbWorldText;
+
+
+/******************************************************************************/
+
+
+DynamicWorldText::DynamicWorldText()
+{
+}
+
+
+DynamicWorldText::~DynamicWorldText()
+{
+  clear();
+}
+
+
+/******************************************************************************/
+
+void DynamicWorldText::clear()
+{
+  AddedMap::iterator ait;
+  for (ait = addedTexts.begin(); ait != addedTexts.end(); ++ait) {
+    WorldText* text = ait->second;
+    delete text;
+  }
+
+  NodeMap::iterator  nit;
+  for (nit = nodes.begin(); nit != nodes.end(); ++nit) {
+    TextSceneNode* node = nit->second;
+    delete node;
+  }
+
+  worldTexts.clear();
+  addedTexts.clear();
+  nodes.clear();
+}
+
+
+/******************************************************************************/
+
+void DynamicWorldText::addRenderNodes(SceneRenderer& renderer)
+{
+  NodeMap::const_iterator it;
+  for (it = nodes.begin(); it != nodes.end(); ++it) {
+    TextSceneNode* node = it->second;
+    node->addRenderNodes(renderer);
+  }
+}
+
+
+void DynamicWorldText::addShadowNodes(SceneRenderer& renderer)
+{
+  NodeMap::const_iterator it;
+  for (it = nodes.begin(); it != nodes.end(); ++it) {
+    TextSceneNode* node = it->second;
+    node->addShadowNodes(renderer);
+  }
+}
+
+
+void DynamicWorldText::renderRadar()
+{
+}
+
+
+/******************************************************************************/
+
+void DynamicWorldText::addText(const WorldText* text)
+{
+  worldTexts.insert(text);
+  nodes[text] = new TextSceneNode(text);
+}
+
+
+bool DynamicWorldText::insertText(WorldText* text)
+{
+  if (World::getWorld() == NULL) {
+    return false;
+  }
+  if (addedTexts.find(text->name) != addedTexts.end()) {
+    return false;
+  }
+  addedTexts[text->name] = text;
+  nodes[text] = new TextSceneNode(text);
+  return true;
+}
+
+
+bool DynamicWorldText::removeText(const std::string& name)
+{
+  AddedMap::iterator textIT = addedTexts.find(name);
+  if (textIT == addedTexts.end()) {
+    return false;
+  }
+  WorldText* text = textIT->second;
+  NodeMap::iterator nodeIT = nodes.find(text);
+  if (nodeIT != nodes.end()) {
+    TextSceneNode* node = nodeIT->second;
+    delete node;
+    nodes.erase(nodeIT);
+  }
+  delete text;
+  addedTexts.erase(textIT);
+  return true;
+}
+
+
+/******************************************************************************/
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/bzflag/DynamicWorldText.h
@@ -0,0 +1,69 @@
+/* bzflag
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef DYNAMIC_WORLD_TEXT_H
+#define DYNAMIC_WORLD_TEXT_H
+
+#include "common.h"
+
+/* system interface headers */
+#include <string>
+#include <set>
+#include <map>
+
+
+class WorldText;
+class TextSceneNode;
+class SceneRenderer;
+
+
+class DynamicWorldText {
+  public:
+    void clear();
+
+    void addRenderNodes(SceneRenderer&);
+    void addShadowNodes(SceneRenderer&);
+    void renderRadar();
+
+    void addText(const WorldText*); // for worldTexts
+    bool insertText(WorldText*);    // for addedTexts
+    bool removeText(const std::string& name);
+
+  private:
+    DynamicWorldText();
+    ~DynamicWorldText();
+
+  private:
+    typedef std::set<const WorldText*>                 WorldMap;
+    typedef std::map<std::string, WorldText*>          AddedMap;
+    typedef std::map<const WorldText*, TextSceneNode*> NodeMap;
+    WorldMap worldTexts; // came from the world file, but uses BZDB text
+    AddedMap addedTexts; // added dynamically from the server
+    NodeMap nodes;
+  
+  public:
+    static DynamicWorldText bzdbWorldText;
+};
+
+
+#define DYNAMICWORLDTEXT (DynamicWorldText::bzdbWorldText)
+
+
+#endif
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
