3a486d52b3e7167f0d0e5218fda0c8f9dc13e9e9
Sean Morrison <brlcad@users.sourceforge.net>
2009-03-29 22:24:09 +0000

    remove dead code, manual deletion of the freetype memory manager isn't necessary
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@19547 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/other/ftgl/src/FTLibrary.cpp
+++ src/other/ftgl/src/FTLibrary.cpp
@@ -44,14 +44,6 @@ FTLibrary::~FTLibrary()
         delete library;
         library= 0;
     }
-
-//  if(manager != 0)
-//  {
-//      FTC_Manager_Done(manager);
-//
-//      delete manager;
-//      manager= 0;
-//  }
 }
 
 
@@ -77,15 +69,6 @@ bool FTLibrary::Initialise()
         library = 0;
         return false;
     }
-
-//  FTC_Manager* manager;
-//
-//  if(FTC_Manager_New(lib, 0, 0, 0, my_face_requester, 0, manager)
-//  {
-//      delete manager;
-//      manager= 0;
-//      return false;
-//  }
 
     return true;
 }
