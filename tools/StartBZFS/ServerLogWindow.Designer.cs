cd3465a71bd102f6e435d0178da5427edbc6113f
Jeff Myers <jeffm2501@users.sourceforge.net>
2009-04-03 05:09:01 +0000

    add a server log window that shows the output and has a nice stop button
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@19580 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ tools/StartBZFS/ServerLogWindow.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace StartBZFS
+{
+    partial class ServerLogWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerLog = new System.Windows.Forms.TextBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ServerLog
+            // 
+            this.ServerLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.ServerLog.Location = new System.Drawing.Point(0, 1);
+            this.ServerLog.Multiline = true;
+            this.ServerLog.Name = "ServerLog";
+            this.ServerLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ServerLog.Size = new System.Drawing.Size(272, 245);
+            this.ServerLog.TabIndex = 0;
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Location = new System.Drawing.Point(190, 252);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // ServerLogWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 282);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.ServerLog);
+            this.Name = "ServerLogWindow";
+            this.Text = "ServerLogWindow";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ServerLogWindow_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ServerLog;
+        private System.Windows.Forms.Button Stop;
+    }
+}
---
+++ tools/StartBZFS/ServerLogWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StartBZFS
+{
+    public partial class ServerLogWindow : Form
+    {
+        public bool stop = false;
+
+        public ServerLogWindow()
+        {
+            InitializeComponent();
+            stop = false;
+        }
+        
+        public void addText ( string line )
+        {
+            ServerLog.Text += line + "\r\n";
+            ServerLog.Select(ServerLog.Text.Length, 1);
+        }
+
+        private void ServerLogWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stop = true;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
