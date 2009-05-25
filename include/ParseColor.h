8089204c5ad6499ab41486342a337d61d2098c99
Jeff Makey <bullet_catcher@users.sourceforge.net>
2009-05-25 20:56:56 +0000

    The license terms are in the file named COPYING, not LICENSE.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@19808 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/ParseColor.h
+++ include/ParseColor.h
@@ -3,9 +3,9 @@
  *
  * This package is free software;  you can redistribute it and/or
  * modify it under the terms of the license found in the file
- * named LICENSE that should have accompanied this file.
+ * named COPYING that should have accompanied this file.
  *
- * THIS PACKAGE IS PROVIDED `AS IS'' AND WITHOUT ANY EXPRESS OR
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
