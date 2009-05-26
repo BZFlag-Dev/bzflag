99f7150ee84c719e9f9e88bc0efed45e1bca7f0d
Jeff Makey <bullet_catcher@users.sourceforge.net>
2009-05-26 00:39:16 +0000

    Standardize the 7 comment lines at the end of the file, providing whitespace rules for the Emacs and vi editors.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@19813 08b3d480-bf2c-0410-a26f-811ee3361c24

--- tools/tankview/simpleSDLView.cpp
+++ tools/tankview/simpleSDLView.cpp
@@ -656,7 +656,7 @@ void SimpleDisplay::removeEventCallback ( SimpleDisplayEventCallbacks *callback 
 
 
 // Local Variables: ***
-// mode:C++ ***
+// mode: C++ ***
 // tab-width: 8 ***
 // c-basic-offset: 2 ***
 // indent-tabs-mode: t ***
