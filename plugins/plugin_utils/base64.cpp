27a6be797213b6b2d8e5fda98a6d4542ca4bf8e2
Jeff Makey <bullet_catcher@users.sourceforge.net>
2009-06-05 05:48:04 +0000

    Add the copyright/license header from the base64.h file.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@19871 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/plugin_utils/base64.cpp
+++ plugins/plugin_utils/base64.cpp
@@ -1,3 +1,25 @@
+/* base64.hpp - base64 encoder/decoder implementing section 6.8 of RFC2045
+ *
+ * Copyright (C) 2002 Ryan Petrie (ryanpetrie@netscape.net)
+ * and released under the zlib license:
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
 namespace base64 {
   const char _to_table[64] =  {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
