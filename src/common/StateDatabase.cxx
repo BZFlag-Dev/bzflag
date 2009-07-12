4f921bced4582c824d537db057854b2297b2d2ca
trepan <trepan@users.sourceforge.net>
2009-07-12 20:04:51 +0000

    * all NaNs or no NaNs for the evalFVecX() calls
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@20142 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/common/StateDatabase.cxx
+++ src/common/StateDatabase.cxx
@@ -334,6 +334,9 @@ fvec2 StateDatabase::evalFVec2(const std::string& name)
              &data[0], &data[1]) != 2) {
     return nanValue;
   }
+  if (isnan(data[0]) || isnan(data[1])) {
+    return nanValue;
+  }
   return fvec2(data[0], data[1]);
 }
 
@@ -349,6 +352,9 @@ fvec3 StateDatabase::evalFVec3(const std::string& name)
              &data[0], &data[1], &data[2]) != 3) {
     return nanValue;
   }
+  if (isnan(data[0]) || isnan(data[1]) || isnan(data[2])) {
+    return nanValue;
+  }
   return fvec3(data[0], data[1], data[2]);
 }
 
@@ -362,6 +368,9 @@ fvec4 StateDatabase::evalFVec4(const std::string& name)
   float data[4];
   if (sscanf(get(name).c_str(), "%f %f %f %f",
              &data[0], &data[1], &data[2], &data[3]) != 4) {
+    return nanValue;
+  }
+  if (isnan(data[0]) || isnan(data[1]) || isnan(data[2]) || isnan(data[3])) {
     return nanValue;
   }
   return fvec4(data[0], data[1], data[2], data[3]);
