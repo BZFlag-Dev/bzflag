bba57415f41677ef54cf4cf622cfe5de6afd8fc4
trepan <trepan@users.sourceforge.net>
2009-07-13 21:45:09 +0000

    * bumped protocol and rev
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@20158 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -21,7 +21,7 @@
 
 // opaque version number increments on protocol incompatibility
 #ifndef BZ_PROTO_VERSION
-#  define BZ_PROTO_VERSION	"0099"
+#  define BZ_PROTO_VERSION	"0100"
 #endif
 // ditto for bzrobots
 #ifndef BZROBOTS_PROTO_VERSION
@@ -42,7 +42,7 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		41
+#  define BZ_REV		42
 #endif
 
 // DEVEL | STABLE | MAINT
