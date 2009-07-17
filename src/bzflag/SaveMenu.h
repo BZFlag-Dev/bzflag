f4d174e7677d508642560037fd1162110a693204
Sean Morrison <brlcad@users.sourceforge.net>
2009-07-17 03:48:46 +0000

    ooops! helps to svn add the files added by the patch.  apply a pretty cool patch from mayur bhosle ( mayurbhosle ), sf patch # 1939139, which provides a SaveMenu that gives the user confirmed feedback that their settings were saved.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@20197 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ src/bzflag/SaveMenu.h
@@ -0,0 +1,64 @@
+/* bzflag
+ * Copyright (c) 1993 - 2008 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef	__SAVEMENU_H__
+#define __SAVEMENU_H__
+
+/* system interface headers */
+
+/* common interface headers */
+#include "BzfEvent.h"
+#include "CommandsStandard.h"
+
+/* local interface headers */
+#include "MenuDefaultKey.h"
+#include "HUDDialog.h"
+#include "HUDuiDefaultKey.h"
+
+
+class SaveMenuDefaultKey : public MenuDefaultKey {
+public:
+  SaveMenuDefaultKey() { }
+  ~SaveMenuDefaultKey() { }
+
+  bool keyPress(const BzfKeyEvent&);
+  bool keyRelease(const BzfKeyEvent&);
+
+};
+
+
+class SaveMenu : public HUDDialog {
+public:
+  SaveMenu();
+  ~SaveMenu();
+
+  HUDuiDefaultKey* getDefaultKey() { return &defaultKey; }
+  void execute();
+  void resize(int width, int height);
+  void setFileName(std::string& fname);
+  std::string getFileName();
+
+private:
+  SaveMenuDefaultKey defaultKey;
+  std::string          filename; 
+};
+
+
+#endif /* __SAVEMENU_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
