0a51d64bd3fe10f96d495b85a06f2e1545a4ea57
Jørgen Pedersen Tjernø <daxxar@users.sourceforge.net>
2009-10-26 01:24:49 +0000

    Assigned copyright for bzrobots.i to Tim Riker. (committing as daxxar instead of jorgenpt to ensure that original author is the one assigning copyright) Fixed error in copyright notice caused by overenthusiastic search&replace in AdvancedRobot.cxx.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@20680 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzrobots/AdvancedRobot.cxx
+++ src/bzrobots/AdvancedRobot.cxx
@@ -1,11 +1,11 @@
 /* bzflag
  * Copyright (c) 1993 - 2009 Tim Riker
  *
- * This package BZRobots::AdvancedRobot::is free software;  you can redistribute it and/or
+ * This package is free software;  you can redistribute it and/or
  * modify it under the terms of the license found in the file
  * named COPYING that should have accompanied this file.
  *
- * THIS PACKAGE BZRobots::AdvancedRobot::is PROVIDED ``AS BZRobots::AdvancedRobot::is'' AND WITHOUT ANY EXPRESS OR
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
