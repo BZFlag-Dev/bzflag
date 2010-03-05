BZFlag-Dev/bzflag

commit 31d57119b0ba5fc2f36dc54b950cac2813881430

only set the "enter" flag after the player has been accepted by the server and sent out to everyone.
git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@20796 08b3d480-bf2c-0410-a26f-811ee3361c24

src/bzfs/bzfs.cxx
@@ -2009,6 +2009,8 @@ static void addPlayer(int playerIndex, GameKeeper::Player *playerData)
     worldEventManager.callEvents(bz_ePlayerJoinEvent,&joinEventData);
   if (spawnSoon)
     playerAlive(playerIndex);
+
+  playerData->player.setEnter();
 }
 
 

include/PlayerInfo.h
@@ -108,6 +108,7 @@ public:
   bool	hasStartedToNotRespond();
   void	hasSent();
   bool	hasPlayedEarly();
+  void  setEnter() {validEnter = true;}
   bool	hasSentEnter() {return validEnter;}
   TimeKeeper  getNextSpawnTime() const;
   void	setSpawnDelay(double delay);

src/game/PlayerInfo.cxx
@@ -190,7 +190,6 @@ bool PlayerInfo::unpackEnter(void *buf, uint16_t &rejectCode, char *rejectMsg)
     strcpy(token, "NONE");
   }
 
-  validEnter = true;
   return true;
 }
 

commit 368adc862e99d967bc87b570c39a6103a37f1781

Process MsgExit normally even if the player is not fully added. Ignore (don't kick) MsgAlive, MsgMessage, and MsgPlayerUpdateSmall received from players that are not fully added.  In lieu of fixing the protocol, this works around a regression introduced in r20796. For other premature messages, improve the log message by displaying the IP address rather than the (possibly nonexistent) host name and by displaying the message code in hex for easier lookups in Protocol.h.
git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/v2_0branch/bzflag@20900 08b3d480-bf2c-0410-a26f-811ee3361c24

src/bzfs/bzfs.cxx
@@ -3650,10 +3650,18 @@ static void handleCommand(int t, const void *rawbuf, bool udp)
 	  case MsgUDPLinkRequest:
 	  case MsgUDPLinkEstablished:
 	  case MsgWantSettings:
+	  case MsgExit:
 		  break;
 
+	  case MsgAlive:
+	  case MsgMessage:
+	  case MsgPlayerUpdateSmall:
+		  // FIXME: this is a workaround for a protocol problem
+		  logDebugMessage(2,"Ignoring premature message 0x%4hx from host %s\n",code,handler->getTargetIP());
+		  return;
+
 	  default:
-		  logDebugMessage(1,"Host %s tried to send invalid message before Enter; %d\n",handler->getHostname(),code);
+		  logDebugMessage(1,"Host %s tried to send invalid message before Enter; 0x%4hx\n",handler->getTargetIP(),code);
 		  rejectPlayer(t, RejectBadRequest, "invalid request");
 		  return;
 	  }