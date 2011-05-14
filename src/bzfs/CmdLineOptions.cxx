b7857af9483cbdaab08af1b3bb238e37acff37db
Bernt Hansen <bthansen@users.sourceforge.net>
2011-05-14 05:28:58 +0000

    Die if we can't parse the configuration file
    There's no point in starting a server on the default port if the user
    blew the commandline options for the -conf file. Just report the
    error and die.
    
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@21426 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/CmdLineOptions.cxx
+++ src/bzfs/CmdLineOptions.cxx
@@ -452,6 +452,7 @@ static char **parseConfFile( const char *file, int &ac)
 
     if (!confStrm.good()) {
       errorHandler.fatalError(std::string("could not find bzflag configuration file"), 0);
+      exit(1);
     }
 
     confStrm.seekg(0, std::ifstream::beg);
@@ -475,6 +476,7 @@ static char **parseConfFile( const char *file, int &ac)
    }
   } else {
     errorHandler.fatalError(std::string("could not find bzflag configuration file"), 0);
+    exit(1);
   }
 
   const char **av = new const char*[tokens.size()+1];
