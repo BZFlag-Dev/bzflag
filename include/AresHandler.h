BZFlag-Dev/bzflag

commit bf02b3e6011879da0680eeed80a27bbcdb7090eb

only init ares once and clean it up when we go home make ares a dll on windows, it just works better that way for some reason....
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@21492 08b3d480-bf2c-0410-a26f-811ee3361c24

include/AresHandler.h
@@ -18,21 +18,19 @@
 
 /* common implementation headers */
 #include "network.h"
-
-extern "C" {
-#ifdef LOCAL_CARES
-#include "bzares.h"
-#include "bzares_version.h"
+#if defined(BUILD_ARES)
+#include "../src/other/ares/ares.h"
 #else
-#include "ares.h"
-#include "ares_version.h"
+#include <ares.h>
 #endif
-}
 
 class AresHandler {
  public:
   AresHandler(int index);
   ~AresHandler();
+
+  static bool	globalInit();
+  static void	globalShutdown();
 
   enum ResolutionStatus {
     None = 0,
@@ -66,6 +64,8 @@ class AresHandler {
   ares_channel	aresChannel;
   ResolutionStatus status;
   bool		aresFailed;
+
+  static bool	globallyInited;
 };
 
 #endif

MSVC/build/ares.vcproj
@@ -21,9 +21,9 @@
 	<Configurations>
 		<Configuration
 			Name="Debug|Win32"
-			OutputDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			IntermediateDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			ConfigurationType="4"
+			OutputDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
+			IntermediateDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
+			ConfigurationType="2"
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			CharacterSet="2"
 			>
@@ -47,7 +47,7 @@
 				Optimization="0"
 				WholeProgramOptimization="false"
 				AdditionalIncludeDirectories="..\..\src\other\ares;.\"
-				PreprocessorDefinitions="WIN32;_DEBUG;_LIB;_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_DEPRECATE;CARES_STATICLIB"
+				PreprocessorDefinitions="WIN32;_DEBUG;_LIB;_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_DEPRECATE;CARES_BUILDING_LIBRARY"
 				MinimalRebuild="true"
 				BasicRuntimeChecks="3"
 				RuntimeLibrary="3"
@@ -68,11 +68,14 @@
 				Name="VCPreLinkEventTool"
 			/>
 			<Tool
-				Name="VCLibrarianTool"
-				OutputFile="$(OutDir)/ares.lib"
+				Name="VCLinkerTool"
+				AdditionalDependencies="ws2_32.lib"
 			/>
 			<Tool
 				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
 			/>
 			<Tool
 				Name="VCXDCMakeTool"
@@ -84,75 +87,18 @@
 				Name="VCFxCopTool"
 			/>
 			<Tool
-				Name="VCPostBuildEventTool"
-			/>
-		</Configuration>
-		<Configuration
-			Name="Release|Win32"
-			OutputDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			IntermediateDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			ConfigurationType="4"
-			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
-			CharacterSet="2"
-			>
-			<Tool
-				Name="VCPreBuildEventTool"
-			/>
-			<Tool
-				Name="VCCustomBuildTool"
-			/>
-			<Tool
-				Name="VCXMLDataGeneratorTool"
-			/>
-			<Tool
-				Name="VCWebServiceProxyGeneratorTool"
-			/>
-			<Tool
-				Name="VCMIDLTool"
-			/>
-			<Tool
-				Name="VCCLCompilerTool"
-				AdditionalIncludeDirectories="..\..\src\other\ares;.\"
-				PreprocessorDefinitions="WIN32;NDEBUG;_LIB;_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_DEPRECATE;CARES_STATICLIB"
-				RuntimeLibrary="2"
-				UsePrecompiledHeader="0"
-				WarningLevel="3"
-				DebugInformationFormat="3"
-			/>
-			<Tool
-				Name="VCManagedResourceCompilerTool"
-			/>
-			<Tool
-				Name="VCResourceCompilerTool"
-			/>
-			<Tool
-				Name="VCPreLinkEventTool"
-			/>
-			<Tool
-				Name="VCLibrarianTool"
-				OutputFile="$(OutDir)/ares.lib"
-			/>
-			<Tool
-				Name="VCALinkTool"
-			/>
-			<Tool
-				Name="VCXDCMakeTool"
-			/>
-			<Tool
-				Name="VCBscMakeTool"
-			/>
-			<Tool
-				Name="VCFxCopTool"
+				Name="VCAppVerifierTool"
 			/>
 			<Tool
 				Name="VCPostBuildEventTool"
+				CommandLine="mkdir ..\..\bin_$(PlatformName)&#x0D;&#x0A;copy $(OutDir)\*.dll ..\..\bin_$(PlatformName)\"
 			/>
 		</Configuration>
 		<Configuration
 			Name="Debug|x64"
 			OutputDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
 			IntermediateDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			ConfigurationType="4"
+			ConfigurationType="2"
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			CharacterSet="2"
 			>
@@ -198,11 +144,13 @@
 				Name="VCPreLinkEventTool"
 			/>
 			<Tool
-				Name="VCLibrarianTool"
-				OutputFile="$(OutDir)/ares.lib"
+				Name="VCLinkerTool"
 			/>
 			<Tool
 				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
 			/>
 			<Tool
 				Name="VCXDCMakeTool"
@@ -214,14 +162,85 @@
 				Name="VCFxCopTool"
 			/>
 			<Tool
+				Name="VCAppVerifierTool"
+			/>
+			<Tool
 				Name="VCPostBuildEventTool"
+				CommandLine="mkdir ..\..\bin_$(PlatformName)&#x0D;&#x0A;copy $(OutDir)\*.dll ..\..\bin_$(PlatformName)\"
+			/>
+		</Configuration>
+		<Configuration
+			Name="Release|Win32"
+			OutputDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
+			IntermediateDirectory="./$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
+			ConfigurationType="2"
+			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
+			CharacterSet="2"
+			>
+			<Tool
+				Name="VCPreBuildEventTool"
+			/>
+			<Tool
+				Name="VCCustomBuildTool"
+			/>
+			<Tool
+				Name="VCXMLDataGeneratorTool"
+			/>
+			<Tool
+				Name="VCWebServiceProxyGeneratorTool"
+			/>
+			<Tool
+				Name="VCMIDLTool"
+			/>
+			<Tool
+				Name="VCCLCompilerTool"
+				AdditionalIncludeDirectories="..\..\src\other\ares;.\"
+				PreprocessorDefinitions="WIN32;NDEBUG;_LIB;_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_DEPRECATE;CARES_STATICLIB"
+				RuntimeLibrary="2"
+				UsePrecompiledHeader="0"
+				WarningLevel="3"
+				DebugInformationFormat="3"
+			/>
+			<Tool
+				Name="VCManagedResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCPreLinkEventTool"
+			/>
+			<Tool
+				Name="VCLinkerTool"
+			/>
+			<Tool
+				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
+			/>
+			<Tool
+				Name="VCXDCMakeTool"
+			/>
+			<Tool
+				Name="VCBscMakeTool"
+			/>
+			<Tool
+				Name="VCFxCopTool"
+			/>
+			<Tool
+				Name="VCAppVerifierTool"
+			/>
+			<Tool
+				Name="VCPostBuildEventTool"
+				CommandLine="mkdir ..\..\bin_$(PlatformName)&#x0D;&#x0A;copy $(OutDir)\*.dll ..\..\bin_$(PlatformName)\"
 			/>
 		</Configuration>
 		<Configuration
 			Name="Release|x64"
 			OutputDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
 			IntermediateDirectory="$(ProjectName)_$(PlatformName)_$(ConfigurationName)"
-			ConfigurationType="4"
+			ConfigurationType="2"
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			CharacterSet="2"
 			>
@@ -260,11 +279,13 @@
 				Name="VCPreLinkEventTool"
 			/>
 			<Tool
-				Name="VCLibrarianTool"
-				OutputFile="$(OutDir)/ares.lib"
+				Name="VCLinkerTool"
 			/>
 			<Tool
 				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
 			/>
 			<Tool
 				Name="VCXDCMakeTool"
@@ -276,7 +297,11 @@
 				Name="VCFxCopTool"
 			/>
 			<Tool
+				Name="VCAppVerifierTool"
+			/>
+			<Tool
 				Name="VCPostBuildEventTool"
+				CommandLine="mkdir ..\..\bin_$(PlatformName)&#x0D;&#x0A;copy $(OutDir)\*.dll ..\..\bin_$(PlatformName)\"
 			/>
 		</Configuration>
 	</Configurations>

MSVC/build/bzflag.sln
@@ -57,6 +57,9 @@ Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "mediafile", "mediafile.vcpr
 	EndProjectSection
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "net", "net.vcproj", "{7B2595ED-CCB8-4D19-87F2-9A52C7F26EF8}"
+	ProjectSection(ProjectDependencies) = postProject
+		{417CB8F4-5AE3-45AC-B903-83DB1556303C} = {417CB8F4-5AE3-45AC-B903-83DB1556303C}
+	EndProjectSection
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "obstacle", "obstacle.vcproj", "{91C01688-CEB5-4CF3-B13D-6EF86D630523}"
 EndProject

MSVC/build/bzflag.vcproj
@@ -89,7 +89,7 @@
 				OutputFile="$(OutDir)/bzflag.exe"
 				LinkIncremental="2"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="..\..\src\3D\debug"
+				AdditionalLibraryDirectories=""
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(OutDir)/bzflag.pdb"
 				SubSystem="2"

MSVC/build/bzfs.vcproj
@@ -74,11 +74,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib"
+				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib  ares.lib"
 				OutputFile="$(OutDir)/bzfs.exe"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="libcurl.lib"
+				AdditionalLibraryDirectories="ares_$(PlatformName)_$(ConfigurationName)"
 				IgnoreDefaultLibraryNames="LIBCMT"
 				ProgramDatabaseFile=".$(OutDir)/bzfs.pdb"
 				SubSystem="1"
@@ -167,11 +167,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib"
+				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib  ares.lib"
 				OutputFile="$(OutDir)/bzfs.exe"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="libcurl.lib"
+				AdditionalLibraryDirectories="ares_$(PlatformName)_$(ConfigurationName)"
 				IgnoreDefaultLibraryNames="LIBCMT"
 				ProgramDatabaseFile=".$(OutDir)/bzfs.pdb"
 				SubSystem="1"
@@ -260,11 +260,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib"
+				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib ares.lib"
 				OutputFile="$(OutDir)/bzfs.exe"
 				LinkIncremental="2"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="libcurl.lib"
+				AdditionalLibraryDirectories="ares_$(PlatformName)_$(ConfigurationName)"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(OutDir)/bzfs.pdb"
 				SubSystem="1"
@@ -354,11 +354,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib"
+				AdditionalDependencies="ws2_32.lib winmm.lib odbc32.lib odbccp32.lib  ares.lib"
 				OutputFile="$(OutDir)/bzfs.exe"
 				LinkIncremental="2"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="libcurl.lib"
+				AdditionalLibraryDirectories="ares_$(PlatformName)_$(ConfigurationName)"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(OutDir)/bzfs.pdb"
 				SubSystem="1"

MSVC/build/config.h
@@ -109,6 +109,9 @@
 #define HAVE_STD__MIN 1
 #define HAVE_STD__MAX 1
 
+#define HAVE_ARES_LIBRARY_INIT 1
+#define BUILD_ARES 1
+
 #ifndef DEBUG_TRACE
 #define DEBUG_TRACE
 inline void W32_DEBUG_TRACE (const char* buffer ) {printf("%s",buffer);}

package/win32/nsis/BZFlag.nsi
@@ -160,6 +160,7 @@ Section "!BZFlag (Required)" BZFlag
   ; Add some DLL files
   SetOutPath $INSTDIR
   File ..\..\..\bin_${PLATFORM}\libcurl.dll
+  File ..\..\..\bin_${PLATFORM}\ares.dll
 
   ; This requires the Visual C++ 2008 SP1 runtime file to be located in
   ; the same directory as the NSIS script

src/bzflag/bzflag.cxx
@@ -37,6 +37,7 @@
 
 /* common headers */
 #include "Address.h"
+#include "AresHandler.h"
 #include "BZDBCache.h"
 #include "BZDBLocal.h"
 #include "BundleMgr.h"
@@ -634,6 +635,7 @@ int			main(int argc, char** argv)
     printFatalError("Failed to initialize winsock.  Terminating.\n");
     return 1;
   }
+  initGlobalAres();
   if (LOBYTE(wsaData.wVersion) != major ||
       HIBYTE(wsaData.wVersion) != minor) {
     printFatalError("Version mismatch in winsock;"
@@ -1359,6 +1361,11 @@ int			main(int argc, char** argv)
   }
 
   // shut down
+
+
+  killGlobalAres();
+  AresHandler::globalShutdown();
+
   if (filter != NULL)
     delete filter;
   filter = NULL;

commit 210dc083fb3e0055f8014e6e268922db13b9d0f9

hack around the ares build issue introduced in r21492
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@21621 08b3d480-bf2c-0410-a26f-811ee3361c24

include/AresHandler.h
@@ -18,8 +18,14 @@
 
 /* common implementation headers */
 #include "network.h"
-#if defined(BUILD_ARES)
+#if defined(_WIN32) && defined(BUILD_ARES)
 #include "../src/other/ares/ares.h"
+#elif !defined(_WIN32) && defined(LOCAL_CARES)
+extern "C" {
+#include "bzares.h"	 
+#include "../src/other/ares/ares.h"
+#include "bzares_version.h"
+}
 #else
 #include <ares.h>
 #endif