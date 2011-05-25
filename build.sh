84d1eee62e391dc8b75f4b0224345cceedd481a2
trepan <trepan@users.sourceforge.net>
2011-05-25 14:17:49 +0000

    * add a simple build script:  make premake4; premake4 gmake; make
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21680 08b3d480-bf2c-0410-a26f-811ee3361c24

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+
+PREMAKE_DIR="other_src/premake"
+
+PREMAKE_EXEC="$PREMAKE_DIR/bin/release/premake4"
+
+
+(cd "$PREMAKE_DIR/build/gmake.unix/" && make)
+
+$PREMAKE_EXEC gmake
+
+make
+
