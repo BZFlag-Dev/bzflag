eb6b0f5774a21deb2f33fba3aade15877abe2db8
trepan <trepan@users.sourceforge.net>
2011-05-26 04:00:47 +0000

    * try 'gmake', then fallback to 'make'   (my linux box doesn't have 'gmake', the g is silent)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21684 08b3d480-bf2c-0410-a26f-811ee3361c24

--- build.sh
+++ build.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
+################################################################################
 
 set -e  # Exit immediately if a command exits with a non-zero status.
 
+################################################################################
 
 PREMAKE_DIR="other_src/premake"
 
 PREMAKE_EXEC="$PREMAKE_DIR/bin/release/premake4"
 
+################################################################################
 
-(cd "$PREMAKE_DIR/build/gmake.unix/" && gmake)
+MAKE=gmake
 
+if ! which $MAKE; then
+  MAKE=make;
+fi
+
+if ! which $MAKE; then
+  echo "Neither 'gmake' nor 'make' are available, bailing out."
+  exit 1
+  MAKE=make;
+fi
+
+################################################################################
+
+(cd "$PREMAKE_DIR/build/gmake.unix/" && $MAKE)
+
+################################################################################
 
 $PREMAKE_EXEC
 
-gmake
+$MAKE
 
+################################################################################
