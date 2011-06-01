e1bc99bc5892a94d408263d8fed6ca80112b69f3
trepan <trepan@users.sourceforge.net>
2011-06-01 13:53:27 +0000

    * build.sh just calls config.sh, and appends a "$MAKE" command * config.sh compiles premake using the build/gmake.unix file,   then uses the new premake exec to embed the lua scripts and   to generate a Makefile. then make is called again to rebuild   the exec.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21771 08b3d480-bf2c-0410-a26f-811ee3361c24

--- build.sh
+++ build.sh
@@ -2,38 +2,6 @@
 
 cd `dirname "$0"`
 
-################################################################################
+. ./config.sh  # config.sh defines $MAKE
 
-set -e  # Exit immediately if a command exits with a non-zero status.
-
-################################################################################
-
-PREMAKE_DIR="other_src/premake"
-
-PREMAKE_EXEC="$PREMAKE_DIR/bin/release/premake4"
-
-################################################################################
-
-MAKE=gmake
-
-if ! which $MAKE; then
-  MAKE=make;
-fi
-
-if ! which $MAKE; then
-  echo "Neither 'gmake' nor 'make' are available, bailing out."
-  exit 1
-  MAKE=make;
-fi
-
-################################################################################
-
-(cd "$PREMAKE_DIR/build/gmake.unix/" && $MAKE)
-
-################################################################################
-
-$PREMAKE_EXEC "$@"
-
-$MAKE
-
-################################################################################
+"$MAKE"
--- config.sh
+++ config.sh
@@ -10,17 +10,19 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 PREMAKE_DIR="other_src/premake"
 
-PREMAKE_EXEC="$PREMAKE_DIR/bin/release/premake4"
+PREMAKE_EXEC="bin/release/premake4"
+
+PREMAKE_FULLEXEC="$PREMAKE_DIR/$PREMAKE_EXEC"
 
 ################################################################################
 
 MAKE=gmake
 
-if ! which $MAKE; then
+if ! which "$MAKE"; then
   MAKE=make;
 fi
 
-if ! which $MAKE; then
+if ! which "$MAKE"; then
   echo "Neither 'gmake' nor 'make' are available, bailing out."
   exit 1
   MAKE=make;
@@ -28,10 +30,17 @@ fi
 
 ################################################################################
 
-(cd "$PREMAKE_DIR/build/gmake.unix/" && $MAKE)
+(cd "$PREMAKE_DIR/build/gmake.unix/" && "$MAKE")
+
+(cd "$PREMAKE_DIR"                      && \
+  "$PREMAKE_EXEC" embed | grep ^Running && \
+  "$PREMAKE_EXEC" gmake | grep ^Running && \
+  "$MAKE")
+
+#  "$PREMAKE_EXEC" embed > /dev/null && \
 
 ################################################################################
 
-$PREMAKE_EXEC "$@"
+"$PREMAKE_FULLEXEC" "$@"
 
 ################################################################################
