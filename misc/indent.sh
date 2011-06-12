df40eb57d4ad4eca7abd365959ccebecc90d0e88
trepan <trepan@users.sourceforge.net>
2011-06-12 00:46:32 +0000

    * using a custom astyle exec, no need to version check it
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21873 08b3d480-bf2c-0410-a26f-811ee3361c24

--- misc/indent.sh
+++ misc/indent.sh
@@ -11,44 +11,6 @@ cd `dirname "$0"`/..
 
 ASTYLE_EXEC="./misc/astyle-bzflag"
 
-MIN_MAJOR=2
-
-MIN_MINOR=2
-
-
-# Get the version
-
-VERSION=`"$ASTYLE_EXEC" --version 2>&1`
-
-if [ $? -ne 0 ]; then
-  echo Could not find astyle.
-  exit
-fi
- 
-echo $VERSION
-
-
-# Parse the version
-
-MAJOR=`echo $VERSION | sed 's/^Artistic Style Version \([0-9]*\)\.[0-9]*$/\1/'`
-MINOR=`echo $VERSION | sed 's/^Artistic Style Version [0-9]*\.\([0-9]*\)$/\1/'`
-
-if [ "$MAJOR" = "$VERSION"  -o  "$MINOR" = "$VERSION" ]; then
-  echo Error parsing the astyle version.
-  exit
-fi
-
-
-# Check the version
-
-if [ $MAJOR -lt $MIN_MAJOR ]; then
-  echo Need astyle 2.02 or better.
-  exit
-elif [ $MAJOR -eq $MIN_MAJOR  -a  $MINOR -lt $MIN_MINOR ]; then
-  echo Need astyle 2.02 or better.
-  exit
-fi
-
 
 # It's go time!
 
