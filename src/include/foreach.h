dc730a2d75e73e30c5d02e0f1504c06efd63d2d2
trepan <trepan@users.sourceforge.net>
2011-06-15 01:37:02 +0000

    * this version of  << foreach(var, con) >>  balks on temporary containers   (it sets up a pointer to the container to avoid redefinition in the macro)
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21914 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/include/foreach.h
+++ src/include/foreach.h
@@ -10,19 +10,29 @@ inline bool foreach_set_inc(T& next, T& iter) {
   return true;
 }
 
+template <typename T>
+struct foreach_container_pointer {
+  foreach_container_pointer(T& t) : ptr(&t) {}
+  T* operator->() { return ptr; }
+  operator bool() const { return false; } // for the "if (X) {} else" setup
+  T* ptr;
+};
+
 #define FOREACH_VAR_SETUP(ITER, ITNEXT, BEGIN) \
   typeof(BEGIN) ITNEXT, ITER = (BEGIN)
 
 #define FOREACH_BASIS(VAR, CON, BEGIN, END) \
-  for (FOREACH_VAR_SETUP(VAR##_ITER, VAR##_NEXT, (CON).BEGIN()); \
+  if (foreach_container_pointer<typeof(CON)> FOREACH_CON = (CON)) {} else \
+  for (FOREACH_VAR_SETUP(VAR##_ITER, VAR##_NEXT, FOREACH_CON->BEGIN()); \
        bool VAR##_FOREACH_ONCE = /* bool used in the next for statement */ \
-         (VAR##_ITER != (CON).END()) \
+         (VAR##_ITER != FOREACH_CON->END()) \
          && foreach_set_inc(VAR##_NEXT, VAR##_ITER); \
        VAR##_ITER = VAR##_NEXT) \
     /* the following for loop is used to setup the VAR reference */ \
-    for (typeof(*((CON).BEGIN())) VAR = *(VAR##_ITER); \
+    for (typeof(*(FOREACH_CON->BEGIN())) VAR = *(VAR##_ITER); \
          VAR##_FOREACH_ONCE; \
          VAR##_FOREACH_ONCE = false)
+
 
 #define foreach(VAR, CON)         FOREACH_BASIS(VAR, CON,  begin,  end)
 #define foreach_reverse(VAR, CON) FOREACH_BASIS(VAR, CON, rbegin, rend)
