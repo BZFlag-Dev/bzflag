3b379eed97c058b67e3543dd7045a722fa023004
trepan <trepan@users.sourceforge.net>
2011-06-18 11:57:26 +0000

    * added the foreach_copy() and foreach_copy_reverse() variants
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/branches/experimental/v2_99continuing/bzflag@21943 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/include/foreach.h
+++ src/include/foreach.h
@@ -1,7 +1,26 @@
 #ifndef FOREACH_H
 #define FOREACH_H
 
+
 #include "typeof.h"
+
+
+// NOTE: the 'copy' variants can be used with temporary containers
+
+
+// foreach              (varname, container) { ... }
+
+// foreach_reverse      (varname, container) { ... }
+
+// foreach_copy         (varname, container) { ... }
+
+// foreach_copy_reverse (varname, container) { ... }
+
+
+#define FOREACH_ITERATOR(VAR) (VAR##_ITER)
+
+
+//============================================================================//
 
 template <typename T>
 inline bool foreach_set_inc(T& next, T& iter) {
@@ -9,6 +28,11 @@ inline bool foreach_set_inc(T& next, T& iter) {
   next++;
   return true;
 }
+
+#define FOREACH_VAR_SETUP(ITER, ITNEXT, BEGIN) \
+  typeof(BEGIN) ITNEXT, ITER = (BEGIN)
+
+//============================================================================//
 
 template <typename T>
 struct foreach_container_pointer {
@@ -18,9 +42,6 @@ struct foreach_container_pointer {
   T* ptr;
 };
 
-#define FOREACH_VAR_SETUP(ITER, ITNEXT, BEGIN) \
-  typeof(BEGIN) ITNEXT, ITER = (BEGIN)
-
 #define FOREACH_BASIS(VAR, CON, BEGIN, END) \
   if (foreach_container_pointer<typeof(CON)> FOREACH_CON = (CON)) {} else \
   for (FOREACH_VAR_SETUP(VAR##_ITER, VAR##_NEXT, FOREACH_CON->BEGIN()); \
@@ -28,14 +49,38 @@ struct foreach_container_pointer {
          (VAR##_ITER != FOREACH_CON->END()) \
          && foreach_set_inc(VAR##_NEXT, VAR##_ITER); \
        VAR##_ITER = VAR##_NEXT) \
-    /* the following for loop is used to setup the VAR reference */ \
+    /* the following for-loop is used to setup the VAR reference */ \
     for (typeof(*(FOREACH_CON->BEGIN())) VAR = *(VAR##_ITER); \
          VAR##_FOREACH_ONCE; \
          VAR##_FOREACH_ONCE = false)
 
-
 #define foreach(VAR, CON)         FOREACH_BASIS(VAR, CON,  begin,  end)
 #define foreach_reverse(VAR, CON) FOREACH_BASIS(VAR, CON, rbegin, rend)
+
+//============================================================================//
+
+template <typename T>
+struct foreach_container_copy : public T {
+  foreach_container_copy(const T& t) : T(t) {}
+  operator bool() const { return false; } // for the "if (X) {} else" setup
+};
+
+#define FOREACH_COPY_BASIS(VAR, CON, BEGIN, END) \
+  if (foreach_container_copy<typeof(CON)> FOREACH_CON = (CON)) {} else \
+  for (FOREACH_VAR_SETUP(VAR##_ITER, VAR##_NEXT, FOREACH_CON.BEGIN()); \
+       bool VAR##_FOREACH_ONCE = /* bool used in the next for statement */ \
+         (VAR##_ITER != FOREACH_CON.END()) \
+         && foreach_set_inc(VAR##_NEXT, VAR##_ITER); \
+       VAR##_ITER = VAR##_NEXT) \
+    /* the following for-loop is used to setup the VAR reference */ \
+    for (typeof(*(FOREACH_CON.BEGIN())) VAR = *(VAR##_ITER); \
+         VAR##_FOREACH_ONCE; \
+         VAR##_FOREACH_ONCE = false)
+
+#define foreach_copy(VAR, CON)         FOREACH_COPY_BASIS(VAR, CON,  begin,  end)
+#define foreach_copy_reverse(VAR, CON) FOREACH_COPY_BASIS(VAR, CON, rbegin, rend)
+
+//============================================================================//
 
 
 #endif // FOREACH_H
