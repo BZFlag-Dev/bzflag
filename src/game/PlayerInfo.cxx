73202fbab0f508e45ba230e31cb80f9490a91b3a
Alfredo Tupone <atupone@users.sourceforge.net>
2011-09-22 19:54:57 +0000

    Changing inizialization order
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22128 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/game/PlayerInfo.cxx
+++ src/game/PlayerInfo.cxx
@@ -30,9 +30,16 @@ WordFilter *PlayerInfo::filterData	= NULL;
 bool	PlayerInfo::simpleFiltering   = true;
 
 PlayerInfo::PlayerInfo(int _playerIndex) :
-  playerIndex(_playerIndex), state(PlayerInLimbo), team(NoTeam),
-  hasDoneEntering(false), flag(-1), spamWarns(0), lastMsgTime(now),
-  paused(false), pausedSince(TimeKeeper::getNullTime()), autopilot(false),
+  playerIndex(_playerIndex),
+  state(PlayerInLimbo),
+  hasDoneEntering(false),
+  team(NoTeam),
+  flag(-1),
+  spamWarns(0),
+  lastMsgTime(now),
+  paused(false),
+  pausedSince(TimeKeeper::getNullTime()),
+  autopilot(false),
   tracker(0)
 {
   notResponding = false;
