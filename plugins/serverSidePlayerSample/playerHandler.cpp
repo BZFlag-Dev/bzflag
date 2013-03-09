cea3d886a4dc7435d05d0ba638ba8390398a3125
Konstantinos Kanavouras <kongr45gpen@users.sourceforge.net>
2013-03-09 17:26:02 +0000

    Add copyright notices and local variables comment on the serverSidePlayerSample plugin
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22691 08b3d480-bf2c-0410-a26f-811ee3361c24

--- plugins/serverSidePlayerSample/playerHandler.cpp
+++ plugins/serverSidePlayerSample/playerHandler.cpp
@@ -1,3 +1,15 @@
+/* bzflag
+ * Copyright (c) 1993-2013 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+ 
 #include "playerHandler.h"
 #include "plugin_utils.h"
 
@@ -30,3 +42,12 @@ void PlayerHandler::shotFired(int player, unsigned short shotID)
 	std::string msg = "Hey, " + playerName + " I bet you think you are special now!";
 	sendChatMessage(msg.c_str());
 }
+
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
