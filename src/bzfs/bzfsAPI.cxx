27954ea526c580fb8db225bd802ad364c332c7c6
Jeff Myers <jeffm2501@users.sourceforge.net>
2013-04-08 01:31:42 +0000

    disable sounds as they are not consistent on all clients and may crash the system
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22731 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/bzfsAPI.cxx
+++ src/bzfs/bzfsAPI.cxx
@@ -2757,19 +2757,20 @@ BZF_API const char* bz_pluginBinPath(void)
 
 BZF_API bool bz_sendPlayCustomLocalSound ( int playerID, const char* soundName )
 {
-  if (playerID == BZ_SERVER || !soundName)
-    return false;
-
-  void *buf, *bufStart = getDirectMessageBuffer();
-  buf = nboPackUShort(bufStart, LocalCustomSound);
-  buf = nboPackUShort(buf, (unsigned short)strlen(soundName));
-  buf = nboPackString(buf, soundName,strlen(soundName));
-  if (playerID == BZ_ALLUSERS)
-    broadcastMessage(MsgCustomSound, (char*)buf - (char*)bufStart, bufStart);
-  else
-    directMessage(playerID,MsgCustomSound, (char*)buf - (char*)bufStart, bufStart);
-
-  return true;
+  return false;
+//   if (playerID == BZ_SERVER || !soundName)
+//     return false;
+// 
+//   void *buf, *bufStart = getDirectMessageBuffer();
+//   buf = nboPackUShort(bufStart, LocalCustomSound);
+//   buf = nboPackUShort(buf, (unsigned short)strlen(soundName));
+//   buf = nboPackString(buf, soundName,strlen(soundName));
+//   if (playerID == BZ_ALLUSERS)
+//     broadcastMessage(MsgCustomSound, (char*)buf - (char*)bufStart, bufStart);
+//   else
+//     directMessage(playerID,MsgCustomSound, (char*)buf - (char*)bufStart, bufStart);
+// 
+//   return true;
 }
 
 // custom pluginHandler
