41e603b2cc635c0ddbbc6b226f5d234938467461
Joshua Bodine <macsforme@users.sourceforge.net>
2013-05-14 11:48:56 +0000

    Changed build behavior so that no build files with dynamic version information are included in version control. This also means that we lose the game version in the product name (at least for now). Also updated mac readme regarding this change.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22756 08b3d480-bf2c-0410-a26f-811ee3361c24

--- Xcode/config.h
+++ Xcode/config.h
@@ -14,6 +14,9 @@
 /* Josh Bodine for use with Xcode project file that no longer just */
 /* calls autotools commands. */
 
+/* Include the auto-generated IDE information header */
+#include "buildinfo.h"
+
 /* BZFlag System Environment */
 #if defined __llvm__ && defined __x86_64__
   #define BZ_BUILD_ARCH_STR "64"
@@ -25,7 +28,7 @@
 #else
   #define BZ_BUILD_DEBUG_STR ""
 #endif
-#define BZ_BUILD_OS "mac" BZ_BUILD_ARCH_STR "xc" BZ_BUILD_XCODE_VERSION_STR BZ_BUILD_DEBUG_STR
+#define BZ_BUILD_OS "mac" BZ_BUILD_ARCH_STR "xc" XCODE_VERSION BZ_BUILD_DEBUG_STR
 
 /* If it's a debug build, use the debug rendering features */
 #ifdef DEBUG
