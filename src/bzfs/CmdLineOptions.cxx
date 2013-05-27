792d088c8d9327e1b3eaa70379bb2a91c22ff463
Jeff Makey <bullet_catcher@users.sourceforge.net>
2013-05-27 02:04:17 +0000

    Remove "const" from the modifiable array of bzfs options.
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22771 08b3d480-bf2c-0410-a26f-811ee3361c24

--- src/bzfs/CmdLineOptions.cxx
+++ src/bzfs/CmdLineOptions.cxx
@@ -497,12 +497,12 @@ static char **parseConfFile( const char *file, int &ac)
     exit(1);
   }
 
-  const char **av = new const char*[tokens.size()+1];
+  char **av = new char*[tokens.size()+1];
   av[0] = strdup("bzfs");
   ac = 1;
   for (std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it)
     av[ac++] = strdup((*it).c_str());
-  return (char **)av;
+  return av;
 }
 
 static char **parseWorldOptions (const char *file, int &ac)
