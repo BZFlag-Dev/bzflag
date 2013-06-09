1f40012e6dafbffaa9bd4d7c7c1cb87e7eb6a2c4
Jeff Makey <bullet_catcher@users.sourceforge.net>
2013-06-09 05:34:47 +0000

    Remove unused default arguments from VotingArbiter constructor, which the LLVM compiler warns make "this constructor a default constructor."
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzflag@22804 08b3d480-bf2c-0410-a26f-811ee3361c24

--- include/VotingArbiter.h
+++ include/VotingArbiter.h
@@ -231,11 +231,11 @@ public:
 };
 
 
-inline VotingArbiter::VotingArbiter(unsigned short int voteTime=60,
-				    unsigned short int vetoTime=20,
-				    unsigned short int votesRequired=2,
-				    float votePercentage=50.1,
-				    unsigned short int voteRepeatTime=300)
+inline VotingArbiter::VotingArbiter(unsigned short int voteTime,
+				    unsigned short int vetoTime,
+				    unsigned short int votesRequired,
+				    float votePercentage,
+				    unsigned short int voteRepeatTime)
   : _votingBooth(NULL),
     _maxVotes(votesRequired+1),
     _voteTime(voteTime),
