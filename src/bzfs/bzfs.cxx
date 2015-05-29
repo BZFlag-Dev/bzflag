da5fee4c5f0052292933c947773bcdca15cfd7db
Joshua Bodine <josh@joshb.us>
2015-05-29 00:40:28 -0700

    Also consider individual player scores, favoring weaker teams, prior to randomizing a new player's automatic team assignment.

--- src/bzfs/bzfs.cxx
+++ src/bzfs/bzfs.cxx
@@ -1702,20 +1702,56 @@ static TeamColor teamSelect(TeamColor t, std::vector<TeamSize> teams)
     if (teams[i].color == t)
       return t;
 
-  // if scores are unequal, choose from the team(s) with the low score
-  int minScore = team[teams[0].color].team.getWins() - team[teams[0].color].team.getLosses();
-  for(int i = 1; i < (int) teams.size(); ++i) {
-    int teamScore = team[teams[i].color].team.getWins() - team[teams[i].color].team.getLosses();
-    if(teamScore < minScore)
-      minScore = teamScore;
+  // for CTF games, eliminate all but the lowest-scoring teams
+  if(clOptions->gameType == ClassicCTF) {
+    int minScore = team[teams[0].color].team.getWins() - team[teams[0].color].team.getLosses();
+    for(int i = 1; i < (int) teams.size(); ++i) {
+      int teamScore = team[teams[i].color].team.getWins() - team[teams[i].color].team.getLosses();
+      if(teamScore < minScore)
+        minScore = teamScore;
+    }
+    int i = 0;
+    while(i < (int) teams.size())
+      if(team[teams[i].color].team.getWins() - team[teams[i].color].team.getLosses() > minScore
+          && team[teams[i].color].team.size > 0) // don't exclude teams with no members
+        teams.erase(teams.begin() + i);
+      else
+        ++i;
   }
-  int i = 0;
-  while(i < (int) teams.size()) {
-    if(team[teams[i].color].team.getWins() - team[teams[i].color].team.getLosses() != minScore
-        && team[teams[i].color].team.size > 0)
-      teams.erase(teams.begin() + i);
-    else
-      ++i;
+
+  // if multiple teams remain, eliminate all but those with the lowest total player scores
+  if(teams.size() > 1) {
+    int *totalPlayerScores = new int[teams.size()];
+    for(int i = 0; i < (int) teams.size(); ++i)
+      totalPlayerScores[i] = 0;
+
+    for(int i = 0; i < GameKeeper::Player::count(); ++i) {
+      GameKeeper::Player *thisPlayer = GameKeeper::Player::getPlayerByIndex(i);
+      if(thisPlayer == 0)
+        continue;
+
+      int thisTeamIndex = 0;
+      while(thisTeamIndex < teams.size()) {
+        if(teams[thisTeamIndex].color == thisPlayer->player.getTeam())
+          break;
+
+        ++thisTeamIndex;
+      }
+      if(thisTeamIndex < teams.size())
+        totalPlayerScores[thisTeamIndex] += thisPlayer->score.getWins() -
+            thisPlayer->score.getLosses();
+    }
+
+    int minScore = totalPlayerScores[0];
+    for(int i = 1; i < (int) teams.size(); ++i)
+      if(totalPlayerScores[i] < minScore)
+        minScore = totalPlayerScores[i];
+    int i = 0;
+    while(i < (int) teams.size())
+      if(totalPlayerScores[i] > minScore)
+        teams.erase(teams.begin() + i);
+      else
+        ++i;
   }
 
   return teams[rand() % teams.size()].color;
