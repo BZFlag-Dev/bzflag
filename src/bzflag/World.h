BZFlag-Dev/bzflag

commit 177a89a8e91f7041c3b67a7864ddc7a74c0faf09

Fix detection of team kill ability so that the warning symbol properly shows when locking onto a teammate on experimental quality.

src/bzflag/World.h
@@ -210,7 +210,7 @@ inline bool		World::allowTeamFlags() const
 
 inline bool		World::allowTeamKills() const
 {
-  return (gameOptions & short(NoTeamKillsGameStyle)) == 0 || !allowTeams();
+  return (gameOptions & short(NoTeamKillsGameStyle)) == 0 && allowTeams();
 }
 
 inline bool		World::allowSuperFlags() const

src/bzflag/playing.cxx
@@ -3803,8 +3803,9 @@ static bool isKillable(const Player *target)
     return true;
   if (myTank->getFlag() == Flags::Colorblindness)
     return true;
-  if (World::getWorld()->allowTeamKills() ||
-    target->getTeam() != myTank->getTeam())
+  if (! World::getWorld()->allowTeamKills())
+    return true;
+  if (target->getTeam() != myTank->getTeam())
     return true;
 
   return false;

commit 0a0e329fb87984168dddb59e0b8f6586bc5c47a8

Fixed team kill functionality that I broke in 177a89a8. Also made the server ignore -noTeamKills and warn when -offa is also specified.

src/bzflag/World.h
@@ -210,7 +210,7 @@ inline bool		World::allowTeamFlags() const
 
 inline bool		World::allowTeamKills() const
 {
-  return (gameOptions & short(NoTeamKillsGameStyle)) == 0 && allowTeams();
+  return (gameOptions & short(NoTeamKillsGameStyle)) == 0;
 }
 
 inline bool		World::allowSuperFlags() const

src/bzflag/playing.cxx
@@ -3803,7 +3803,7 @@ static bool isKillable(const Player *target)
     return true;
   if (myTank->getFlag() == Flags::Colorblindness)
     return true;
-  if (! World::getWorld()->allowTeamKills())
+  if (! World::getWorld()->allowTeamKills() || ! World::getWorld()->allowTeams())
     return true;
   if (target->getTeam() != myTank->getTeam())
     return true;

src/bzfs/CmdLineOptions.cxx
@@ -864,7 +864,10 @@ void parse(int argc, char **argv, CmdLineOptions &options, bool fromWorldFile)
     }
     else if (strcmp(argv[i], "-noTeamKills") == 0) {
       // disable team killing
-      options.gameOptions |= int(NoTeamKillsGameStyle);
+      if(options.gameType == OpenFFA)
+	std::cerr << "noteamkills check WARNING: -noTeamKills is incompatible with -offa, ignoring" << std::endl;
+      else
+	options.gameOptions |= int(NoTeamKillsGameStyle);
     }
     else if (strcmp(argv[i], "-p") == 0) {
       // use a different port
@@ -1171,6 +1174,10 @@ void parse(int argc, char **argv, CmdLineOptions &options, bool fromWorldFile)
       if (options.gameType == RabbitChase || options.gameType == ClassicCTF) {
 	std::cerr << "Open (Teamless) Free-for-all incompatible with other modes" << std::endl;
 	std::cerr << "Open Free-for-all assumed" << std::endl;
+      }
+      if ((options.gameOptions & int(NoTeamKillsGameStyle)) != 0) {
+	std::cerr << "offa check WARNING: -noTeamKills is incompatible with -offa, ignoring" << std::endl;
+	options.gameOptions ^= int(NoTeamKillsGameStyle);
       }
       options.gameType = OpenFFA;
     }