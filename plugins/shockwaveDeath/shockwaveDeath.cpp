06070e8db061093e1df159752f28af19488f4deb
Scott Wichser <blast007@users.sourceforge.net>
2016-05-23 05:52:19 -0500

    Clean up and document the shockwaveDeath source.

--- plugins/shockwaveDeath/shockwaveDeath.cpp
+++ plugins/shockwaveDeath/shockwaveDeath.cpp
@@ -7,40 +7,40 @@
 class SWDeathHandler : public bz_Plugin
 {
 public:
-	virtual const char* Name (){return "Shockwave Death";}
-	virtual void Init ( const char* config);
+  virtual const char* Name (){return "Shockwave Death";}
+  virtual void Init ( const char* config);
   virtual void	Event ( bz_EventData *eventData );
 };
 
 BZ_PLUGIN(SWDeathHandler)
 
-void SWDeathHandler::Init( const char* commandLine )
+void SWDeathHandler::Init( const char* /*commandLine*/ )
 {
   bz_debugMessage(4,"shockwaveDeath plugin loaded");
 
+  // Register our event
   Register(bz_ePlayerDieEvent);
-
-  std::string param = commandLine;
-
-  if (param == "usevictim")
-	  bz_debugMessage(0,"shockwaveDeath plugin no longer takes any parameters");
 }
 
 void SWDeathHandler::Event ( bz_EventData *eventData )
 {
+  // We only handle the player death event, so bail out otherwise
   if (eventData->eventType != bz_ePlayerDieEvent)
     return;
 
+  // Cast our event data so we can access the death position
   bz_PlayerDieEventData_V1 *dieData = (bz_PlayerDieEventData_V1*)eventData;
 
-  int playerToUse = BZ_SERVER;
-
+  // Retrieve the current reload time value
   float reloadTime = (float)bz_getBZDBDouble("_reloadTime");
 
+  // If _swDeathReloadFactor is defined and greater than zero, multiply the
+  // reload time by this factor
   if (bz_BZDBItemExists("_swDeathReloadFactor") && bz_getBZDBDouble("_swDeathReloadFactor") > 0)
     reloadTime *= (float)bz_getBZDBDouble("_swDeathReloadFactor");
 
-  bz_fireWorldWep("SW",reloadTime,playerToUse,dieData->state.pos,0,0,0,0.0f);
+  // Fire a shockwave where the player died
+  bz_fireWorldWep("SW", reloadTime, BZ_SERVER, dieData->state.pos, 0, 0, 0, 0.0f);
 }
 
 // Local Variables: ***
