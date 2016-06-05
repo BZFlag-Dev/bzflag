2d19db0cd5f4f5cb3fededbeaf7bacb2a00afde2
Scott Wichser <blast007@users.sourceforge.net>
2016-06-05 14:54:41 -0500

    RogueGenocide plugin: Clean up whitespace and formatting, update documentation, and make the plugin default to not triggering the genocide effect when a rogue kills themself.

--- plugins/RogueGenocide/README.RogueGenocide.txt
+++ plugins/RogueGenocide/README.RogueGenocide.txt
@@ -1,8 +1,19 @@
-========================================================================
-    DYNAMIC LINK LIBRARY : RogueGenocide Project Overview
-========================================================================
+BZFlag Server Plugin: RogueGenocide
+================================================================================
 
-This is the RogueGenocide plugin.  It causes all of the spawned rogue
-players to die if one of them is hit by a Genocide shot, just as if
-they were all on the same team.  A rogue player firing such a shot
-will die, too.
+Normally, the genocide flag will not affect rogue players, since they are not a
+true team.  This plugin changes that behavior so that rogue players are
+affected.  This would also allow a rouge player to shoot another rogue with the
+genocide flag and wipe out all rogues (including themself).  A rogue shooting
+themself does not trigger the genocide effect unless the 'suicide' option is
+passed when loading the plugin.
+
+
+Loading the plugin
+--------------------------------------------------------------------------------
+
+To load the plugin with default settings, use:
+  -loadplugin RogueGenocide
+
+To allow a rogue to shoot themself to trigger a genocide, use:
+  -loadplugin RogueGenocide,suicide
--- plugins/RogueGenocide/RogueGenocide.cpp
+++ plugins/RogueGenocide/RogueGenocide.cpp
@@ -9,67 +9,66 @@
 class RogueGenoHandler : public bz_Plugin
 {
 public:
-	virtual const char* Name (){return "Rogue Genocide";}
-	virtual void Init ( const char* config);
+  virtual const char* Name (){return "Rogue Genocide";}
+  virtual void Init (const char* config);
 
-  virtual void Event ( bz_EventData *eventData );
+  virtual void Event (bz_EventData *eventData);
 
-  bool noSuicide;
+  bool allowSuicide;
 };
 
 BZ_PLUGIN(RogueGenoHandler)
 
-void RogueGenoHandler::Init( const char* commandLine )
+void RogueGenoHandler::Init(const char* commandLine)
 {
   Register(bz_ePlayerDieEvent);
 
   std::string param = commandLine;
 
-  noSuicide = (param == "nosuicide");
+  allowSuicide = (param == "suicide");
 }
 
-void RogueGenoHandler::Event ( bz_EventData *eventData )
+void RogueGenoHandler::Event (bz_EventData *eventData)
 {
   switch (eventData->eventType) {
-  default:
-    // no clue
-    break;
+    default:
+      // no clue
+      break;
 
-  // wait for a tank death and start checking for genocide and rogues
-  case bz_ePlayerDieEvent:
+    // wait for a tank death and start checking for genocide and rogues
+    case bz_ePlayerDieEvent:
     {
-      bz_PlayerDieEventData_V1	*dieData = (bz_PlayerDieEventData_V1*)eventData;
+      bz_PlayerDieEventData_V1 *dieData = (bz_PlayerDieEventData_V1*)eventData;
       //if its not a genocide kill, dont care
-      if (dieData->flagKilledWith != "G" )
+      if (dieData->flagKilledWith != "G")
 	break;
       // if the tank killed was not a rogue, let the server/client do the normal killing
-      if (dieData->team != eRogueTeam )
+      if (dieData->team != eRogueTeam)
 	break;
       // option to disallow rogues getting points for shooting themselves
-      if ( noSuicide && dieData->killerID == dieData->playerID )
+      if (!allowSuicide && dieData->killerID == dieData->playerID)
 	break;
 
       // if the tank killed was a rogue, kill all rogues.
-      bz_APIIntList	*playerList = bz_newIntList();
+      bz_APIIntList *playerList = bz_newIntList();
 
       bz_getPlayerIndexList(playerList);
 
-      for ( unsigned int i = 0; i < playerList->size(); i++)
-      {
+      for (unsigned int i = 0; i < playerList->size(); i++) {
 	int targetID = (*playerList)[i];
-	bz_BasePlayerRecord *playRec = bz_getPlayerByIndex ( targetID );
-	if (!playRec) continue;
+	bz_BasePlayerRecord *playRec = bz_getPlayerByIndex (targetID);
+	if (!playRec)
+	  continue;
 
 	// the sucker is a spawned rogue, kill him.  This generates another death event,
 	// so if you kill another rogue with geno while you are a rogue you end up dead too.
 	// and you get both messages (victim and be careful)
-	if ( playRec->spawned && playRec->team == eRogueTeam )
-	{
-	  bz_killPlayer( targetID, false, dieData->killerID, "G" );
+	if (playRec->spawned && playRec->team == eRogueTeam) {
+	  bz_killPlayer(targetID, false, dieData->killerID, "G");
 	  bz_sendTextMessage(BZ_SERVER, targetID, "You were a victim of Rogue Genocide");
 
 	  // oops, I ended up killing myself (directly or indirectly) with Genocide!
-	  if ( targetID == dieData->killerID )
+	  if (targetID == dieData->killerID)
 	    bz_sendTextMessage(BZ_SERVER, targetID, "You should be more careful with Genocide!");
 	}
 
