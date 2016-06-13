05c912f05b749190f1d76b665d889ad91da0f374
Scott Wichser <blast007@users.sourceforge.net>
2016-06-13 05:48:37 -0500

    wwzones plugin: Clean up and simplify some code, ignore the dt value in the map object, and update the documentation.

--- plugins/wwzones/README.wwzones.txt
+++ plugins/wwzones/README.wwzones.txt
@@ -1,103 +1,125 @@
-========================================================================
-    DYNAMIC LINK LIBRARY : wwzones Project Overview
-========================================================================
+BZFlag Server Plugin: wwzones
+================================================================================
 
-wwzones plugin
+Original author: LouMan
 
-Author: LouMan
-
-Credit to JeffM2501 for base zone and custom map object code (from flagStay plugin), which I have modified to fit this application.
-
-Description:
-
-The plugin will fire a map defined world weapon whenever a player is in a map defined volume.  It could be used for landmines, booby traps, clearing campers in difficult locations, general amusement, etc.  Multiple zones with different weapons, etc. may be used in same map file.
-
--------------------------------------------------------------------------------------
-
-The parameters of the wwzone map definition are follows:
+The plugin will fire a map defined world weapon whenever a player is in a map
+defined volume (either rectangular or circular).  It could be used for
+landmines, booby traps, clearing campers in difficult locations, general
+amusement, etc.  Multiple zones with different weapons, etc. may be used in
+same map file.
 
 
-bbox <Xmin> <Xmax> <Ymin> <Ymax> <Zmin> <Zmax>
-or
-cylinder <X> <Y> <Zmin> <Zmax> <radius>
+Loading the plugin
+--------------------------------------------------------------------------------
 
-These define the volume on the map that will trigger a world weapon whenever players enter it.
+This plugin takes no optional arguments, so load it with:
 
-
-zoneweapon <flagType> <lifetime> <X> <Y> <Z> <tilt> <direction> <shotID> <DT>
-
-This defines the world weapon.
-<flagType> is a valid type of flag to use for the world weapon (e.g. SW, GM, L, etc.)
-<lifetime> defines the life of the weapon's shots.
-<X> <Y> <Z> defines the location of the world weapon.
-<tilt> defined the tilt of the world weapon.
-<direction> defines the direction of the world weapon.
-<shotID> ...not sure what this does ;) but it is an input to the API world weapon function.
-<DT> defines the delay time of the world weapon.
+  -loadplugin wwzones
 
 
-playermessage "your message here"
+Map Objects
+--------------------------------------------------------------------------------
 
-If used (optional), sends custom message to player when he/she triggers world weapon.
+The plugin adds a 'wwzone' object type.  You will specify this when creating a
+custom zone.  Both zone shapes will use a 'pos' (position), various attributes
+for controlling the world weapon and messages.  The rectangular type will use a
+'size' and 'rot' (rotation) attribute, and the circular type will use a 'radius'
+and 'height' attribute.
+
+Here are the common attributes and how to use them:
+
+Define the weapon that will be triggered from this wwzone:
+  zoneweapon <flagType> <lifetime> <X> <Y> <Z> <tilt> <direction> <shotID> <DT>
+
+  <flagType>
+    This is a valid type of flag to use for the world weapon (e.g. SW, GM, etc)
+  <lifetime>
+    This defines (in seconds) how long the shot should exist.
+  <X> <Y> <Z>
+    This defines the starting position of the world weapon.
+  <tilt>
+    This defines the tilt angle of the world weapon (in degrees) with 0 being
+    level to the ground.
+  <direction>
+    This defines the rotation angle (in degrees) with 0 pointing east, and
+    moving in a counter-clockwise direction.  So 90 is north, 45 is northeast,
+    and so on.
+  <shotID>
+    This value should be 0 unless you know what you're doing. Using a value of 0
+    will let the server generate an appropriate shot ID for you.
+  <DT>
+    This value is completely ignored, so just pass a 0 to it.
+  
+This optional attribute sends custom message to player when he/she triggers the
+world weapon.  It is sent as a private message to the player:
+  playermessage "your message here"
+
+This optional attribute sends a custom message to all players when the world
+weapon is triggered:
+  servermessage "your message here"
+
+This optional attribute will issue a plugin defined message to all players
+stating which type of world weapon was triggered by whom:
+  infomessage
+
+This optional attribute will cause the world weapon to fire releated every
+<seconds> seconds while player is in defined volume:
+  repeat <seconds>
+
+  <seconds>
+    This defines the repeat time of the weapon (minimum 0.1 seconds).  If you
+    specify only "repeat" but don't include a value after it, it will repeat
+    every 0.5 seconds.  If you don't include repeat in the wwzone definition, it
+    will only fire once when player enters zone.
+
+This optional attribute will delay the initial fire of world weapon by <seconds>
+seconds:
+  timedelay <seconds>
 
 
-servermessage "your message here"
-
-If used (optional), sends custom message to all players when a world weapon is triggered.
-
-
-infomessage
-
-If used (optional), this will issue standard message to all players stating which type of world weapon was triggered by whom.  This message cannot be customized.
-
-
-repeat <seconds>
-
-If used (optional), will repeat weapon fire while player is in defined volume.
-<seconds> defines the repeat time of the weapon (minimum 0.1 seconds).
-If <seconds> not specified, default is 0.5 seconds.
-If repeat is not included in definition, weapon will only fire once when player enters zone.
-
-
-timedelay <seconds>
-
-If used (optional), will delay initial fire of world weapon by specified number of seconds.
-
--------------------------------------------------------------------------------------
-
-Examples of map (.bzw) entries:
-
+# This will fire a shockwave every 0.25 seconds (approximately) after a player
+# is in the volume defined by the bbox for more than 2 seconds.  In this case, a
+# square volume 50 x 50 x 1 centered at coordinates -50 50 1.  The shockwaves
+# will occur at coordinates 0 0 1 and will send message "You triggered
+# Shockwave!" to player that triggered it, when initially fired.
 
 wwzone
-  bbox -80 -30 -80 -30 0 2
+  pos -50 50 0
+  size 50 50 1
   zoneweapon SW 1 0 0 1 0 0 0 0
   playermessage "You triggered Shockwave!"
   repeat .25
   timedelay 2
 end
 
-This will fire a shockwave every 0.25 seconds (approximately) after a player is in the volume defined by the bbox for more than 2 seconds.  In this case, a square volume 50 x 50 x 1 centered at coordinates -50 50 1.  The shockwaves will occur at coordinates 0 0 1 and will send message "You triggered Shockwave!" to player that triggered it, when initially fired.
 
+# This will fire a laser, every 0.5 seconds (approximately) while a player is in
+# the volume defined by the bbox.  In this case, a square volume 50 x 50 x 1
+# centered at coordinates 50 50 1.  The laser will occur at coordinates 5 5 1
+# and be tilted at 45 degrees with a heading of 25 degrees.  When initially
+# fired, there is a message sent to all players "Laserz rule!"
 
 wwzone
-  bbox 30 80 30 80 0 2
+  pos 50 50 0
+  size 50 50 1
   zoneweapon L 1 5 5 1 45 25 0 0
   servermessage "Laserz rule!"
   repeat
 end
 
-This will fire a laser, every 0.5 seconds (approximately) while a player is in the volume defined by the bbox.  In this case, a square volume 50 x 50 x 1 centered at coordinates 50 50 1.  The laser will occur at coordinates 5 5 1 and be tilted at 45 degrees with a heading of 25 degrees.  When initially fired, there is a message sent to all players "Laserz rule!"
 
+# This will fire a GM one time after a player has been in the volume defined by
+# the cylinder for more than 1 second.  In this case, a cylindrical volume with
+# a radius of 20, centered at coordinates 50 -50 3.  The GM will shoot from
+# coordinates 0 0 1 with 0 tilt and a heading of 0 degrees.  When initially
+# fired, there is a message sent to all players "GM triggered by <callsign>."
 
 wwzone
-  cylinder 50 -50 0 6 20
+  pos 50 -50 0
+  radius 20
+  height 6
   zoneweapon GM 1 0 0 1 0 0 0 0
   infomessage
   timedelay 1
 end
-
-This will fire a GM one time after a player has been in the volume defined by the cylinder for more than 1 second.  In this case, a cylindrical volume with a radius of 20, centered at coordinates 50 -50 3.  The GM will shoot from coordinates 0 0 1 with 0 tilt and a heading of 0 degrees.  When initially fired, there is a message sent to all players "GM triggered by <callsign>."
-
--------------------------------------------------------------------------------------
-
-Please report any bugs or issues with this plugin to LouMan, and good luck.
--- plugins/wwzones/wwzones.cpp
+++ plugins/wwzones/wwzones.cpp
@@ -1,61 +1,63 @@
-// wwzones.cpp : Defines the entry point for the DLL application.
+/* bzflag
+ * Copyright (c) 1993-2016 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #include "bzfsAPI.h"
 #include <map>
-#include <cmath>
-
-class WWINFO
-{
-public:
-	WWINFO()
-	{
-		tickTime = 0.5;
-	}
-	double tickTime;
-};
-
-WWINFO wwinfo;
 
 class WWZEventHandler : public bz_Plugin, bz_CustomMapObjectHandler
 {
 public:
+  virtual const char* Name(){return "World Weapon Zones";}
+  virtual void Init(const char* config);
+  virtual void Cleanup();
+  virtual void Event(bz_EventData *eventData);
 
-	virtual const char* Name (){return "World Weapon Zones";}
-	virtual void Init ( const char* config);
-	virtual void Cleanup ();
-	virtual void Event ( bz_EventData *eventData );
-
-	virtual bool MapObject ( bz_ApiString object, bz_CustomMapObjectInfo *data );
-
+  virtual bool MapObject (bz_ApiString object, bz_CustomMapObjectInfo *data);
 };
 
 BZ_PLUGIN(WWZEventHandler)
 
-void WWZEventHandler::Init (const char* /*commandLineParameter*/){
+void WWZEventHandler::Init (const char* /*commandLineParameter*/)
+{
+  // Register our custom world object
+  bz_registerCustomMapObject("WWZONE", this);
 
-	bz_registerCustomMapObject("WWZONE",this);
-	Register(bz_eTickEvent);
+  // Register our events
+  Register(bz_eTickEvent);
 
-	MaxWaitTime = (float)wwinfo.tickTime;
+  // Set an initial minimum wait time of 0.5 seconds
+  MaxWaitTime = 0.5f;
 }
 
-void WWZEventHandler::Cleanup (void){
+void WWZEventHandler::Cleanup (void)
+{
+  // Remove our events
+  Flush();
 
-	Flush();
-	bz_removeCustomMapObject("WWZONE");
+  // Remove our custom world object
+  bz_removeCustomMapObject("WWZONE");
 }
 
 class WWZPlyrInfo
 {
 public:
-	WWZPlyrInfo()
-	{
-		wwzplyrID = -1;
-		wwzPlyrInTime = 0;
-	}
+  WWZPlyrInfo()
+  {
+    wwzplyrID = -1;
+    wwzPlyrInTime = 0;
+  }
 
-	int wwzplyrID;
-	double wwzPlyrInTime;
+  int wwzplyrID;
+  double wwzPlyrInTime;
 };
 
 WWZPlyrInfo wwzNewPlyr;
@@ -63,213 +65,178 @@ WWZPlyrInfo wwzNewPlyr;
 class WWZone : public bz_CustomZoneObject
 {
 public:
-	WWZone() : bz_CustomZoneObject()
-	{
-		zoneWeapon = "";
-		zoneWeaponLifetime = 0;
-		zoneWeaponPosition[0] = 0;
-		zoneWeaponPosition[1] = 0;
-		zoneWeaponPosition[2] = 0;
-		zoneWeaponTilt = 0;
-		zoneWeaponDirection = 0;
-		zoneWeaponShotID = 0;
-		zoneWeaponDT = 0;
-		zoneWeaponRepeat = false;
-		zoneWeaponFired = false;
-		zoneWeaponLastFired = 0;
-		zoneWeaponMinFireTime = 0.5;
-		zoneWeaponTimeDelay = 0;
-		zoneWeaponInfoMessage = false;
-		zoneWeaponSentMessage = false;
-		wwzPlyrList.clear();
-	}
+  WWZone() : bz_CustomZoneObject()
+  {
+    zoneWeapon = "";
+    zoneWeaponLifetime = 0;
+    zoneWeaponPosition[0] = 0;
+    zoneWeaponPosition[1] = 0;
+    zoneWeaponPosition[2] = 0;
+    zoneWeaponTilt = 0;
+    zoneWeaponDirection = 0;
+    zoneWeaponShotID = 0;
+    zoneWeaponRepeat = false;
+    zoneWeaponFired = false;
+    zoneWeaponLastFired = 0;
+    zoneWeaponMinFireTime = 0.5;
+    zoneWeaponTimeDelay = 0;
+    zoneWeaponInfoMessage = false;
+    zoneWeaponSentMessage = false;
+    wwzPlyrList.clear();
+  }
 
-	std::vector <WWZPlyrInfo> wwzPlyrList;
+  std::vector <WWZPlyrInfo> wwzPlyrList;
 
-	bz_ApiString zoneWeapon;
-	float zoneWeaponLifetime, zoneWeaponPosition[3], zoneWeaponTilt, zoneWeaponDirection, zoneWeaponDT;
-	double zoneWeaponMinFireTime, zoneWeaponTimeDelay, zoneWeaponLastFired;
-	bool zoneWeaponRepeat, zoneWeaponInfoMessage, zoneWeaponFired, zoneWeaponSentMessage;
-	int zoneWeaponShotID;
+  bz_ApiString zoneWeapon;
+  float zoneWeaponLifetime, zoneWeaponPosition[3], zoneWeaponTilt, zoneWeaponDirection;
+  double zoneWeaponMinFireTime, zoneWeaponTimeDelay, zoneWeaponLastFired;
+  bool zoneWeaponRepeat, zoneWeaponInfoMessage, zoneWeaponFired, zoneWeaponSentMessage;
+  int zoneWeaponShotID;
 
-	std::string playermessage;
-	std::string servermessage;
+  std::string playermessage;
+  std::string servermessage;
 };
 
 std::vector <WWZone> zoneList;
 
-bool WWZEventHandler::MapObject ( bz_ApiString object, bz_CustomMapObjectInfo *data )
+bool WWZEventHandler::MapObject (bz_ApiString object, bz_CustomMapObjectInfo *data)
 {
-	if (object != "WWZONE" || !data)
-		return false;
+  if (object != "WWZONE" || !data)
+    return false;
 
-	WWZone newZone;
-	newZone.handleDefaultOptions(data);
+  WWZone newZone;
+  newZone.handleDefaultOptions(data);
 
-	// parse all the chunks
-	for ( unsigned int i = 0; i < data->data.size(); i++ )
-	{
-		std::string line = data->data.get(i).c_str();
+  // parse all the chunks
+  for (unsigned int i = 0; i < data->data.size(); i++) {
+    std::string line = data->data.get(i).c_str();
 
-		bz_APIStringList *nubs = bz_newStringList();
-		nubs->tokenize(line.c_str()," ",0,true);
+    bz_APIStringList *nubs = bz_newStringList();
+    nubs->tokenize(line.c_str(), " ", 0, true);
 
-		if ( nubs->size() > 0)
-		{
-			std::string key = bz_toupper(nubs->get(0).c_str());
+    if (nubs->size() > 0) {
+      std::string key = bz_toupper(nubs->get(0).c_str());
 
-			if ( key == "ZONEWEAPON" && nubs->size() > 9)
-			{
-				newZone.zoneWeapon = nubs->get(1);
-				newZone.zoneWeaponLifetime = (float)atof(nubs->get(2).c_str());
-				newZone.zoneWeaponPosition[0] = (float)atof(nubs->get(3).c_str());
-				newZone.zoneWeaponPosition[1] = (float)atof(nubs->get(4).c_str());
-				newZone.zoneWeaponPosition[2] = (float)atof(nubs->get(5).c_str());
-				newZone.zoneWeaponTilt = (float)(atof(nubs->get(6).c_str()) * M_PI/180.0);	// convert degrees to radians
-				newZone.zoneWeaponDirection = (float)(atof(nubs->get(7).c_str()) * M_PI/180.0);	// convert degrees to radians
-				newZone.zoneWeaponShotID = (int)atoi(nubs->get(8).c_str());
-				newZone.zoneWeaponDT = (float)atof(nubs->get(9).c_str());
-			}
-			else if ( key == "REPEAT" && nubs->size() == 1 )
-			{
-				newZone.zoneWeaponRepeat = true;
-			}
-			else if ( key == "REPEAT" && nubs->size() > 1 )
-			{
-				newZone.zoneWeaponRepeat = true;
-				newZone.zoneWeaponMinFireTime = (double)atof(nubs->get(1).c_str());
-				if (newZone.zoneWeaponMinFireTime < wwinfo.tickTime && newZone.zoneWeaponMinFireTime >= 0.1)
-					wwinfo.tickTime = newZone.zoneWeaponMinFireTime - .05;  //tick faster than ww calls
-			}
-			else if ( key == "TIMEDELAY" && nubs->size() > 1 )
-			{
-				newZone.zoneWeaponTimeDelay = (double)atof(nubs->get(1).c_str());
-				if (newZone.zoneWeaponTimeDelay < 0)
-					newZone.zoneWeaponTimeDelay = 0;
-			}
-			else if ( key == "PLAYERMESSAGE" && nubs->size() > 1 )
-			{
-				newZone.playermessage = nubs->get(1).c_str();
-			}
-			else if ( key == "SERVERMESSAGE" && nubs->size() > 1 )
-			{
-				newZone.servermessage = nubs->get(1).c_str();
-			}
-			else if ( key == "INFOMESSAGE" )
-			{
-				newZone.zoneWeaponInfoMessage = true;
-			}
-		}
-		bz_deleteStringList(nubs);
-	}
-	zoneList.push_back(newZone);
-
-	MaxWaitTime = (float)wwinfo.tickTime;
-	return true;
+      if (key == "ZONEWEAPON" && nubs->size() > 9) {
+        newZone.zoneWeapon = nubs->get(1);
+        newZone.zoneWeaponLifetime = (float)atof(nubs->get(2).c_str());
+        newZone.zoneWeaponPosition[0] = (float)atof(nubs->get(3).c_str());
+        newZone.zoneWeaponPosition[1] = (float)atof(nubs->get(4).c_str());
+        newZone.zoneWeaponPosition[2] = (float)atof(nubs->get(5).c_str());
+        newZone.zoneWeaponTilt = (float)(atof(nubs->get(6).c_str()) * M_PI/180.0);  // convert degrees to radians
+        newZone.zoneWeaponDirection = (float)(atof(nubs->get(7).c_str()) * M_PI/180.0);  // convert degrees to radians
+	newZone.zoneWeaponShotID = (int)atoi(nubs->get(8).c_str());
+      } else if (key == "REPEAT" && nubs->size() == 1) {
+        newZone.zoneWeaponRepeat = true;
+      } else if (key == "REPEAT" && nubs->size() > 1) {
+        newZone.zoneWeaponRepeat = true;
+        newZone.zoneWeaponMinFireTime = (double)atof(nubs->get(1).c_str());
+        if (newZone.zoneWeaponMinFireTime < MaxWaitTime && newZone.zoneWeaponMinFireTime >= 0.1)
+          MaxWaitTime = (float)newZone.zoneWeaponMinFireTime - 0.05f;  //tick faster than ww calls
+      } else if (key == "TIMEDELAY" && nubs->size() > 1) {
+        newZone.zoneWeaponTimeDelay = (double)atof(nubs->get(1).c_str());
+        if (newZone.zoneWeaponTimeDelay < 0)
+          newZone.zoneWeaponTimeDelay = 0;
+      } else if (key == "PLAYERMESSAGE" && nubs->size() > 1) {
+        newZone.playermessage = nubs->get(1).c_str();
+      } else if (key == "SERVERMESSAGE" && nubs->size() > 1) {
+        newZone.servermessage = nubs->get(1).c_str();
+      } else if (key == "INFOMESSAGE") {
+        newZone.zoneWeaponInfoMessage = true;
+      }
+    }
+    bz_deleteStringList(nubs);
+  }
+  zoneList.push_back(newZone);
+  return true;
 }
 
 inline bool wasHere(int zoneNum, int plyrNum)
 {
-	for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++)
-	{
-		if (plyrNum == zoneList[zoneNum].wwzPlyrList[j].wwzplyrID)
-			return true;
-	}
+  for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++) {
+    if (plyrNum == zoneList[zoneNum].wwzPlyrList[j].wwzplyrID)
+      return true;
+  }
 
-	wwzNewPlyr.wwzplyrID = plyrNum;
-	wwzNewPlyr.wwzPlyrInTime = bz_getCurrentTime();
+  wwzNewPlyr.wwzplyrID = plyrNum;
+  wwzNewPlyr.wwzPlyrInTime = bz_getCurrentTime();
 
-	zoneList[zoneNum].wwzPlyrList.push_back(wwzNewPlyr);
-	zoneList[zoneNum].zoneWeaponSentMessage = false;
-	zoneList[zoneNum].zoneWeaponFired = false;
+  zoneList[zoneNum].wwzPlyrList.push_back(wwzNewPlyr);
+  zoneList[zoneNum].zoneWeaponSentMessage = false;
+  zoneList[zoneNum].zoneWeaponFired = false;
 
-	return false;
+  return false;
 }
 
 inline void notHere(int zoneNum, int plyrNum)
 {
-	for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++)
-	{
-		if (plyrNum == zoneList[zoneNum].wwzPlyrList[j].wwzplyrID)
-		{
-			zoneList[zoneNum].wwzPlyrList.erase(zoneList[zoneNum].wwzPlyrList.begin() + j);
-			zoneList[zoneNum].zoneWeaponFired = false;
-			zoneList[zoneNum].zoneWeaponSentMessage = false;
-			return;
-		}
-	}
-
-	return;
+  for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++) {
+    if (plyrNum == zoneList[zoneNum].wwzPlyrList[j].wwzplyrID) {
+      zoneList[zoneNum].wwzPlyrList.erase(zoneList[zoneNum].wwzPlyrList.begin() + j);
+      zoneList[zoneNum].zoneWeaponFired = false;
+      zoneList[zoneNum].zoneWeaponSentMessage = false;
+      return;
+    }
+  }
 }
 
 inline bool OKToFire(int zoneNum, int plyrNum)
 {
-	for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++)
-	{
-		if (zoneList[zoneNum].wwzPlyrList[j].wwzplyrID == plyrNum)
-		{
-			if ((bz_getCurrentTime() - zoneList[zoneNum].wwzPlyrList[j].wwzPlyrInTime) > zoneList[zoneNum].zoneWeaponTimeDelay && !zoneList[zoneNum].zoneWeaponFired)
-			{
-				zoneList[zoneNum].wwzPlyrList[j].wwzPlyrInTime = bz_getCurrentTime();
-				return true;
-			}
-		}
-	}
+  for (unsigned int j = 0; j < zoneList[zoneNum].wwzPlyrList.size(); j++) {
+    if (zoneList[zoneNum].wwzPlyrList[j].wwzplyrID == plyrNum) {
+      if ((bz_getCurrentTime() - zoneList[zoneNum].wwzPlyrList[j].wwzPlyrInTime) > zoneList[zoneNum].zoneWeaponTimeDelay && !zoneList[zoneNum].zoneWeaponFired) {
+        zoneList[zoneNum].wwzPlyrList[j].wwzPlyrInTime = bz_getCurrentTime();
+        return true;
+      }
+    }
+  }
 
-	return false;
+  return false;
 }
 
-void WWZEventHandler::Event ( bz_EventData *eventData )
+void WWZEventHandler::Event (bz_EventData *eventData)
 {
-	if (eventData->eventType != bz_eTickEvent)
-		return;
+  if (eventData->eventType != bz_eTickEvent)
+    return;
 
-	bz_APIIntList *playerList = bz_newIntList();
-	bz_getPlayerIndexList ( playerList );
+  bz_APIIntList *playerList = bz_newIntList();
+  bz_getPlayerIndexList (playerList);
 
-	for ( unsigned int h = 0; h < playerList->size(); h++ )
-	{
-	bz_BasePlayerRecord *player = bz_getPlayerByIndex(playerList->operator[](h));
+  for (unsigned int h = 0; h < playerList->size(); h++) {
+    bz_BasePlayerRecord *player = bz_getPlayerByIndex(playerList->operator[](h));
 
-			if (player){
+    if (player) {
+      for (unsigned int i = 0; i < zoneList.size(); i++) {
+        if (zoneList[i].pointInZone(player->lastKnownState.pos) && player->spawned) {
+          if (wasHere(i, player->playerID) && OKToFire(i, player->playerID) && !zoneList[i].zoneWeaponFired) {
+            bz_fireWorldWep(zoneList[i].zoneWeapon.c_str(), zoneList[i].zoneWeaponLifetime, BZ_SERVER, zoneList[i].zoneWeaponPosition, zoneList[i].zoneWeaponTilt, zoneList[i].zoneWeaponDirection, zoneList[i].zoneWeaponShotID, 0);
+            zoneList[i].zoneWeaponFired = true;
+            zoneList[i].zoneWeaponLastFired = bz_getCurrentTime();
+          } else {
+            if ((bz_getCurrentTime() - zoneList[i].zoneWeaponLastFired) > zoneList[i].zoneWeaponMinFireTime && zoneList[i].zoneWeaponRepeat)
+	      zoneList[i].zoneWeaponFired = false;
+          }
 
-				for ( unsigned int i = 0; i < zoneList.size(); i++ )
-				{
-					if (zoneList[i].pointInZone(player->lastKnownState.pos) && player->spawned)
-					{
-						if (wasHere(i, player->playerID) && OKToFire(i, player->playerID) && !zoneList[i].zoneWeaponFired)
-						{
-							bz_fireWorldWep (zoneList[i].zoneWeapon.c_str(),zoneList[i].zoneWeaponLifetime,BZ_SERVER,zoneList[i].zoneWeaponPosition,zoneList[i].zoneWeaponTilt,zoneList[i].zoneWeaponDirection,zoneList[i].zoneWeaponShotID,zoneList[i].zoneWeaponDT);
-							zoneList[i].zoneWeaponFired = true;
-							zoneList[i].zoneWeaponLastFired = bz_getCurrentTime();
-						}
-						else
-						{
-							if ((bz_getCurrentTime() - zoneList[i].zoneWeaponLastFired) > zoneList[i].zoneWeaponMinFireTime && zoneList[i].zoneWeaponRepeat)
-							zoneList[i].zoneWeaponFired = false;
-						}
+          if (!zoneList[i].zoneWeaponSentMessage && zoneList[i].zoneWeaponFired) {
+            if (!zoneList[i].playermessage.empty())
+              bz_sendTextMessage(BZ_SERVER, player->playerID, zoneList[i].playermessage.c_str());
+            if (!zoneList[i].servermessage.empty())
+              bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, zoneList[i].servermessage.c_str());
+            if (zoneList[i].zoneWeaponInfoMessage)
+              bz_sendTextMessagef(BZ_SERVER, BZ_ALLUSERS, "%s triggered by %s.", zoneList[i].zoneWeapon.c_str(), player->callsign.c_str());
 
-						if (!zoneList[i].zoneWeaponSentMessage && zoneList[i].zoneWeaponFired)
-						{
-							if (!zoneList[i].playermessage.empty())
-								bz_sendTextMessage(BZ_SERVER,player->playerID,zoneList[i].playermessage.c_str());
-							if (!zoneList[i].servermessage.empty())
-								bz_sendTextMessage(BZ_SERVER,BZ_ALLUSERS,zoneList[i].servermessage.c_str());
-							if (zoneList[i].zoneWeaponInfoMessage)
-								bz_sendTextMessagef(BZ_SERVER,BZ_ALLUSERS,"%s triggered by %s.", zoneList[i].zoneWeapon.c_str(), player->callsign.c_str());
-
-							zoneList[i].zoneWeaponSentMessage = true;
-						}
-					}
-					else
-						notHere(i, player->playerID);
-				}
-				bz_freePlayerRecord(player);
-			}
+            zoneList[i].zoneWeaponSentMessage = true;
+          }
+        } else {
+          notHere(i, player->playerID);
 	}
+      }
+      bz_freePlayerRecord(player);
+    }
+  }
 
-	bz_deleteIntList(playerList);
-	return;
+  bz_deleteIntList(playerList);
+  return;
 }
 
 // Local Variables: ***
