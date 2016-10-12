eee3df00f8121135d1108cc5d3760e09d2b8ea9c
Joshua Bodine <josh@joshb.us>
2016-10-11 22:27:39 -0700

    Bumped version number to 2.4.9, and fixed some formatting in the ChangeLog.

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -46,12 +46,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		8
+#  define BZ_REV		9
 #endif
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"MAINT"
+#  define BZ_BUILD_TYPE		"DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993-2016 Tim Riker";
