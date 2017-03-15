f6055efa44a2f85fc68674005ca750d3649311d2
Joshua Bodine <josh@joshb.us>
2017-03-14 20:12:57 -0700

    Bump version to 2.4.11.

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -46,12 +46,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV		10
+#  define BZ_REV		11
 #endif
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE		"MAINT"
+#  define BZ_BUILD_TYPE		"DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993-2017 Tim Riker";
