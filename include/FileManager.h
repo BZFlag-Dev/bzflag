c136308903a77ab8e7ed5bfebec5c13fecee0711
Joshua Bodine <josh@joshb.us>
2020-04-30 16:03:07 -0700

    Updated copyright year to 2020.

--- include/FileManager.h
+++ include/FileManager.h
@@ -1,5 +1,5 @@
 /* bzflag
- * Copyright (c) 1993-2018 Tim Riker
+ * Copyright (c) 1993-2020 Tim Riker
  *
  * This package is free software;  you can redistribute it and/or
  * modify it under the terms of the license found in the file
