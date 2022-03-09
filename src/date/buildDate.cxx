9b6b92a176bebd0d1dc819acd62621fd88b1c42d
Scott Wichser <blast007@users.sourceforge.net>
2022-03-08 20:30:48 -0600

    Bump version to 2.4.25

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -48,12 +48,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV        24
+#  define BZ_REV        25
 #endif
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE     "MAINT"
+#  define BZ_BUILD_TYPE     "DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993-2021 Tim Riker";
