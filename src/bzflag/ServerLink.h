1bba4822860594f92166de949e99bdf6c38dcda0
Tim Riker <Tim@Rikers.org>
2023-04-02 22:17:21 -0600

    remove struct

--- src/bzflag/ServerLink.h
+++ src/bzflag/ServerLink.h
@@ -102,9 +102,9 @@ private:
     State               state = SocketError;
     int                 fd = -1;
 
-    struct sockaddr     usendaddr;
+    sockaddr_in6        usendaddr;
     int                 urecvfd;
-    struct sockaddr     urecvaddr; // the clients udp listen address
+    sockaddr_in6        urecvaddr; // the clients udp listen address
     bool                ulinkup;
 
     PlayerId            id;
